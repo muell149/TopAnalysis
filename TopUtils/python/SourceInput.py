f63bb1111e1d11e1e629f51f1e89222ac565f31d
kreczko <>
2008-09-23 16:25:03 +0000

    added enriched QCD option

--- TopUtils/python/SourceInput.py
+++ TopUtils/python/SourceInput.py
@@ -4,6 +4,7 @@ source['ttbar'] = 'include "TopAnalysis/Configuration/test/csa07AllEventsSkim_Tt
 source['wjets'] = 'include "TopAnalysis/Configuration/test/csa07AllEventsSkim_Wjets.cff"'
 source['zjets'] = 'include "TopAnalysis/Configuration/test/csa07AllEventsSkim_Zjets.cff"'
 source['qcd'] = 'include "TopAnalysis/Configuration/test/csa07AllEventsSkim_QCD.cff"'
+source['qcdmu'] = 'include "TopAnalysis/Configuration/test/csa07AllEventsSkim_QCDMu.cff"'
 
 #source['ttbar'] = 'include "TopAnalysis/Configuration/test/csa07AllEventsSkim_ttbar.cff"'
 #source['wjets'] = 'include "TopAnalysis/Configuration/test/csa07AllEventsSkim_wjets.cff"'
