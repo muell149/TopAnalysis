3d85c29c9902b05022b0be652fb0a4abe049722e
rwolf <>
2008-11-12 17:45:27 +0000

    clean up after python transition

--- TopAnalyzer/python/MuonAnalyzer_cfi.py
+++ TopAnalyzer/python/MuonAnalyzer_cfi.py
@@ -1,26 +1,29 @@
 import FWCore.ParameterSet.Config as cms
 
 analyzeMuon = cms.EDAnalyzer("MuonAnalyzer",
+    weight  = cms.InputTag("eventWeight"),
+    input   = cms.InputTag("allLayer1Muons"),
+    hist    = cms.string('analyzeMuon.hist'),
+                             
+    ## fill id  histograms
+    id = cms.PSet(
+    ),    
     doId = cms.bool(True),
-    weight = cms.InputTag("eventWeight"),
-    doKin = cms.bool(True),
-    res = cms.PSet(
-        binsPt = cms.vdouble(0.0, 20.0, 30.0, 50.0, 80.0, 
-            120.0, 180.0),
-        binsEta = cms.vdouble(-2.5, -1.3, -0.5, 0.0, 0.5, 
-            1.3, 2.5),
-        matchDR = cms.double(0.1),
-        binsPhi = cms.vdouble(-3.14, -1.57, 0.0, 1.57, 3.14)
-    ),
-    doRes = cms.bool(True),
-    hist = cms.string('analyzeMuon.hist'),
+
+    ## fill kin histograms
     kin = cms.PSet(
         jets = cms.InputTag("allLayer1Jets")
     ),
-    input = cms.InputTag("allLayer1Muons"),
-    id = cms.PSet(
+    doKin = cms.bool(True),
 
-    )
+    ## fill res histograms
+    res = cms.PSet(
+        binsPt  = cms.vdouble( 0.0, 20.0, 30.0, 50.0, 80.0, 120.0, 180.0),
+        binsEta = cms.vdouble(-2.5, -1.3, -0.5,  0.0,  0.5,   1.3,   2.5),
+        matchDR = cms.double (0.1),
+        binsPhi = cms.vdouble(-3.14, -1.57, 0.0, 1.57, 3.14)
+    ),                             
+    doRes = cms.bool(True)
 )
 
 
---
+++ TopAnalyzer/test/countLeptons_cfg.py
@@ -0,0 +1,66 @@
+import FWCore.ParameterSet.Config as cms
+
+#-------------------------------------------------
+# test cfg file for tqaflayer1 & 2 production from
+# fullsim for semi-leptonic ttbar events 
+#-------------------------------------------------
+process = cms.Process("TEST")
+
+## configure message logger
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
+
+#-------------------------------------------------
+# process configuration
+#-------------------------------------------------
+
+## define input
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+    #PAT test sample
+    'file:/afs/cern.ch/cms/PRS/top/cmssw-data/relval200-for-pat-testing/FullSimTTBar-2_1_X_2008-07-08_STARTUP_V4-AODSIM.100.root'
+    )
+)
+
+## define maximal number of events to loop over
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+
+## configure process options
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(False)
+)
+
+## configure geometry
+process.load("Configuration.StandardSequences.Geometry_cff")
+
+## configure conditions
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.GlobalTag.globaltag = cms.string('STARTUP_V4::All')
+
+## Magnetic field now needs to be in the high-level py
+process.load("Configuration.StandardSequences.MagneticField_cff")
+
+#-------------------------------------------------
+# tqaf configuration; if you want just to produce 
+# tqafLayer2 on top of an already existing
+# tqafLayer1 just comment the standard tqafLayer1
+# production sequence
+#-------------------------------------------------
+
+## add event weight information
+from TopAnalysis.TopUtils.EventWeightPlain_cfi import *
+process.eventWeight = eventWeight
+
+## initialize lepton counter
+process.load("TopAnalysis.TopAnalyzer.LeptonNumberAnalyzer_cfi")
+
+## register TFileService
+process.TFileService = cms.Service("TFileService",
+    fileName = cms.string('analyzeLeptonNumber.root')
+)
+
+process.p0 = cms.Path(process.eventWeight *
+                      process.countLeptons
+                      )
