32ff3a3422b2f60065c1da4fc55635172ec94b49
henderle <>
2008-11-25 17:39:49 +0000

    eta/phi resolution added, cosmetic changes and higher matchDR

--- TopAnalyzer/python/JetAnalyzer_cfi.py
+++ TopAnalyzer/python/JetAnalyzer_cfi.py
@@ -1,23 +1,31 @@
 import FWCore.ParameterSet.Config as cms
 
 analyzeJets = cms.EDAnalyzer("JetAnalyzer",
-    doId = cms.bool(True),
-    weight = cms.InputTag("eventWeight"),
-    doKin = cms.bool(True),
-    res = cms.PSet(
-        binsPt = cms.vdouble(0.0, 10.0, 30.0, 50.0, 70.0, 100.0, 150.0, 200.0),
-        matchDR = cms.double(0.1),
-        nJets = cms.int32(4)
-    ),
-    doRes = cms.bool(True),
-    hist = cms.string('analyzeJets.hist'),
-    kin = cms.PSet(
-        nJets = cms.int32(4)
-    ),
-    input = cms.InputTag("allLayer1Jets"),
+    weight  = cms.InputTag("eventWeight"),
+    input   = cms.InputTag("allLayer1Jets"),
+    hist    = cms.string('analyzeJets.hist'),
+
+    ## fill id  histograms
     id = cms.PSet(
         nJets = cms.int32(4)
-    )
+    ),                          
+    doId = cms.bool(True),
+
+    ## fill kin histograms
+    kin = cms.PSet(
+        nJets = cms.int32(4)
+    ),                          
+    doKin = cms.bool(True),
+
+    ## fill res histograms
+    res = cms.PSet(
+        binsPt  = cms.vdouble(  0.0,  10.0, 30.0, 50.0, 70.0, 100.0, 150.0, 200.0),
+        binsEta = cms.vdouble( -2.5,  -1.3, -0.5,  0.0,  0.5,   1.3,  2.5),
+        binsPhi = cms.vdouble(-3.14, -1.57,  0.0, 1.57, 3.14),
+        matchDR = cms.double (0.5),
+        nJets   = cms.int32  (4)
+    ),
+    doRes = cms.bool(True)
 )
 
 
