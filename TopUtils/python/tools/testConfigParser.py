b8dda4e2cc38ee52b84538d143fbe950bf19d330
kreczko <>
2008-12-02 20:20:23 +0000

    some small changes in config

--- TopUtils/python/tools/testConfigParser.py
+++ TopUtils/python/tools/testConfigParser.py
@@ -10,13 +10,17 @@ from ConfigParser import ConfigParser, ConfigError
 """
 class testConfigParser(unittest.TestCase):   
     
+    #called after constructor, sets the variables
     def setUp(self):
         self.testxml = 'test/Testconfig.xml'
         self.falsexml = 'test/FalseConfig.xml'
         self.parser = ConfigParser(self.testxml)
         self.falseParser = ConfigParser(self.falsexml)
         
+    #test method
     def testInit(self):
+        #check if the ConfigParser constructor throws an IOException 
+        #if the file doesn't exist
         self.assertRaises(IOError, ConfigParser, 'nothere')
 
         
@@ -25,7 +29,7 @@ class testConfigParser(unittest.TestCase):
         
     def testGetNodeList(self):
         node = self.parser.getNodeList('include')
-        self.assertEqual(node.length, 2)
+        self.assertEqual(node.length, 3)
         self.assertEqual(node[0].attributes.length, 1)
         self.assertEqual(node[1].attributes.length, 1)
         self.assertEqual(node[0].attributes.item(0).name, 'file')
@@ -33,18 +37,14 @@ class testConfigParser(unittest.TestCase):
         
     def testGetAttributeValue(self):
         nodes = self.parser.getNodeList('include')
-        v1 = self.parser.getAttributeValue(nodes[0], 'file')
         self.assertRaises(ConfigError,self.parser.getAttributeValue, nodes[0], 'color')
-        #v2 = self.parser.getAttributeValue(nodes[0], 'value')
-        #self.assertEqual(v1, 'setup1')
         self.assertEqual(self.parser.getAttributeValue(nodes[0], 'file'), 'test/hist.xml')
         
     def testReadIncludes(self):
         self.assertRaises(ConfigError, self.falseParser.readIncludes)
         includes = self.parser.readIncludes()
         self.assertTrue('test/hist.xml' in includes )
-        #self.assertEqual(includes['setup2'], 'test/hist1.xml')
-        self.assertEqual(len(includes), 2)
+        self.assertEqual(len(includes), 3)
         
         
 """
@@ -59,8 +59,20 @@ def suite():
     return suite
 
         
-
-
+"""
+for running outside of eclipse (with 'python testConfigParser.py')
+"""
 if __name__ == "__main__":
-
-   unittest.main()
+    #do the whole suite
+    #unittest.main()
+    
+    #create a testsuite with chosen test methods
+    fewtest = unittest.TestSuite()
+    #add function testInit to test suite
+    fewtest.addTest(testConfigParser("testInit"))
+    fewtest.addTest(testConfigParser("testReadIncludes"))
+    #run the created suite
+    unittest.TextTestRunner(verbosity=2).run(fewtest)
+    
+    #another way to run the whole suite with choosing the output level
+    #unittest.TextTestRunner(verbosity=2).run(suite())
