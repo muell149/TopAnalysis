f502f9750b84fccabc3335a686e514291d0de8d4
rwolf <>
2008-12-02 10:08:27 +0000

    cleanup from move from own filter towards official PhysicsTools filter on that issue

--- TopFilter/interface/QualityFilterFwd.h
+++
@@ -1,7 +0,0 @@
-#include "DataFormats/PatCandidates/interface/Muon.h"
-
-// quality filter expansion for muons (could later be extended for electrons
-#include "TopAnalysis/TopFilter/interface/QualityFilter.h"
-
-typedef QualityFilter   <std::vector<pat::Muon>     > MuonQualityFilter;
-
