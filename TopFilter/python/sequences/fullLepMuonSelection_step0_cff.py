9533419a45295f03498c8223b00b5ce757c422d0
dammann <>
2009-01-06 12:28:25 +0000

    name changes to allow using preselection and selection in one path

--- TopFilter/python/sequences/fullLepMuonSelection_step0_cff.py
+++ TopFilter/python/sequences/fullLepMuonSelection_step0_cff.py
@@ -2,29 +2,29 @@ import FWCore.ParameterSet.Config as cms
 
 from TopAnalysis.TopAnalyzer.MuonAnalyzer_cfi import analyzeMuon
 ## -------------------------------------Muon eta  |  Muon pt  | Jets eta  |  Jets pt
-fullLepMuon00 = analyzeMuon.clone()    ##     -     |     -     |     -     |     -
-fullLepMuon10 = analyzeMuon.clone()    ##     x     |     -     |     -     |     -
-fullLepMuon20 = analyzeMuon.clone()    ##     x     |     x     |     -     |     -
-fullLepMuon01 = analyzeMuon.clone()    ##     -     |     -     |     x     |     -
-fullLepMuon02 = analyzeMuon.clone()    ##     -     |     -     |     x     |     x
-fullLepMuon11 = analyzeMuon.clone()    ##     x     |     -     |     x     |     -
-fullLepMuon12 = analyzeMuon.clone()    ##     x     |     -     |     x     |     x
-fullLepMuon21 = analyzeMuon.clone()    ##     x     |     x     |     x     |     -
-fullLepMuon22 = analyzeMuon.clone()    ##     x     |     x     |     x     |     x
+fullLepMuon00pre = analyzeMuon.clone()    ##     -     |     -     |     -     |     -
+fullLepMuon10pre = analyzeMuon.clone()    ##     x     |     -     |     -     |     -
+fullLepMuon20pre = analyzeMuon.clone()    ##     x     |     x     |     -     |     -
+fullLepMuon01pre = analyzeMuon.clone()    ##     -     |     -     |     x     |     -
+fullLepMuon02pre = analyzeMuon.clone()    ##     -     |     -     |     x     |     x
+fullLepMuon11pre = analyzeMuon.clone()    ##     x     |     -     |     x     |     -
+fullLepMuon12pre = analyzeMuon.clone()    ##     x     |     -     |     x     |     x
+fullLepMuon21pre = analyzeMuon.clone()    ##     x     |     x     |     x     |     -
+fullLepMuon22pre = analyzeMuon.clone()    ##     x     |     x     |     x     |     x
 
 ## put any extra configuration here:
 
 from TopAnalysis.TopAnalyzer.JetAnalyzer_cfi  import analyzeJets
 ## -------------------------------------Muon eta  |  Muon pt  | Jets eta  |  Jets pt
-fullLepJets00 = analyzeJets.clone()    ##     -     |     -     |     -     |     -
-fullLepJets10 = analyzeJets.clone()    ##     x     |     -     |     -     |     -
-fullLepJets20 = analyzeJets.clone()    ##     x     |     x     |     -     |     -
-fullLepJets01 = analyzeJets.clone()    ##     -     |     -     |     x     |     -
-fullLepJets02 = analyzeJets.clone()    ##     -     |     -     |     x     |     x
-fullLepJets11 = analyzeJets.clone()    ##     x     |     -     |     x     |     -
-fullLepJets12 = analyzeJets.clone()    ##     x     |     -     |     x     |     x
-fullLepJets21 = analyzeJets.clone()    ##     x     |     x     |     x     |     -
-fullLepJets22 = analyzeJets.clone()    ##     x     |     x     |     x     |     x
+fullLepJets00pre = analyzeJets.clone()    ##     -     |     -     |     -     |     -
+fullLepJets10pre = analyzeJets.clone()    ##     x     |     -     |     -     |     -
+fullLepJets20pre = analyzeJets.clone()    ##     x     |     x     |     -     |     -
+fullLepJets01pre = analyzeJets.clone()    ##     -     |     -     |     x     |     -
+fullLepJets02pre = analyzeJets.clone()    ##     -     |     -     |     x     |     x
+fullLepJets11pre = analyzeJets.clone()    ##     x     |     -     |     x     |     -
+fullLepJets12pre = analyzeJets.clone()    ##     x     |     -     |     x     |     x
+fullLepJets21pre = analyzeJets.clone()    ##     x     |     x     |     x     |     -
+fullLepJets22pre = analyzeJets.clone()    ##     x     |     x     |     x     |     x
 
 ## put any extra configuration here:
 
@@ -33,30 +33,30 @@ fullLepJets22 = analyzeJets.clone()    ##     x     |     x     |     x     |   
 from TopAnalysis.TopFilter.selections.fullLepMuonSelection_step0_cff import *
 
 from TopAnalysis.TopFilter.filters.EtaEventFilter_cfi import filterMuonEta
-filterFullLep10 = filterMuonEta.clone()
+prefilterFullLep10 = filterMuonEta.clone()
 from TopAnalysis.TopFilter.filters.EtaEventFilter_cfi import filterJetsEta
-filterFullLep01 = filterJetsEta.clone()
+prefilterFullLep01 = filterJetsEta.clone()
 from TopAnalysis.TopFilter.filters.PtEventFilter_cfi  import filterMuonPt
-filterFullLep20 = filterMuonPt.clone()
+prefilterFullLep20 = filterMuonPt.clone()
 from TopAnalysis.TopFilter.filters.PtEventFilter_cfi  import filterJetsPt
-filterFullLep02 = filterJetsPt.clone()
+prefilterFullLep02 = filterJetsPt.clone()
 
 ## put any extra configuration here:
-filterFullLep10.cuts = fullLepMuonEta
-filterFullLep01.cuts = fullLepJetsEta
-filterFullLep20.cuts = fullLepMuonPt
-filterFullLep02.cuts = fullLepJetsPt
+prefilterFullLep10.cuts = fullLepMuonEta
+prefilterFullLep01.cuts = fullLepJetsEta
+prefilterFullLep20.cuts = fullLepMuonPt
+prefilterFullLep02.cuts = fullLepJetsPt
 
 ## define sequences
-selectFullLepMuon = cms.Sequence(fullLepMuon00 + fullLepJets00 *
-                                 filterFullLep10 *
-                                 fullLepMuon10 + fullLepJets10 *
-                                 filterFullLep20 *
-                                 fullLepMuon20 + fullLepJets20 *
-                                 filterFullLep01 *
-                                 fullLepMuon21 + fullLepJets21 *
-                                 filterFullLep02 *
-                                 fullLepMuon22 + fullLepJets22
-                                 )
+preselectFullLepMuon = cms.Sequence(fullLepMuon00pre + fullLepJets00pre *
+                                    prefilterFullLep10 *
+                                    fullLepMuon10pre + fullLepJets10pre *
+                                    prefilterFullLep20 *
+                                    fullLepMuon20pre + fullLepJets20pre *
+                                    prefilterFullLep01 *
+                                    fullLepMuon21pre + fullLepJets21pre *
+                                    prefilterFullLep02 *
+                                    fullLepMuon22pre + fullLepJets22pre
+                                   )
 
 
