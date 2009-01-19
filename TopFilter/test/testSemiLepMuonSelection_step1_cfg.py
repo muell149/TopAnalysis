0e472d2646e428444f7b4fcc00c3f5827a106511
snaumann <>
2009-01-19 15:24:59 +0000

    run on PAT-tuple at DESY

--- TopFilter/test/testSemiLepMuonSelection_step1_cfg.py
+++ TopFilter/test/testSemiLepMuonSelection_step1_cfg.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.Config as cms
 # test cfg file for tqaflayer1 & 2 production from
 # fullsim for semi-leptonic ttbar events 
 #-------------------------------------------------
-process = cms.Process("TEST")
+process = cms.Process("HH")
 
 ## configure message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
@@ -19,7 +19,18 @@ process.MessageLogger.cerr.threshold = 'INFO'
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
     #PAT test sample
-    'file:/afs/cern.ch/cms/PRS/top/cmssw-data/relval200-for-pat-testing/FullSimTTBar-2_1_X_2008-07-08_STARTUP_V4-AODSIM.100.root'
+    #'file:/afs/cern.ch/cms/PRS/top/cmssw-data/relval200-for-pat-testing/FullSimTTBar-2_1_X_2008-07-08_STARTUP_V4-AODSIM.100.root'
+    #PAT-tuple at DESY
+    '/store/mc/Summer08/TauolaTTbar/USER/IDEAL_V9_PAT_v2/0000/12B02891-66CF-DD11-9F1C-00E08178C11F.root',
+    '/store/mc/Summer08/TauolaTTbar/USER/IDEAL_V9_PAT_v2/0000/48B6CB13-E9CC-DD11-8FC0-00E08179183F.root',
+    '/store/mc/Summer08/TauolaTTbar/USER/IDEAL_V9_PAT_v2/0000/6289DFE2-EBCC-DD11-A923-00E0817918BB.root',
+    '/store/mc/Summer08/TauolaTTbar/USER/IDEAL_V9_PAT_v2/0000/689A17B1-ECCC-DD11-8E7D-00E08179186B.root',
+    '/store/mc/Summer08/TauolaTTbar/USER/IDEAL_V9_PAT_v2/0000/828ECBD8-3ACC-DD11-A0C3-001A6478A7C4.root',
+    '/store/mc/Summer08/TauolaTTbar/USER/IDEAL_V9_PAT_v2/0000/AA8BFCA6-31CF-DD11-A7F4-00E08178C18F.root',
+    '/store/mc/Summer08/TauolaTTbar/USER/IDEAL_V9_PAT_v2/0000/C68584CC-C6CB-DD11-9714-0013D3D85A8B.root',
+    '/store/mc/Summer08/TauolaTTbar/USER/IDEAL_V9_PAT_v2/0000/D03CE81C-8BDD-DD11-82BE-00E081791767.root',
+    '/store/mc/Summer08/TauolaTTbar/USER/IDEAL_V9_PAT_v2/0000/DAF9D007-42CD-DD11-A455-00E081B08BC9.root',
+    '/store/mc/Summer08/TauolaTTbar/USER/IDEAL_V9_PAT_v2/0000/F4E0BF7B-42CD-DD11-94BB-00E08178C0F3.root'
     )
 )
 
@@ -51,14 +62,14 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 #-------------------------------------------------
 
 ## std sequence for tqaf layer1
-process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_cff")
-process.p0 = cms.Path(process.tqafLayer1)
+#process.load("TopQuarkAnalysis.TopObjectProducers.tqafLayer1_cff")
+#process.p0 = cms.Path(process.tqafLayer1)
 
 ## necessary fixes to run 2.2.X on 2.1.X data
 ## comment this when running on samples produced
 ## with 22X
-from PhysicsTools.PatAlgos.tools.cmsswVersionTools import run22XonSummer08AODSIM
-run22XonSummer08AODSIM(process)
+#from PhysicsTools.PatAlgos.tools.cmsswVersionTools import run22XonSummer08AODSIM
+#run22XonSummer08AODSIM(process)
 
 #-------------------------------------------------
 # private uni Hamburg analysis code
