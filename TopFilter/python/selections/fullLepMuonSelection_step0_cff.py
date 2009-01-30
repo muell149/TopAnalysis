359b0758e2505d92989ae3102fefc30b5635c117
dammann <>
2009-01-30 14:25:57 +0000

    small changes in preselection cuts

--- TopFilter/python/selections/fullLepMuonSelection_step0_cff.py
+++ TopFilter/python/selections/fullLepMuonSelection_step0_cff.py
@@ -3,32 +3,31 @@ import FWCore.ParameterSet.Config as cms
 ## muon preselection
 fullLepMuonEta = cms.PSet(
     mode = cms.uint32 ( 0),
-    name = cms.string ('preFullLepMuonEta'),
+    name = cms.string ('preFullLepMuonEtaMu'),
     min  = cms.vdouble(-2.5,-2.5),
     max  = cms.vdouble( 2.5, 2.5)
 )
 fullLepMuonPt = cms.PSet(
     mode = cms.uint32 ( 0),    
-    name = cms.string ('preFullLepMuonPt' ),
+    name = cms.string ('preFullLepMuonPtMu' ),
     min  = cms.vdouble(  15.0,   15.0),
     max  = cms.vdouble(9999.0, 9999.0)
 )
 
-## jets preselection
+## jets preselection 
+## (only one jet is demanded to have a signal free reference sample)
 fullLepJetsEta = cms.PSet(
     mode = cms.uint32 ( 0),    
-    name = cms.string ('preFullLepJetsEta'),
-    min  = cms.vdouble(-3.25, -3.25),
-    max  = cms.vdouble( 3.25,  3.25)
+    name = cms.string ('preFullLepMuonEtaJet'),
+    min  = cms.vdouble(-3.25,-3.25 ),    
+    max  = cms.vdouble( 3.25, 3.25 )  
 )
 fullLepJetsPt = cms.PSet(
     mode = cms.uint32 ( 0),    
-    name = cms.string ('preFullLepJetsPt' ),
-    min  = cms.vdouble(   5.0,    5.0),
-    max  = cms.vdouble(9999.0, 9999.0)
+    name = cms.string ('preFullLepMuonPtJet' ),
+    min  = cms.vdouble(  10.0,   10.0 ),  
+    max  = cms.vdouble(9999.0, 9999.0 )    
 )
 
-## isolation preselection
-# not needed for preselection since dimuon is seldom enough
 
 
