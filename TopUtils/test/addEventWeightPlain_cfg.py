85c9094380bbb9406112b1986a9c428f4e947d76
rwolf <>
2009-02-17 15:53:46 +0000

    adding plain weight to the event content

---
+++ TopUtils/test/addEventWeightPlain_cfg.py
@@ -0,0 +1,56 @@
+import FWCore.ParameterSet.Config as cms
+
+#-------------------------------------------------
+# cfg file for the analysis of selected muon
+# quantities
+#-------------------------------------------------
+process = cms.Process("Muon")
+
+## configure message logger
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.threshold = 'INFO'
+#process.MessageLogger.cerr.FwkReport.reportEvery = 100
+
+#-------------------------------------------------
+# process configuration
+#-------------------------------------------------
+
+## define input
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring(
+    ## add your favourite file here
+    )
+)
+
+## define maximal number of events to loop over
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(100)
+)
+
+## configure process options
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(False)
+)
+
+#-------------------------------------------------
+# add event weight
+#-------------------------------------------------
+
+## add event weight information
+process.load("TopAnalysis.TopUtils.EventWeightPlain_cfi")
+process.eventWeight.nevts = 1
+process.eventWeight.xsec  = 1
+process.eventWeight.lumi  = 1
+process.eventWeight.eff   = 1
+
+## configure output module
+process.out = cms.OutputModule("PoolOutputModule",
+    fileName     = cms.untracked.string('patTuple_semiLepMuonSelection_step0.root')
+    eventContent = cms.PSet(
+      outputCommands = cms.untracked.vstring('keep *_*_*_*')  ## keep all
+    )
+)
+
+process.p1 = cms.Path(process.eventWeight *
+                      process.out
+                      )
