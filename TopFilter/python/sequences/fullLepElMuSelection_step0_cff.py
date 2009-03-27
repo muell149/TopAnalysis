af41c234620a42bde5695629364f2ecf93dd2e75
dammann <>
2009-03-27 09:46:28 +0000

    removed unneeded extra configuration

--- TopFilter/python/sequences/fullLepElMuSelection_step0_cff.py
+++ TopFilter/python/sequences/fullLepElMuSelection_step0_cff.py
@@ -11,22 +11,6 @@ fullLepElMuElec221pre = analyzeElec.clone()    ##     x     |     x     |     x 
 fullLepElMuElec222pre = analyzeElec.clone()    ##     x     |     x     |     x     |     x     |     x     |     x
 
 ## put any extra configuration here:
-fullLepElMuElec000pre.input = cms.InputTag("selectedLayer1Electrons")                         
-fullLepElMuElec100pre.input = cms.InputTag("selectedLayer1Electrons")                          
-fullLepElMuElec200pre.input = cms.InputTag("selectedLayer1Electrons")                           
-fullLepElMuElec210pre.input = cms.InputTag("selectedLayer1Electrons")                         
-fullLepElMuElec220pre.input = cms.InputTag("selectedLayer1Electrons")
-fullLepElMuElec221pre.input = cms.InputTag("selectedLayer1Electrons")
-fullLepElMuElec222pre.input = cms.InputTag("selectedLayer1Electrons")
-
-fullLepElMuElec000pre.useEventWeight = cms.bool(False)
-fullLepElMuElec100pre.useEventWeight = cms.bool(False)
-fullLepElMuElec200pre.useEventWeight = cms.bool(False)
-fullLepElMuElec210pre.useEventWeight = cms.bool(False)
-fullLepElMuElec220pre.useEventWeight = cms.bool(False)
-fullLepElMuElec221pre.useEventWeight = cms.bool(False)
-fullLepElMuElec222pre.useEventWeight = cms.bool(False)
-
 fullLepElMuElec000pre.doId = cms.bool(False)
 fullLepElMuElec100pre.doId = cms.bool(False)
 fullLepElMuElec200pre.doId = cms.bool(False)
@@ -47,21 +31,6 @@ fullLepElMuMuon221pre = analyzeMuon.clone()    ##     x     |     x     |     x 
 fullLepElMuMuon222pre = analyzeMuon.clone()    ##     x     |     x     |     x     |     x     |     x     |     x
 
 ## put any extra configuration here:
-fullLepElMuMuon000pre.input = cms.InputTag("selectedLayer1Muons")             
-fullLepElMuMuon100pre.input = cms.InputTag("selectedLayer1Muons")                       
-fullLepElMuMuon200pre.input = cms.InputTag("selectedLayer1Muons")                       
-fullLepElMuMuon210pre.input = cms.InputTag("selectedLayer1Muons")                      
-fullLepElMuMuon220pre.input = cms.InputTag("selectedLayer1Muons")
-fullLepElMuMuon221pre.input = cms.InputTag("selectedLayer1Muons")
-fullLepElMuMuon222pre.input = cms.InputTag("selectedLayer1Muons")
-
-fullLepElMuMuon000pre.useEventWeight = cms.bool(False)
-fullLepElMuMuon100pre.useEventWeight = cms.bool(False)
-fullLepElMuMuon200pre.useEventWeight = cms.bool(False)
-fullLepElMuMuon210pre.useEventWeight = cms.bool(False)
-fullLepElMuMuon220pre.useEventWeight = cms.bool(False)
-fullLepElMuMuon221pre.useEventWeight = cms.bool(False)
-fullLepElMuMuon222pre.useEventWeight = cms.bool(False)
  
 
 from TopAnalysis.TopAnalyzer.JetAnalyzer_cfi  import analyzeJets
@@ -75,21 +44,6 @@ fullLepElMuJets221pre = analyzeJets.clone()    ##     x     |     x     |     x 
 fullLepElMuJets222pre = analyzeJets.clone()    ##     x     |     x     |     x     |     x     |     x     |     x
 
 ## put any extra configuration here:
-fullLepElMuJets000pre.input = cms.InputTag("selectedLayer1Jets")    
-fullLepElMuJets100pre.input = cms.InputTag("selectedLayer1Jets")                          
-fullLepElMuJets200pre.input = cms.InputTag("selectedLayer1Jets")    
-fullLepElMuJets210pre.input = cms.InputTag("selectedLayer1Jets")    
-fullLepElMuJets220pre.input = cms.InputTag("selectedLayer1Jets")
-fullLepElMuJets221pre.input = cms.InputTag("selectedLayer1Jets")
-fullLepElMuJets222pre.input = cms.InputTag("selectedLayer1Jets")
-
-fullLepElMuJets000pre.useEventWeight = False
-fullLepElMuJets100pre.useEventWeight = False                   
-fullLepElMuJets200pre.useEventWeight = False
-fullLepElMuJets210pre.useEventWeight = False
-fullLepElMuJets220pre.useEventWeight = False
-fullLepElMuJets221pre.useEventWeight = False
-fullLepElMuJets222pre.useEventWeight = False
 
 
 ## import selection cuts here
@@ -115,13 +69,6 @@ prefilterFullLepElMu001.cuts = fullLepJetsEta
 prefilterFullLepElMu200.cuts = fullLepElecPt
 prefilterFullLepElMu020.cuts = fullLepMuonPt
 prefilterFullLepElMu002.cuts = fullLepJetsPt
-
-prefilterFullLepElMu100.useEventWeight = False
-prefilterFullLepElMu010.useEventWeight = False
-prefilterFullLepElMu001.useEventWeight = False
-prefilterFullLepElMu200.useEventWeight = False
-prefilterFullLepElMu020.useEventWeight = False
-prefilterFullLepElMu002.useEventWeight = False
 
 
 ## define sequences
