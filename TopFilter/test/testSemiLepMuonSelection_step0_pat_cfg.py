c910a7ddf0d2614d0f42c8ba367f2fbe60f655c5
rwolf <>
2009-04-07 14:23:52 +0000

    update of step0 selection

--- TopFilter/test/testSemiLepMuonSelection_step0_pat_cfg.py
+++ TopFilter/test/testSemiLepMuonSelection_step0_pat_cfg.py
@@ -49,6 +49,6 @@ process.out = cms.OutputModule("PoolOutputModule",
     fileName = cms.untracked.string('patTuple_semiLepMuonSelection_step0.root')
 )
 
-process.p1 = cms.Path(process.selectSemiLepMuon *
+process.p1 = cms.Path(process.preselectSemiLepMuon *
                       process.out
                       )
