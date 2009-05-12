46bd1a1c0445cf52352ba956b1fe72c9ffb38868
snaumann <>
2009-05-12 10:49:02 +0000

    adapt to PATv2: allLayer1METs -> layer1METs

--- TopUtils/python/JetEnergyScale_cfi.py
+++ TopUtils/python/JetEnergyScale_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 scaledJetEnergy = cms.EDProducer("JetEnergyScale",
     inputJets            = cms.InputTag("allLayer1Jets"),
-    inputMETs            = cms.InputTag("allLayer1METs"),
+    inputMETs            = cms.InputTag("layer1METs"),
     scaleFactor          = cms.double(1.0),
     jetPTthresholdForMET = cms.double(20.),
     jetEMlimitForMET     = cms.double(0.9)
