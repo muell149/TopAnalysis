51c38a9cc4ef556d69412b38c68e05853031c0bd
kreczko <>
2009-05-19 12:14:17 +0000

    added relative combination

--- TopAnalyzer/test/testAnalyzeIsolation_pat.py
+++ TopAnalyzer/test/testAnalyzeIsolation_pat.py
@@ -45,16 +45,22 @@ before.modulename = "findQCDBkgMVA"
 after.modulename = "findQCDBkgMVA"
 process.load("TopAnalysis.TopUtils.QCDBkgMVAComputer_cff")
 ## import selection cuts here
-from TopAnalysis.TopFilter.selectlions.semiLepMuonSelection_step1_cff import *
+from TopAnalysis.TopFilter.selections.semiLepMuonSelection_step1_cff import *
 from TopAnalysis.TopFilter.filters.SemiLepMuonEventFilter_cfi import filterSemiLepMuonEvent
+filterSemiLepMuonEvent.lepEtaFilter = semiLepMuonEta
+filterSemiLepMuonEvent.lepPtFilter = semiLepMuonPt
+filterSemiLepMuonEvent.lepPtFilter.min = [20.0]
+filterSemiLepMuonEvent.jetEtaFilter = semiLepJetsEta
+filterSemiLepMuonEvent.jetPtFilter = semiLepJetsPt
+filterSemiLepMuonEvent.jetPtFilter.min = [20.0, 20.0, 20.0, 20.0]
+filterSemiLepMuonEvent.trkIsoFilter = semiLepMuonIsolationTrk
+filterSemiLepMuonEvent.calIsoFilter = semiLepMuonIsolationCal
+#filterSemiLepMuonEvent.jetDistFilter = semiLepMuonJetDistance
+filterSemiLepMuonEvent.combIsoFilter = semiLepMuonIsolationRelComb
 #process.load("TopAnalysis.TopFilter.sequences.semiLepMuonSelection_step1_cff")
 process.tight = filterSemiLepMuonEvent.clone()
 process.loose = filterSemiLepMuonEvent.clone()
-process.loose.trkIso = False
-process.loose.calIso = False
-process.loose.jetDist = False
-semiLepJetsPt.min = 20.0, 20.0, 20.0, 20.0
-semiLepMuonPt.min = [20.0]
+process.loose.relComb = False
 ## std sequence to produce the ttGenEvt
 process.load("TopQuarkAnalysis.TopEventProducers.sequences.ttGenEvent_cff")
 ## configure ttGenEventFilters
