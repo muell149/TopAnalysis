bee07e0022c8f6b4867049779ff7730ab50a10a1
dammann <>
2009-06-08 15:45:22 +0000

    readded input to keep filter sequences working

--- TopFilter/python/filters/IsolationEventFilter_cfi.py
+++ TopFilter/python/filters/IsolationEventFilter_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 ## define basic event filters
 filterMuonIso = cms.EDFilter("MuonIsolationEventFilter",
-    input  = cms.VInputTag(''),    
+    input  = cms.VInputTag('selectedLayer1Muons'),    
     useEventWeight = cms.bool(True),
     weight = cms.InputTag("eventWeight"), 
     cuts   = cms.PSet(
