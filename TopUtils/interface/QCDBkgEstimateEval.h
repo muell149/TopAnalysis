e80e6db61fb00ae0a83e22d33d4cfeb9b5a1b82d
kreczko <>
2009-06-25 12:19:26 +0000

    sorted variable list, now the same as in the configuration files

--- TopUtils/interface/QCDBkgEstimateEval.h
+++ TopUtils/interface/QCDBkgEstimateEval.h
@@ -15,24 +15,43 @@ inline double evaluateQCDBkgEstimateSelector(PhysicsTools::MVAComputerCache& mva
 	if (training)
 		values.push_back(PhysicsTools::Variable::Value(PhysicsTools::MVATrainer::kWeightId, weight));
 
+	values.push_back(PhysicsTools::Variable::Value("dphiMETJet1", sigsel.dphiMETJet1()));
+	values.push_back(PhysicsTools::Variable::Value("dphiMETJet2", sigsel.dphiMETJet2()));
+	values.push_back(PhysicsTools::Variable::Value("dphiMETJet3", sigsel.dphiMETJet3()));
 	values.push_back(PhysicsTools::Variable::Value("dphiMETJet4", sigsel.dphiMETJet4()));
+
 	values.push_back(PhysicsTools::Variable::Value("dphiMETLepton", sigsel.dphiMETLepton()));
+	values.push_back(PhysicsTools::Variable::Value("dphiMuJ1J2", sigsel.dphiMuJ1J2()));
 	values.push_back(PhysicsTools::Variable::Value("METTimesLeadingJet", sigsel.METTimesLeadingJet()));
 	values.push_back(PhysicsTools::Variable::Value("sumEtJet3And4", sigsel.sumEtJet3And4()));
+
 	values.push_back(PhysicsTools::Variable::Value("sumAllJetsEtAndLepton", sigsel.sumAllJetsEtAndLepton()));
 	values.push_back(PhysicsTools::Variable::Value("vecSumAllJetsAndLepton", sigsel.vecSumAllJetsAndLepton()));
 	values.push_back(PhysicsTools::Variable::Value("sumEtleadingJetAnd2TimesLeptonPt",
 			sigsel.sumEtleadingJetAnd2TimesLeptonPt()));
-	values.push_back(PhysicsTools::Variable::Value("dphiMETJet2", sigsel.dphiMETJet2()));
 	values.push_back(PhysicsTools::Variable::Value("MET", sigsel.MET()));
-	values.push_back(PhysicsTools::Variable::Value("dphiMuJ1J2", sigsel.dphiMuJ1J2()));
+
 	values.push_back(PhysicsTools::Variable::Value("leptonPt", sigsel.leptonPt()));
+
 	values.push_back(PhysicsTools::Variable::Value("aplanarity", sigsel.aplanarity()));
 	values.push_back(PhysicsTools::Variable::Value("sphericity", sigsel.sphericity()));
 	values.push_back(PhysicsTools::Variable::Value("circularity", sigsel.circularity()));
 	values.push_back(PhysicsTools::Variable::Value("isotropy", sigsel.isotropy()));
-	//values.push_back(PhysicsTools::Variable::Value("sumAllJetsEt", sigsel.sumAllJetsEt()));
 
+	values.push_back(PhysicsTools::Variable::Value("Jet3EtOverJet1EtJet3Et", sigsel.Jet3EtOverJet1EtJet3Et()));
+	values.push_back(PhysicsTools::Variable::Value("Jet3EtOverJet2EtJet3Et", sigsel.Jet3EtOverJet2EtJet3Et()));
+	values.push_back(PhysicsTools::Variable::Value("Jet4EtOverJet1EtJet4Et", sigsel.Jet4EtOverJet1EtJet4Et()));
+	values.push_back(PhysicsTools::Variable::Value("Jet4EtOverJet2EtJet4Et", sigsel.Jet4EtOverJet2EtJet4Et()));
+
+	values.push_back(PhysicsTools::Variable::Value("invariantMassJ3andJ4", sigsel.invariantMassJ3andJ4()));
+	values.push_back(PhysicsTools::Variable::Value("DeltaPhiMuonJet3", sigsel.DeltaPhiMuonJet3()));
+	values.push_back(PhysicsTools::Variable::Value("DeltaPhiMuonJet4", sigsel.DeltaPhiMuonJet4()));
+	values.push_back(PhysicsTools::Variable::Value("deltaPhiJet1Jet2", sigsel.deltaPhiJet1Jet2()));
+
+	values.push_back(PhysicsTools::Variable::Value("DeltaPhiTtbar", sigsel.DeltaPhiTtbar()));
+	values.push_back(PhysicsTools::Variable::Value("TriJetTMass", sigsel.TriJetTMass()));
+	values.push_back(PhysicsTools::Variable::Value("DeltaPhiTimesDeltaEta", sigsel.DeltaPhiTimesDeltaEta()));
+	values.push_back(PhysicsTools::Variable::Value("minDeltaPhiMETJets", sigsel.minDeltaPhiMETJets()));
 
 	return mvaComputer->eval(values);
 
