e959955fb060c08699662a7379447281cad40ef8
dammann <>
2009-08-06 14:48:09 +0000

    removed unneeded includments

--- TopFilter/interface/DiMuonMassFilter.h
+++ TopFilter/interface/DiMuonMassFilter.h
@@ -1,19 +1,9 @@
 #ifndef DiMuonMassFilter_h  
 #define DiMuonMassFilter_h
 
-#include <memory>
-#include <string>
-#include <vector>
-#include <fstream>
 #include <iostream>
-
-#include "TH1F.h"
-#include "TH2F.h"
-#include "TFile.h"
-#include "TMath.h"
-
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/EDFilter.h"
 #include "FWCore/Framework/interface/Event.h"
 #include "FWCore/ParameterSet/interface/InputTag.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
@@ -21,23 +11,7 @@
 #include "FWCore/Utilities/interface/EDMException.h"
 #include "FWCore/ServiceRegistry/interface/Service.h"
 #include "PhysicsTools/UtilAlgos/interface/TFileService.h"
-
-#include "AnalysisDataFormats/TopObjects/interface/TtDilepEvtSolution.h"
-#include "TopAnalysis/TopAnalyzer/interface/TtDiMuonTopObjAna.h"
-#include "TopAnalysis/TopAnalyzer/interface/TtDiMuonGenEvtAna.h"
-#include "TopAnalysis/TopAnalyzer/interface/TtDiMuonKinSolAna.h"
-
-#include "DataFormats/Math/interface/deltaR.h"
-#include "DataFormats/TrackReco/interface/Track.h"
-#include "DataFormats/PatCandidates/interface/Jet.h"
-#include "DataFormats/CaloTowers/interface/CaloTower.h"
-
 #include "DataFormats/PatCandidates/interface/Muon.h"
-#include "TopAnalysis/TopUtils/interface/NameScheme.h"
-#include "TopAnalysis/TopUtils/interface/RootSystem.h"
-#include "TopAnalysis/TopUtils/interface/RootHistograms.h"
-
-#include "FWCore/Framework/interface/EDFilter.h"
 
 class DiMuonMassFilter : public edm::EDFilter {
 
