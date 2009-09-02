d366226886cf9001dafbdd26988e33caba3f1b3c
snaumann <>
2009-09-02 10:02:38 +0000

    recovering from Roger's cleanup ;-) (until someone has revised this code or shown me a better tool to exclude events with overlapping jets, I will just use it as it was before)

--- TopFilter/plugins/SealModule.cc
+++ TopFilter/plugins/SealModule.cc
@@ -6,6 +6,9 @@ DEFINE_FWK_MODULE(DiMuonMassFilter);
 #include "TopAnalysis/TopFilter/plugins/MuonJetOverlapSelector.h"
 DEFINE_FWK_MODULE(MuonJetOverlapSelector);
 
+#include "TopAnalysis/TopFilter/plugins/JetOverlapEventFilter.h"
+DEFINE_FWK_MODULE(JetOverlapEventFilter);
+
 #include "TopAnalysis/TopFilter/plugins/SemiLeptonicTopJetSelector.h"
 DEFINE_FWK_MODULE(SemiLeptonicTopJetSelector);
 
