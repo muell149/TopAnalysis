c8be08be77377938bd16c5e83599d22c01f590ff
rwolf <>
2009-09-16 18:10:37 +0000

    here we are..

---
+++ TopAnalyzer/python/JetResolution_cfi.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+analyzeJetResolution = cms.EDAnalyzer("JetResolutionAnalyzer",
+    ## input collection                             
+    src = cms.InputTag("selectedLayer1Jets"),
+    ## event weight
+    weight = cms.InputTag("eventWeight"),
+    ## use the weight or not                             
+    useWeight = cms.bool(True),
+    ## analyzer specific configurables
+    analyze   = cms.PSet(
+      matchDR = cms.double ( 0.1),
+      binsPt  = cms.vdouble( 0.0,   20.0,  30.0,  50.0,  80.0, 120.0, 180.0),
+      binsEta = cms.vdouble(-2.5,   -1.3,  -0.5,   0.0,   0.5,   1.3,   2.5),
+      binsPhi = cms.vdouble(              -3.14, -1.57,   0.0,  1.57,  3.14)
+    )    
+)
+
+
+
