6f587761a47e320e48d26df8b39057c556f503a0
henderle <>
2009-09-17 08:29:18 +0000

    replaced some "muon" by "jet"

--- TopAnalyzer/python/JetQuality_cfi.py
+++ TopAnalyzer/python/JetQuality_cfi.py
@@ -1,16 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
-analyzeMuonQuality = cms.EDAnalyzer("JetQualityAnalyzer",
+analyzeJetQuality = cms.EDAnalyzer("JetQualityAnalyzer",
     ## input collection                             
     src = cms.InputTag("selectedLayer1Jets"),
     ## event weight
     weight = cms.InputTag("eventWeight"),
     ## use the weight or not                             
     useWeight = cms.bool(True),
-    ## special parameters for muon quality analysis
+    ## special parameters for jet quality analysis
     analyze   = cms.PSet(
       ## fill histograms for 1.,2.,3.,... leading
-      ## muon? -1 corresponds to 'all'
+      ## jet? -1 corresponds to 'all'
       index  = cms.int32(-1),
       ## flavor tag for the flavor dependend JEC monitor
       ## plots; the form is expected as described in
