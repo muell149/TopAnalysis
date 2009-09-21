e6ae7c53b05116f66ae028f3d251a80bbb4a831c
rwolf <>
2009-09-21 07:51:04 +0000

    repair remove all accident from yesterday :-( -- these files still need the correct blkock sizes...

---
+++ Configuration/python/ttbarx09.py
@@ -0,0 +1,37 @@
+from TopAnalysis.Configuration.defines import sizes 
+from TopAnalysis.Configuration.defines import source
+
+######################################################################
+#                                                                     
+# -----------                                                         
+#                                                                     
+#                                                                     
+# Events      :                                                       
+# xsec(pb)  LO:                                                       
+# xsec(pb) NLO:                                                       
+# eff         :                                                       
+######################################################################
+sizes ['ttbarx09_0'] = 99999999
+source['ttbarx09_0'] ='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_0_ttbar09.root\','
+source['ttbarx09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_10_ttbar09.root\','
+source['ttbarx09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_11_ttbar09.root\','
+source['ttbarx09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_12_ttbar09.root\','
+source['ttbarx09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_13_ttbar09.root\','
+source['ttbarx09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_14_ttbar09.root\','
+source['ttbarx09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_15_ttbar09.root\','
+source['ttbarx09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_16_ttbar09.root\','
+source['ttbarx09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_17_ttbar09.root\','
+source['ttbarx09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_18_ttbar09.root\''
+
+sizes ['ttbarx09_1'] = 99999999
+source['ttbarx09_1'] ='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_19_ttbar09.root\','
+source['ttbarx09_1']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_1_ttbar09.root\','
+source['ttbarx09_1']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_2_ttbar09.root\','
+source['ttbarx09_1']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_3_ttbar09.root\','
+source['ttbarx09_1']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_4_ttbar09.root\','
+source['ttbarx09_1']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_5_ttbar09.root\','
+source['ttbarx09_1']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_6_ttbar09.root\','
+source['ttbarx09_1']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_7_ttbar09.root\','
+source['ttbarx09_1']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_8_ttbar09.root\','
+source['ttbarx09_1']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_9_ttbar09.root\''
+
---
+++ Configuration/python/ttbary09.py
@@ -0,0 +1,25 @@
+from TopAnalysis.Configuration.defines import sizes 
+from TopAnalysis.Configuration.defines import source
+
+######################################################################
+#                                                                     
+# -----------                                                         
+#                                                                     
+#                                                                     
+# Events      :                                                       
+# xsec(pb)  LO:                                                       
+# xsec(pb) NLO:                                                       
+# eff         :                                                       
+######################################################################
+sizes ['ttbary09_0'] = 99999999
+source['ttbary09_0'] ='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_sig_0_ttbarx09.root\','
+source['ttbary09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_sig_1_ttbarx09.root\','
+source['ttbary09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_sig_2_ttbarx09.root\','
+source['ttbary09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_sig_3_ttbarx09.root\','
+source['ttbary09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_sig_4_ttbarx09.root\','
+source['ttbary09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_sig_5_ttbarx09.root\','
+source['ttbary09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_sig_6_ttbarx09.root\','
+source['ttbary09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_sig_7_ttbarx09.root\','
+source['ttbary09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_sig_8_ttbarx09.root\','
+source['ttbary09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_sig_9_ttbarx09.root\''
+
