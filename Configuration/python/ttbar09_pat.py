531c738c99f2dd5494440e184f16c548c5888a0b
rwolf <>
2009-09-21 08:20:26 +0000

    added file extension for files not hosted on dcache

--- Configuration/python/ttbar09_pat.py
+++ Configuration/python/ttbar09_pat.py
@@ -3,35 +3,35 @@ from TopAnalysis.Configuration.defines import source
 
 ######################################################################
 #                                                                     
-# -----------                                                         
+# Summer09/TTbar/AODSIM/MC_31X_V3_AODSIM-v1
 #                                                                     
 #                                                                     
-# Events      :                                                       
-# xsec(pb)  LO:                                                       
-# xsec(pb) NLO:                                                       
-# eff         :                                                       
+# Events      :  529750                                                     
+# xsec(pb)  LO:      --                                                  
+# xsec(pb) NLO:     165                                                   
+# eff         :     1.0                                                  
 ######################################################################
 sizes ['ttbarx09_0'] = 99999999
-source['ttbarx09_0'] ='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_0_ttbar09.root\','
-source['ttbarx09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_10_ttbar09.root\','
-source['ttbarx09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_11_ttbar09.root\','
-source['ttbarx09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_12_ttbar09.root\','
-source['ttbarx09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_13_ttbar09.root\','
-source['ttbarx09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_14_ttbar09.root\','
-source['ttbarx09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_15_ttbar09.root\','
-source['ttbarx09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_16_ttbar09.root\','
-source['ttbarx09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_17_ttbar09.root\','
-source['ttbarx09_0']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_18_ttbar09.root\''
+source['ttbarx09_0'] ='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_0_ttbar09.root\','
+source['ttbarx09_0']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_10_ttbar09.root\','
+source['ttbarx09_0']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_11_ttbar09.root\','
+source['ttbarx09_0']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_12_ttbar09.root\','
+source['ttbarx09_0']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_13_ttbar09.root\','
+source['ttbarx09_0']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_14_ttbar09.root\','
+source['ttbarx09_0']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_15_ttbar09.root\','
+source['ttbarx09_0']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_16_ttbar09.root\','
+source['ttbarx09_0']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_17_ttbar09.root\','
+source['ttbarx09_0']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_18_ttbar09.root\''
 
 sizes ['ttbarx09_1'] = 99999999
-source['ttbarx09_1'] ='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_19_ttbar09.root\','
-source['ttbarx09_1']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_1_ttbar09.root\','
-source['ttbarx09_1']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_2_ttbar09.root\','
-source['ttbarx09_1']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_3_ttbar09.root\','
-source['ttbarx09_1']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_4_ttbar09.root\','
-source['ttbarx09_1']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_5_ttbar09.root\','
-source['ttbarx09_1']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_6_ttbar09.root\','
-source['ttbarx09_1']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_7_ttbar09.root\','
-source['ttbarx09_1']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_8_ttbar09.root\','
-source['ttbarx09_1']+='\'/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_9_ttbar09.root\''
+source['ttbarx09_1'] ='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_19_ttbar09.root\','
+source['ttbarx09_1']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_1_ttbar09.root\','
+source['ttbarx09_1']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_2_ttbar09.root\','
+source['ttbarx09_1']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_3_ttbar09.root\','
+source['ttbarx09_1']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_4_ttbar09.root\','
+source['ttbarx09_1']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_5_ttbar09.root\','
+source['ttbarx09_1']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_6_ttbar09.root\','
+source['ttbarx09_1']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_7_ttbar09.root\','
+source['ttbarx09_1']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_8_ttbar09.root\','
+source['ttbarx09_1']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_all_9_ttbar09.root\''
 
