bfc589a29dd98e00bb7ce386c3d10fa95db10f80
henderle <>
2009-09-28 14:32:40 +0000

    added tightMatchedJets

--- TopFilter/python/sequences/generatorMatching_cff.py
+++ TopFilter/python/sequences/generatorMatching_cff.py
@@ -2,8 +2,9 @@ import FWCore.ParameterSet.Config as cms
 
 ## muon selector
 from PhysicsTools.PatAlgos.selectionLayer1.muonSelector_cfi import *
-## jet selector
+## jet selectors
 from PhysicsTools.PatAlgos.selectionLayer1.jetSelector_cfi import *
+from TopAnalysis.TopFilter.sequences.jetSelection_cff import *
 
 ## ---
 ##    setup jet parton matching
@@ -22,13 +23,29 @@ matchedCharmJets   = selectedLayer1Jets.clone(src = 'selectedLayer1Jets',
 matchedUdsJets     = selectedLayer1Jets.clone(src = 'selectedLayer1Jets',
                                               cut = 'abs(partonFlavour()) < 4'
                                               )
+tightMatchedLightQJets  = selectedLayer1Jets.clone(src = 'goodJets',
+                                              cut = 'abs(partonFlavour()) < 5'
+                                              )
+tightMatchedBottomJets  = selectedLayer1Jets.clone(src = 'goodJets',
+                                              cut = 'abs(partonFlavour()) = 5'
+                                              )
+tightMatchedCharmJets   = selectedLayer1Jets.clone(src = 'goodJets',
+                                              cut = 'abs(partonFlavour()) = 4'
+                                              )
+tightMatchedUdsJets     = selectedLayer1Jets.clone(src = 'goodJets',
+                                              cut = 'abs(partonFlavour()) < 4'
+                                              )
 
 ## four sets of differently matched jet collections
 ## jets are matched to parton level
-matchJetsToPartons = cms.Sequence(matchedLightQJets +
-                                  matchedBottomJets +
-                                  matchedCharmJets  +
-                                  matchedUdsJets
+matchJetsToPartons = cms.Sequence(matchedLightQJets      +
+                                  matchedBottomJets      +
+                                  matchedCharmJets       +
+                                  matchedUdsJets         +
+                                  tightMatchedLightQJets +
+                                  tightMatchedBottomJets +
+                                  tightMatchedCharmJets  +
+                                  tightMatchedUdsJets
                                   )
 
 ## muon matching to generator level
