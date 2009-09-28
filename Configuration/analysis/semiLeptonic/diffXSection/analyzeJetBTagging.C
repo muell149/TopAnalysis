e1f76121225ec115ffe567a07eedfd5505b2968b
henderle <>
2009-09-28 15:11:25 +0000

    changed from "all, signal and background" to "bottom jets and light quark jets"

--- Configuration/analysis/semiLeptonic/diffXSection/analyzeJetBTagging.C
+++ Configuration/analysis/semiLeptonic/diffXSection/analyzeJetBTagging.C
@@ -8,14 +8,14 @@
 #include <TCanvas.h>
 #include <TLegend.h>
 
-enum styles {kAll, kSignal, kBackground};
+enum styles {kLightQ, kBottom};
 
 void canvasStyle(TCanvas& canv);
 void histogramStyle(TH1& hist, unsigned int style);
 void axesStyle(TH1& hist, const char* titleX, const char* titleY);
 
 
-void analyzeJetBTagging()
+void analyzeJetBTagging(TString selection = "all")
 {
   // ---
   //    set root style 
@@ -24,135 +24,120 @@ void analyzeJetBTagging()
   //gROOT->SetStyle("Plain");
 
   // ---
-  //    open input files
+  //    open input file
   // ---
-  std::vector<TFile*> files_;
-  files_.push_back(new TFile("./rootfiles/analyzeSelection_all.root") );
-  files_.push_back(new TFile("./rootfiles/analyzeSelection_sig.root") );
-  files_.push_back(new TFile("./rootfiles/analyzeSelection_bkg.root") );
+  TFile* file = new TFile("./rootfiles/analyzeSemiLeptonicSelection_"+selection+".root");
+
+  std::vector<TString> jet_B_or_not_jet_B_;
+  jet_B_or_not_jet_B_.push_back("tightMatchedLightQJetQuality");
+  jet_B_or_not_jet_B_.push_back("tightMatchedBJetQuality");
 
   // ---
   //    get histograms
   // ---
   std::vector<TH1F*> btag0_, btag1_, btag2_, btag3_;
-  for(unsigned int idx=0; idx<files_.size(); ++idx) {
-    btag0_.push_back( (TH1F*)files_[idx]->Get("tightLeadingJetQuality/btagSimpleSecVtx_"    ) );
-    btag1_.push_back( (TH1F*)files_[idx]->Get("tightLeadingJetQuality/btagSimpleSecVtxNeg_" ) );
-    btag2_.push_back( (TH1F*)files_[idx]->Get("tightLeadingJetQuality/btagTrkCntHighEff_"   ) );
-    btag3_.push_back( (TH1F*)files_[idx]->Get("tightLeadingJetQuality/btagTrkCntHighPurity" ) );
+  for(unsigned int idx=0; idx<jet_B_or_not_jet_B_.size(); ++idx) {
+    btag0_.push_back( (TH1F*)file->Get(jet_B_or_not_jet_B_[idx]+"/btagSimpleSecVtx_"    )->Clone() );
+    btag1_.push_back( (TH1F*)file->Get(jet_B_or_not_jet_B_[idx]+"/btagSimpleSecVtxNeg_" )->Clone() );
+    btag2_.push_back( (TH1F*)file->Get(jet_B_or_not_jet_B_[idx]+"/btagTrkCntHighEff_"   )->Clone() );
+    btag3_.push_back( (TH1F*)file->Get(jet_B_or_not_jet_B_[idx]+"/btagTrkCntHighPurity" )->Clone() );
   }
 
   // ---
-  //    close input files
+  //    close input file
   // ---
-  for(unsigned int idx=0; idx<files_.size(); ++idx) {
-    //files_[idx]->Close();
-  }
+  //file->Close();
+
   
   // ---
   //    configure histograms
   // ---
 
   // btag0_
-  histogramStyle(*btag0_  [kAll       ], kAll       );
-  histogramStyle(*btag0_  [kSignal    ], kSignal    );
-  histogramStyle(*btag0_  [kBackground], kBackground);
-
+  histogramStyle(*btag0_  [kLightQ    ], kLightQ    );
+  histogramStyle(*btag0_  [kBottom], kBottom);
 
   // btag1_
-  histogramStyle(*btag1_ [kAll       ], kAll       );
-  histogramStyle(*btag1_ [kSignal    ], kSignal    );
-  histogramStyle(*btag1_ [kBackground], kBackground);
+  histogramStyle(*btag1_ [kLightQ    ], kLightQ    );
+  histogramStyle(*btag1_ [kBottom], kBottom);
 
   // btag2_
-  histogramStyle(*btag2_[kAll       ], kAll       );
-  histogramStyle(*btag2_[kSignal    ], kSignal    );
-  histogramStyle(*btag2_[kBackground], kBackground);
+  histogramStyle(*btag2_[kLightQ    ], kLightQ    );
+  histogramStyle(*btag2_[kBottom], kBottom);
 
   // btag3_
-  histogramStyle(*btag3_[kAll       ], kAll       );
-  histogramStyle(*btag3_[kSignal    ], kSignal    );
-  histogramStyle(*btag3_[kBackground], kBackground);
-
-  // ---
-  //    do the printing for n_
-  // ---
-  TCanvas* canv0 = new TCanvas("canv0", "canv0", 600, 600); canvasStyle(*canv0);
+  histogramStyle(*btag3_[kLightQ    ], kLightQ    );
+  histogramStyle(*btag3_[kBottom], kBottom);
 
   // create a legend (in upper right corner)
-  TLegend *leg0 = new TLegend(0.45, 0.65, 1.05, 0.9);
+  TLegend *leg0 = new TLegend(0.45, 0.67, 1.05, 0.92);
   leg0->SetFillStyle(0);
   leg0->SetBorderSize(0);
   leg0->SetHeader("Top-Antitop(Phythia)");
-  leg0->AddEntry( btag0_[kAll       ] , "inclusive"         , "PL" );
-  leg0->AddEntry( btag0_[kSignal    ] , "semi-lep. ( #mu )" , "FL" );
-  leg0->AddEntry( btag0_[kBackground] , "other decays"      , "FL" );
+  leg0->AddEntry( btag0_[kLightQ] , "light quarks"  , "FL" );
+  leg0->AddEntry( btag0_[kBottom] , "bottom quarks" , "FL" );
 
   // create a legend (in upper center)
-  TLegend *leg1 = new TLegend(0.35, 0.65, 1.05, 0.9);
+  TLegend *leg1 = new TLegend(0.35, 0.67, 1.05, 0.92);
   leg1->SetFillStyle(0);
   leg1->SetBorderSize(0);
   leg1->SetHeader("Top-Antitop(Phythia)");
-  leg1->AddEntry( btag0_[kAll       ] , "inclusive"         , "PL" );
-  leg1->AddEntry( btag0_[kSignal    ] , "semi-lep. ( #mu )" , "FL" );
-  leg1->AddEntry( btag0_[kBackground] , "other decays"      , "FL" );
+  leg1->AddEntry( btag0_[kLightQ] , "light quarks"  , "FL" );
+  leg1->AddEntry( btag0_[kBottom] , "bottom quarks" , "FL" );
+
+  // ---
+  //    do the printing for Simple Secondary Vtx
+  // ---
+  TCanvas* canv0 = new TCanvas("canv0", "canv0", 600, 600); canvasStyle(*canv0);
   
   // draw canvas
-  canv0->cd(0);
-  axesStyle(*btag0_[kAll], "BTag(Simple Secondary Vtx)", "events");
-  btag0_[kAll       ]->SetMinimum(0.);
-  btag0_[kAll       ]->SetMaximum( 1.3* btag0_[kAll]->GetMaximum() );
-  btag0_[kAll       ]->Draw();
-  btag0_[kSignal    ]->Draw("same");
-  btag0_[kBackground]->Draw("same");
-  //btag0_  [kAll       ]->Draw("esame");
+  canv0->cd(0)->SetLogy();
+  axesStyle(*btag0_[kLightQ], "BTag(Simple Secondary Vtx)", "events");
+//   btag0_[kLightQ       ]->SetMinimum(0.);
+  btag0_[kLightQ]->SetMaximum( 3.3* btag0_[kLightQ]->GetMaximum() );
+  btag0_[kLightQ]->Draw();
+  btag0_[kBottom]->Draw("same");
   leg0->Draw("same");
 
   // ---
-  //    do the printing for pt_
+  //    do the printing for Simple Secondary VtxNeg
   // ---
   TCanvas* canv1 = new TCanvas("canv1", "canv1", 600, 600); canvasStyle(*canv1);
 
   // draw canvas
   canv1->cd(0);
-  axesStyle(*btag1_[kAll], "BTag_{Neg}(Simple Secondary Vtx)", "events");
-  btag1_[kAll       ]->SetMaximum( 1.3* btag1_[kAll]->GetMaximum() );
-  btag1_[kAll       ]->Draw();
-  btag1_[kSignal    ]->Draw("same");
-  btag1_[kBackground]->Draw("same");
-  //btag1_ [kAll       ]->Draw("same");
+  axesStyle(*btag1_[kLightQ], "BTag_{Neg}(Simple Secondary Vtx)", "events");
+  btag1_[kLightQ    ]->SetMaximum( 1.3* btag1_[kLightQ]->GetMaximum() );
+  btag1_[kLightQ    ]->Draw();
+  btag1_[kBottom]->Draw("same");
   leg0->Draw("same");
 
   // ---
-  //    do the printing for eta_
+  //    do the printing for Track Counting high eff
   // ---
   TCanvas* canv2 = new TCanvas("canv2", "canv2", 600, 600); canvasStyle(*canv2);
 
   // draw canvas
   canv2->cd(0);
-  axesStyle(*btag2_[kAll], "BTag_{Eff}(Track Counting)", "events");
-  btag2_[kAll       ]->SetMinimum( 0 );
-  btag2_[kAll       ]->SetMaximum( 1.3* btag2_[kAll]->GetMaximum() );
-  btag2_[kAll       ]->Draw();
-  btag2_[kSignal    ]->Draw("same");
-  btag2_[kBackground]->Draw("same");
-  //eta_[kAll       ]->Draw("same");
+  axesStyle(*btag2_[kLightQ], "BTag_{Eff}(Track Counting)", "events");
+  btag2_[kLightQ]->SetMinimum( 0 );
+  btag2_[kLightQ]->SetMaximum( 1.3* btag2_[kLightQ]->GetMaximum() );
+  btag2_[kLightQ]->Draw();
+  btag2_[kBottom]->Draw("same");
   leg1->Draw("same");
 
   // ---
-  //    do the printing for phi_
+  //    do the printing for Track Counting high pure
   // ---
   TCanvas* canv3 = new TCanvas("canv3", "canv3", 600, 600); canvasStyle(*canv3);
 
   // draw canvas
   canv3->cd(0);
-  axesStyle(*btag3_[kAll], "BTag_{Pure}(Track Counting)", "events");
-  btag3_[kAll       ]->SetMinimum( 0 );
-  btag3_[kAll       ]->SetMaximum( 1.3* btag3_[kAll]->GetMaximum() );
-  btag3_[kAll       ]->Draw();
-  btag3_[kSignal    ]->Draw("same");
-  btag3_[kBackground]->Draw("same");
-  //btag3_[kAll       ]->Draw("same");
+  axesStyle(*btag3_[kLightQ], "BTag_{Pure}(Track Counting)", "events");
+  btag3_[kLightQ]->SetMinimum( 0 );
+  btag3_[kLightQ]->SetMaximum( 1.3* btag3_[kLightQ]->GetMaximum() );
+  btag3_[kLightQ]->Draw();
+  btag3_[kBottom]->Draw("same");
   leg1->Draw("same");
 }
 
@@ -169,33 +154,22 @@ void histogramStyle(TH1& hist, unsigned int style)
 {
   // pre-defined line style
   std::vector<int> color;
-  color.push_back( kBlack); 
-  color.push_back( kRed  ); 
   color.push_back( kBlue );
+  color.push_back( kRed  ); 
 
   // pre-defined fill style
   std::vector<int> fill;
-  fill.push_back(   1); 
-  fill.push_back(3004); 
   fill.push_back(3005);
-
-  // pre-defined marker style
-  std::vector<int> marker;
-  marker.push_back( 20);
+  fill.push_back(3004); 
 
   // set line width
-  style==kAll ? hist.SetLineWidth(6) : hist.SetLineWidth(3);
+  hist.SetLineWidth(3);
 
   hist.SetStats(kFALSE);
   hist.SetLineColor  (color[style]);
   hist.SetFillColor  (color[style]);
   hist.SetMarkerColor(color[style]);
   hist.SetFillStyle  (fill [style]);
-
-  if(style==kAll){
-    hist.SetMarkerStyle(marker[style]);
-    hist.SetMarkerSize(1.4);
-  }
 }
 
 void axesStyle(TH1& hist, const char* titleX, const char* titleY) 
