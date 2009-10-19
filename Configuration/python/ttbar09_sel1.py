0045b44f031ee869204f9ae1596a79827b6b45b3
rwolf <>
2009-10-19 17:56:57 +0000

    selected samples after different steps

---
+++ Configuration/python/ttbar09_sel1.py
@@ -0,0 +1,25 @@
+from TopAnalysis.Configuration.defines import sizes 
+from TopAnalysis.Configuration.defines import source
+
+######################################################################
+#                                                                     
+# -----------                                                         
+#                                                                     
+#                                                                     
+# Events      :    10833                                                
+# xsec(pb)  LO:       --                                              
+# xsec(pb) NLO:      165                                                 
+# eff         :      1.0                                              
+######################################################################
+sizes ['ttbarzs09_0'] = 10833
+source['ttbarzs09_0'] ='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_selected_sig_0_ttbary09.root\','
+source['ttbarzs09_0']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_selected_sig_1_ttbary09.root\','
+source['ttbarzs09_0']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_selected_sig_2_ttbary09.root\','
+source['ttbarzs09_0']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_selected_sig_3_ttbary09.root\','
+source['ttbarzs09_0']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_selected_sig_4_ttbary09.root\','
+source['ttbarzs09_0']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_selected_sig_5_ttbary09.root\','
+source['ttbarzs09_0']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_selected_sig_6_ttbary09.root\','
+source['ttbarzs09_0']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_selected_sig_7_ttbary09.root\','
+source['ttbarzs09_0']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_selected_sig_8_ttbary09.root\','
+source['ttbarzs09_0']+='\'file:/afs/naf.desy.de/user/r/rwolf/data/samples/ttbar09/patTuple_selected_sig_9_ttbary09.root\''
+
