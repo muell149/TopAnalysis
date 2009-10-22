ae34a9de07ac64c7b3a8ac35f847a376d80c7793
henderle <>
2009-10-22 11:38:23 +0000

    Now the trackMuons which are input to the looseMuons have looser cuts: pt > 10GeV and abs(eta) < 2.5. The original cuts pt > 20GeV and abs(eta) < 2.1 are applied subsequently for the goodMuons.

--- TopFilter/python/sequences/muonSelection_cff.py
+++ TopFilter/python/sequences/muonSelection_cff.py
@@ -11,61 +11,74 @@ from PhysicsTools.PatAlgos.selectionLayer1.muonSelector_cfi import *
 combinedMuons = selectedLayer1Muons.clone(src = 'selectedLayer1Muons',
                                           cut = 'combinedMuon.isNull = 0'
                                           )
-centralMuons  = selectedLayer1Muons.clone(src = 'selectedLayer1Muons',
+triggerMuons  = selectedLayer1Muons.clone(src = 'selectedLayer1Muons',
                                           cut = 'combinedMuon.isNull = 0 &'
-                                                'abs(eta) < 2.1'
-                                          )
-goodMuons     = selectedLayer1Muons.clone(src = 'selectedLayer1Muons',
-                                          cut = 'combinedMuon.isNull = 0 &'
-                                                'abs(eta) < 2.1 & pt > 20.'
+                                                'abs(eta) < 2.5 & pt > 10.'
                                           )
 
 ## a kinematically well defined global muon,
 ## with some chance to pass the std triggers
-selectGoodMuons = cms.Sequence(combinedMuons *
-                               centralMuons
-                               )
+selectTriggerMuons = cms.Sequence(combinedMuons *
+                                  triggerMuons
+                                  )
                                
 ## check tracker related muon qualities
-validHits     = selectedLayer1Muons.clone(src = 'goodMuons',
+validHits     = selectedLayer1Muons.clone(src = 'triggerMuons',
                                           cut = 'track.numberOfValidHits >= 11'
                                           )
-chi2Fit       = selectedLayer1Muons.clone(src = 'goodMuons',
+chi2Fit       = selectedLayer1Muons.clone(src = 'triggerMuons',
                                           cut = 'combinedMuon.normalizedChi2 < 10.0'
                                           )
-trackSig      = selectedLayer1Muons.clone(src = 'goodMuons',
+trackSig      = selectedLayer1Muons.clone(src = 'triggerMuons',
                                           cut = 'abs(track.d0) < 0.2'
                                           )
-trackMuons    = selectedLayer1Muons.clone(src = 'goodMuons',
+trackMuons    = selectedLayer1Muons.clone(src = 'triggerMuons',
                                           cut = 'track.numberOfValidHits >= 11 &'
                                                 'combinedMuon.normalizedChi2 < 10.0 &'
                                                 'abs(track.d0) < 0.2'
                                           )
 
-## a goodMuon with good tracker quality
-selectTrackMuons = cms.Sequence(goodMuons *
-                                validHits *
+## a triggerMuon with good tracker quality
+selectTrackMuons = cms.Sequence(validHits *
                                 chi2Fit   *
-                                trackSig
+                                trackSig  *
+                                trackMuons
                                 )
+
+## check muon kinematics
+centralMuons  = selectedLayer1Muons.clone(src = 'trackMuons',
+                                          cut = 'abs(eta) < 2.1'
+                                          )
+highptMuons   = selectedLayer1Muons.clone(src = 'trackMuons',
+                                          cut = 'pt > 20.'
+                                          )
+goodMuons     = selectedLayer1Muons.clone(src = 'trackMuons',
+                                          cut = 'pt > 20. & abs(eta) < 2.1'
+                                          )
+## a kinematically well defined trackMuon
+selectGoodMuons = cms.Sequence(centralMuons *
+                               highptMuons  *
+                               goodMuons
+                               )
+
 
 ## check for well behaved MIP qualities in
 ## both calorimeters
-ecalDep       = selectedLayer1Muons.clone(src = 'trackMuons',
+ecalDep       = selectedLayer1Muons.clone(src = 'goodMuons',
                                           cut = 'ecalIsoDeposit.candEnergy < 4'
                                           )
-hcalDep       = selectedLayer1Muons.clone(src = 'trackMuons',
+hcalDep       = selectedLayer1Muons.clone(src = 'goodMuons',
                                           cut = 'hcalIsoDeposit.candEnergy < 6'
                                           )
-goldenMuons   = selectedLayer1Muons.clone(src = 'trackMuons',
+goldenMuons   = selectedLayer1Muons.clone(src = 'goodMuons',
                                           cut = 'ecalIsoDeposit.candEnergy < 4 &'
                                                 'hcalIsoDeposit.candEnergy < 6'
                                           )
-## a trackMuon withgood MIP characteristics
+## a goodMuon withgood MIP characteristics
 ## in both calorimeters
-selectGoldenMuons = cms.Sequence(trackMuons *
-                                 ecalDep    *
-                                 hcalDep
+selectGoldenMuons = cms.Sequence(ecalDep    *
+                                 hcalDep    *
+                                 goldenMuons
                                  )
 
 ## check for isolation
@@ -79,9 +92,8 @@ relIsoMuons   = selectedLayer1Muons.clone(src = 'goldenMuons',
                                           cut = '(trackIso+caloIso)/pt < 0.05'
                                           )
 
-## a goodMuons which are isolated
-selectIsolatedMuons = cms.Sequence(goldenMuons   *
-                                   caloIsoMuons  *
+## a goldenMuons which are isolated
+selectIsolatedMuons = cms.Sequence(caloIsoMuons  *
                                    trackIsoMuons *
                                    relIsoMuons
                                    )
