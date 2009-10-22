ee049b48e835f697a753ef9f8baed8332978040c
dammann <>
2009-10-22 17:32:34 +0000

    simplified, added comments

--- TopAnalyzer/interface/DimuonAnalyzer.h
+++ TopAnalyzer/interface/DimuonAnalyzer.h
@@ -25,38 +25,55 @@
 #include "TopAnalysis/TopUtils/interface/RootSystem.h"
 #include "TopAnalysis/TopUtils/interface/RootHistograms.h"
 
+/**
+   \class   DimuonAnalyzer DimuonAnalyzer.h "TopAnalysis/TopAnalyzer/interface/DimuonAnalyzer.h"
+
+   \brief   EDAnalyzer for the dimuon mass and some isolation plots
+
+   The main histograms plot the invariant mass of the two leading muons separately for like and 
+   unlike charge in events with at least two muons. Other histograms show the cut efficiency for 
+   different kinds of isolation cuts on both muons. 
+*/
+
 class TH1F;
 class TH2F;
 class DimuonAnalyzer : public edm::EDAnalyzer {
 
   public:
-
+    /// default constructor
     explicit DimuonAnalyzer(const edm::ParameterSet&);
+    /// default destructor
     ~DimuonAnalyzer();
     
   private:
-  
+    /// everything which has to be done before the event loop: book fileservice and histograms
     virtual void beginJob(const edm::EventSetup&);
+    /// everything which has to be done during the event loop: analyze and fill histos
     virtual void analyze(const edm::Event&, const edm::EventSetup&);
+    /// everything which has to be done after the event loop: empty 
     virtual void endJob();
-    
-    bool matches(const reco::GenParticle*, const reco::GenParticle*);
-    
-    edm::InputTag muons_, ref_;
-    bool matchMuons_;     
-    bool useEvtWgt_;  
-    std::string hist_;     
+
+    /// input muon collection
+    edm::InputTag muons_;
+    /// input bool: use a event weight?     
+    bool useEvtWgt_;
+    /// define a PatMuonCollection as a vector of PatMuons      
     typedef std::vector<pat::Muon> PatMuonCollection; 
-    
+    /// plots for right-charge dimuon mass    
     TH1F * dimassRC_;
+    /// plots for wrong-charge dimuon mass
     TH1F * dimassWC_;
       
-            
+    /// correlation between dimuon mass and isolation        
     TH2F * isoDimassCorrelation_;   
     
+    /// isolation efficiency for abslute cut
     TH1F * absCount_;
+    /// isolation efficiency for relative cut
     TH1F * relCount_;
+    /// isolation efficiency for combined cut
     TH1F * combCount_;
+    /// isolation efficiency for quadratically added combined isolation
     TH1F * diCombCount_;
 };
 
--- TopAnalyzer/src/DimuonAnalyzer.cc
+++ TopAnalyzer/src/DimuonAnalyzer.cc
@@ -1,35 +1,28 @@
 #include "TLorentzVector.h"
-
-#include "DataFormats/Candidate/interface/Candidate.h"
-#include "DataFormats/Math/interface/deltaR.h"
 #include "TopAnalysis/TopAnalyzer/interface/DimuonAnalyzer.h"
 
+/// default constructor
 DimuonAnalyzer::DimuonAnalyzer(const edm::ParameterSet& cfg):
-  muons_      ( cfg.getParameter<edm::InputTag>( "muons"          ) ),
-  ref_        ( cfg.getParameter<edm::InputTag>( "ref"            ) ),  
-  matchMuons_ ( cfg.getParameter<bool>         ( "doMatch"        ) ),  
-  useEvtWgt_  ( cfg.getParameter<bool>         ( "useEventWeight" ) ),  
-  hist_       ( cfg.getParameter<std::string>  ( "hist"           ) )  
+  muons_      ( cfg.getParameter<edm::InputTag>( "muons"          ) ), 
+  useEvtWgt_  ( cfg.getParameter<bool>         ( "useEventWeight" ) ) 
 {
 }
 
+/// default destructor
 DimuonAnalyzer::~DimuonAnalyzer()
 {
 }
 
+/// everything which has to be done before the event loop: book fileservice and histograms
 void
 DimuonAnalyzer::beginJob(const edm::EventSetup&)
 {
-  if( hist_.empty() )
-    return;
-
   edm::Service<TFileService> fs;
   if( !fs ){
     throw edm::Exception( edm::errors::Configuration,
                           "TFile Service is not registered in cfg file" );
   }
 
-  ofstream hist(hist_.c_str(), std::ios::out);
   // define logarithmic bins for a histogram with 50 bins going from 10^0 to 10^2.4 =~ 250
   const int nbins = 50;
   double logmin =  0.0;
@@ -39,75 +32,62 @@ DimuonAnalyzer::beginJob(const edm::EventSetup&)
     double log = logmin + (logmax-logmin)*i/nbins;
     bins[i] = std::pow(10.0, log);
   }
-
-  NameScheme e("dimuon");
-  dimassRC_    = fs->make<TH1F>(e.name(hist, "dimassRC"    ), e.name("dimassRC"    ), nbins, bins);  
-  dimassWC_    = fs->make<TH1F>(e.name(hist, "dimassWC"    ), e.name("dimassWC"    ), nbins, bins);
-         
-  isoDimassCorrelation_ = fs->make<TH2F>(e.name(hist, "isoDimassCorrelation" ), e.name("isoDimassCorrelation" ), 10,   0.,   200., 10, 0., 1.);
+  /**
+     histogram definitions
+  **/ 
+  // invariant muon muon mass for right charge  
+  dimassRC_    = fs->make<TH1F>( "dimassRC", "dimassRC", nbins, bins);
+  // invariant muon muon mass for wrong charge   
+  dimassWC_    = fs->make<TH1F>( "dimassWC", "dimassWC", nbins, bins);
+  // correlation between invariant mass and absolute combined isolation        
+  isoDimassCorrelation_ = fs->make<TH2F>( "isoDimassCorrelation", "isoDimassCorrelation", 10,   0.,   200., 10, 0., 1.);
   
-  absCount_    = fs->make<TH1F>(e.name( hist, "absCount"    ), e.name("absCount"    ),100,  0.,  50.0);        
-  relCount_    = fs->make<TH1F>(e.name( hist, "relCount"    ), e.name("relCount"    ),100,  0.,   5.0); 
-  combCount_   = fs->make<TH1F>(e.name( hist, "combCount"   ), e.name("combCount"   ),100,  0.0,  1.0); 
-  diCombCount_ = fs->make<TH1F>(e.name( hist, "diCombCount" ), e.name("diCombCount" ),100,  0.0,  2.0);
+  // absolute isolation efficiency
+  absCount_    = fs->make<TH1F>( "absCount"   , "absCount"    ,100,  0.,  50.0); 
+  // relative isolation efficiency       
+  relCount_    = fs->make<TH1F>( "relCount"   , "relCount"    ,100,  0.,   5.0); 
+  // combined isolation efficiency
+  combCount_   = fs->make<TH1F>( "combCount"  , "combCount"   ,100,  0.0,  1.0); 
+  // quadratically added combined isolation
+  diCombCount_ = fs->make<TH1F>( "diCombCount", "diCombCount" ,100,  0.0,  2.0);
 }
 
+/// everything which has to be done during the event loop: analyze and fill histos
 void
 DimuonAnalyzer::analyze(const edm::Event& evt, const edm::EventSetup&)
 {
+  // get handle on muon collection
   edm::Handle<PatMuonCollection> muons; 
   evt.getByLabel(muons_, muons); 
-                 
+   
+  // get weight when indicated, else weight is 1               
   double weight = 1.;
   if(useEvtWgt_){ 
     edm::Handle<double> weightHandle; 
     evt.getByLabel("eventWeight", weightHandle); 
     weight = *weightHandle;
   }
-
+  // test if muon collection contains at least two muons
   if(muons->size()<2) return;
-        
-  PatMuonCollection::const_reference mu1 = muons->at(0);
-  PatMuonCollection::const_reference mu2 = muons->at(1);   
   
-  if(matchMuons_){       
-    if(!mu1.genLepton() || !mu2.genLepton()){ 
-      return;
-    }
-    edm::Handle<reco::GenParticleCollection> genParticles;
-    evt.getByLabel(ref_, genParticles);
-    
-    const reco::GenParticle* genMuMinus = 0;
-    const reco::GenParticle* genMuPlus = 0;
-    
-    for(reco::GenParticleCollection::const_iterator part = genParticles->begin(); part!=genParticles->end(); ++part ){
-      if(part->pdgId()== 13) genMuMinus = &*part;
-      if(part->pdgId()==-13) genMuPlus  = &*part;
-    }
-    
-    if(!genMuMinus || !genMuPlus) return;
-    
-    const reco::GenParticle* genMu1 = mu1.genLepton();
-    const reco::GenParticle* genMu2 = mu2.genLepton();    
-    
-    if( !(matches(genMu1, genMuPlus) && matches(genMu2, genMuMinus)) && !(matches(genMu2, genMuPlus) && matches(genMu1, genMuMinus)) ){ 
-      return;
-    }
-  } 
-      
+  // get leading muons      
+  PatMuonCollection::const_reference mu1 = muons->at(0);
+  PatMuonCollection::const_reference mu2 = muons->at(1);     
+  
+  // calculate dimass    
   TLorentzVector diLepLVector = TLorentzVector(mu1.px()+mu2.px(), mu1.py()+mu2.py(), 
-                                               mu1.pz()+mu2.pz(), mu1.energy()+mu2.energy());
-   
+                                               mu1.pz()+mu2.pz(), mu1.energy()+mu2.energy());   
   double dilepMass = (diLepLVector).M();
        
-  
+  // fill dimuon mass histograms:
+  //right charge
   if(mu1.charge()*mu2.charge()<0.)  
     dimassRC_  ->Fill( dilepMass, weight);    
-  
+  // wrong charge
   if(mu1.charge()*mu2.charge()>0.) 
     dimassWC_  ->Fill( dilepMass, weight);    
      
-    
+  // get different isos  
   double absTrackIso1 = mu1.trackIso();
   double absTrackIso2 = mu2.trackIso();
   
@@ -124,10 +104,12 @@ DimuonAnalyzer::analyze(const edm::Event& evt, const edm::EventSetup&)
   double combIso2 = mu2.pt()/(mu2.pt()+mu2.trackIso()+mu2.caloIso());     
   
   double diCombIso = sqrt(combIso1*combIso1+combIso2*combIso2);
-    
+  
+  // fill dimass-isolation correlation hist  
   isoDimassCorrelation_->Fill( dilepMass, combIso1, weight);  
   isoDimassCorrelation_->Fill( dilepMass, combIso2, weight);
-      
+  
+  // fill isolation efficiency hists    
   for(int i=1;i<=100;++i){
     if(diCombIso>0.02*i){
       diCombCount_->SetBinContent(i,diCombCount_->GetBinContent(i)+1);
@@ -144,16 +126,10 @@ DimuonAnalyzer::analyze(const edm::Event& evt, const edm::EventSetup&)
   }     
 }
 
+/// everything which has to be done during the event loop: analyze and fill histos: empty
 void
 DimuonAnalyzer::endJob()
 {
-}
-
-bool
-DimuonAnalyzer::matches(const reco::GenParticle* p1, const reco::GenParticle* p2)
-{
-  if( deltaR(p1->phi(), p1->eta(), p2->phi(), p2->eta()) < 0.01 ) return true;
-  return false;
 }
 
 #include "FWCore/Framework/interface/MakerMacros.h"
