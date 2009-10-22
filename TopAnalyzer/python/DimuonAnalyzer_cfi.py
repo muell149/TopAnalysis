a9b8a8ebde7267815ef927f865834feaa97ed42a
dammann <>
2009-10-22 17:31:53 +0000

    simplified

--- TopAnalyzer/python/DimuonAnalyzer_cfi.py
+++ TopAnalyzer/python/DimuonAnalyzer_cfi.py
@@ -1,19 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 analyzeMuonPair = cms.EDAnalyzer("DimuonAnalyzer",
-
-    # Sources
-    muons = cms.InputTag("selectedLayer1Muons"),
-    ref = cms.InputTag("decaySubset"), 
-    
-    # Are the events weighted?
+    # source
+    muons = cms.InputTag("selectedLayer1Muons"),     
+    # option to weight events
     useEventWeight = cms.bool(True), 
-    
-    # HistFile          
-    hist = cms.string('muonPair.hist'), 
-    
-    # match with mc        
-    doMatch = cms.bool(False)
 )
 
 
