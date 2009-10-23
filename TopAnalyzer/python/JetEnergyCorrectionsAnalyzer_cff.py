cec63b2ed7680a02845683313f349cc0c2b117ce
snaumann <>
2009-10-23 09:58:18 +0000

    use geom hypothesis in addition to genMatch

--- TopAnalyzer/python/JetEnergyCorrectionsAnalyzer_cff.py
+++ TopAnalyzer/python/JetEnergyCorrectionsAnalyzer_cff.py
@@ -49,17 +49,40 @@ ttSemiLepHypGenMatch_multilevel = cms.Sequence(ttSemiLepHypGenMatchRaw  *
                                                )
 
 ##########################################################################################
+## produce geom hypothesis for different jet energy correction levels
+##########################################################################################
+
+ttSemiLepHypGeomRaw  = ttSemiLepHypGeom.clone(useBTagging = True, jetCorrectionLevel = "raw" )
+ttSemiLepHypGeomOff  = ttSemiLepHypGeom.clone(useBTagging = True, jetCorrectionLevel = "off" ) ## L1
+ttSemiLepHypGeomRel  = ttSemiLepHypGeom.clone(useBTagging = True, jetCorrectionLevel = "rel" ) ## L2
+ttSemiLepHypGeomAbs  = ttSemiLepHypGeom.clone(useBTagging = True, jetCorrectionLevel = "abs" ) ## L3
+ttSemiLepHypGeomEmf  = ttSemiLepHypGeom.clone(useBTagging = True, jetCorrectionLevel = "emf" ) ## L4
+ttSemiLepHypGeomHad  = ttSemiLepHypGeom.clone(useBTagging = True, jetCorrectionLevel = "had" ) ## L5
+ttSemiLepHypGeomUe   = ttSemiLepHypGeom.clone(useBTagging = True, jetCorrectionLevel = "ue"  ) ## L6
+ttSemiLepHypGeomPart = ttSemiLepHypGeom.clone(useBTagging = True, jetCorrectionLevel = "part") ## L7
+
+ttSemiLepHypGeom_multilevel = cms.Sequence(ttSemiLepHypGeomRaw  *
+                                           ttSemiLepHypGeomOff  *
+                                           ttSemiLepHypGeomRel  *
+                                           ttSemiLepHypGeomAbs  *
+                                           ttSemiLepHypGeomEmf  *
+                                           ttSemiLepHypGeomHad  *
+                                           ttSemiLepHypGeomUe   *
+                                           ttSemiLepHypGeomPart
+                                           )
+
+##########################################################################################
 ## produce ttSemiLeptonicEvent for different jet energy correction levels
 ##########################################################################################
 
-ttSemiLepEventRaw  = ttSemiLepEvent.clone(hypotheses = ["ttSemiLepHypGenMatchRaw" ])
-ttSemiLepEventOff  = ttSemiLepEvent.clone(hypotheses = ["ttSemiLepHypGenMatchOff" ]) ## L1
-ttSemiLepEventRel  = ttSemiLepEvent.clone(hypotheses = ["ttSemiLepHypGenMatchRel" ]) ## L2
-ttSemiLepEventAbs  = ttSemiLepEvent.clone(hypotheses = ["ttSemiLepHypGenMatchAbs" ]) ## L3
-ttSemiLepEventEmf  = ttSemiLepEvent.clone(hypotheses = ["ttSemiLepHypGenMatchEmf" ]) ## L4
-ttSemiLepEventHad  = ttSemiLepEvent.clone(hypotheses = ["ttSemiLepHypGenMatchHad" ]) ## L5
-ttSemiLepEventUe   = ttSemiLepEvent.clone(hypotheses = ["ttSemiLepHypGenMatchUe"  ]) ## L6
-ttSemiLepEventPart = ttSemiLepEvent.clone(hypotheses = ["ttSemiLepHypGenMatchPart"]) ## L7
+ttSemiLepEventRaw  = ttSemiLepEvent.clone(hypotheses = ["ttSemiLepHypGenMatchRaw" , "ttSemiLepHypGeomRaw" ])
+ttSemiLepEventOff  = ttSemiLepEvent.clone(hypotheses = ["ttSemiLepHypGenMatchOff" , "ttSemiLepHypGeomOff" ]) ## L1
+ttSemiLepEventRel  = ttSemiLepEvent.clone(hypotheses = ["ttSemiLepHypGenMatchRel" , "ttSemiLepHypGeomRel" ]) ## L2
+ttSemiLepEventAbs  = ttSemiLepEvent.clone(hypotheses = ["ttSemiLepHypGenMatchAbs" , "ttSemiLepHypGeomAbs" ]) ## L3
+ttSemiLepEventEmf  = ttSemiLepEvent.clone(hypotheses = ["ttSemiLepHypGenMatchEmf" , "ttSemiLepHypGeomEmf" ]) ## L4
+ttSemiLepEventHad  = ttSemiLepEvent.clone(hypotheses = ["ttSemiLepHypGenMatchHad" , "ttSemiLepHypGeomHad" ]) ## L5
+ttSemiLepEventUe   = ttSemiLepEvent.clone(hypotheses = ["ttSemiLepHypGenMatchUe"  , "ttSemiLepHypGeomUe"  ]) ## L6
+ttSemiLepEventPart = ttSemiLepEvent.clone(hypotheses = ["ttSemiLepHypGenMatchPart", "ttSemiLepHypGeomPart"]) ## L7
 
 ttSemiLepEvent_multilevel = cms.Sequence(ttSemiLepEventRaw  *
                                          ttSemiLepEventOff  *
@@ -72,27 +95,50 @@ ttSemiLepEvent_multilevel = cms.Sequence(ttSemiLepEventRaw  *
                                          )
 
 ##########################################################################################
-## analyze ttSemiLeptonicEvent for different jet energy correction levels
+## analyze genMatch hypothesis for different jet energy correction levels
 ##########################################################################################
 
-analyzeJetEnergyCorrections_raw  = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventRaw" )
-analyzeJetEnergyCorrections_off  = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventOff" ) ## L1
-analyzeJetEnergyCorrections_rel  = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventRel" ) ## L2
-analyzeJetEnergyCorrections_abs  = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventAbs" ) ## L3
-analyzeJetEnergyCorrections_emf  = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventEmf" ) ## L4
-analyzeJetEnergyCorrections_had  = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventHad" ) ## L5
-analyzeJetEnergyCorrections_ue   = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventUe"  ) ## L6
-analyzeJetEnergyCorrections_part = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventPart") ## L7
+analyzeJetEnergyGenMatch_raw  = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventRaw" , hypoKey = "kGenMatch")
+analyzeJetEnergyGenMatch_off  = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventOff" , hypoKey = "kGenMatch") ## L1
+analyzeJetEnergyGenMatch_rel  = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventRel" , hypoKey = "kGenMatch") ## L2
+analyzeJetEnergyGenMatch_abs  = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventAbs" , hypoKey = "kGenMatch") ## L3
+analyzeJetEnergyGenMatch_emf  = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventEmf" , hypoKey = "kGenMatch") ## L4
+analyzeJetEnergyGenMatch_had  = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventHad" , hypoKey = "kGenMatch") ## L5
+analyzeJetEnergyGenMatch_ue   = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventUe"  , hypoKey = "kGenMatch") ## L6
+analyzeJetEnergyGenMatch_part = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventPart", hypoKey = "kGenMatch") ## L7
 
-analyzeJetEnergyCorrections_multilevel = cms.Sequence(analyzeJetEnergyCorrections_raw  *
-                                                      analyzeJetEnergyCorrections_off  *
-                                                      analyzeJetEnergyCorrections_rel  *
-                                                      analyzeJetEnergyCorrections_abs  *
-                                                      analyzeJetEnergyCorrections_emf  *
-                                                      analyzeJetEnergyCorrections_had  *
-                                                      analyzeJetEnergyCorrections_ue   *
-                                                      analyzeJetEnergyCorrections_part
-                                                      )
+analyzeJetEnergyGenMatch_multilevel = cms.Sequence(analyzeJetEnergyGenMatch_raw  *
+                                                   analyzeJetEnergyGenMatch_off  *
+                                                   analyzeJetEnergyGenMatch_rel  *
+                                                   analyzeJetEnergyGenMatch_abs  *
+                                                   analyzeJetEnergyGenMatch_emf  *
+                                                   analyzeJetEnergyGenMatch_had  *
+                                                   analyzeJetEnergyGenMatch_ue   *
+                                                   analyzeJetEnergyGenMatch_part
+                                                   )
+
+##########################################################################################
+## analyze geom hypothesis for different jet energy correction levels
+##########################################################################################
+
+analyzeJetEnergyGeom_raw  = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventRaw" , hypoKey = "kGeom")
+analyzeJetEnergyGeom_off  = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventOff" , hypoKey = "kGeom") ## L1
+analyzeJetEnergyGeom_rel  = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventRel" , hypoKey = "kGeom") ## L2
+analyzeJetEnergyGeom_abs  = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventAbs" , hypoKey = "kGeom") ## L3
+analyzeJetEnergyGeom_emf  = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventEmf" , hypoKey = "kGeom") ## L4
+analyzeJetEnergyGeom_had  = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventHad" , hypoKey = "kGeom") ## L5
+analyzeJetEnergyGeom_ue   = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventUe"  , hypoKey = "kGeom") ## L6
+analyzeJetEnergyGeom_part = analyzeJetEnergyCorrections.clone(semiLepEvt = "ttSemiLepEventPart", hypoKey = "kGeom") ## L7
+
+analyzeJetEnergyGeom_multilevel = cms.Sequence(analyzeJetEnergyGeom_raw  *
+                                               analyzeJetEnergyGeom_off  *
+                                               analyzeJetEnergyGeom_rel  *
+                                               analyzeJetEnergyGeom_abs  *
+                                               analyzeJetEnergyGeom_emf  *
+                                               analyzeJetEnergyGeom_had  *
+                                               analyzeJetEnergyGeom_ue   *
+                                               analyzeJetEnergyGeom_part
+                                               )
 
 ##########################################################################################
 ## bundle everything into one sequence
@@ -101,7 +147,9 @@ analyzeJetEnergyCorrections_multilevel = cms.Sequence(analyzeJetEnergyCorrection
 makeJetEnergyCorrectionsAnalysis = cms.Sequence(ttSemiLepJetPartonMatch *
                                                 ttSemiLepGenJetPartonMatch *
                                                 ttSemiLepHypGenMatch_multilevel *
+                                                ttSemiLepHypGeom_multilevel *
                                                 ttSemiLepEvent_multilevel *
-                                                analyzeJetEnergyCorrections_multilevel
+                                                analyzeJetEnergyGenMatch_multilevel *
+                                                analyzeJetEnergyGeom_multilevel
                                                 )
                                                 
