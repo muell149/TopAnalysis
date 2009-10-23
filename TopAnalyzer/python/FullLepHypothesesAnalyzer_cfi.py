05d511325566526c4dda478688b56d3aeeaff263
dammann <>
2009-10-23 12:15:54 +0000

    removed hist string

--- TopAnalyzer/python/FullLepHypothesesAnalyzer_cfi.py
+++ TopAnalyzer/python/FullLepHypothesesAnalyzer_cfi.py
@@ -1,12 +1,11 @@
 import FWCore.ParameterSet.Config as cms
 
 analyzeFullLepHypotheses = cms.EDAnalyzer("FullLepHypothesesAnalyzer",
-    hist = cms.string('analyzeFullLepHypotheses.hist'),
     hypoKey = cms.InputTag("ttFullLepHypKinSolution","Key"),
     weight = cms.InputTag("eventWeight"),
     FullLepEvent = cms.InputTag("ttFullLepEvent"),
     alsoWrongCharge = cms.bool(True),
-    wantSummary = cms.bool(False),
+    wantSummary = cms.bool(False)
 )
 
 
