bae890fd138a3303180291ae36ec34c955c6c0d0
snaumann <>
2009-11-01 12:06:11 +0000

    22X to 33X migration

--- TopUtils/test/testJetEnergyScale_cfg.py
+++ TopUtils/test/testJetEnergyScale_cfg.py
@@ -12,12 +12,7 @@ process.MessageLogger.cerr.threshold = 'INFO'
 ## define input
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    ## test-file on cern afs
-    'file:/afs/cern.ch/cms/PRS/top/cmssw-data/relval200-for-pat-testing/FullSimTTBar-2_1_X_2008-07-08_STARTUP_V4-AODSIM.100.root'
-    ## one file from the madgraph ttbar sample
-    #'/store/mc/Fall08/TTJets-madgraph/GEN-SIM-RECO/IDEAL_V9_v2/0000/027B51E9-8EED-DD11-9045-0015C5E9C0E1.root'
-    ## one file from the tauola ttbar sample
-    #'/store/mc/Summer08/TauolaTTbar/GEN-SIM-RECO/IDEAL_V9_v2/0009/0054812D-26F7-DD11-99D7-001F2908F0E4.root'
+    '/store/mc/Summer09/TTbar/AODSIM/MC_31X_V3_AODSIM-v1/0026/F899CDFB-6C89-DE11-B2FD-00304894558A.root'
     )
 )
 
@@ -28,14 +23,14 @@ process.maxEvents = cms.untracked.PSet(
 
 ## configure process options
 process.options = cms.untracked.PSet(
-    wantSummary = cms.untracked.bool(False)
+    wantSummary = cms.untracked.bool(True)
 )
 
 ## configure geometry & conditions
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('IDEAL_V9::All')
+process.GlobalTag.globaltag = cms.string('MC_31X_V3::All')
 
 ## pat sequences
 process.load("PhysicsTools.PatAlgos.patSequences_cff")
@@ -46,15 +41,12 @@ process.scaledJetEnergy.scaleFactor = 1.1
 process.selectedLayer1Jets.src = "scaledJetEnergy:allLayer1Jets"
 ## don't forget to use "scaledJetEnergy:layer1METs" as InputTag for your subsequent modules
 
-## necessary fixes to run 2.2.X on 2.1.X data
-## comment this when running on samples produced with 22X
-from PhysicsTools.PatAlgos.tools.cmsswVersionTools import run22XonSummer08AODSIM
-run22XonSummer08AODSIM(process)
-process.patDefaultSequence.remove(process.patPFCandidateIsoDepositSelection)
-process.patDefaultSequence.remove(process.patPFTauIsolation) 
+## necessary to run with 33X on 31X AOD samples
+from PhysicsTools.PatAlgos.tools.cmsswVersionTools import *
+run33xOn31xMC(process)
+restrictInputToAOD31X(process)
 
-process.p = cms.Path(process.beforeLayer1Objects *   # part of the patDefaultSequence
-                     process.allLayer1Objects *      # part of the patDefaultSequence
+process.p = cms.Path(process.allLayer1Objects *      # part of the patDefaultSequence
                      process.scaledJetEnergy *
                      process.selectedLayer1Objects * # part of the patDefaultSequence
                      process.cleanLayer1Objects *    # part of the patDefaultSequence
