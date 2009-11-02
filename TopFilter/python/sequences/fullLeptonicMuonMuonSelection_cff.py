f6a46fc324ee1f7110c202f68ba8e5a422fc4446
dammann <>
2009-11-02 16:35:58 +0000

    separated collection building and selection into two sequences

--- TopFilter/python/sequences/fullLeptonicMuonMuonSelection_cff.py
+++ TopFilter/python/sequences/fullLeptonicMuonMuonSelection_cff.py
@@ -18,36 +18,43 @@ from PhysicsTools.PatAlgos.selectionLayer1.electronCountFilter_cfi import *
 tightMuons = selectedLayer1Muons.clone(src = 'selectedLayer1Muons', 
                                        cut = 'combinedMuon.isNull = 0'
 					     '& pt > 20.'
-					     '& abs(eta) < 2.1'    
+					     '& abs(eta) < 2.4'    
                 			     '& track.numberOfValidHits >= 11' 
 					     '& abs(track.d0) < 0.2'
 					     '& combinedMuon.normalizedChi2 < 10.0'
 					     '& ecalIsoDeposit.candEnergy < 4'
 					     '& hcalIsoDeposit.candEnergy < 6'
-					     '& (trackIso+caloIso)/pt < 0.25' )
+					     '& (trackIso+caloIso)/pt < 0.25' 
+				      )
 
 ## tight electron selection for veto
 looseElectrons = selectedLayer1Electrons.clone(src = 'selectedLayer1Electrons', 
                                                cut = 'et > 20.' 
 					             '& abs(eta) < 2.4' 
-					             '& (trackIso+caloIso)/et <  0.2')
+					             '& (trackIso+caloIso)/et <  0.2'
+					      )
 
   
 ## tight jet selection
-bJetCandidates = selectedLayer1Jets.clone(src = 'selectedLayer1Jets', 
-                                          cut = 'pt > 40.' 
-					        '& abs(eta) < 2.3' 
-						'& 0.05 < emEnergyFraction' 
-						'& emEnergyFraction < 0.95')
+hardJets = selectedLayer1Jets.clone(src = 'selectedLayer1Jets', 
+                                    cut = 'pt > 40.' 
+					  '& abs(eta) < 2.3' 
+				          '& 0.05 < emEnergyFraction' 
+					  '& emEnergyFraction < 0.95'
+			           )
 
 ## at least two tight muons
 muonSelection  = countLayer1Muons.clone(src = 'tightMuons', minNumber = 2, maxNumber = 999)
 ## no additional tight electron
 electronVeto   = countLayer1Electrons.clone(src = 'looseElectrons', maxNumber = 0)
 ## at least two jets
-jetSelection = countLayer1Jets.clone(src = 'bJetCandidates', minNumber = 2)
+jetSelection   = countLayer1Jets.clone(src = 'bJetCandidates', minNumber = 2)
 
-fullLeptonicMuonMuonSelection = cms.Sequence((tightMuons      * muonSelection ) *
-                                             #(looseElectrons  * electronVeto  ) *
-                                             (bJetCandidates  * jetSelection  )
+
+buildCollections = cms.Sequence(tightMuons *
+                                hardJets 
+			       )			       			       
+
+fullLeptonicMuonMuonSelection = cms.Sequence(muonSelection #* jetSelection *
+                                             #looseElectrons  * electronVeto   
                                             )
