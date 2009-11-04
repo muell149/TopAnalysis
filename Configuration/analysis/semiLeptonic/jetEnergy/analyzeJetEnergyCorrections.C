e6a1b3652ad847930dc9f617e970ff9be0bd8293
snaumann <>
2009-11-04 09:19:06 +0000

    zoom on y-axis of 2-dim. plots for W mass and top mass

--- Configuration/analysis/semiLeptonic/jetEnergy/analyzeJetEnergyCorrections.C
+++ Configuration/analysis/semiLeptonic/jetEnergy/analyzeJetEnergyCorrections.C
@@ -523,42 +523,42 @@ void analyzeJetEnergyCorrections(TString name = "analyzeJetEnergyCorrections.roo
     // W mass
     
     canvasMassW_2dim->cd(1);
-    drawResponse(massW_Pt1[i], i, "p_{T,j} (rec) [GeV]", "m_{jj} [GeV]", title(algo), true, 0., 160., 80.4);
+    drawResponse(massW_Pt1[i], i, "p_{T,j} (rec) [GeV]", "m_{jj} [GeV]", title(algo), true, 30., 130., 80.4);
 
     canvasMassW_2dim->cd(2);
-    drawResponse(massW_Eta1[i], i, "#eta_{j} (rec)", "m_{jj} [GeV]", title(algo), false, 0., 160., 80.4);
+    drawResponse(massW_Eta1[i], i, "#eta_{j} (rec)", "m_{jj} [GeV]", title(algo), false, 30., 130., 80.4);
 
     canvasMassW_2dim->cd(3);
-    drawResponse(massW_PtW[i], i, "p_{T,jj} (rec) [GeV]", "m_{jj} [GeV]", title(algo), true, 0., 160., 80.4);
+    drawResponse(massW_PtW[i], i, "p_{T,jj} (rec) [GeV]", "m_{jj} [GeV]", title(algo), true, 30., 130., 80.4);
 
     canvasMassW_2dim->cd(6);
-    drawResponse(massW_EtaW[i], i, "#eta_{jj} (rec)", "m_{jj} [GeV]", title(algo), false, 0., 160., 80.4);
+    drawResponse(massW_EtaW[i], i, "#eta_{jj} (rec)", "m_{jj} [GeV]", title(algo), false, 30., 130., 80.4);
 
     // top mass
 
     canvasMassT_2dim->cd(1);
-    drawResponse(massT_Pt1[i], i, "p_{T,j} (rec) [GeV]", "m_{jjb} [GeV]", title(algo), true, 0., 350., 172.5);
+    drawResponse(massT_Pt1[i], i, "p_{T,j} (rec) [GeV]", "m_{jjb} [GeV]", title(algo), true, 60., 290., 172.5);
 
     canvasMassT_2dim->cd(2);
-    drawResponse(massT_Eta1[i], i, "#eta_{j} (rec)", "m_{jjb} [GeV]", title(algo), false, 0., 350., 172.5);
+    drawResponse(massT_Eta1[i], i, "#eta_{j} (rec)", "m_{jjb} [GeV]", title(algo), false, 60., 290., 172.5);
 
     canvasMassT_2dim->cd(3);
-    drawResponse(massT_PtT[i], i, "p_{T,jjb} (rec) [GeV]", "m_{jjb} [GeV]", title(algo), true, 0., 350., 172.5);
+    drawResponse(massT_PtT[i], i, "p_{T,jjb} (rec) [GeV]", "m_{jjb} [GeV]", title(algo), true, 60., 290., 172.5);
 
     canvasMassT_2dim->cd(4);
-    drawResponse(massT_PtB[i], i, "p_{T,b} (rec) [GeV]", "m_{jjb} [GeV]", title(algo), true, 0., 350., 172.5);
+    drawResponse(massT_PtB[i], i, "p_{T,b} (rec) [GeV]", "m_{jjb} [GeV]", title(algo), true, 60., 290., 172.5);
 
     canvasMassT_2dim->cd(5);
-    drawResponse(massT_EtaB[i], i, "#eta_{b} (rec)", "m_{jjb} [GeV]", title(algo), false, 0., 350., 172.5);
+    drawResponse(massT_EtaB[i], i, "#eta_{b} (rec)", "m_{jjb} [GeV]", title(algo), false, 60., 290., 172.5);
 
     canvasMassT_2dim->cd(6);
-    drawResponse(massT_EtaT[i], i, "#eta_{jjb} (rec)", "m_{jjb} [GeV]", title(algo), false, 0., 350., 172.5);
+    drawResponse(massT_EtaT[i], i, "#eta_{jjb} (rec)", "m_{jjb} [GeV]", title(algo), false, 60., 290., 172.5);
 
     canvasMassT_2dim->cd(7);
-    drawResponse(deltaM_PtB[i], i, "p_{T,b} (rec) [GeV]", "m_{jjb}-m_{jj} [GeV]", title(algo), true, 0., 180., 92.1);
+    drawResponse(deltaM_PtB[i], i, "p_{T,b} (rec) [GeV]", "m_{jjb}-m_{jj} [GeV]", title(algo), true, 30., 150., 92.1);
 
     canvasMassT_2dim->cd(8);
-    drawResponse(deltaM_EtaB[i], i, "#eta_{b} (rec)", "m_{jjb}-m_{jj} [GeV]", title(algo), false, 0., 180., 92.1);
+    drawResponse(deltaM_EtaB[i], i, "#eta_{b} (rec)", "m_{jjb}-m_{jj} [GeV]", title(algo), false, 30., 150., 92.1);
 
   }
 
