d8431137d5b7580bbd4cd3e525ca774415fbb21a
dammann <>
2009-11-09 14:59:26 +0000

    added Hists

--- TopAnalyzer/python/DimuonAnalyzer_cfi.py
+++ TopAnalyzer/python/DimuonAnalyzer_cfi.py
@@ -4,7 +4,9 @@ analyzeMuonPair = cms.EDAnalyzer("DimuonAnalyzer",
     # source
     muons = cms.InputTag("selectedLayer1Muons"),     
     # option to weight events
-    useEventWeight = cms.bool(True), 
+    useEventWeight = cms.bool(True),
+    # bins of di-combined two muon isolation
+    isoBins = cms.vdouble(0, 0.5, 0.6, 0.7, 0.9) 
 )
 
 
