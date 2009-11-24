7cfd337e19616d149ff3f3ae1780580cfc2f34ac
rwolf <>
2009-11-24 15:56:32 +0000

    added pb in comments for xsec

--- TopUtils/python/EventWeightPlain_cfi.py
+++ TopUtils/python/EventWeightPlain_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 eventWeight = cms.EDFilter("EventWeightPlain",
   nevts = cms.uint32(  1),    ## add the maximal number of events in the sample
-  xsec  = cms.double(1.0),    ## add the xsection for production
+  xsec  = cms.double(1.0),    ## add the xsection for production in pb
   eff   = cms.double(1.0),    ## add filter efficiencies
   lumi  = cms.double(1.0)     ## add the target lumi you aim for
 )
