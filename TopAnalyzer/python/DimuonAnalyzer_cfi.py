9b9235aee4e30b139a14c345bd15340c7f1d8cb7
dammann <>
2009-11-25 09:52:09 +0000

    changed binning

--- TopAnalyzer/python/DimuonAnalyzer_cfi.py
+++ TopAnalyzer/python/DimuonAnalyzer_cfi.py
@@ -6,7 +6,7 @@ analyzeMuonPair = cms.EDAnalyzer("DimuonAnalyzer",
     # option to weight events
     useEventWeight = cms.bool(True),
     # bins of di-combined two muon isolation
-    isoBins = cms.vdouble(0, 0.5, 0.6, 0.7, 0.9) 
+    isoBins = cms.vdouble(0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0) 
 )
 
 
