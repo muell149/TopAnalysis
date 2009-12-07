bea6a9d930e71ad8b5d473251423dc5cbdae3d4e
snaumann <>
2009-12-07 10:36:10 +0000

    create first patTuple from Run 123596 (using GlobalTag and 'GR09_P_V7::All' and not adding SisCone jets for the time being)

--- Configuration/data/commonTemplate_data_cfg.py
+++ Configuration/data/commonTemplate_data_cfg.py
@@ -17,21 +17,39 @@ process.MessageLogger.categories.append('topFilter')
 process.MessageLogger.cerr.topFilter = cms.untracked.PSet(
     limit = cms.untracked.int32(-1)
 )
-#process.MessageLogger.cerr.FwkReport.reportEvery = 100
+process.MessageLogger.cerr.FwkReport.reportEvery = 100
 
 ## define input
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    # >>> modify here<<<
-    '/store/mc/Summer09/TTbar/AODSIM/MC_31X_V3_AODSIM-v1/0026/F2B6764A-6D89-DE11-8585-0018FEFAC384.root')
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/0A71AE7F-4DE2-DE11-8B2F-001D09F251CC.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/14D22A92-62E2-DE11-9B14-000423D990CC.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/263E80C6-41E2-DE11-A194-001617C3B66C.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/26EC6965-4CE2-DE11-ABCF-003048D373AE.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/2A6903C0-68E2-DE11-B6BA-001D09F28D54.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/2EC732CC-5CE2-DE11-A781-001D09F290BF.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/4693BF9A-40E2-DE11-BDBD-000423D944F8.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/54A3A373-4CE2-DE11-8658-000423D99AAA.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/5C4B3A8E-63E2-DE11-A02A-000423D99E46.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/68ED95B5-50E2-DE11-B4C8-001D09F27003.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/76CCDA0D-8AE2-DE11-AF65-0030487A3C9A.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/7E34865F-45E2-DE11-896A-000423D98F98.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/7EFA67BE-66E2-DE11-AE17-001617C3B79A.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/903D2066-61E2-DE11-9F6E-0019B9F704D6.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/A0FC9BDF-65E2-DE11-A6A1-000423D174FE.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/A4D9D21B-58E2-DE11-8F7A-000423D986A8.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/B60CC58F-5CE2-DE11-9FC7-001D09F24DDF.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/D0586F90-5FE2-DE11-8976-001D09F24691.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/E432BCD7-55E2-DE11-B670-001617C3B6CC.root',
+    '/store/data/BeamCommissioning09/MinimumBias/RECO/v2/000/123/596/F67BCF17-48E2-DE11-98B1-000423D94534.root'
+    )
 )
-# >>> modify here<<<
-#process.source.firstRun = cms.untracked.uint32(122314)
+process.source.firstRun = cms.untracked.uint32(123596)
 
 
 ## define maximal number of events to loop over
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100)
+    input = cms.untracked.int32(-1)
 )
 
 ## configure process options
@@ -43,8 +61,7 @@ process.options = cms.untracked.PSet(
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-# >>> modify here<<<
-process.GlobalTag.globaltag = cms.string('MC_31X_V3::All')
+process.GlobalTag.globaltag = cms.string('GR09_P_V7::All')
 
 #-------------------------------------------------
 # pat configuration
@@ -73,15 +90,15 @@ from PhysicsTools.PatAlgos.tools.jetTools import *
 #                 genJetCollection = cms.InputTag("iterativeCone5GenJets")
 #                 )
 
-addJetCollection(process,cms.InputTag('sisCone5CaloJets'),
-                 'SC5',
-                 doJTA        = True,
-                 doBTagging   = True,
-                 jetCorrLabel = ('SC5','Calo'),
-                 doType1MET   = True,
-                 doL1Counters = False,
-                 genJetCollection=cms.InputTag("sisCone5GenJets")
-                 )
+#addJetCollection(process,cms.InputTag('sisCone5CaloJets'),
+#                 'SC5',
+#                 doJTA        = True,
+#                 doBTagging   = True,
+#                 jetCorrLabel = ('SC5','Calo'),
+#                 doType1MET   = True,
+#                 doL1Counters = False,
+#                 genJetCollection=cms.InputTag("sisCone5GenJets")
+#                 )
 
 #-------------------------------------------------
 # private Uni Hamburg analysis code
@@ -127,7 +144,7 @@ process.out = cms.OutputModule("PoolOutputModule",
     process.EventSelection,
     outputCommands = cms.untracked.vstring('drop *'),
     dropMetaDataForDroppedData = cms.untracked.bool(True),                                     
-    fileName = cms.untracked.string('patTuple.root')
+    fileName = cms.untracked.string('patTuple123596.root')
 )
 # save pat output
 from PhysicsTools.PatAlgos.patEventContent_cff import *
