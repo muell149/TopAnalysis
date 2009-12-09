d0d6a9e207bc2191019d340afd1a1f307fa30642
dammann <>
2009-12-09 13:54:03 +0000

    changed default

--- TopFilter/python/filters/RunEventFilter_cfi.py
+++ TopFilter/python/filters/RunEventFilter_cfi.py
@@ -2,22 +2,22 @@ import FWCore.ParameterSet.Config as cms
 
 selectEvents = cms.EDAnalyzer("RunEventFilter",
     
-    #select a range of runs you want to select
-    runs = cms.string('122314'),
+    ##select a range of runs you want to select
+    runs = cms.string('123575,123596'),
     
-    #select runs or ranges of runs which you want to exclude
+    ##select runs or ranges of runs which you want to exclude    
     #excludedRuns = cms.string(''),
     
     
-    #use this if you want to select or reject events in certain runs
-    # if you want to exclude events instead of selecting them
-    # start the selected_evts string with a '!'    
-    selectedEvents = cms.VPSet(        
-      cms.PSet(
-        run_number  = cms.uint32(122314),
-	selected_evts = cms.string('13688798,13693046-13702674')
-      )                    
-    )
+    ## use this if you want to select or reject events in certain runs
+    ## if you want to exclude events instead of selecting them
+    ## start the selected_evts string with a '!'        
+    #selectedEvents = cms.VPSet(        
+    #  cms.PSet(
+    #    run_number  = cms.uint32(),
+    #	selected_evts = cms.string('')
+    #  )                    
+    #)
 )
 
 
