dbb895df5ce7e09a0ae74eccecb4482cabf249ba
dammann <>
2009-12-09 13:29:12 +0000

    event filter for triggers

---
+++ TopFilter/plugins/TriggerFilter.cc
@@ -0,0 +1,49 @@
+#include "TopAnalysis/TopFilter/plugins/TriggerFilter.h"
+
+TriggerFilter::TriggerFilter(const ParameterSet& cfg)
+{
+  trigResults_ = cfg.getParameter<InputTag>       ("TriggerResults");
+  hltPaths_L3_ = cfg.getParameter<vector<string> >("hltPaths_L3"   );
+}
+
+TriggerFilter::~TriggerFilter()
+{
+}
+
+void
+TriggerFilter::beginJob(const EventSetup&)
+{
+    n_TrigPaths = hltPaths_L3_.size();     
+}
+
+bool
+TriggerFilter::filter(Event& evt, const EventSetup&)
+{
+  Handle<TriggerResults> trigResults; 
+  evt.getByLabel(trigResults_, trigResults); 
+
+  if(trigResults.failedToGet()) return false;
+  
+  bool trigFired = false;
+  int n_Triggers = trigResults->size();
+
+  TriggerNames trigName;
+  trigName.init(*trigResults);
+
+  for(int i_Trig = 0; i_Trig<n_Triggers; ++i_Trig){    
+    if(!trigResults.product()->accept(i_Trig)) continue;        
+    for(int i = 0; i<n_TrigPaths; i++){
+      if(!(trigName.triggerName(i_Trig)== hltPaths_L3_[i])) continue; 	    	    	    	    
+      trigFired = true;     	        
+    }      
+  }  
+  return trigFired;
+}
+
+void
+TriggerFilter::endJob()
+{
+}
+
+#include "FWCore/Framework/interface/MakerMacros.h"
+DEFINE_FWK_MODULE( TriggerFilter );
---
+++ TopFilter/plugins/TriggerFilter.h
@@ -0,0 +1,52 @@
+#ifndef TriggerFilter_h
+#define TriggerFilter_h
+
+#include <string>
+#include <vector>
+
+#include "DataFormats/Common/interface/TriggerResults.h"
+#include "FWCore/Framework/interface/TriggerNames.h"
+#include "FWCore/Framework/interface/EDFilter.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/ParameterSet/interface/InputTag.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+#include "FWCore/ServiceRegistry/interface/Service.h"
+#include "FWCore/Utilities/interface/EDMException.h"
+
+/**
+   \class   TriggerFilter TriggerFilter.h "TopAnalysis/TopFilter/plugins/TriggerFilter.h"
+
+   \brief   EDFilter for trigger 
+
+   This filter filters for triggers given in the config file. 
+*/
+
+using namespace std;
+using namespace edm;
+
+class TriggerFilter : public EDFilter {
+
+  public:
+    /// default constructor
+    explicit TriggerFilter(const ParameterSet&);
+    /// default destructor
+    ~TriggerFilter();
+    
+  private:
+    /// initiate n_TrigPaths
+    virtual void beginJob(const EventSetup&);
+    /// look which triggers have fired and compare to given set of triggers
+    virtual bool filter(Event&, const EventSetup&);
+    /// empty
+    virtual void endJob();
+        
+     /// triger result input collection	
+    InputTag trigResults_;
+    /// triggers to be studied given in config
+    vector<string> hltPaths_L3_;
+    /// number of trigger paths given in config
+    int n_TrigPaths;      
+};
+
+#endif
