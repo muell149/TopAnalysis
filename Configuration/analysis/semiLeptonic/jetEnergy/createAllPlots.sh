cec88e01caa44ff964871888c3c81aad0c225848
snaumann <>
2010-01-08 10:51:20 +0000

    adapt to changed content of note

--- Configuration/analysis/semiLeptonic/jetEnergy/createAllPlots.sh
+++ Configuration/analysis/semiLeptonic/jetEnergy/createAllPlots.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-TopAnalysis/Configuration/analysis/semiLeptonic/jetEnergy/analyze "analyzeJetEnergyCorrections_ak5.root"         "GenMatch"
-TopAnalysis/Configuration/analysis/semiLeptonic/jetEnergy/analyze "analyzeJetEnergyCorrections_ak5_inclBkg.root" "Geom"
-TopAnalysis/Configuration/analysis/semiLeptonic/jetEnergy/analyze "analyzeJetEnergyCorrections_ic5.root"         "GenMatch"
-TopAnalysis/Configuration/analysis/semiLeptonic/jetEnergy/analyze "analyzeJetEnergyCorrections_kt4.root"         "GenMatch"
-TopAnalysis/Configuration/analysis/semiLeptonic/jetEnergy/analyze "analyzeJetEnergyCorrections_sc5.root"         "GenMatch"
+TopAnalysis/Configuration/analysis/semiLeptonic/jetEnergy/analyze "analyzeJetEnergyCorrections_ak5.root"      "GenMatch"
+TopAnalysis/Configuration/analysis/semiLeptonic/jetEnergy/analyze "analyzeJetEnergyCorrections_ic5.root"      "GenMatch"
+TopAnalysis/Configuration/analysis/semiLeptonic/jetEnergy/analyze "analyzeJetEnergyCorrections_kt4.root"      "GenMatch"
+TopAnalysis/Configuration/analysis/semiLeptonic/jetEnergy/analyze "analyzeJetEnergyCorrections_sc5.root"      "GenMatch"
+TopAnalysis/Configuration/analysis/semiLeptonic/jetEnergy/analyze "analyzeJetEnergyCorrections_7TeV_ak5.root" "GenMatch"
 
 cp -r analyzeJetEnergyCorrections_ak5/         ~/latex/notes/JECTopValidation
-cp -r analyzeJetEnergyCorrections_ak5_inclBkg/ ~/latex/notes/JECTopValidation
 cp -r analyzeJetEnergyCorrections_ic5/         ~/latex/notes/JECTopValidation
 cp -r analyzeJetEnergyCorrections_kt4/         ~/latex/notes/JECTopValidation
 cp -r analyzeJetEnergyCorrections_sc5/         ~/latex/notes/JECTopValidation
+cp -r analyzeJetEnergyCorrections_7TeV_ak5/    ~/latex/notes/JECTopValidation
