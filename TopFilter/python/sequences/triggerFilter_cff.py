cf32a9d5ec25e438640580a7bb995a0927491332
eschliec <>
2010-01-20 12:25:01 +0000

    added 2 more triggers (QuadJet30, HT200)

--- TopFilter/python/sequences/triggerFilter_cff.py
+++ TopFilter/python/sequences/triggerFilter_cff.py
@@ -8,3 +8,7 @@ from HLTrigger.HLTfilters.hltHighLevel_cfi import *
 ## ---
 ## single-muon trigger, threshold pt > 9 GeV, no isolation requirements
 hltMu9 = hltHighLevel.clone(HLTPaths = ["HLT_Mu9"])
+## 4 jet trigger, threshold pt > 30 GeV
+hltQuadJet30 = hltHighLevel.clone(HLTPaths = ["HLT_QuadJet30"])
+## sum of pts of jets HT > 200 GeV
+hltHt200 = hltHighLevel.clone(HLTPaths = ["HLT_HT200"])
