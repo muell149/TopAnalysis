fa18515489e947947d1c35942c3b5d9f8a859335
rwolf <>
2010-01-25 18:45:06 +0000

    added electron trigger for tag and probe

--- TopFilter/python/sequences/triggerFilter_cff.py
+++ TopFilter/python/sequences/triggerFilter_cff.py
@@ -8,6 +8,8 @@ from HLTrigger.HLTfilters.hltHighLevel_cfi import *
 ## ---
 ## single-muon trigger, threshold pt > 9 GeV, no isolation requirements
 hltMu9 = hltHighLevel.clone(HLTPaths = ["HLT_Mu9"])
+## single-electron trigger, threshold et > 15 GeV + electronID, no isolation requirements
+hltEle15 = hltHighLevel.clone(HLTPaths = ["HLT_Ele15_SW_EleId_L1R"])
 ## 4 jet trigger, threshold pt > 30 GeV
 hltQuadJet30 = hltHighLevel.clone(HLTPaths = ["HLT_QuadJet30"])
 ## sum of pts of jets HT > 200 GeV
