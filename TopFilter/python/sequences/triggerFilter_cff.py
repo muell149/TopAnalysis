72f24d8d6f5a5370f8c0f69b5e824a1240071844
eschliec <>
2010-01-29 10:51:22 +0000

    added some more triggers

--- TopFilter/python/sequences/triggerFilter_cff.py
+++ TopFilter/python/sequences/triggerFilter_cff.py
@@ -12,5 +12,9 @@ hltMu9 = hltHighLevel.clone(HLTPaths = ["HLT_Mu9"])
 hltEle15 = hltHighLevel.clone(HLTPaths = ["HLT_Ele15_SW_EleId_L1R"])
 ## 4 jet trigger, threshold pt > 30 GeV
 hltQuadJet30 = hltHighLevel.clone(HLTPaths = ["HLT_QuadJet30"])
-## sum of pts of jets HT > 200 GeV
+## sum of pts of jets HT > 200 GeV (uncorrected)
 hltHt200 = hltHighLevel.clone(HLTPaths = ["HLT_HT200"])
+## 1 jet trigger, threshold pt > 80 GeV
+hltJet80 = hltHighLevel.clone(HLTPaths = ["HLT_Jet80"])
+## 2 jet trigger, threshold pt > 50 GeV (uncorrected)
+hltDiJet50 = hltHighLevel.clone(HLTPaths = ["HLT_DiJetAve50U"])
