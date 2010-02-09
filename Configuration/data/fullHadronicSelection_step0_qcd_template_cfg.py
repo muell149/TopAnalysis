6653a01d1ebbe51f559dc4972e89e776fa8a7211
eschliec <>
2010-02-09 10:25:10 +0000

    removed sc and ic jetCollections, added ak pf jetCollection, implemented trigger matching for calo and pf jets, switched to ttbar jec for 7TeV, embedded calotowers and pf constituents into jets

--- Configuration/data/fullHadronicSelection_step0_qcd_template_cfg.py
+++ Configuration/data/fullHadronicSelection_step0_qcd_template_cfg.py
@@ -4,7 +4,7 @@ import FWCore.ParameterSet.Config as cms
 # cfg file for preselected pat layer1 object 
 # creation
 #-------------------------------------------------
-process = cms.Process("PatTuple")
+process = cms.Process("PAT")
 
 ## configure message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
@@ -17,7 +17,7 @@ process.MessageLogger.cerr.threshold = 'INFO'
 
 ## define input
 process.source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring('')
+    fileNames = cms.untracked.vstring('/store/mc/Summer09/QCD_Pt1400/GEN-SIM-RECO/MC_31X_V3_7TeV-v1/0004/2874E107-F69E-DE11-AC8E-003048C757D4.root')
 )
 
 ## define maximal number of events to loop over
@@ -35,7 +35,7 @@ process.load("Configuration.StandardSequences.Geometry_cff")
 
 ## configure conditions
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('MC_31X_V9::All')
+process.GlobalTag.globaltag = cms.string('MC_3XY_V18::All')
 
 ## Magnetic field now needs to be in the high-level py
 process.load("Configuration.StandardSequences.MagneticField_cff")
@@ -45,7 +45,6 @@ process.load("PhysicsTools.PatAlgos.patSequences_cff")
 
 ## process path
 process.pat = cms.Path(process.patDefaultSequence)
-
 
 #-------------------------------------------------
 # event weight configuration
@@ -66,33 +65,28 @@ process.wght = cms.Path(process.eventWeight)
 
 from PhysicsTools.PatAlgos.tools.jetTools import *
 
-addJetID(process, "iterativeCone5CaloJets", "ic5")
-addJetCollection(process,cms.InputTag('iterativeCone5CaloJets'),
-                 'IC5',
+#addJetID only for CaloJets
+#addJetID(process, "antikt5PFJets", "pf")
+addJetCollection(process,cms.InputTag('antikt5PFJets'),'PF',
                  doJTA        = True,
                  doBTagging   = True,
-                 jetCorrLabel = ('IC5', 'Calo'),
-                 doType1MET   = True,
-                 doL1Cleaning = False,                 
+                 jetCorrLabel = ('AK5', 'PF'),
+                 doType1MET   = False,
+                 doL1Cleaning = False,
                  doL1Counters = False,
-                 genJetCollection=cms.InputTag("iterativeCone5GenJets"),
-                 doJetID      = False,
-                 jetIdLabel   = "ic5"
+                 genJetCollection=cms.InputTag("antikt5GenJets"),
+                 doJetID      = False
                  )
 
-addJetID( process, "sisCone5CaloJets", "sc5")
-addJetCollection(process,cms.InputTag('sisCone5CaloJets'),
-                 'SC5',
-                 doJTA        = True,
-                 doBTagging   = True,
-                 jetCorrLabel = ('SC5','Calo'),
-                 doType1MET   = True,
-                 doL1Cleaning = False,                                  
-                 doL1Counters = False,
-                 genJetCollection=cms.InputTag("sisCone5GenJets"),
-                 doJetID      = False,
-                 jetIdLabel   = "sc5"                 
-                 )
+## sample type used for flavour dependend jet corrections
+process.jetCorrFactors.corrSample   = 'Summer09_7TeV'
+process.jetCorrFactors.sampleType   = 'ttbar'
+process.jetCorrFactorsPF.corrSample = 'Summer09_7TeV'
+process.jetCorrFactorsPF.sampleType = 'ttbar'
+
+## embedding of jet constituents into the jets
+process.allLayer1Jets.embedCaloTowers = True
+process.allLayer1JetsPF.embedPFCandidates = True
 
 #-------------------------------------------------
 # preselection paths (involved jet collections
@@ -101,18 +95,13 @@ addJetCollection(process,cms.InputTag('sisCone5CaloJets'),
 
 ## preselection for ak5 jets 
 process.selectedLayer1Jets.cut = 'abs(eta) < 3.0 & pt>20'
-process.countLayer1JetsAK5 = process.countLayer1Jets.clone(src = 'selectedLayer1Jets'   , minNumber = 6)
+process.countLayer1JetsAK5 = process.countLayer1Jets.clone(src = 'selectedLayer1Jets' , minNumber = 6)
 process.ak5Selection = cms.Path(process.countLayer1JetsAK5 )   
 
-## preselection for ic5 jets 
-process.selectedLayer1JetsIC5.cut = 'abs(eta) < 3.0 & pt>20'
-process.countLayer1JetsIC5 = process.countLayer1Jets.clone(src = 'selectedLayer1JetsIC5', minNumber = 6)
-process.ic5Selection = cms.Path(process.countLayer1JetsIC5 )   
-
-## preselection for sc5 jets 
-process.selectedLayer1JetsSC5.cut = 'abs(eta) < 3.0 & pt>20'
-process.countLayer1JetsSC5 = process.countLayer1Jets.clone(src = 'selectedLayer1JetsSC5', minNumber = 6)
-process.sc5Selection = cms.Path(process.countLayer1JetsSC5 )   
+## preselection for ak5 pf jets 
+process.selectedLayer1JetsPF.cut = 'abs(eta) < 3.0 & pt>20'
+process.countLayer1JetsAK5PF = process.countLayer1Jets.clone(src = 'selectedLayer1JetsPF' , minNumber = 6)
+process.ak5PfSelection = cms.Path(process.countLayer1JetsAK5PF )   
 
 #-------------------------------------------------
 # process output; first the event selection is
@@ -125,7 +114,7 @@ process.sc5Selection = cms.Path(process.countLayer1JetsSC5 )
 ## define event selection
 process.EventSelection = cms.PSet(
     SelectEvents = cms.untracked.PSet(
-        SelectEvents = cms.vstring('ak5Selection', 'ic5Selection', 'sc5Selection')
+        SelectEvents = cms.vstring('ak5PfSelection', 'ak5Selection')
     )
 )
 
@@ -134,7 +123,7 @@ process.out = cms.OutputModule("PoolOutputModule",
     process.EventSelection,
     outputCommands = cms.untracked.vstring('drop *'),
     dropMetaDataForDroppedData = cms.untracked.bool(True),                                     
-    fileName = cms.untracked.string('patTuple_qcd.root')
+    fileName = cms.untracked.string('patTuple.root')
 )
 
 #-------------------------------------------------
@@ -142,13 +131,60 @@ process.out = cms.OutputModule("PoolOutputModule",
 # needs to be known first)
 #-------------------------------------------------
 
+## embedding of trigger information into the pat tuple
+process.jetTriggerMatchHLTJets = cms.EDFilter( "PATTriggerMatcherDRLessByR",
+                                 src     = cms.InputTag( "selectedLayer1Jets" ),
+                                 matched = cms.InputTag( "patTrigger" ),
+                                 andOr          = cms.bool( False ),
+                                 filterIdsEnum  = cms.vstring( 'TriggerJet' ),
+                                 filterIds      = cms.vint32( 0 ), # This is for CMSSW_3_1_X; '85' would be the code for HLT jet here.
+                                 filterLabels   = cms.vstring( '*' ),
+                                 pathNames      = cms.vstring( '*' ),
+                                 collectionTags = cms.vstring( '*' ),
+                                 #maxDPtRel = cms.double( 0.5 ),
+                                 maxDeltaR = cms.double( 0.5 ),
+                                 resolveAmbiguities    = cms.bool( True ),
+                                 resolveByMatchQuality = cms.bool( False )
+                                 )
+
+process.pfJetTriggerMatchHLTJets = process.jetTriggerMatchHLTJets.clone(src = "selectedLayer1JetsPF")
+
+from PhysicsTools.PatAlgos.tools.trigTools import *
+switchOnTrigger( process , 'selected')
+
+process.patTriggerEvent.patTriggerMatches = ['jetTriggerMatchHLTJets', 'pfJetTriggerMatchHLTJets']
+
+process.patTriggerMatcher += process.jetTriggerMatchHLTJets
+process.patTriggerMatcher += process.pfJetTriggerMatchHLTJets
+
+process.patTriggerMatcher.remove( process.electronTriggerMatchHltElectrons)
+process.patTriggerMatcher.remove( process.electronTriggerMatchHltElectrons)
+process.patTriggerMatcher.remove( process.electronTriggerMatchL1Electrons)
+process.patTriggerMatcher.remove( process.tauTriggerMatchTriggerTaus)
+process.patTriggerMatcher.remove( process.muonTriggerMatchL1Muons)
+process.patTriggerMatcher.remove( process.muonTriggerMatchAll)
+process.patTriggerMatcher.remove( process.muonTriggerMatchNone)
+
+process.triggerMatchedSelectedLayer1Jets = cms.EDProducer("PATTriggerMatchJetEmbedder",
+                                                          matches = cms.VInputTag("jetTriggerMatchHLTJets"),
+                                                          src = cms.InputTag("selectedLayer1Jets")
+                                                          )
+
+process.triggerMatchedSelectedLayer1JetsPF = cms.EDProducer("PATTriggerMatchJetEmbedder",
+                                                            matches = cms.VInputTag("pfJetTriggerMatchHLTJets"),
+                                                            src = cms.InputTag("selectedLayer1JetsPF")
+                                                            )
+
+process.patDefaultSequence += process.triggerMatchedSelectedLayer1Jets
+process.patDefaultSequence += process.triggerMatchedSelectedLayer1JetsPF
+
 from PhysicsTools.PatAlgos.tools.cmsswVersionTools import *
 # run the 3.3.x software on Summer 09 MC from 3.1.x:
 #   - change the name from "ak" (3.3.x) to "antikt) (3.1.x)
 #   - run jet ID (not run in 3.1.x)
 run33xOn31xMC( process,
-               jetSrc = cms.InputTag("antikt5CaloJets"),
-               jetIdTag = "antikt5"
+               jetSrc = 'antikt5CaloJets',
+               jetIdTag = 'antikt5'
                )
 
 ## add selectedLayer1Candidates
@@ -163,6 +199,8 @@ process.out.outputCommands += patEventContentNoLayer1Cleaning
 process.out.outputCommands += ["keep *_selectedLayer1Jets*_*_*",
                                "keep *_layer1METs*_*_*"]
 process.out.outputCommands += ["keep *_eventWeight_*_*"]
+process.out.outputCommands += ['keep *_triggerMatchedSelectedLayer1Jets_*_*']
+process.out.outputCommands += ['keep *_triggerMatchedSelectedLayer1Jets*_*_*']
 # drop stuff which is not needed
 process.out.outputCommands += ["drop *_towerMaker_*_*"]
 
