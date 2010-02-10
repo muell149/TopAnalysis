240ddf8416f8d3a49fc2643f1a6be1e696b825d6
mgoerner <>
2010-02-10 10:33:11 +0000

    adapted binning for pt, included phi and eta, moved from reco to pat-Objects, updated comments

--- TopAnalyzer/interface/MuonCrossSection.h
+++ TopAnalyzer/interface/MuonCrossSection.h
@@ -1,7 +1,7 @@
 #ifndef MuonCrossSection_h
 #define MuonCrossSection_h
 
-#include "DataFormats/Candidate/interface/Candidate.h"
+#include "DataFormats/PatCandidates/interface/Muon.h"
 #include "TopAnalysis/TopAnalyzer/interface/SingleObject.h"
 #include "AnalysisDataFormats/TopObjects/interface/TtSemiLeptonicEvent.h"
 #include "TMath.h"
@@ -9,20 +9,20 @@
 /**
    \class   MuonCrossSection MuonCrossSection.h "TopAnalysis/TopAnalyzer/interface/MuonCrossSection.h"
 
-   \brief   Derived class to analyze the kinematics of top quarks on reconstruction and generator level
+   \brief   Derived class to determine differential cross section for top production in semimuonic decay channel
 
-   The structure keeps histograms for the kinematics of top quarks and ttbar pairs. These histograms 
-   can be filled from the TtSemiLeptonicEvent class (with an extra option to require matching for 
-   purity & stability studies) or from the TtGenEvent class. The class is derived from the 
-   SingleObject<Collection> interface, which makes it usable in full framework or fwlite. 
+   The structure keeps histograms for the kinematics of muons originating from semileptonic ttbar decays.
+   The class is derived from the SingleObject<Collection> interface, which makes it usable in full framework or fwlite. 
 */
 
 namespace CrossSection{
- // binning for top & ttbar cross section histograms
- double muonPt[] = {   0. ,    30. ,    50. ,    75. ,   150. };
+ // binning for cross section histograms
+ double muonPt[]  = {   0. ,    20. ,    40.,    65. ,   150.                 };
+ double muonEta[] = {  -2.1,   -0.75,    0.0,    0.75,   2.1                  };
+ double muonPhi[] = {  -3.0,   -2.0 ,   -1.0,    0.0 ,   1.0 ,   2.0 ,   3.0  };
 }
 
-class MuonCrossSection : public SingleObject<const edm::View<reco::Candidate> > {
+class MuonCrossSection : public SingleObject<const edm::View<pat::Muon> > {
   
  public:
   /// default constructor for generator level analysis in fw lite
@@ -43,9 +43,14 @@ class MuonCrossSection : public SingleObject<const edm::View<reco::Candidate> > 
   /// histogramm booking for fw
   void book(edm::Service<TFileService>& fileService);
   /// histogram filling interface for reconstruction level for access with fw or fwlite
-  void fill(const edm::View<reco::Candidate>& muons, const double& weight=1.);
+  void fill(const edm::View<pat::Muon>& muons, const double& weight=1.);
   /// everything which needs to be done after the event loop
   void process(){};
+
+
+ private:
+  /// histogram container for correlation plots
+  std::map<std::string, TH2*> corrs_;
 };
 
 #endif
