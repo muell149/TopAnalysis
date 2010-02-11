5306ffc2f90a0ce34ed97e1bdea2fa958ce6af52
eschliec <>
2010-02-11 14:28:27 +0000

    added 2D correlation plots for all event shape variable combinations

--- TopAnalyzer/src/EventShapes.cc
+++ TopAnalyzer/src/EventShapes.cc
@@ -81,6 +81,26 @@ EventShapes::book(edm::Service<TFileService>& fs)
   /// and measures the 4-jet structure of the event (D vanishes for a planar event)
   hists_["D"] = fs->make<TH1F>( "D" , "D" ,  100 , 0. ,  1. );
 
+  /** 
+      Correlations of Event Shape Variables
+  **/
+
+  hists2D_["iso_cir"] = fs->make<TH2F>( "iso_cir" , "iso_cir" ,  100 , 0. ,  1. , 100 , 0. , 1. );
+  hists2D_["iso_sph"] = fs->make<TH2F>( "iso_sph" , "iso_sph" ,  100 , 0. ,  1. , 100 , 0. , 1. );
+  hists2D_["iso_apl"] = fs->make<TH2F>( "iso_apl" , "iso_apl" ,  100 , 0. ,  1. , 100 , 0. , 1. );
+  hists2D_["iso_c"  ] = fs->make<TH2F>( "iso_c"   , "iso_c"   ,  100 , 0. ,  1. , 100 , 0. , 1. );
+  hists2D_["iso_d"  ] = fs->make<TH2F>( "iso_d"   , "iso_d"   ,  100 , 0. ,  1. , 100 , 0. , 1. );
+  hists2D_["cir_sph"] = fs->make<TH2F>( "cir_sph" , "cir_sph" ,  100 , 0. ,  1. , 100 , 0. , 1. );
+  hists2D_["cir_apl"] = fs->make<TH2F>( "cir_apl" , "cir_apl" ,  100 , 0. ,  1. , 100 , 0. , 1. );
+  hists2D_["cir_c"  ] = fs->make<TH2F>( "cir_c"   , "cir_c"   ,  100 , 0. ,  1. , 100 , 0. , 1. );
+  hists2D_["cir_d"  ] = fs->make<TH2F>( "cir_d"   , "cir_d"   ,  100 , 0. ,  1. , 100 , 0. , 1. );
+  hists2D_["sph_apl"] = fs->make<TH2F>( "sph_apl" , "sph_apl" ,  100 , 0. ,  1. , 100 , 0. , 1. );
+  hists2D_["sph_c"  ] = fs->make<TH2F>( "sph_c"   , "sph_c"   ,  100 , 0. ,  1. , 100 , 0. , 1. );
+  hists2D_["sph_d"  ] = fs->make<TH2F>( "sph_d"   , "sph_d"   ,  100 , 0. ,  1. , 100 , 0. , 1. );
+  hists2D_["apl_c"  ] = fs->make<TH2F>( "apl_c"   , "apl_c"   ,  100 , 0. ,  1. , 100 , 0. , 1. );
+  hists2D_["apl_d"  ] = fs->make<TH2F>( "apl_d"   , "apl_d"   ,  100 , 0. ,  1. , 100 , 0. , 1. );
+  hists2D_["c_d"    ] = fs->make<TH2F>( "c_d"     , "c_d"     ,  100 , 0. ,  1. , 100 , 0. , 1. );
+
 }
 
 std::vector<math::XYZVector> makeVecForEventShape(edm::View<reco::Candidate> jets, double scale = 1.) {
@@ -129,6 +149,22 @@ EventShapes::fill(const edm::View<reco::Candidate>& jets, const double& weight)
   hists_.find("C"           )->second->Fill( C_           , weight );
   // D of event
   hists_.find("D"           )->second->Fill( D_           , weight );
+
+  hists2D_.find("iso_cir")->second->Fill( isotropy_    , circularity_ , weight );
+  hists2D_.find("iso_sph")->second->Fill( isotropy_    , sphericity_  , weight );
+  hists2D_.find("iso_apl")->second->Fill( isotropy_    , aplanarity_  , weight );
+  hists2D_.find("iso_c"  )->second->Fill( isotropy_    , C_           , weight );
+  hists2D_.find("iso_d"  )->second->Fill( isotropy_    , D_           , weight );
+  hists2D_.find("cir_sph")->second->Fill( circularity_ , sphericity_  , weight );
+  hists2D_.find("cir_apl")->second->Fill( circularity_ , aplanarity_  , weight );
+  hists2D_.find("cir_c"  )->second->Fill( circularity_ , C_           , weight );
+  hists2D_.find("cir_d"  )->second->Fill( circularity_ , D_           , weight );
+  hists2D_.find("sph_apl")->second->Fill( sphericity_  , aplanarity_  , weight );
+  hists2D_.find("sph_c"  )->second->Fill( sphericity_  , C_           , weight );
+  hists2D_.find("sph_d"  )->second->Fill( sphericity_  , D_           , weight );
+  hists2D_.find("apl_c"  )->second->Fill( aplanarity_  , C_           , weight );
+  hists2D_.find("apl_d"  )->second->Fill( aplanarity_  , D_           , weight );
+  hists2D_.find("c_d"    )->second->Fill( C_           , D_           , weight );
 }
 
 /// return the desired correction step from the configuration string, which is expected to be of type 'step' or 'step:flavor'
@@ -153,59 +189,3 @@ const std::string EventShapes::correctionFlavor() const
   }
   return flavor;
 }
-
-// /// histogram filling for fwlite and for full fw
-// void
-// EventShapes::fill(const edm::View<reco::Candidate>& jets, const double& weight)
-// {
-//   /**
-//      Calculate Event Shape Variables
-//   **/
-
-//   EventShapeVariables eventshape(makeVecForEventShape(jets));
-
-//   double aplanarity_  = eventshape.aplanarity();
-//   double sphericity_  = eventshape.sphericity();
-//   double circularity_ = eventshape.circularity();
-//   double isotropy_    = eventshape.isotropy();
-//   double C_           = eventshape.C();
-//   double D_           = eventshape.D();
-
-//   /** 
-//       Fill Event Shape Variables
-//   **/
-
-//   // aplanarity of event
-//   hists_.find("aplanarity"  )->second->Fill( aplanarity_  , weight );
-//   // sphericity of event
-//   hists_.find("sphericity"  )->second->Fill( sphericity_  , weight );
-//   // circularity of event
-//   hists_.find("circularity" )->second->Fill( circularity_ , weight );
-//   // isotropy of event
-//   hists_.find("isotropy"    )->second->Fill( isotropy_    , weight );
-//   // C of event
-//   hists_.find("C"           )->second->Fill( C_           , weight );
-//   // D of event
-//   hists_.find("D"           )->second->Fill( D_           , weight );
-
-//   /** 
-//       Fill Event Shape Variables
-//   **/
-// /*
-//   for(edm::View<pat::Jet>::const_iterator jet=jets.begin(); jet!=jets.end(); ++jet, ++index){
-//     if( index_<0 || index_==index ){
-//       // energy of the jet
-//       hists_.find( "en"  )->second->Fill( jet->correctedJet(correctionStep(), correctionFlavor()).energy() , weight );
-//       // transverse momentum of the jet
-//       hists_.find( "pt"  )->second->Fill( jet->correctedJet(correctionStep(), correctionFlavor()).pt() , weight );
-//       // pseudorapidity eta of the jet
-//       hists_.find( "eta" )->second->Fill( jet->eta() , weight );
-//       // azimuthal angle phi of the jet
-//       hists_.find( "phi" )->second->Fill( jet->phi() , weight );
-//     }
-//   }
-//   // jet multiplicty is always filled the same way
-//   // independent from the choice of index_
-//   hists_.find("n"  )->second->Fill( jets.size() , weight );
-// */
-// }
