5a3af8eedf823d03bc1582f2ddf3bc74eae11480
snaumann <>
2010-02-17 09:37:56 +0000

    update GlobalTag in order to run in 34X

--- TopUtils/test/testJetEnergyScale_cfg.py
+++ TopUtils/test/testJetEnergyScale_cfg.py
@@ -30,7 +30,7 @@ process.options = cms.untracked.PSet(
 process.load("Configuration.StandardSequences.Geometry_cff")
 process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
-process.GlobalTag.globaltag = cms.string('MC_31X_V3::All')
+process.GlobalTag.globaltag = cms.string('MC_3XY_V18::All')
 
 ## pat sequences
 process.load("PhysicsTools.PatAlgos.patSequences_cff")
