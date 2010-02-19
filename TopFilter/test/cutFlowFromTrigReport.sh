8f9b0b8ac2780d5922f564f186747122ec5f3745
snaumann <>
2010-02-19 12:06:51 +0000

    added some printout with instructions

--- TopFilter/test/cutFlowFromTrigReport.sh
+++ TopFilter/test/cutFlowFromTrigReport.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+if [ ! $1 ]
+    then
+    echo "Please specify file containing the TrigReport or use --help"
+    exit 0
+fi
+
+if [ $1 = "--help" ]
+    then
+    echo "The file containig the TrigReport must be given as parameter to this script."
+    echo "Example:"
+    echo "[First] cmsRun TopAnalysis/TopFilter/test/testSemiLepMuonSelection_cfg.py > out.txt 2>> out.txt"
+    echo "[Then ] TopAnalysis/TopFilter/test/cutFlowFromTrigReport.sh out.txt"
+    exit 0
+fi
+
 ## look for beginning of desired summary table
 start="TrigReport............Modules.in.Path:.p1"
 a=`sed -ne /$start/= $1`
