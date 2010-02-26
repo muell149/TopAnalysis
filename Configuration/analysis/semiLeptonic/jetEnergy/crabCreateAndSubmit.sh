dc933b224cc7f0d99a384d0d1f3b049b48b971b0
snaumann <>
2010-02-26 16:23:03 +0000

    stupid scripts for lazy people (like me)

---
+++ Configuration/analysis/semiLeptonic/jetEnergy/crabCreateAndSubmit.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+list="crabJetEnergyCorrections_aod_ak5 \
+      crabJetEnergyCorrections_aod_ic5 \
+      crabJetEnergyCorrections_aod_kt4 \
+      crabJetEnergyCorrections_aod_sc5"
+
+for i in $list
+  do
+  echo "==========================================================================================="
+  echo $i
+  echo "+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+  crab -cfg TopAnalysis/Configuration/analysis/semiLeptonic/jetEnergy/$i.cfg -create -submit
+done
