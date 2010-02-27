ace5517ea24a15aa450770f48fa97c2bdb48dd91
mgoerner <>
2010-02-27 11:33:33 +0000

    config files based on analyzeMuonDiffXSec_cfg.py, using JES-shifting module +/-10%(1.1/0.9) and changing the genlevel-filter to ttbarOtherThanSemileptonicMuon/NoGenLevelFilter(BG/All)

---
+++ Configuration/analysis/semiLeptonic/diffXSection/analyzeMuonJES11_cfg.py
@@ -0,0 +1,30 @@
+## ---
+##    this configfile does the same like analyzeMuonDiffXSec_cfg.py
+##    but with a JES shift of +10%
+## ---
+
+## get the mother file
+execfile("/afs/naf.desy.de/user/g/goerner/semileptonic341/analyzeMuonDiffXSec_cfg.py")
+
+## get JES-shifting module
+process.load("TopAnalysis.TopUtils.JetEnergyScale_cfi")
+# set input collection- needed while running on pat tuples
+process.scaledJetEnergy.inputJets = "selectedLayer1Jets"
+# JES +10%
+process.scaledJetEnergy.scaleFactor = 1.1
+
+## include module to create JES-shifted collection
+process.p1.replace(process.semiLeptonicSelection,
+                   process.scaledJetEnergy * process.semiLeptonicSelection)
+
+## change monitoring to shifted collection
+process.p1.replace(process.unshiftedJets,
+                   process.shiftedJets)
+
+## change input collection to JES-shifted collection 
+process.centralJets.src = "scaledJetEnergy:selectedLayer1Jets"
+process.reliableJets.src = "scaledJetEnergy:selectedLayer1Jets"
+process.goodJets.src = "scaledJetEnergy:selectedLayer1Jets"
+
+## change output name 
+process.TFileService.fileName = 'analyzeDiffXSecJES11_testSig.root'
