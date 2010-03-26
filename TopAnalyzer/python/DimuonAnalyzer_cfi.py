46217fd845c3eca7318874985b0c906baf5f0d56
dammann <>
2010-03-26 15:58:15 +0000

    moved QCD BG estimation from dimuon analyzer into separate module

--- TopAnalyzer/python/DimuonAnalyzer_cfi.py
+++ TopAnalyzer/python/DimuonAnalyzer_cfi.py
@@ -4,9 +4,7 @@ analyzeMuonPair = cms.EDAnalyzer("DimuonAnalyzer",
     # source
     muons = cms.InputTag("selectedPatMuons"),     
     # option to weight events
-    useEventWeight = cms.bool(True),
-    # bins of di-combined two muon isolation
-    isoBins = cms.vdouble(0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0) 
+    useEventWeight = cms.bool(True),    
 )
 
 
