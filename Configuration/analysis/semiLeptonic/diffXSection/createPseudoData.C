de2c77188129fe251de92ba571ea72c1fbdbf367
mgoerner <>
2010-04-07 14:23:54 +0000

    add arbitrary number of plots, output directory = original directory for all plots

--- Configuration/analysis/semiLeptonic/diffXSection/createPseudoData.C
+++ Configuration/analysis/semiLeptonic/diffXSection/createPseudoData.C
@@ -17,45 +17,64 @@ void smearing(TH1F& src, TH1F& data);
 void loadingFiles();
 void loadingHists(TString plot);
 void combineFiles(float luminosity);
+void poisson(TString directory, TString plot, TFile& outputfile);
 
 std::vector<float> lumiweight_;
 std::vector<TH1F*> hists_;
 std::vector<TFile*> files_;
 
-
-void createPseudoData()
-{
-
+void createPseudoData(){
   // get the files
   loadingFiles(); 
-
-  // get the histograms
-  loadingHists("analyzeTightMuonCrossSection/pt");  
-
-  // define output
-  TH1F* pseudoData=(TH1F*)hists_[0]->Clone(); pseudoData->Clear();
-  
-  // normalize to luminosity and add all samples
-  combineFiles(50.0);
-  
-  // apply the poisson smearing
-  smearing(*hists_[files_.size()], *pseudoData);
-  
-  // write output
-  TFile f("pseudoData7TeV50pb.root", "recreate");  
-  pseudoData->Write("pt");
-  
+  // definition of output file
+  TFile f("./pseudoData7TeV50pb.root", "recreate");
+  // list of plots you want to add and their directories within the input samples
+  poisson("analyzeTightMuonCrossSection", "pt"        , f);
+  poisson("analyzeTightMuonCrossSection", "eta"       , f);
+  poisson("analyzeTightMuonCrossSection", "phi"       , f);
+  poisson("analyzeTightMuonCrossSection", "ptMuPlus"  , f);
+  poisson("analyzeTightMuonCrossSection", "etaMuPlus" , f);
+  poisson("analyzeTightMuonCrossSection", "phiMuPlus" , f);
+  poisson("analyzeTightMuonCrossSection", "ptMuMinus" , f);
+  poisson("analyzeTightMuonCrossSection", "etaMuMinus", f);
+  poisson("analyzeTightMuonCrossSection", "phiMuMinus", f);
+  f.Close();
 }
 
+void poisson(TString directory, TString plot, TFile& outputfile)
+{
+  // get the histograms, erase existing at the beginning
+  hists_.clear();
+  loadingHists(directory+"/"+plot);  
+  // define output plot
+  TH1F* pseudoData=(TH1F*)hists_[0]->Clone(); pseudoData->Clear();
+  // normalize to luminosity and add all samples
+  combineFiles(50.0);
+  // apply the poisson smearing COMMENT OUT IF YOU JUST WANT TO COMBINE PLOTS
+  smearing(*hists_[files_.size()], *pseudoData);
+  // write output to directory
+  if(outputfile.GetDirectory(directory)==0){
+    outputfile.mkdir(directory);
+    outputfile.cd(directory);
+  }
+  pseudoData->Write(plot);  
+}
 
 void loadingFiles()
 {
   // add all contributing samples here
-  files_.push_back(new TFile("./diffXSecFromSignal/diffXSecSigMcAtNlo7TeV.root" ) );
-  files_.push_back(new TFile("./diffXSecFromSignal/diffXSecWjetsMadgraph7.root" ) );
-  files_.push_back(new TFile("./diffXSecFromSignal/diffXSecBkgMcAtNlo7TeV.root" ) );
-  files_.push_back(new TFile("./diffXSecFromSignal/diffXSecZjetsMadgraph7.root" ) );
-  files_.push_back(new TFile("./diffXSecFromSignal/diffXSecQCDPythia7TeV.root"  ) );
+  files_.push_back(new TFile("./diffXSecFromSignal/diffXSecSigMcAtNlo7TeV.root"    ) );
+  files_.push_back(new TFile("./diffXSecFromSignal/diffXSecWjetsMadgraph7TeV.root" ) );
+  files_.push_back(new TFile("./diffXSecFromSignal/diffXSecBkgMcAtNlo7TeV.root"    ) );
+  files_.push_back(new TFile("./diffXSecFromSignal/diffXSecZjetsMadgraph7TeV.root" ) );
+  files_.push_back(new TFile("./diffXSecFromSignal/diffXSecQCDPythia7TeV.root"     ) );
+}
+
+void loadingHists(TString plot)
+{  
+  for(unsigned int idx=0; idx<files_.size(); ++idx) {
+    hists_ .push_back( (TH1F*)files_[idx]->Get(plot) );
+  }
 }
 
 void smearing(TH1F& src, TH1F& data)
@@ -67,14 +86,6 @@ void smearing(TH1F& src, TH1F& data)
    data.SetBinError(ibin, sqrt(evts));
  }
 }
-
-void loadingHists(TString plot)
-{  
-  for(unsigned int idx=0; idx<files_.size(); ++idx) {
-    hists_ .push_back( (TH1F*)files_[idx]->Get(plot) );
-  }
-}
-
 
 void combineFiles(float luminosity)
 {
