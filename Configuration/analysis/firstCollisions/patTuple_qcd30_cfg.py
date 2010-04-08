210b61558132860838d3830d4cfbe644db1ba91c
snaumann <>
2010-04-08 12:10:17 +0000

    remove trigger bit 0 as it is not simulated

--- Configuration/analysis/firstCollisions/patTuple_qcd30_cfg.py
+++ Configuration/analysis/firstCollisions/patTuple_qcd30_cfg.py
@@ -3,6 +3,9 @@ execfile("TopAnalysis/Configuration/analysis/firstCollisions/patTuple_cfg.py")
 ## correct global tag
 process.GlobalTag.globaltag = 'START3X_V26::All'
 
+## remove trigger bit 0 as this one is not simulated
+process.hltLevel1GTSeed.L1SeedsLogicalExpression = '(40 OR 41) AND NOT (36 OR 37 OR 38 OR 39) AND NOT ((42 AND NOT 43) OR (43 AND NOT 42))'
+
 ## tighter jet cuts
 from PhysicsTools.PatAlgos.selectionLayer1.jetSelector_cfi import *
 process.goodJets.cut = ('abs(eta) < 2.6 & pt > 30. &'
