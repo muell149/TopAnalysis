7bdb0a080b6687577d9bf74d2d0e0833d23bbad9
snaumann <>
2010-04-08 12:11:27 +0000

    add number of events to comment

--- Configuration/analysis/firstCollisions/Source_Run132473_cff.py
+++ Configuration/analysis/firstCollisions/Source_Run132473_cff.py
@@ -1,7 +1,7 @@
 import FWCore.ParameterSet.Config as cms
 
 ## from /MinimumBias/Commissioning10-PromptReco-v8/RECO/
-## ??? "multijet" events in 4 files dCache at DESY
+## 19 "multijet" events in 4 files dCache at DESY
 
 process.source.fileNames = [
     '/store/user/snaumann/firstCollisions_7TeV/patTuple_Run132473_1.root',
