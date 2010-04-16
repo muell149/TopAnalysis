2bb176459d1bcb0bfe898d8fa19ad37ded0b2311
rwolf <>
2010-04-16 19:19:02 +0000

    elevated dem::View to reco::Candidate, removed reco::GenParticle fill function, added improved histogram fill functions

--- TopAnalyzer/interface/MuonKinematics.h
+++ TopAnalyzer/interface/MuonKinematics.h
@@ -10,13 +10,12 @@
 
    \brief   Derived class to analyze the kinematics of muons on reconstruction and generator level
 
-   The structure keeps histograms for the kinematics of muons only(!). These histograms can be 
-   filled from edm::View<pat::Muon>'s or from std::vector<reco::GenParticles>'s. The class 
-   is derived from the SingleObject<Collection> interface, which makes it usable in fwfull  or 
-   fwlite. 
+   This class keeps histograms for the kinematics of muons only(!). These histograms can 
+   be filled from edm::View<reco::Candidate> collections. The class is derived from the 
+   SingleObject<Collection> interface, which makes it usable in fwfull or fwlite. 
 */
 
-class MuonKinematics : public SingleObject<const edm::View<pat::Muon> > {
+class MuonKinematics : public SingleObject<const edm::View<reco::Candidate> > {
 
  public:
   /// default constructor for fw lite
@@ -34,12 +33,21 @@ class MuonKinematics : public SingleObject<const edm::View<pat::Muon> > {
   void book();
   /// histogramm booking for fwfull
   void book(edm::Service<TFileService>& fileService);
-  /// histogram filling for fwlite and for fwfull from reco objects
-  void fill(const edm::View<pat::Muon>& muons, const double& weight=1.);
-  /// histogram filling for fwlite and for fwfull from generator objects
-  void fill(const std::vector<reco::GenParticle>& muons, const double& weight=1.);
+  /// histogram filling for fwlite and for fwfull
+  void fill(const edm::View<reco::Candidate>& muons, const double& weight=1.);
   /// everything which needs to be done after the event loop
-  void process() {};
+  void process(){}
+
+ private:
+  /// check if histogram was booked in the corresponding map. Note that the 
+  /// template C is needed to make this function accessible for maps that 
+  /// contain any kind of type keyed via strings
+  template<typename C> 
+  bool booked(std::map<std::string, C> map, const std::string histName) const { return map.find(histName.c_str())!=map.end(); }
+  /// fill 1-dimensional histogram if it had been booked before
+  void fill(const std::string histName, double valueA, double weight) const { if(booked(hists_, histName)) hists_.find(histName)->second->Fill(valueA, weight); }
+  /// fill 2-dimensional histogram if it had been booked before
+  void fill(const std::string histName, double valueA, double valueB, double weight) const { if(booked(hists2D_, histName)) hists2D_.find(histName)->second->Fill(valueA, valueB, weight); }
 
  private:
   /// index of jet to be plotted
