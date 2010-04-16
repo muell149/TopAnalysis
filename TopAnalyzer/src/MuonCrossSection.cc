1aaa023e149e4b294fe1344c2eaf5504188cfc36
rwolf <>
2010-04-16 19:24:05 +0000

    compete rework

--- TopAnalyzer/src/MuonCrossSection.cc
+++ TopAnalyzer/src/MuonCrossSection.cc
@@ -1,16 +1,14 @@
-#include <Math/Boost.h>
-#include <Math/VectorUtil.h>
-#include "TopAnalysis/TopAnalyzer/interface/MuonCrossSection.h"
 #include "FWCore/MessageLogger/interface/MessageLogger.h"
-
+#include "TopAnalysis/TopAnalyzer/interface/MuonCrossSection.h"
 
 /// default constructor for generator level analysis in fw lite
-MuonCrossSection::MuonCrossSection()
+MuonCrossSection::MuonCrossSection(int index) : index_(index)
 {
 }
 
 /// default constructor for full fw
-MuonCrossSection::MuonCrossSection(const edm::ParameterSet& cfg)
+MuonCrossSection::MuonCrossSection(const edm::ParameterSet& cfg) :
+  index_( cfg.getParameter<int>( "index" ) )
 {
 }
 
@@ -21,57 +19,42 @@ void MuonCrossSection::book()
       Muon Variables for Cross Section Measurement Reco Particles
   **/
   // muon pt
-  hists_["pt" ] = new TH1F( "pt",   "pt",   6, CrossSection::muonPt  );
+  hists_["pt"    ] = new TH1F( "pt"    ,  "pt",   6, CrossSection::muonPt  );
   // muon eta
-  hists_["eta"] = new TH1F( "eta", "eta",   4, CrossSection::muonEta );
+  hists_["eta"   ] = new TH1F( "eta"   , "eta",   4, CrossSection::muonEta );
   // muon phi
-  hists_["phi"] = new TH1F( "phi", "phi",   6, CrossSection::muonPhi );
+  hists_["phi"   ] = new TH1F( "phi"   , "phi",   6, CrossSection::muonPhi );
 
-  /** 
-      Muon Variables for Cross Section Measurement Generator Particles
+ /** 
+      Control Distribution plot
   **/
-  // muon pt
-  hists_["ptGen" ] = new TH1F( "ptGen",   "ptGen",   6, CrossSection::muonPt  );
-  // muon eta
-  hists_["etaGen"] = new TH1F( "etaGen", "etaGen",   4, CrossSection::muonEta );
-  // muon phi
-  hists_["phiGen"] = new TH1F( "phiGen", "phiGen",   6, CrossSection::muonPhi );
+  // muon multiplicty
+  hists_["charge"] = new TH1F( "charge"   , "charge"   ,   2 ,       -1.    ,     1.);
+  // positive charged muon pt
+  hists_["ptp"   ] = new TH1F( "ptPlus"   , "pt(mu+)"  ,   6, CrossSection::muonPt  );
+  // positive charged muon eta
+  hists_["etap"  ] = new TH1F( "etaPlus"  , "eta(mu+)" ,   4, CrossSection::muonEta );
+  // positive charged muon phi
+  hists_["phip"  ] = new TH1F( "phiPlus"  , "phi(mu+)" ,   6, CrossSection::muonPhi );
+  // negative charged muon pt
+  hists_["ptm"   ] = new TH1F( "ptMinus"  , "pt(mu-)"  ,   6, CrossSection::muonPt  );
+  // negative charged muon eta
+  hists_["etam"  ] = new TH1F( "etaMinus" , "eta(mu-)" ,   4, CrossSection::muonEta );
+  // negative charged muon phi
+  hists_["phim"  ] = new TH1F( "phiMinus" , "phi(mu-)" ,   6, CrossSection::muonPhi );
 
-  /** 
+ /** 
       Correlation Plots
   **/
   // gen-rec level correlation muon pt
-  corrs_["muonPt_"    ] = new TH2F( "muonPt_"    , "muonPt_"    ,  400,    0.,  400.,     400,   0.,  400.);
+  hists2D_["pt_" ] = new TH2F( "pt_"    , "pt_"    ,  400,    0.,  400.,     400,   0.,  400.);
   // gen-rec level correlation muon eta
-  corrs_["muonEta_"   ] = new TH2F( "muonEta_"   , "muonEta_"   ,  800,   -4.,    4.,     800,  -4.,    4.);
+  hists2D_["eta_"] = new TH2F( "eta_"   , "eta_"   ,  800,   -4.,    4.,     800,  -4.,    4.);
   // gen-rec level correlation muon phi
-  corrs_["muonPhi_"   ] = new TH2F( "muonPhi_"   , "muonPhi_"   ,  628, -3.14,  3.14,     628,-3.14,  3.14);
-
-  /** 
-      Muon Charge Distribution plot
-  **/
-  // muon multiplicty
-  hists_["charge"  ] = new TH1F( "charge"   , "charge"   ,  4 ,  0  ,  4 );
-
-  /** 
-      Charge dependent Muon Variables for Cross Section Measurement Reco Particles
-  **/
-  // positive charged muon pt
-  hists_["ptMuPlus"   ] = new TH1F( "ptMuPlus", "ptMuPlus"  ,   6, CrossSection::muonPt  );
-  // positive charged muon eta
-  hists_["etaMuPlus"  ] = new TH1F( "etaMuPlus", "etaMuPlus",   4, CrossSection::muonEta );
-  // positive charged muon phi
-  hists_["phiMuPlus"  ] = new TH1F( "phiMuPlus", "phiMuPlus",   6, CrossSection::muonPhi );
-  // negative charged muon pt
-  hists_["ptMuMinus"  ] = new TH1F( "ptMuMinus", "ptMuMinus"  ,   6, CrossSection::muonPt  );
-  // negative charged muon eta
-  hists_["etaMuMinus" ] = new TH1F( "etaMuMinus", "etaMuMinus",   4, CrossSection::muonEta );
-  // negative charged muon phi
-  hists_["phiMuMinus" ] = new TH1F( "phiMuMinus", "phiMuMinus",   6, CrossSection::muonPhi );
-
+  hists2D_["phi_"] = new TH2F( "phi_"   , "phi_"   ,  628, -3.14,  3.14,     628,-3.14,  3.14);
 }
 
-/// histogramm booking for fw
+/// histogramm booking for full fw
 void MuonCrossSection::book(edm::Service<TFileService>& fs)
 {
   /** 
@@ -85,127 +68,79 @@ void MuonCrossSection::book(edm::Service<TFileService>& fs)
   hists_["phi" ] = fs->make<TH1F>( "phi"   , "phi",   6, CrossSection::muonPhi );
 
   /** 
-      Muon Variables for Cross Section Measurement Generator Particles
+      Control Distribution plot
   **/
-  // muon pt
-  hists_["ptGen"  ] = fs->make<TH1F>( "ptGen"    ,  "ptGen",   6, CrossSection::muonPt  );
-  // muon eta
-  hists_["etaGen" ] = fs->make<TH1F>( "etaGen"   , "etaGen",   4, CrossSection::muonEta );
-  // muon phi
-  hists_["phiGen" ] = fs->make<TH1F>( "phiGen"   , "phiGen",   6, CrossSection::muonPhi );
+  // muon multiplicty
+  hists_["charge"] = fs->make<TH1F>( "charge"   , "charge"   ,   2 ,       -1.    ,     1.);
+  // positive charged muon pt
+  hists_["ptp"   ] = fs->make<TH1F>( "ptPlus"   , "pt(mu+)"  ,   6, CrossSection::muonPt  );
+  // positive charged muon eta
+  hists_["etap"  ] = fs->make<TH1F>( "etaPlus"  , "eta(mu+)" ,   4, CrossSection::muonEta );
+  // positive charged muon phi
+  hists_["phip"  ] = fs->make<TH1F>( "phiPlus"  , "phi(mu+)" ,   6, CrossSection::muonPhi );
+  // negative charged muon pt
+  hists_["ptm"   ] = fs->make<TH1F>( "ptMinus"  , "pt(mu-)"  ,   6, CrossSection::muonPt  );
+  // negative charged muon eta
+  hists_["etam"  ] = fs->make<TH1F>( "etaMinus" , "eta(mu-)" ,   4, CrossSection::muonEta );
+  // negative charged muon phi
+  hists_["phim"  ] = fs->make<TH1F>( "phiMinus" , "phi(mu-)" ,   6, CrossSection::muonPhi );
 
- /** 
+  /** 
       Correlation Plots
   **/
   // gen-rec level correlation muon pt
-  corrs_["muonPt_"    ] = fs->make<TH2F>( "muonPt_"    , "muonPt_"    ,  400,    0.,  400.,     400,   0.,  400.);
+  hists2D_["pt_" ] = fs->make<TH2F>( "pt_"    , "pt_"    ,  400,    0.,  400.,     400,   0.,  400.);
   // gen-rec level correlation muon eta
-  corrs_["muonEta_"   ] = fs->make<TH2F>( "muonEta_"   , "muonEta_"   ,  800,   -4.,    4.,     800,  -4.,    4.);
+  hists2D_["eta_"] = fs->make<TH2F>( "eta_"   , "eta_"   ,  800,   -4.,    4.,     800,  -4.,    4.);
   // gen-rec level correlation muon phi
-  corrs_["muonPhi_"   ] = fs->make<TH2F>( "muonPhi_"   , "muonPhi_"   ,  628, -3.14,  3.14,     628,-3.14,  3.14);
-
-  /** 
-      Muon Charge Distribution plot
-  **/
-  // muon multiplicty
-  hists_["charge"  ] = fs->make<TH1F>( "charge"   , "charge"   ,  4 ,  0  ,  4 );
-
-
-  /** 
-      Charge dependent Muon Variables for Cross Section Measurement Reco Particles
-  **/
-  // positive charged muon pt
-  hists_["ptMuPlus"   ] = fs->make<TH1F>( "ptMuPlus", "ptMuPlus"  ,   6, CrossSection::muonPt   );
-  // positive charged muon eta
-  hists_["etaMuPlus"  ] = fs->make<TH1F>( "etaMuPlus", "etaMuPlus",   4, CrossSection::muonEta  );
-  // positive charged muon phi
-  hists_["phiMuPlus"  ] = fs->make<TH1F>( "phiMuPlus", "phiMuPlus",   6, CrossSection::muonPhi  );
-  // negative charged muon pt
-  hists_["ptMuMinus"  ] = fs->make<TH1F>( "ptMuMinus", "ptMuMinus"  ,   6, CrossSection::muonPt  );
-  // negative charged muon eta
-  hists_["etaMuMinus" ] = fs->make<TH1F>( "etaMuMinus", "etaMuMinus",   4, CrossSection::muonEta );
-  // negative charged muon phi
-  hists_["phiMuMinus" ] = fs->make<TH1F>( "phiMuMinus", "phiMuMinus",   6, CrossSection::muonPhi );
-  //  std::cout << "booking ok" << std::endl;
+  hists2D_["phi_"] = fs->make<TH2F>( "phi_"   , "phi_"   ,  628, -3.14,  3.14,     628,-3.14,  3.14);
 }
 
 /// histogram filling with fwlite or full framework
 void
-MuonCrossSection::fill(const edm::View<pat::Muon>& muons, const double& weight)
+MuonCrossSection::fill(const edm::View<reco::Candidate>& muons, const double& weight)
 {
-  /** 
-      Fill Kinematic Variables
-  **/
-  for(edm::View<pat::Muon>::const_iterator muon=muons.begin(); muon!=muons.end(); ++muon){
+  // index for the leading, 2. leading, 3. leading muon to be compared with index_ 
+  // from the module config where index_=-1 means 'fill all muons' and index_=n
+  // n>=-1 means 'fill only n-th leading muon'
+  int index=0;
+  for(edm::View<reco::Candidate>::const_iterator muon=muons.begin(); muon!=muons.end(); ++muon, ++index){
+    if( index_<0 || index_==index ){
+      // make sure charge is of abs value 1
+      int charge=muon->charge()/abs(muon->charge());
 
-
-    //------------------------------//
-    //--filling for reco particles--//
-    //------------------------------//
-
-    // transverse momentum of the muon
-    hists_.find( "pt"   )->second->Fill( muon->et()  , weight );
-     // pseudorapidity of the muon
-    hists_.find( "eta"  )->second->Fill( muon->eta() , weight );
-     // azimuth angle of the muon
-    hists_.find( "phi"  )->second->Fill( muon->phi() , weight );
- 
-
-    //-----------------------------------//
-    //--filling for generator particles--//
-    //-----------------------------------//
-
-    if(muon->genLepton()){
-    // transverse momentum of the muon
-    hists_.find( "ptGen"   )->second->Fill( muon->genLepton()->et()  , weight );
-     // pseudorapidity of the muon
-    hists_.find( "etaGen"  )->second->Fill( muon->genLepton()->eta() , weight );
-     // azimuth angle of the muon
-    hists_.find( "phiGen"  )->second->Fill( muon->genLepton()->phi() , weight );
- 
-    //---------------------------------//
-    //--filling for correlation plots--//
-    //---------------------------------//
-
-    // transverse momentum of the muon
-    corrs_.find("muonPt_"  )->second->Fill( muon->genLepton()->et() ,  muon->et()   , weight );
-    // pseudorapidity of the muon
-    corrs_.find("muonEta_" )->second->Fill( muon->genLepton()->eta(),  muon->eta()  , weight );
-    // azimuth angle of the muon
-    corrs_.find("muonPhi_" )->second->Fill( muon->genLepton()->phi(),  muon->phi()  , weight );
-    }
-    else{
-      edm::LogWarning ( "NoGenLepton" ) << "no associated GenLepton found";
-    } 
-    
-    //----------------------------//
-    //--charge dependent filling--//
-    //----------------------------//
-
-    // filling for negative charged muons
-    if(muon->charge()<0){
-    hists_.find("charge"  )->second->Fill( 1 , weight );
- 
-    // filling for reco particles
-    // transverse momentum of the muon
-    hists_.find( "ptMuMinus"   )->second->Fill( muon->et()  , weight );
-     // pseudorapidity of the muon
-    hists_.find( "etaMuMinus"  )->second->Fill( muon->eta() , weight );
-     // azimuth angle of the muon
-    hists_.find( "phiMuMinus"  )->second->Fill( muon->phi() , weight );
-    }
-    // filling for positive charged muons
-    if(muon->charge()>0){
-    hists_.find("charge"  )->second->Fill( 3 , weight );
-
-    // filling for reco particles
-    // transverse momentum of the muon
-    hists_.find( "ptMuPlus"   )->second->Fill( muon->et()  , weight );
-    // pseudorapidity of the muon
-    hists_.find( "etaMuPlus"  )->second->Fill( muon->eta() , weight );
-    // azimuth angle of the muon
-    hists_.find( "phiMuPlus"  )->second->Fill( muon->phi() , weight );
+      // transverse momentum of the muon
+      fill( "pt"    ,  muon->et()  , weight );
+      // pseudorapidity of the muon
+      fill( "eta"   ,  muon->eta() , weight );
+      // azimuth angle of the muon
+      fill( "phi"   ,  muon->phi() , weight );
+      // fill charge of the muon
+      fill( "charge",  0.5*charge  , weight );
+      // fill histograms for charged muons
+      fill( charged(std::string("pt"),  charge),  muon->et()  , weight );
+      // pseudorapidity of the muon
+      fill( charged(std::string("eta"), charge),  muon->eta() , weight );
+      // azimuth angle of the muon
+      fill( charged(std::string("phi"), charge),  muon->phi() , weight );
     }
   }
 }
 
+/// histogram filling with fwlite or full framework
+void
+MuonCrossSection::fill(const edm::View<reco::Candidate>& recs, const edm::View<reco::GenParticle>& gens, const double& weight)
+{
+  for(edm::View<reco::GenParticle>::const_iterator gen=gens.begin(); gen!=gens.end(); ++gen){
+    for(edm::View<reco::Candidate>::const_iterator rec=recs.begin(); rec!=recs.end(); ++rec){
+      // transverse momentum of the muon
+      hists2D_.find("pt_"  )->second->Fill( gen->pt () ,  rec->pt () , weight );
+      // pseudorapidity of the muon
+      hists2D_.find("eta_" )->second->Fill( gen->eta() ,  rec->eta() , weight );
+      // azimuth angle of the muon
+      hists2D_.find("phi_" )->second->Fill( gen->phi(),   rec->phi() , weight );
+    }
+  }
+  // fill 1-dimensional histograms from rec input
+  fill(recs, weight);
+}
