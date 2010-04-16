9c1d02925171d0de6097bd1d92a7c7c0ed478f7a
rwolf <>
2010-04-16 19:16:42 +0000

    added more comments + a set of histogram fill functions for more comfortable and safer histogram filling

--- TopAnalyzer/interface/SingleObject.h
+++ TopAnalyzer/interface/SingleObject.h
@@ -22,10 +22,21 @@
 
    \brief   Interface class to analyze single objects within full framework of fwlite
 
-   The class is an interface to a common analyer or a corresponding fwlite mimic
+   Ths class is an interface to a common analyer or a corresponding fwlite mimic
    in TopAnalysis/TopAnalzyer/bin. It takes a single object class as argument. 
    An interface to a common book, fill & process function is expected by the common 
-   analyzer (mimic) class. Each derived class has to provide these.
+   analyzer (mimic) class. Each derived class has to provide these functions. It 
+   provides containers for one and 2-dimensional histogram management and corres-
+   ponding fill functions. The fill functions are of type:
+
+   fill("name", valueA, weight)         (to fill 1-dimensional histograms)
+   fill("name", valueA, valueB, weight) (to fill 2-dimensional histograms)
+
+   where _name_ is expectec to be to be the name of the histogram under which it
+   has been booked for module internal histogram management (i.e. the std::string
+   in the map container), _valueA_ and _valueB_ are the values of the quantities 
+   to be filled (e.g. muon->pt()) and weight is the weight with which the histo-
+   grams is to be filled.
 */
 
 template <typename Collection> 
@@ -53,6 +64,14 @@ class SingleObject{
   virtual void fill(const Collection& inputCollection, const double& weight=1.) = 0;
   /// everything which needs to be done after the event loop
   virtual void process() = 0;
+
+ protected:
+  /// check if histogram was booked in the corresponding map
+  bool booked(std::map<std::string, TH1*> map, const std::string histName) const { return map.find(histName.c_str())!=map.end(); };
+  /// fill 1-dimensional histogram if it had been booked before
+  void fill(const std::string histName, double valueA, double weight) const { if(booked(hists_, histName)) hists_.find(histName)->second->Fill(valueA, weight); };
+  /// fill 2-dimensional histogram if it had been booked before
+  void fill(const std::string histName, double valueA, double valueB, double weight) const { if(booked(hists2D_, histName)) hists2D_.find(histName)->second->Fill(valueA, valueB, weight); };
 
  protected:
   /// histogram container
