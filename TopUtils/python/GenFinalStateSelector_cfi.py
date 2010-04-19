861b60071aa33626fdcb9f1a31c7f1d2ccc659c0
aryan <>
2010-04-19 16:31:16 +0000

    moved from decaySubset to genParticles -- ATTENTION the decaySubset has problems with tau decays; this needs to be fixed in later versions of TQAF

--- TopUtils/python/GenFinalStateSelector_cfi.py
+++ TopUtils/python/GenFinalStateSelector_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 genFinalStateSelector = cms.EDFilter("GenFinalStateSelector",
   ## input collection for the selector
-  src = cms.InputTag("decaySubset"),
+  src = cms.InputTag("genParticles"),
   ## number of expected electrons in the final state
   elecs = cms.int32(1),
   ## number of expected muons in the final state
