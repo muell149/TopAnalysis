f9029c2a8cee61a638f5b0e1f5976368f49e350b
henderle <>
2010-04-21 11:55:29 +0000

    run on rereco, add pfMET, add genJetCollection, embed muonIsoDeposits, embed pfCandidates and caloTowers

--- Configuration/analysis/semiLeptonic/diffXSection/patTuple_production_cfg.py
+++ Configuration/analysis/semiLeptonic/diffXSection/patTuple_production_cfg.py
@@ -7,26 +7,41 @@ from PhysicsTools.PatAlgos.tools.coreTools import *
 ## Remove Photons and Taus from the Event
 removeSpecificPATObjects(process, ['Photons','Taus'])
 
-from PhysicsTools.PatAlgos.tools.cmsswVersionTools import *
-## Run 3_3_x version on 3_1_x-samples
-run33xOn31xMC(process,
-               jetSrc = cms.InputTag("antikt5CaloJets"),
-               jetIdTag = "antikt5"
-              )
+from PhysicsTools.PatAlgos.tools.metTools import *
+## Add PfMET to the event content
+addPfMET(process, 'PF')
+
+from PhysicsTools.PatAlgos.tools.cmsswVersionTools import run33xOnReRecoMC
+run33xOnReRecoMC(process)
 
 ## Add particle flow jets
 from PhysicsTools.PatAlgos.tools.jetTools import *
 #addJetID(process, "antikt5PFJets", "pf")
-addJetCollection(process,cms.InputTag('antikt5PFJets'),'PF',
-              doJTA        = True,
-              doBTagging   = True,
-              jetCorrLabel = ('AK5', 'PF'),
-              doType1MET   = False,
-              doL1Cleaning = False,
-              doL1Counters = False,
-              genJetCollection=cms.InputTag("antikt5GenJets"),
-              doJetID      = False,
-              ) 
+addJetCollection(process,cms.InputTag('ak5PFJets'),'AK5','PF',
+                 doJTA        = True,
+                 doBTagging   = True,
+                 jetCorrLabel = ('AK5', 'PF'),
+                 doType1MET   = False,
+                 doL1Cleaning = False,
+                 doL1Counters = False,
+                 genJetCollection=cms.InputTag('ak5GenJets'),
+                 doJetID      = True,
+                 ) 
+
+# embed IsoDeposits
+process.patMuons.isoDeposits = cms.PSet(
+    tracker = cms.InputTag("muIsoDepositTk"),
+    ecal    = cms.InputTag("muIsoDepositCalByAssociatorTowers","ecal"),
+    hcal    = cms.InputTag("muIsoDepositCalByAssociatorTowers","hcal"),
+    user    = cms.VInputTag(
+                            cms.InputTag("muIsoDepositCalByAssociatorTowers","ho"),
+                            cms.InputTag("muIsoDepositJets")
+                            ),
+    )
+
+## embedding of jet constituents into the jets
+process.patJets.embedCaloTowers = True
+process.patJetsAK5PF.embedPFCandidates = True
 
 ## Check the Event Content
 process.content = cms.EDAnalyzer("EventContentAnalyzer")
@@ -37,23 +52,24 @@ process.p = cms.Path(
 
 ## Special Replacements
 
+process.patJetCorrFactors.corrSample = 'Summer09_7TeV_ReReco332'
 ## sample type used for flavour dependend jet corrections
 process.patJetCorrFactors.sampleType = 'ttbar'
-process.patJetCorrFactorsPF.sampleType = 'ttbar'
+process.patJetCorrFactorsAK5PF.sampleType = 'ttbar'
 
 ## Define Event Contet
 from PhysicsTools.PatAlgos.patEventContent_cff import *
 process.out.fileName = cms.untracked.string('PATtuple.root')
 process.out.outputCommands = patExtraAodEventContent
 process.out.outputCommands+= patEventContentNoCleaning
-process.out.outputCommands+= ['keep *_selectedPatJets*_*_*', 'keep *_patMETs*_*_*']
+process.out.outputCommands+= ['drop *_towerMaker_*_*', 'keep *_ak5GenJets_*_*']
 
 
 ## Maximal Number of Events
-process.maxEvents.input    = 100
+process.maxEvents.input    = 1000
 
 ## Input Files for Testing
-process.source.fileNames   = ['/store/mc/Summer09/ZmumuJet_Pt50to80/GEN-SIM-RECO/MC_31X_V3-v1/0034/A02B9E45-F18D-DE11-ADC7-00E0812DB027.root']
+process.source.fileNames   = ['/store/mc/Spring10/TTbarJets-madgraph/AODSIM/START3X_V26_S09-v1/0016/2CB76F28-9D47-DF11-959F-003048C693E4.root']
 
 ## Options and Output Report
 process.options.wantSummary = False
