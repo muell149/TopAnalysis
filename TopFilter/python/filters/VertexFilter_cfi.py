c1b560b5fea766a2b47a2f9d00679b9043ddfc50
dammann <>
2010-04-28 11:39:12 +0000

    RIP

--- TopFilter/python/filters/VertexFilter_cfi.py
+++
@@ -1,8 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-filterOnPrimaryVertex = cms.EDAnalyzer("VertexFilter",
-
-    vertices      = cms.InputTag('offlinePrimaryVertices'),
-    position_cuts = cms.vdouble(1.,1.,20.),
-    n_tracks      = cms.uint32(2)
-)
