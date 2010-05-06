9d201b6d328fc434cf6ca0d96fd5d9473dc94691
mgoerner <>
2010-05-06 08:27:09 +0000

    take in account also JES effect on MET

--- Configuration/analysis/semiLeptonic/diffXSection/analyzeMuonJES09BG_cfg.py
+++ Configuration/analysis/semiLeptonic/diffXSection/analyzeMuonJES09BG_cfg.py
@@ -6,10 +6,19 @@
 ## get the mother file
 execfile("/afs/naf.desy.de/user/g/goerner/semileptonic356/analyzeMuonDiffXSecBG_cfg.py")
 
+## change input collections to JES-shifted collections
+from PhysicsTools.PatAlgos.tools.helpers import massSearchReplaceAnyInputTag
+massSearchReplaceAnyInputTag(process.p1, 'selectedPatJets', 'scaledJetEnergy:selectedPatJets') 
+massSearchReplaceAnyInputTag(process.p2, 'selectedPatJets', 'scaledJetEnergy:selectedPatJets')
+massSearchReplaceAnyInputTag(process.p1, 'patMETs'        , 'scaledJetEnergy:patMETs') 
+massSearchReplaceAnyInputTag(process.p2, 'patMETs'        , 'scaledJetEnergy:patMETs')
+
 ## get JES-shifting module
 process.load("TopAnalysis.TopUtils.JetEnergyScale_cfi")
 # set input collection- needed while running on pat tuples
 process.scaledJetEnergy.inputJets = "selectedPatJets"
+process.scaledJetEnergy.inputMETs = "patMETs"
+
 # JES -10%
 process.scaledJetEnergy.scaleFactor = 0.9
 
@@ -20,18 +29,6 @@ process.p1.replace(process.semiLeptonicSelection,
 ## change monitoring to shifted collection
 process.monitorCuts+= process.shiftedJets
 
-## change input collection to JES-shifted collection 
-process.centralJets.src =  'scaledJetEnergy:selectedPatJets'
-process.reliableJets.src = 'scaledJetEnergy:selectedPatJets'
-process.goodJets.src =     'scaledJetEnergy:selectedPatJets'
-
 ## change output name 
 process.TFileService.fileName = 'analyzeDiffXSecJES09_testBkg.root'
 
-# replace label names when running on old (3_4_X) MC samples
-inputs = ['Electrons','Jets','Taus','Muons','Photons']
-from PhysicsTools.PatAlgos.tools.helpers import massSearchReplaceAnyInputTag
-for input in inputs:
-    massSearchReplaceAnyInputTag(process.p1, 'patMETs', 'layer1METs')    
-    massSearchReplaceAnyInputTag(process.p1, 'selectedPat'+input, 'selectedLayer1'+input)
-    massSearchReplaceAnyInputTag(process.p1, 'scaledJetEnergy:selectedPatJets', 'scaledJetEnergy:selectedLayer1Jets')
