1ba21f4f1f8314b9d7e9a690081fbb2d5eb56463
snaumann <>
2010-05-12 09:56:27 +0000

    - removed SiSCone - make executables if they were not already created

--- Configuration/analysis/semiLeptonic/jetEnergy/createAllPlots.sh
+++ Configuration/analysis/semiLeptonic/jetEnergy/createAllPlots.sh
@@ -1,22 +1,35 @@
 #!/bin/sh
 
-anaCor=TopAnalysis/Configuration/analysis/semiLeptonic/jetEnergy/analyzeCorrections
-anaRes=TopAnalysis/Configuration/analysis/semiLeptonic/jetEnergy/analyzeResolutionBias
-
+codeDir=TopAnalysis/Configuration/analysis/semiLeptonic/jetEnergy
 targetDir=~/latex/notes/JECTopValidation
+
+anaCor=$codeDir/analyzeCorrections
+anaRes=$codeDir/analyzeResolutionBias
+
+if test ! -x $anaCor
+    then
+    cd $codeDir
+    make corrections
+    cd -
+fi
 
 $anaCor "analyzeJetEnergyCorrections_ak5.root"         "GenMatch"
 $anaCor "analyzeJetEnergyCorrections_ak5_mcatnlo.root" "GenMatch"
 $anaCor "analyzeJetEnergyCorrections_ic5.root"         "GenMatch"
 $anaCor "analyzeJetEnergyCorrections_kt4.root"         "GenMatch"
-$anaCor "analyzeJetEnergyCorrections_sc5.root"         "GenMatch"
 
 cp -r analyzeJetEnergyCorrections_ak5/         $targetDir
 cp -r analyzeJetEnergyCorrections_ak5_mcatnlo/ $targetDir
 cp -r analyzeJetEnergyCorrections_ic5/         $targetDir
 cp -r analyzeJetEnergyCorrections_kt4/         $targetDir
-cp -r analyzeJetEnergyCorrections_sc5/         $targetDir
 
-#$anaRes "analyzeJetEnergyResolutionBias.root" "analyzeJetEnergyResolutionBias_mcatnlo.root"
-#
-#cp -r analyzeJetEnergyResolutionBias $targetDir
+if test ! -x $anaRes
+    then
+    cd $codeDir
+    make resolution
+    cd -
+fi
+
+$anaRes "analyzeJetEnergyResolutionBias.root" "analyzeJetEnergyResolutionBias_mcatnlo.root"
+
+cp -r analyzeJetEnergyResolutionBias $targetDir
