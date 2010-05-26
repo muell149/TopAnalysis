86d014f9ff320c8b59ff2c04054d1e976fafa403
mgoerner <>
2010-05-26 11:13:29 +0000

    extended gen lepton colllections to be more flexible

--- TopFilter/python/sequences/genSelection_cff.py
+++ TopFilter/python/sequences/genSelection_cff.py
@@ -13,36 +13,55 @@ from PhysicsTools.PatAlgos.selectionLayer1.jetCountFilter_cfi import *
 ## muon count filter
 from PhysicsTools.PatAlgos.selectionLayer1.muonCountFilter_cfi import *
 
+
 ## set up simple (gen) lepton collections coming directly from W without cuts
-simpleGenWMuons = isolatedGenMuons.clone(src="genParticles",
-                                     target = cms.PSet(pdgId  = cms.vstring("13"),
-                                                       status = cms.int32( 3)    ),
-                                   ancestor = cms.PSet(pdgId = cms.vstring("24") ) )
+simpleGenWMuons     = isolatedGenMuons.clone(src="genParticles",
+                                 target = cms.PSet(pdgId  = cms.vstring("13"), status = cms.int32( 3)),
+                                 ancestor = cms.PSet(pdgId = cms.vstring("24") )                      )
 
 simpleGenWElectrons = isolatedGenMuons.clone(src="genParticles",
-                                         target = cms.PSet(pdgId  = cms.vstring("11"),
-                                                           status = cms.int32( 3)    ),
-                                       ancestor = cms.PSet(pdgId = cms.vstring("24") ) )
-simpleGenWTaus = isolatedGenMuons.clone(src="genParticles",
-                                    target = cms.PSet(pdgId  = cms.vstring("15"),
-                                                      status = cms.int32( 3)    ),
-                                  ancestor = cms.PSet(pdgId = cms.vstring("24") ) )
+                                 target = cms.PSet(pdgId  = cms.vstring("11"), status = cms.int32( 3)),
+                                 ancestor = cms.PSet(pdgId = cms.vstring("24") )                      )
+
+simpleGenWTaus      = isolatedGenMuons.clone(src="genParticles",
+                                 target = cms.PSet(pdgId  = cms.vstring("15"), status = cms.int32( 3)),
+                                 ancestor = cms.PSet(pdgId = cms.vstring("24") )                      )
 
 
+## introduce kinematic range for collections with lepton from W
+selectedGenWMuons     = selectedGenParticles.clone(src = 'simpleGenWMuons',
+                                                   cut = 'abs(eta) < 2.1 & pt > 20.')
+
+selectedGenWElectrons = selectedGenParticles.clone(src = 'simpleGenWElectrons',
+                                                   cut = 'abs(eta) < 2.1 & pt > 20.')
+
+selectedGenWTaus      = selectedGenParticles.clone(src = 'simpleGenWTaus',
+                                                   cut = 'abs(eta) < 2.1 & pt > 20.')
 
 ## configure jet and Muon selections for cutflow
-selectedGenJetCollection = selectedGenJets.clone(src = 'ak5GenJets',
-                                                 cut = 'abs(eta) < 2.4 & pt > 30.'
-                                                 )
+selectedGenJetCollection  = selectedGenJets.clone(src = 'ak5GenJets',
+                                                  cut = 'abs(eta) < 2.4 & pt > 30.')
 
 selectedGenMuonCollection = selectedGenParticles.clone(src = 'isolatedGenMuons',
-                                                       cut = 'abs(eta) < 2.1 & pt > 20.'
-                                                       )
-
+                                                       cut = 'abs(eta) < 2.1 & pt > 20.')
 
 semiLeptGenCollections = cms.Sequence(selectedGenMuonCollection  *
                                       selectedGenJetCollection
                                       )
+
+## inroduce some distributions to investigate genjet-cut effects
+status1GenWMuons    = isolatedGenMuons.clone(src="genParticles",
+                                 target = cms.PSet(pdgId  = cms.vstring("13"), status = cms.int32( 1)),
+                                 ancestor = cms.PSet(pdgId = cms.vstring("24") )                      )
+
+centralStatus3WMuons = selectedGenParticles.clone(src = 'simpleGenWMuons',
+                                                  cut = 'abs(eta) < 2.1'  )
+
+centralStatus1WMuons = selectedGenParticles.clone(src = 'status1GenWMuons',
+                                                  cut = 'abs(eta) < 2.1'  )
+
+centralStatus1WTauMuons = selectedGenParticles.clone(src = 'isolatedGenMuons',
+                                                     cut = 'abs(eta) < 2.1'  )
 
 ## set up the muon selection
 genMuonSelection  = countPatMuons.clone(src = 'selectedGenMuonCollection',
@@ -56,11 +75,20 @@ leadingGenJetSelection = countPatJets.clone(src = 'selectedGenJetCollection',
                                             )
 
 ## collect different steps within sequences
-introduceGenWCollections = cms.Sequence(simpleGenWMuons     *
-                                        simpleGenWElectrons *
-                                        simpleGenWTaus
-                                        )
+introduceGenWCollections = cms.Sequence(simpleGenWMuons       *
+                                        simpleGenWElectrons   *
+                                        simpleGenWTaus        *
+                                        selectedGenWMuons     *
+                                        selectedGenWElectrons *
+                                        selectedGenWTaus      )
+
 fullGenSelection = cms.Sequence(isolatedGenMuons*
                                 genMuonSelection*
                                 leadingGenJetSelection                                
                                 )
+
+introduceTestMuCollections = cms.Sequence(status1GenWMuons*
+                                          centralStatus3WMuons*
+                                          centralStatus1WMuons*
+                                          centralStatus1WTauMuons
+                                          )
