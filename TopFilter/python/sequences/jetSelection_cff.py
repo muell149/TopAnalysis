cbb5eee8f4ba4ac4c746d1935895a3c971bdaee1
eschliec <>
2010-05-28 13:21:57 +0000

    edited selectGoodJets to really select goodJets; changed to official bDiscriminators from October09Exercise where available; cosmetics

--- TopFilter/python/sequences/jetSelection_cff.py
+++ TopFilter/python/sequences/jetSelection_cff.py
@@ -9,44 +9,45 @@ from PhysicsTools.PatAlgos.selectionLayer1.jetSelector_cfi import *
 
 ## getting started
 centralJets  = selectedPatJets.clone(src = 'selectedPatJets',
-                                        cut = 'abs(eta) < 2.4'
-                                        )
+                                     cut = 'abs(eta) < 2.4'
+                                     )
 reliableJets = selectedPatJets.clone(src = 'selectedPatJets',
-                                        cut = 'abs(eta) < 2.4 & pt > 30.'
-                                        )
+                                     cut = 'abs(eta) < 2.4 & pt > 30.'
+                                     )
 goodJets     = selectedPatJets.clone(src = 'selectedPatJets',
-                                        cut = 'abs(eta) < 2.4 & pt > 30. &'
-                                              '0.05 < emEnergyFraction   & '
-                                              '0.95 > emEnergyFraction'
-                                        )
+                                     cut = 'abs(eta) < 2.4 & pt > 30. &'
+                                           '0.05 < emEnergyFraction   & '
+                                           '0.95 > emEnergyFraction'
+                                     )
 
 ## a kinematically well defined jet with
 ## reliable calibration and a robust rej
 ## of photons, electrons and prompt pi0's
-selectGoodJets = cms.Sequence(centralJets *
-                              reliableJets
+selectGoodJets = cms.Sequence(centralJets  *
+                              reliableJets *
+                              goodJets
                               )
 
                                
 ## check for different btag properties
 trackCountingHighPurBJets     = selectedPatJets.clone(src = 'goodJets',
-                                                         cut = 'bDiscriminator(\"trackCountingHighPurBJetTags\") > 3.0'
-                                                         )
+                                                      cut = 'bDiscriminator(\"trackCountingHighPurBJetTags\") > 2.17'
+                                                      )
 trackCountingHighEffBJets     = selectedPatJets.clone(src = 'goodJets',
-                                                         cut = 'bDiscriminator(\"trackCountingHighEffBJetTags\") > 3.0'
-                                                         )
+                                                      cut = 'bDiscriminator(\"trackCountingHighEffBJetTags\") > 1.9'
+                                                      )
 simpleSecondaryVertexBJets    = selectedPatJets.clone(src = 'goodJets',
-                                                         cut = 'bDiscriminator(\"simpleSecondaryVertexBJetTags\") > 3.0'
-                                                         )
+                                                      cut = 'bDiscriminator(\"simpleSecondaryVertexBJetTags\") > 2.02'
+                                                      )
 simpleSecondaryVertexNegBJets = selectedPatJets.clone(src = 'goodJets',
-                                                         cut = 'bDiscriminator(\"simpleSecondaryVertexNegativeBJetTags\") > 3.0'
-                                                         )
+                                                      cut = 'bDiscriminator(\"simpleSecondaryVertexNegativeBJetTags\") > 3.0'
+                                                      )
 combinedSecondaryVertexBJets  = selectedPatJets.clone(src = 'goodJets',
-                                                         cut = 'bDiscriminator(\"combinedSecondaryVertexBJetTags\") > 0.9'
-                                                         )
+                                                      cut = 'bDiscriminator(\"combinedSecondaryVertexBJetTags\") > 0.9'
+                                                      )
 softMuonBJets                 = selectedPatJets.clone(src = 'goodJets',
-                                                         cut = 'bDiscriminator(\"softMuonBJetTags\") > 0.3'
-                                                         )
+                                                      cut = 'bDiscriminator(\"softMuonBJetTags\") > 0.3'
+                                                      )
 
 ## a goodJet fullfilling different btag
 ## criteria
