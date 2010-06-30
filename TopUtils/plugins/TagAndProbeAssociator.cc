fd6f7af0e5725c861e4221ac82ab872027ae6efd
rwolf <>
2010-06-30 17:02:05 +0000

    removed this ansatz. Nice idea but it will never be fixed by edm

--- TopUtils/plugins/TagAndProbeAssociator.cc
+++
@@ -1,8 +0,0 @@
-#include "DataFormats/Candidate/interface/Candidate.h"
-#include "CommonTools/UtilAlgos/interface/PhysObjectMatcher.h"
-#include "CommonTools/UtilAlgos/interface/DummyMatchSelector.h"
-
-typedef reco::PhysObjectMatcher<edm::View<reco::Candidate>, edm::View<reco::Candidate>, reco::DummyMatchSelector<reco::Candidate, reco::Candidate> > TagAndProbeAssociator;
-
-#include "FWCore/Framework/interface/MakerMacros.h"
-DEFINE_FWK_MODULE( TagAndProbeAssociator );
