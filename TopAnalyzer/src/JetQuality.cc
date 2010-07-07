6df76588d85a227262299144f0ed43d0f0ec6d49
eschliec <>
2010-07-07 14:22:00 +0000

    included PFJetID variables, added more btaggers, corrected calculation of n60 and n90

--- TopAnalyzer/src/JetQuality.cc
+++ TopAnalyzer/src/JetQuality.cc
@@ -17,32 +17,69 @@ void
 JetQuality::book()
 {
   /** 
-      Selection Variables
-  **/
-  // electromagnetic fraction of jet energy
-  hists_["emf"                  ] = new TH1F( "emf"                  ,  "emf"                  ,   40,   0.,    1. );
-  // btag track counting high purity
-  hists_["btagTrkCntHighPurity" ] = new TH1F( "btagTrkCntHighPurity" ,  "btagTrkCntHighPurity" ,   80,   0.,   20. );
-
-  /** 
-      Monitoring Variables
+      Calo and PF JetID Variables
   **/
   // number of jet constituents
-  hists_["n_"                   ] = new TH1F( "n_"                     ,  "n_"                   ,   50,   0.,  50. );
+  hists_["n_"] = new TH1F( "n_" , "n_"  , 50, 0., 50. );
+
+  /** 
+      Other Calo and PF Jet Variables
+  **/
   // number of jet constituents carrying 90% of the jet energy
-  hists_["n90_"                 ] = new TH1F( "n90_"                   ,  "n90_"                 ,   50,   0.,   1. );
+  hists_["n90_"] = new TH1F( "n90_" , "n90_" , 50, 0., 50. );
   // number of jet constituents carrying 60% of the jet energy
-  hists_["n60_"                 ] = new TH1F( "n60_"                   ,  "n60_"                 ,   50,   0.,   1. );
+  hists_["n60_"] = new TH1F( "n60_" , "n60_" , 50, 0., 50. );
+
+  /** 
+      Calo JetID Variables
+  **/
+  // electromagnetic fraction of jet energy
+  hists_["emf"     ] = new TH1F( "emf"      , "emf"      ,  44, 0.,   1.1 );
   // number of rec hits carrying 90% of the jet energy
-  hists_["n90Hits_"             ] = new TH1F( "n90Hits_"               ,  "n90Hits_"             ,   50,   0.,  50. );
+  hists_["n90Hits_"] = new TH1F( "n90Hits_" , "n90Hits_" , 100, 0., 100.  );
   // fraction of jet energy comming from the leading hybrid photo diode within the jet
-  hists_["fHPD_"                ] = new TH1F( "fHPD_"                  ,  "fHPD_"                ,   50,   0.,   1. );
+  hists_["fHPD_"   ] = new TH1F( "fHPD_"    , "fHPD_"    ,  50, 0.,   1.  );
+
+  /** 
+      PF JetID Variables
+  **/
+  // fraction of energy carried by neutral hadrons
+  hists_["nhf"] = new TH1F( "nhf" , "nhf" , 40, 0.,  1. );
+  // fraction of energy carried by neutral em (photons)
+  hists_["nef"] = new TH1F( "nef" , "nef" , 40, 0.,  1. );
+  // fraction of energy carried by charged hadrons
+  hists_["chf"] = new TH1F( "chf" , "chf" , 40, 0.,  1. );
+  // fraction of energy carried by charged em (electrons)
+  hists_["cef"] = new TH1F( "cef" , "cef" , 40, 0.,  1. );
+  // multiplicity of charged hadrons
+  hists_["nch"] = new TH1F( "nch" , "nch" , 50, 0., 50. );
+
+  /** 
+      B Tags for Jets
+  **/
+  // btag track counting high purity
+  hists_["btagTrkCntHighPurity" ] = new TH1F( "btagTrkCntHighPurity" ,  "btagTrkCntHighPurity" ,   80,     0.,   20. );
   // btag track counting high efficiency
-  hists_["btagTrkCntHighEff_"   ] = new TH1F( "btagTrkCntHighEff_"     ,  "btagTrkCntHighEff_"   ,   80,   0.,  20. ); 
+  hists_["btagTrkCntHighEff_"   ] = new TH1F( "btagTrkCntHighEff_"   ,  "btagTrkCntHighEff_"   ,   80,     0.,   20. ); 
   // btag simple secondary vertex
-  hists_["btagSimpleSecVtx_"    ] = new TH1F( "btagSimpleSecVtx_"      ,  "btagSimpleSecVtx_"    ,   80,  -5.,  15. ); 
+  hists_["btagSimpleSecVtx_"    ] = new TH1F( "btagSimpleSecVtx_"    ,  "btagSimpleSecVtx_"    ,   80,    -5.,   15. ); 
   // btag simple secondary vertex (neg bjet tags)
-  hists_["btagSimpleSecVtxNeg_" ] = new TH1F( "btagSimpleSecVtxNeg_"   ,  "btagSimpleSecVtxNeg_" ,   80, -20.,   0. ); 
+  hists_["btagSimpleSecVtxNeg_" ] = new TH1F( "btagSimpleSecVtxNeg_" ,  "btagSimpleSecVtxNeg_" ,   80, -1005., -985. ); 
+  // btag combined secondary vertex
+  hists_["btagCombSecVtx_"      ] = new TH1F( "btagCombSecVtx_"      , "btagCombSecVtx_"       ,   80,    -2.,    2. );
+  // btag combined secondary vertex using MVA
+  hists_["btagCombSecVtxMVA_"   ] = new TH1F( "btagCombSecVtxMVA_"   , "btagCombSecVtxMVA_"    ,   80,    -2.,    2. );
+  // btag b jet b probability (newer algo than the following)
+  hists_["btagJetBProbability_" ] = new TH1F( "btagJetBProbability_" , "btagJetBProbability_"  ,   80,     0.,   11. );
+  // btag b jet probability
+  hists_["btagJetProbability_"  ] = new TH1F( "btagJetProbability_"  , "btagJetProbability_"   ,   80,     0.,    4. );
+  // btag soft electron taggers (not implemented for PFJets)
+  hists_["btagSoftElecByPt_"    ] = new TH1F( "btagSoftElecByPt_"    , "btagSoftElecByPt_"     ,   80,     0.,   20. );
+  hists_["btagSoftElecByIP3d_"  ] = new TH1F( "btagSoftElecByIP3d_"  , "btagSoftElecByIP3d_"   ,   80,   -40.,  110. );
+  // btag soft muon taggers
+  hists_["btagSoftMuon_"        ] = new TH1F( "btagSoftMuon_"        , "btagSoftMuon_"         ,   10,     0.,    1. );
+  hists_["btagSoftMuonByPt_"    ] = new TH1F( "btagSoftMuonByPt_"    , "btagSoftMuonByPt_"     ,   80,     0.,   20. );
+  hists_["btagSoftMuonByIP3d_"  ] = new TH1F( "btagSoftMuonByIP3d_"  , "btagSoftMuonByIP3d_"   ,  160,  -100.,  100. );
 
   /** 
       JEC Monitoring Variables
@@ -75,32 +112,69 @@ void
 JetQuality::book(edm::Service<TFileService>& fs)
 {
   /** 
-      Selection Variables
-  **/
-  // electromagnetic fraction of jet energy
-  hists_["emf"                  ] = fs->make<TH1F>( "emf"                  ,  "emf"                  ,   40,   0.,    1. );
-  // btag track counting high purity
-  hists_["btagTrkCntHighPurity" ] = fs->make<TH1F>( "btagTrkCntHighPurity" ,  "btagTrkCntHighPurity" ,   80,   0.,   20. );
-
-  /** 
-      Monitoring Variables
+      Calo and PF JetID Variables
   **/
   // number of jet constituents
-  hists_["n_"                   ] = fs->make<TH1F>( "n_"                     ,  "n_"                   ,   50,   0.,  50. );
+  hists_["n_"] = fs->make<TH1F>( "n_" , "n_"  , 50, 0., 50. );
+
+  /** 
+      Other Calo and PF Jet Variables
+  **/
   // number of jet constituents carrying 90% of the jet energy
-  hists_["n90_"                 ] = fs->make<TH1F>( "n90_"                   ,  "n90_"                 ,   50,   0.,   1. );
+  hists_["n90_"] = fs->make<TH1F>( "n90_" , "n90_" , 50, 0., 50. );
   // number of jet constituents carrying 60% of the jet energy
-  hists_["n60_"                 ] = fs->make<TH1F>( "n60_"                   ,  "n60_"                 ,   50,   0.,   1. );
+  hists_["n60_"] = fs->make<TH1F>( "n60_" , "n60_" , 50, 0., 50. );
+
+  /** 
+      Calo JetID Variables
+  **/
+  // electromagnetic fraction of jet energy
+  hists_["emf"     ] = fs->make<TH1F>( "emf"      , "emf"      ,  44, 0.,   1.1 );
   // number of rec hits carrying 90% of the jet energy
-  hists_["n90Hits_"             ] = fs->make<TH1F>( "n90Hits_"               ,  "n90Hits_"             ,   50,   0.,  50. );
+  hists_["n90Hits_"] = fs->make<TH1F>( "n90Hits_" , "n90Hits_" , 100, 0., 100.  );
   // fraction of jet energy comming from the leading hybrid photo diode within the jet
-  hists_["fHPD_"                ] = fs->make<TH1F>( "fHPD_"                  ,  "fHPD_"                ,   50,   0.,   1. );
+  hists_["fHPD_"   ] = fs->make<TH1F>( "fHPD_"    , "fHPD_"    ,  50, 0.,   1.  );
+
+  /** 
+      PF JetID Variables
+  **/
+  // fraction of energy carried by neutral hadrons
+  hists_["nhf"] = fs->make<TH1F>( "nhf" , "nhf" , 40, 0.,  1. );
+  // fraction of energy carried by neutral em (photons)
+  hists_["nef"] = fs->make<TH1F>( "nef" , "nef" , 40, 0.,  1. );
+  // fraction of energy carried by charged hadrons
+  hists_["chf"] = fs->make<TH1F>( "chf" , "chf" , 40, 0.,  1. );
+  // fraction of energy carried by charged em (electrons)
+  hists_["cef"] = fs->make<TH1F>( "cef" , "cef" , 40, 0.,  1. );
+  // multiplicity of charged hadrons
+  hists_["nch"] = fs->make<TH1F>( "nch" , "nch" , 50, 0., 50. );
+
+  /** 
+      B Tags for Jets
+  **/
+  // btag track counting high purity
+  hists_["btagTrkCntHighPurity" ] = fs->make<TH1F>( "btagTrkCntHighPurity" , "btagTrkCntHighPurity"  ,   80,     0.,   20. );
   // btag track counting high efficiency
-  hists_["btagTrkCntHighEff_"   ] = fs->make<TH1F>( "btagTrkCntHighEff_"     ,  "btagTrkCntHighEff_"   ,   80,   0.,  20. ); 
+  hists_["btagTrkCntHighEff_"   ] = fs->make<TH1F>( "btagTrkCntHighEff_"   , "btagTrkCntHighEff_"    ,   80,     0.,   20. ); 
   // btag simple secondary vertex
-  hists_["btagSimpleSecVtx_"    ] = fs->make<TH1F>( "btagSimpleSecVtx_"      ,  "btagSimpleSecVtx_"    ,   80,  -5.,  15. ); 
+  hists_["btagSimpleSecVtx_"    ] = fs->make<TH1F>( "btagSimpleSecVtx_"    , "btagSimpleSecVtx_"     ,   80,    -5.,   15. ); 
   // btag simple secondary vertex (neg bjet tags)
-  hists_["btagSimpleSecVtxNeg_" ] = fs->make<TH1F>( "btagSimpleSecVtxNeg_"   ,  "btagSimpleSecVtxNeg_" ,   80,-1005.,-985.); 
+  hists_["btagSimpleSecVtxNeg_" ] = fs->make<TH1F>( "btagSimpleSecVtxNeg_" , "btagSimpleSecVtxNeg_"  ,   80, -1005., -985. ); 
+  // btag combined secondary vertex
+  hists_["btagCombSecVtx_"      ] = fs->make<TH1F>( "btagCombSecVtx_"      , "btagCombSecVtx_"       ,   88,    -1.1,   1.1 );
+  // btag combined secondary vertex using MVA
+  hists_["btagCombSecVtxMVA_"   ] = fs->make<TH1F>( "btagCombSecVtxMVA_"   , "btagCombSecVtxMVA_"    ,   88,    -1.1,   1.1 );
+  // btag b jet b probability (newer algo than the following)
+  hists_["btagJetBProbability_" ] = fs->make<TH1F>( "btagJetBProbability_" , "btagJetBProbability_"  ,   80,     0.,   11. );
+  // btag b jet probability
+  hists_["btagJetProbability_"  ] = fs->make<TH1F>( "btagJetProbability_"  , "btagJetProbability_"   ,   80,     0.,    4. );
+  // btag soft electron taggers (not implemented for PFJets)
+  hists_["btagSoftElecByPt_"    ] = fs->make<TH1F>( "btagSoftElecByPt_"    , "btagSoftElecByPt_"     ,   80,     0.,   20. );
+  hists_["btagSoftElecByIP3d_"  ] = fs->make<TH1F>( "btagSoftElecByIP3d_"  , "btagSoftElecByIP3d_"   ,   80,   -40.,  110. );
+  // btag soft muon taggers
+  hists_["btagSoftMuon_"        ] = fs->make<TH1F>( "btagSoftMuon_"        , "btagSoftMuon_"         ,   50,     0.,    1. );
+  hists_["btagSoftMuonByPt_"    ] = fs->make<TH1F>( "btagSoftMuonByPt_"    , "btagSoftMuonByPt_"     ,   80,     0.,   20. );
+  hists_["btagSoftMuonByIP3d_"  ] = fs->make<TH1F>( "btagSoftMuonByIP3d_"  , "btagSoftMuonByIP3d_"   ,  160,  -100.,  100. );
 
   /** 
       JEC Monitoring Variables
@@ -136,36 +210,82 @@ JetQuality::fill(const edm::View<pat::Jet>& jets, const double& weight)
   for(edm::View<pat::Jet>::const_iterator jet=jets.begin(); jet!=jets.end(); ++jet, ++index){
     // NOTE: These histograms still need to be filled apropriately
     if( index_<0 || index_==index ){
-      /**
-	 Fill Selection Variables
-      **/
-      // electromagnetic fraction of jet energy
-      hists_.find( "emf"                 )->second->Fill( jet->emEnergyFraction() , weight );
-      // btag track counting high purity
-      hists_.find( "btagTrkCntHighPurity")->second->Fill( jet->bDiscriminator("trackCountingHighPurBJetTags") , weight );
-      
+
       /** 
-	  Fill Monitoring Variables
+	  Fill Calo and PF JetID Variables
       **/
+
       // number of jet constituents
       unsigned int n100 = 0;
       if( jet->isCaloJet() ) n100 = jet->getCaloConstituents().size();	
       else if( jet->isPFJet() ) n100 = jet->getPFConstituents().size();
       hists_.find( "n_"   )->second->Fill( n100 , weight );
+
+      /** 
+	  Fill Other Calo and PF Jet Variables
+      **/
+
       // number of jet constituents carrying 90% of the jet energy
-      hists_.find( "n90_" )->second->Fill( jet->n90()/n100 , weight );
+      hists_.find( "n90_" )->second->Fill( jet->n90() , weight );
       // number of jet constituents carrying 60% of the jet energy
-      hists_.find( "n60_" )->second->Fill( jet->n60()/n100 , weight );
-      // number of rec hits carrying 90% of the jet energy
-      hists_.find( "n90Hits_" )->second->Fill( jet->jetID().n90Hits , weight );
-      // fraction of jet energy comming from the leading hybrid photo diode within the jet
-      hists_.find( "fHPD_" )->second->Fill( jet->jetID().fHPD , weight );
+      hists_.find( "n60_" )->second->Fill( jet->n60() , weight );
+
+
+      /** 
+	  Fill Calo JetID Variables
+      **/
+      if( jet->isCaloJet() ){
+	// electromagnetic fraction of jet energy
+	hists_.find( "emf"      )->second->Fill( jet->emEnergyFraction() , weight );
+	// number of rec hits carrying 90% of the jet energy
+	hists_.find( "n90Hits_" )->second->Fill( jet->jetID().n90Hits    , weight );
+	// fraction of jet energy comming from the leading hybrid photo diode within the jet
+	hists_.find( "fHPD_"    )->second->Fill( jet->jetID().fHPD       , weight );
+      }
+      
+      /** 
+	  PF JetID Variables
+      **/
+      if( jet->isPFJet() ){
+	// fraction of energy carried by neutral hadrons
+	hists_.find( "nhf" )->second->Fill( jet->neutralHadronEnergyFraction()           , weight );
+	// fraction of energy carried by neutral em (photons)
+	hists_.find( "nef" )->second->Fill( jet->neutralEmEnergyFraction()               , weight );
+	// fraction of energy carried by charged hadrons
+	hists_.find( "chf" )->second->Fill( jet->chargedHadronEnergyFraction()           , weight );
+	// fraction of energy carried by charged em (electrons)
+	hists_.find( "cef" )->second->Fill( jet->chargedEmEnergyFraction()               , weight );
+	// multiplicity of charged hadrons
+	hists_.find( "nch" )->second->Fill( jet->pfSpecific().mChargedHadronMultiplicity , weight );
+      }
+
+      /** 
+	  Fill B Tags for Jets
+      **/
+      // btag track counting high purity
+      hists_.find( "btagTrkCntHighPurity" )->second->Fill( jet->bDiscriminator("trackCountingHighPurBJetTags")          , weight );
       // btag track counting high efficiency
-      hists_.find( "btagTrkCntHighEff_"  )->second->Fill( jet->bDiscriminator("trackCountingHighEffBJetTags") , weight );
+      hists_.find( "btagTrkCntHighEff_"   )->second->Fill( jet->bDiscriminator("trackCountingHighEffBJetTags")          , weight );
       // btag simple secondary vertex
-      hists_.find( "btagSimpleSecVtx_"   )->second->Fill( jet->bDiscriminator("simpleSecondaryVertexBJetTags") , weight );
+      hists_.find( "btagSimpleSecVtx_"    )->second->Fill( jet->bDiscriminator("simpleSecondaryVertexBJetTags")         , weight );
       // btag simple secondary vertex (neg bjet tags) makes no sense yet, value is -1000 for every jet regardless of the btagging
-      hists_.find( "btagSimpleSecVtxNeg_")->second->Fill( jet->bDiscriminator("simpleSecondaryVertexNegativeBJetTags") , weight );
+      hists_.find( "btagSimpleSecVtxNeg_" )->second->Fill( jet->bDiscriminator("simpleSecondaryVertexNegativeBJetTags") , weight );
+
+      // btag combined secondary vertex
+      hists_.find( "btagCombSecVtx_"      )->second->Fill( jet->bDiscriminator("combinedSecondaryVertexBJetTags")       , weight );
+      // btag combined secondary vertex using MVA
+      hists_.find( "btagCombSecVtxMVA_"   )->second->Fill( jet->bDiscriminator("combinedSecondaryVertexMVABJetTags")    , weight );
+      // btag b jet b probability (newer algo than the following)
+      hists_.find( "btagJetBProbability_" )->second->Fill( jet->bDiscriminator("jetBProbabilityBJetTags")               , weight );
+      // btag b jet probability
+      hists_.find( "btagJetProbability_"  )->second->Fill( jet->bDiscriminator("jetProbabilityBJetTags")                , weight );
+      // btag soft electron taggers (not implemented for PFJets)
+      hists_.find( "btagSoftElecByPt_"    )->second->Fill( jet->bDiscriminator("softElectronByPtBJetTags")              , weight );
+      hists_.find( "btagSoftElecByIP3d_"  )->second->Fill( jet->bDiscriminator("softElectronByIP3dBJetTags")            , weight );
+      // btag soft muon taggers
+      hists_.find( "btagSoftMuon_"        )->second->Fill( jet->bDiscriminator("softMuonBJetTags")                      , weight );
+      hists_.find( "btagSoftMuonByPt_"    )->second->Fill( jet->bDiscriminator("softMuonByPtBJetTags")                  , weight );
+      hists_.find( "btagSoftMuonByIP3d_"  )->second->Fill( jet->bDiscriminator("softMuonByIP3dBJetTags")                , weight );
 
       /** 
 	  Fill JEC Monitoring Variables
