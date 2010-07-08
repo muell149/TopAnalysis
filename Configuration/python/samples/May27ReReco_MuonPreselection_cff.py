d8ccd3de455d637533dc1bff5505238fb016e485
dammann <>
2010-07-08 13:17:31 +0000

    removed old commented files

--- Configuration/python/samples/May27ReReco_MuonPreselection_cff.py
+++ Configuration/python/samples/May27ReReco_MuonPreselection_cff.py
@@ -12,63 +12,18 @@ import FWCore.ParameterSet.Config as cms
 #  integrated HF lumi: 11.39/nb                                                                                                                  
 # integrated VTX lumi: 11.67/nb
 #
-#    number of events: 80076
+#    number of events: 89229
 #
 #        preselection: general qualtity cuts (see below)  AND 
 #                      ( 1 global muon with pt > 5GeV  OR  2 global muons )
 #
-#            cut flow:
-#                         before:                 after:
-#  physicsDeclared:       1661462                 1502658
-#  hltLevel1GTSeed:       1502658                 1224625
-#    primVtxFilter:       1224625                 1214552
-#  beamScrapFilter:       1214552                 1214552
-#  HBHENoiseFilter:       1214552                 1214335
+#            cut flow: not available yet
 #
-#      oneHardMuon:       1214335                   70756
-#   twoGlobalMuons:       1214335                   11430 
 #                                              
 ###########################################################################################
 
 source = cms.Source("PoolSource",
-    fileNames = cms.untracked.vstring(
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_1.root',		
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_2.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_3.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_4.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_5.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_6.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_7.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_8.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_9.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_10.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_11.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_12.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_13.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_14.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_15.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_16.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_17.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_18.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_19.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_20.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_21.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_22.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_23.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_24.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_25.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_26.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_27.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_28.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_29.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_30.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_31.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_32.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_33.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_34.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_35.root',
-#        '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2_36.root',
-	
+    fileNames = cms.untracked.vstring(	
         '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2b_10_1_ZGc.root',
         '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2b_11_1_0y0.root',
         '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2b_12_2_NAb.root',
@@ -106,11 +61,6 @@ source = cms.Source("PoolSource",
         '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2b_8_1_Air.root',
         '/store/user/dammann/36X/Collisions10/Commissioning10_May27thSkim_SD_Mu_v2b_9_1_q8w.root',
 	
-#        '/store/user/dammann/36X/Collisions10/Run2010A_May27thReReco_Mu_v2_1.root',		
-#        '/store/user/dammann/36X/Collisions10/Run2010A_May27thReReco_Mu_v2_2.root',
-#        '/store/user/dammann/36X/Collisions10/Run2010A_May27thReReco_Mu_v2_3.root',
-#        '/store/user/dammann/36X/Collisions10/Run2010A_May27thReReco_Mu_v2_4.root'	
-
         '/store/user/dammann/36X/Collisions10/Run2010A_May27thReReco_Mu_v2b_1_1_JBd.root',
         '/store/user/dammann/36X/Collisions10/Run2010A_May27thReReco_Mu_v2b_2_1_Mko.root',
         '/store/user/dammann/36X/Collisions10/Run2010A_May27thReReco_Mu_v2b_3_5_lTd.root',
