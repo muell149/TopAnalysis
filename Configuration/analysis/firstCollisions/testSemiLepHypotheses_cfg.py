c692df79c023d8b54a227d02abb69f2aa70c6c79
snaumann <>
2010-07-30 14:28:13 +0000

    change configuration to analyze first good lepton+jet candidates (4 events)

--- Configuration/analysis/firstCollisions/testSemiLepHypotheses_cfg.py
+++ Configuration/analysis/firstCollisions/testSemiLepHypotheses_cfg.py
@@ -18,9 +18,27 @@ process.MessageLogger.cerr.KinFitter = cms.untracked.PSet(
 ## define input
 process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(
-    'file://ExpressPhysics_Run2010A-Express-v2_FEVT_136100_922_273730582.root'
+    #####################
+    ## mu+jets candidates
+    #####################
+    ## Run 138746, Event 54892889, LumiSection 99 (only 3 jets)
+    ## Run 138750, Event 38790862, LumiSection 204 (only 3 jets)
+    #'file:PR.root'
+    ## Run 139971, Event 166160760, LumiSection 229 (only 3 jets)
+    ## Run 140158, Event 112262487, LumiSection 130
+    ## Run 140158, Event 260945301, LumiSection 297
+    #'file:PD.root'
+    ####################
+    ## e+jets candidates
+    ####################
+    ## Run 140059, Event 200726415, LumiSection 230 (only 3 jets when removing electron)
+    #'file:PickedEventsTagged_1_1_phF.root',
+    ## Run 140331, Event 440601613, LumiSection 524
+    #'file:PickedEventsTagged_1_1_AGK.root'
+    ## Run 140385, Event 90009543, LumiSection 101
+    'file:PickedEventsTagged_2_1_JNK.root'
      ),
-     skipEvents = cms.untracked.uint32(0)                            
+     skipEvents = cms.untracked.uint32(0)
 )
 
 ## define maximal number of events to loop over
@@ -39,58 +57,58 @@ process.load("Configuration.StandardSequences.MagneticField_cff")
 process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
 process.GlobalTag.globaltag = cms.string('START36_V4::All')
 
-## use jets and MET from particle flow (True or False)
-usePFlow = True
-
 ## std sequence for pat
 process.load("PhysicsTools.PatAlgos.patSequences_cff")
-
-process.patJetCorrFactors.corrSample = "Spring10"
-process.patJetCorrFactors.sampleType = "ttbar"
-
-if(usePFlow):
-    from PhysicsTools.PatAlgos.tools.jetTools import *
-    switchJetCollection(process,cms.InputTag('ak5PFJets'),
-                        doJTA        = True,
-                        doBTagging   = True,
-                        jetCorrLabel = ('AK5', 'PF'),
-                        doType1MET   = True,
-                        genJetCollection=cms.InputTag("ak5GenJets"),
-                        doJetID      = True
-                        )
-    ## setting the pt cut to 31 GeV was only a dirty workaround for this first e+jets event
-    process.selectedPatJets.cut = ('pt > 31. &'
-                                   'abs(eta) < 2.4 &'
-                                   'chargedHadronEnergyFraction > 0.0 &'
-                                   'neutralHadronEnergyFraction < 1.0 &'
-                                   'chargedEmEnergyFraction     < 1.0 &'
-                                   'neutralEmEnergyFraction     < 1.0 &'
-                                   'pfSpecific.mChargedHadronMultiplicity > 0 &'
-                                   'nConstituents > 0')
-    process.patMETs.metSource = "pfMet"
-    process.patMETs.addMuonCorrections = False
-else:
-    process.selectedPatJets.cut = ('pt > 30. &'
-                                   'abs(eta) < 2.4 &'
-                                   'emEnergyFraction > 0.01 &'
-                                   'jetID.n90Hits > 1 &'
-                                   'jetID.fHPD < 0.98 '
-                                   )
 
 from PhysicsTools.PatAlgos.tools.coreTools import *
 removeMCMatching(process, ['All'])
 
+process.patJetCorrFactors.corrSample = "Spring10"
+process.patJetCorrFactors.sampleType = "ttbar"
+
+process.selectedPatJets.cut = ('pt > 30. &'
+                               'abs(eta) < 2.4 &'
+                               'emEnergyFraction > 0.01 &'
+                               'jetID.n90Hits > 1 &'
+                               'jetID.fHPD < 0.98 '
+                               )
+
+process.cleanPatMuons.preselection = ('isGlobalMuon & isTrackerMuon &'
+                                      'pt > 20. &'
+                                      'abs(eta) < 2.1 &'
+                                      '(trackIso+caloIso)/pt < 0.1 &'
+                                      'innerTrack.numberOfValidHits > 10 &'
+                                      'globalTrack.normalizedChi2 < 10.0 &'
+                                      'globalTrack.hitPattern.numberOfValidMuonHits > 0 &'
+                                      'abs(dB) < 0.02'
+                                      )
+process.cleanPatMuons.checkOverlaps = cms.PSet(
+    jets = cms.PSet(src       = cms.InputTag("selectedPatJets"),
+                    algorithm = cms.string("byDeltaR"),
+                    preselection        = cms.string(""),
+                    deltaR              = cms.double(0.3),
+                    checkRecoComponents = cms.bool(False),
+                    pairCut             = cms.string(""),
+                    requireNoOverlaps   = cms.bool(True),
+                    )
+)
+
 process.cleanPatJets.checkOverlaps.tkIsoElectrons.requireNoOverlaps = True
 
-process.load("TopAnalysis.TopAnalyzer.JetKinematics_cfi")
-process.analyzeJetKinematics.src = "cleanPatJets"
+## dummy analyzers
+process.load("TopQuarkAnalysis.Examples.TopJetAnalyzer_cfi")
+process.load("TopQuarkAnalysis.Examples.TopMuonAnalyzer_cfi")
+process.load("TopQuarkAnalysis.Examples.TopElecAnalyzer_cfi")
+process.analyzeJet .input     = "cleanPatJets"
+process.analyzeMuon.inputMuon = "cleanPatMuons"
+process.analyzeElec.input     = "cleanPatElectrons"
 
 ## sequence for the TtSemiLeptonicEvent
 process.load("TopQuarkAnalysis.TopEventProducers.sequences.ttSemiLepEvtBuilder_cff")
 
 ## enable per-event printout from the TtSemiLeptonicEvent
 ## available verbosity levels: 0, 1, 2, 3, 11, 12, 13
-process.ttSemiLepEvent.verbosity = 13
+process.ttSemiLepEvent.verbosity = 12
 
 ## choose which hypotheses to produce
 from TopQuarkAnalysis.TopEventProducers.sequences.ttSemiLepEvtBuilder_cff import *
@@ -104,17 +122,20 @@ useElectronsForAllTtSemiLepHypotheses(process)
 
 ## more configurations for hypotheses
 setForAllTtSemiLepHypotheses(process, "jets", "cleanPatJets")
+#setForAllTtSemiLepHypotheses(process, "leps", "cleanPatMuons")
+setForAllTtSemiLepHypotheses(process, "leps", "cleanPatElectrons")
 #setForAllTtSemiLepHypotheses(process, "jetCorrectionLevel", "had")
 setForAllTtSemiLepHypotheses(process, "maxNJets", -1)
 setForAllTtSemiLepHypotheses(process, "maxNComb", -1)
 setForAllTtSemiLepHypotheses(process, "useBTagging", True)
-setForAllTtSemiLepHypotheses(process, "minBDiscBJets", 1.90)
+setForAllTtSemiLepHypotheses(process, "bTagAlgorithm", "trackCountingHighEffBJetTags")
+setForAllTtSemiLepHypotheses(process, "minBDiscBJets", -99999)
 setForAllTtSemiLepHypotheses(process, "maxBDiscLightJets", 3.99)
 
 ## set KinFit constraints
 ## 1: Whad-mass, 2: Wlep-mass, 3: thad-mass,
 ## 4: tlep-mass, 5: nu-mass, 6: equal t-masses
-process.kinFitTtSemiLepEventHypothesis.constraints = ([2,6])
+process.kinFitTtSemiLepEventHypothesis.constraints = ([1,2,6])
 
 # register TFileService
 process.TFileService = cms.Service("TFileService",
@@ -123,5 +144,7 @@ process.TFileService = cms.Service("TFileService",
 
 ## end path   
 process.path = cms.Path(process.patDefaultSequence *
-                        process.analyzeJetKinematics *
+                        process.analyzeJet *
+                        process.analyzeMuon *
+                        process.analyzeElec *
                         process.makeTtSemiLepEvent)
