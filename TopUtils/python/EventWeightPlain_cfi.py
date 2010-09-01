fa9d863a249a0c14d0f6f4fe7c947695453adc7b
snaumann <>
2010-09-01 09:33:02 +0000

    fixed module types: EDFilter -> EDProducer (CMSSW was not complaining before but now its stricter

--- TopUtils/python/EventWeightPlain_cfi.py
+++ TopUtils/python/EventWeightPlain_cfi.py
@@ -1,6 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
-eventWeight = cms.EDFilter("EventWeightPlain",
+eventWeight = cms.EDProducer("EventWeightPlain",
   nevts = cms.uint32(  1),    ## add the maximal number of events in the sample
   xsec  = cms.double(1.0),    ## add the xsection for production in pb
   eff   = cms.double(1.0),    ## add filter efficiencies
