f37b276be0c00a184cc30b23959a5b30b4d38e86
eschliec <>
2010-09-20 12:25:29 +0000

    now this analyzer analyzes HLT and L1 QuadJet efficiencies

--- TopAnalyzer/src/TriggerEfficiency.cc
+++ TopAnalyzer/src/TriggerEfficiency.cc
@@ -16,8 +16,10 @@ TriggerEfficiency::beginJob()
   edm::Service<TFileService> fs;
   if( !fs ) throw edm::Exception( edm::errors::Configuration, "TFile Service is not registered in cfg file" );
 
-  // efficiency of QuadJetXX trigger vs. pt
-  hists_["effy"] = fs->make<TH1F>( "effy" , "efficiency" , 100,  0. , 100. );
+  // efficiency of HLT_QuadJetXX trigger vs. pt
+  hists_["effy_hlt"] = fs->make<TH1F>( "effy_hlt" , ";p_{t} [GeV];efficiency" , 100,  0. , 100. );
+  // efficiency of L1_QuadJetX trigger vs. pt
+  hists_["effy_l1" ] = fs->make<TH1F>( "effy_l1"  , ";p_{t} [GeV];efficiency" , 100,  0. , 100. );
  }
 
 /// analyze triggers and fill histograms
@@ -29,7 +31,7 @@ TriggerEfficiency::analyze(const edm::Event& event, const edm::EventSetup&)
 
   if( !triggerEvent.failedToGet() ) {
 
-    hists_.find("effy")->second->Fill(-1);
+    hists_.find("effy_hlt")->second->Fill(-1);
 
     pat::TriggerFilterRefVector filters = triggerEvent->acceptedFilters();
     for(pat::TriggerFilterRefVector::const_iterator filter = filters.begin(); filter != filters.end(); ++filter){
@@ -42,12 +44,22 @@ TriggerEfficiency::analyze(const edm::Event& event, const edm::EventSetup&)
 	  for(int idx = 1; idx <= hists_.find("effy")->second->GetNbinsX(); ++idx){
 	    
 	    if(objects[3]->pt() >= idx){
-	      hists_.find("effy")->second->Fill(idx);
+	      hists_.find("effy_hlt")->second->Fill(idx);
 	    }
 	  }
 	}
       }
     }
+
+    std::multimap< double, pat::TriggerObject > sortedL1Jets;
+    std::vector< pat::TriggerObject > objects = (*triggerEvent->objects());
+    for(std::vector< pat::TriggerObject >::const_iterator object = objects.begin(); object != objects.end(); ++object){
+      if( object->hasFilterId(-84) || object->hasFilterId(-85) || object->hasFilterId(-86) ){
+	sortedL1Jets.insert(std::pair< double, pat::TriggerObject >(object->pt(),(*object)));
+      }
+    }
+    if(sortedL1Jets.size() > 3) hists_.find("effy_l1")->second->Fill((--(--(--sortedL1Jets.rbegin())))->second.pt());
+    else                        hists_.find("effy_l1")->second->Fill(-1.);
   }
 }
 
