4e87cdd92ab761f6caf335f15b354df408510c06
henderle <>
2010-11-03 13:09:26 +0000

    goodJetsPF20,goodJetsPF25,goodJetsPF30 are now with overlap filter on muons, goodJetsPF without overlap filter

--- TopFilter/python/sequences/jetSelection_cff.py
+++ TopFilter/python/sequences/jetSelection_cff.py
@@ -2,10 +2,42 @@ import FWCore.ParameterSet.Config as cms
 
 ## jet selector
 from PhysicsTools.PatAlgos.selectionLayer1.jetSelector_cfi import *
+from PhysicsTools.PatAlgos.cleaningLayer1.jetCleaner_cfi import *
 
 ## ---
 ##    setup jet quality studies
 ## ---
+
+noOverlapJetsPF = cleanPatJets.clone(
+    src = cms.InputTag("selectedPatJetsAK5PF"), 
+
+    # preselection (any string-based cut on pat::Jet)
+    preselection = cms.string(''),
+
+    # overlap checking configurables
+    checkOverlaps = cms.PSet(
+        muons = cms.PSet(
+           src       = cms.InputTag("vertexSelectedMuons"),
+           algorithm = cms.string("byDeltaR"),
+           preselection        = cms.string('pt > 20. & abs(eta) < 2.1 &'
+                                            'combinedMuon.isNull = 0 &'
+                                            'isTrackerMuon() =1 &'
+                                            '(trackIso+caloIso)/pt < 0.05 &'
+                                            'innerTrack.numberOfValidHits >= 11 &'
+                                            'globalTrack.normalizedChi2 < 10.0 &'
+                                            'globalTrack.hitPattern.numberOfValidMuonHits>0 &'
+                                            'abs(dB)<0.02 &'
+                                            'innerTrack.hitPattern.pixelLayersWithMeasurement>=1 &'
+                                            'numberOfMatches>1'),
+           deltaR              = cms.double(0.1),
+           checkRecoComponents = cms.bool(False), # don't check if they share some AOD object ref
+           pairCut             = cms.string(""),
+           requireNoOverlaps   = cms.bool(True), # overlaps don't cause the jet to be discared
+        )
+    ),
+    # finalCut (any string-based cut on pat::Jet)
+    finalCut = cms.string(''),
+)
 
 ## getting started
 centralJets  = selectedPatJets.clone(src = 'selectedPatJets',
@@ -20,22 +52,49 @@ goodJets     = selectedPatJets.clone(src = 'selectedPatJets',
                                            'jetID.fHPD < 0.98         &'
                                            'jetID.n90Hits > 1'
                                      )
-goodJetsPF20 = selectedPatJets.clone(src = 'selectedPatJetsAK5PF',
-                                     cut = 'abs(eta) < 2.4 & pt > 20.          &'
-                                           'chargedHadronEnergyFraction > 0.0  &'
-                                           'neutralHadronEnergyFraction/corrFactor("raw") < 0.99 &'
-                                           'chargedEmEnergyFraction/corrFactor("raw")     < 0.99 &'
-                                           'neutralEmEnergyFraction/corrFactor("raw")     < 0.99 &'
-                                           'chargedMultiplicity > 0            &'
-                                           'nConstituents > 1'
-                                      )
-
-goodJetsPF25 = selectedPatJets.clone(src = 'goodJetsPF20',
+goodJetsPF = selectedPatJets.clone(src = 'selectedPatJetsAK5PF',
+                                   cut = 'abs(eta) < 2.4 & pt > 20. &'
+                                         'chargedHadronEnergyFraction > 0.0  &'
+                                         'neutralHadronEnergyFraction/corrFactor("raw") < 0.99 &'
+                                         'chargedEmEnergyFraction/corrFactor("raw")     < 0.99 &'
+                                         'neutralEmEnergyFraction/corrFactor("raw")     < 0.99 &'
+                                         'chargedMultiplicity > 0            &'
+                                         'nConstituents > 1'
+                                   )
+centralJetsPF20  = selectedPatJets.clone(src = 'noOverlapJetsPF',
+                                     cut = 'abs(eta) < 2.4'
+                                     )
+centralJetsPF25  = selectedPatJets.clone(src = 'centralJetsPF20',
                                      cut = 'pt > 25.'
                                      )
-goodJetsPF30 = selectedPatJets.clone(src = 'goodJetsPF20',
+centralJetsPF30  = selectedPatJets.clone(src = 'centralJetsPF20',
                                      cut = 'pt > 30.'
                                      )
+reliableJetsPF20 = selectedPatJets.clone(src = 'noOverlapJetsPF',
+                                     cut = 'abs(eta) < 2.4 & pt > 20.'
+                                     )
+reliableJetsPF25 = selectedPatJets.clone(src = 'reliableJetsPF20',
+                                     cut = 'pt > 25.'
+                                     )
+reliableJetsPF30 = selectedPatJets.clone(src = 'reliableJetsPF20',
+                                     cut = 'pt > 30.'
+                                     )
+goodJetsPF20     = selectedPatJets.clone(src = 'noOverlapJetsPF',
+                                         cut = 'abs(eta) < 2.4 & pt > 20.          &'
+                                               'chargedHadronEnergyFraction > 0.0  &'
+                                               'neutralHadronEnergyFraction/corrFactor("raw") < 0.99 &'
+                                               'chargedEmEnergyFraction/corrFactor("raw")     < 0.99 &'
+                                               'neutralEmEnergyFraction/corrFactor("raw")     < 0.99 &'
+                                               'chargedMultiplicity > 0            &'
+                                               'nConstituents > 1'
+                                         )
+
+goodJetsPF25     = selectedPatJets.clone(src = 'goodJetsPF20',
+                                         cut = 'pt > 25.'
+                                         )
+goodJetsPF30     = selectedPatJets.clone(src = 'goodJetsPF20',
+                                         cut = 'pt > 30.'
+                                         )
 ## N-1 collections
 noEtaJets      = selectedPatJets.clone(src = 'selectedPatJets',
                                        cut = 'pt > 30.                  &'
@@ -68,7 +127,8 @@ nofHPDJets     = selectedPatJets.clone(src = 'selectedPatJets',
 ## a kinematically well defined jet with
 ## reliable calibration and a robust rej
 ## of photons, electrons and prompt pi0's
-selectGoodJets = cms.Sequence(centralJets  *
+selectGoodJets = cms.Sequence(noOverlapJetsPF  *
+                              centralJets  *
                               reliableJets *
                               goodJets
                               )
