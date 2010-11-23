4f648358e24fa076e3ce0deb593b872c0823e7e9
henderle <>
2010-11-23 11:15:55 +0000

    jet type can be switched

--- Configuration/analysis/semiLeptonic/diffXSection/analyzeMuonJES09All_cfg.py
+++ Configuration/analysis/semiLeptonic/diffXSection/analyzeMuonJES09All_cfg.py
@@ -1,28 +1,44 @@
 ## ---
 ##    this configfile does the same like analyzeMuonDiffXSec_cfg.py
-##    but with a JES shift of -10% but without filtering on gen level
+##    but shifting JES down and without filtering on gen level
 ## ---
 
 ## get the mother file
-execfile("/afs/naf.desy.de/user/g/goerner/semileptonic361/analyzeMuonDiffXSecAll_cfg.py")
+execfile("analyzeMuonDiffXSecAll_cfg.py")
 
 ## change input collections to JES-shifted collections
 from PhysicsTools.PatAlgos.tools.helpers import massSearchReplaceAnyInputTag
-massSearchReplaceAnyInputTag(process.p1, 'selectedPatJets', 'scaledJetEnergy:selectedPatJets') 
-massSearchReplaceAnyInputTag(process.p2, 'selectedPatJets', 'scaledJetEnergy:selectedPatJets')
-massSearchReplaceAnyInputTag(process.p3, 'selectedPatJets', 'scaledJetEnergy:selectedPatJets')
-massSearchReplaceAnyInputTag(process.p1, 'patMETs'        , 'scaledJetEnergy:patMETs') 
-massSearchReplaceAnyInputTag(process.p2, 'patMETs'        , 'scaledJetEnergy:patMETs')
-massSearchReplaceAnyInputTag(process.p3, 'patMETs'        , 'scaledJetEnergy:patMETs')
+if(jetType=="particleFlow"):
+    massSearchReplaceAnyInputTag(process.p1, 'selectedPatJetsAK5PF', 'scaledJetEnergy:selectedPatJetsAK5PF') 
+    massSearchReplaceAnyInputTag(process.p2, 'selectedPatJetsAK5PF', 'scaledJetEnergy:selectedPatJetsAK5PF')
+    massSearchReplaceAnyInputTag(process.p3, 'selectedPatJetsAK5PF', 'scaledJetEnergy:selectedPatJetsAK5PF')
+    massSearchReplaceAnyInputTag(process.p1, 'patMETsPF'           , 'scaledJetEnergy:patMETsPF') 
+    massSearchReplaceAnyInputTag(process.p2, 'patMETsPF'           , 'scaledJetEnergy:patMETsPF')
+    massSearchReplaceAnyInputTag(process.p3, 'patMETsPF'           , 'scaledJetEnergy:patMETsPF')
+elif(jetType=="Calo"):
+    massSearchReplaceAnyInputTag(process.p1, 'selectedPatJets', 'scaledJetEnergy:selectedPatJets') 
+    massSearchReplaceAnyInputTag(process.p2, 'selectedPatJets', 'scaledJetEnergy:selectedPatJets')
+    massSearchReplaceAnyInputTag(process.p3, 'selectedPatJets', 'scaledJetEnergy:selectedPatJets')
+    massSearchReplaceAnyInputTag(process.p1, 'patMETs'        , 'scaledJetEnergy:patMETs') 
+    massSearchReplaceAnyInputTag(process.p2, 'patMETs'        , 'scaledJetEnergy:patMETs')
+    massSearchReplaceAnyInputTag(process.p3, 'patMETs'        , 'scaledJetEnergy:patMETs')
+else:
+    print "unknown jetType"
 
 ## get JES-shifting module
 process.load("TopAnalysis.TopUtils.JetEnergyScale_cfi")
 # set input collection- needed while running on pat tuples
-process.scaledJetEnergy.inputJets = "selectedPatJets"
-process.scaledJetEnergy.inputMETs = "patMETs"
+if(jetType=="particleFlow"):
+    process.scaledJetEnergy.inputJets = "selectedPatJetsAK5PF"
+    process.scaledJetEnergy.inputMETs = "patMETsPF"
+elif(jetType=="Calo"):
+    process.scaledJetEnergy.inputJets = "selectedPatJets"
+    process.scaledJetEnergy.inputMETs = "patMETs"
+else:
+    print "unknown jetType"
 
-# JES -10%
-process.scaledJetEnergy.scaleFactor = 0.9
+# JES -5%
+process.scaledJetEnergy.scaleFactor = 0.95
 
 ## include module to create JES-shifted collection
 process.p1.replace(process.semiLeptonicSelection,
