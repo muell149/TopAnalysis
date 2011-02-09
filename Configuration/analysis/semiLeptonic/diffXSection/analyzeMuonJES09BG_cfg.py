7dc3f3facecb03344a7d6bbe1eef667c19ff6a18
rwolf <>
2011-02-09 10:44:05 +0000

    update to new JetMET recommendation JetMET+2.5% flat

--- Configuration/analysis/semiLeptonic/diffXSection/analyzeMuonJES09BG_cfg.py
+++ Configuration/analysis/semiLeptonic/diffXSection/analyzeMuonJES09BG_cfg.py
@@ -8,7 +8,7 @@ execfile("analyzeMuonDiffXSecBG_cfg.py")
 
 # JES down
 process.scaledJetEnergy.scaleType   = "top:down"
-process.scaledJetEnergy.scaleFactor = 0.982
+process.scaledJetEnergy.scaleFactor = 0.975
 if(jetType=="particleFlow"):
     process.scaledJetEnergy.payload = "AK5PF"
 elif(jetType=="Calo"):
