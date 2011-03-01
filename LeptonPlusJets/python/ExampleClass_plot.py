2c1b789589d8572c1229b357755091e5a38d0e12
rwolf <>
2011-03-01 20:25:31 +0000

    and what about this simplification of the plt.py configs?

--- LeptonPlusJets/python/ExampleClass_plot.py
+++ LeptonPlusJets/python/ExampleClass_plot.py
@@ -1,29 +1,14 @@
 import FWCore.ParameterSet.Config as cms
 
+from TopAnalysis.LeptonPlusJets.samples_plt import topSignal, topBG
+
 plot = cms.PSet(
-## indicate verbosity level for debugging
-verbose = cms.bool(True),
-## luminosity in pb-1
-lumi = cms.double(10),
-## vector of input samples
-inputs = cms.VPSet(
-    cms.PSet(
-    ## input file
-    file  = cms.string('file:~goerner/semileptonic387/diffXSecFromSignal/analysisRootFiles/muonDiffXSecSigMadD6TFall10PF.root'),
-    ## label in legend
-    label = cms.string('Top Signal'),
-    ## normalization scale (expected to be to 1pb)
-    scale = cms.double(1.0)
-    ),
-    cms.PSet(
-    ## input file
-    file  = cms.string('file:~goerner/semileptonic387/diffXSecFromSignal/analysisRootFiles/muonDiffXSecBkgMadD6TFall10PF.root'),
-    ## label in legend
-    label = cms.string('Top Other'),
-    ## normalization scale (expected to be to 1pb)
-    scale = cms.double(1.0)
-    ),
-),
-## histograms of interest
-hists = cms.vstring('centralJetKinematics/pt','centralJetKinematics/eta', 'test/myHist'),
-)
+    ## indicate verbosity level for debugging
+    verbose = cms.bool(True),
+    ## luminosity in pb-1
+    lumi    = cms.double(10),
+    ## vector of input samples
+    inputs  = cms.VPSet( topSignal, topBG ),
+    ## histograms of interest
+    hists   = cms.vstring('centralJetKinematics/pt','centralJetKinematics/eta', 'test/myHist'),
+    )
