a6580fa33dfacc1851659e0bcf3376764a91c75e
jlange <>
2011-05-13 16:29:11 +0000

    the binning used for AN-2010-090

--- TopAnalyzer/python/TagAndProbeAnalyzer_cfi.py
+++ TopAnalyzer/python/TagAndProbeAnalyzer_cfi.py
@@ -12,19 +12,26 @@ tagAndProbeAnalyzer = cms.EDAnalyzer("TagAndProbeAnalyzer", # -> TagAndProbeAnal
     ptCut =  cms.double(20.),
     etaCut = cms.double(2.1),
     ## bin numbers; in case variable bins are desired: set to -1
-    nBinsPt     = cms.int32(15),
-    nBinsEta    = cms.int32(30),
-    nBinsPhi    = cms.int32(12),
+    #nBinsPt     = cms.int32(15),
+    nBinsPt     = cms.int32(-1),
+    #nBinsEta    = cms.int32(20),
+    nBinsEta    = cms.int32(-1),
+    nBinsPhi    = cms.int32(10),
     nBinsMult   = cms.int32(10),
-    nBinsMinDR  = cms.int32(25),
+    #nBinsMinDR  = cms.int32(25),
+    nBinsMinDR  = cms.int32(-1),
     nBinsMuMult = cms.int32(10),
     ## in case of fixed bins: 1st vector entry=xlow, 2nd vector entry=xup (left and right edges of histo)
-    ## in case of variable bins: vector of low-edges for each bin
-    binsPt     = cms.vdouble(  0.  , 150.  ),
-    binsEta    = cms.vdouble( -5.  ,   5.  ),
+    ## in case of variable bins: vector of low-edges for each bin (including overflow), i.e. nBins+1 entries
+    #binsPt     = cms.vdouble(  0.  , 150.  ),
+    binsPt     = cms.vdouble(  0.,10.,20.,30.,40.,50.,75.,100.,150. ),
+    #binsEta    = cms.vdouble( -3.  ,   3.  ),
+    binsEta    = cms.vdouble( -3.,-2.55,-2.1,-1.65,-1.2,-0.9,-0.45,0.,0.45,0.9,1.2,1.65,2.1,2.55,3. ),
     binsPhi    = cms.vdouble( -3.14,   3.14),
     binsMult   = cms.vdouble(  0.  ,  10.  ),
-    binsMinDR  = cms.vdouble(  0.  ,  10.  ),
+    #binsMinDR  = cms.vdouble(  0.  ,  10.  ),
+    #binsMinDR  = cms.vdouble(  0.,0.3,0.7,1.,1.5,2.,2.5,3.,4.,5.,6. ),
+    binsMinDR  = cms.vdouble( 0.,0.3,0.7,1.,1.5,2.,2.5,3.,4.,5.,6. ),
     binsMuMult = cms.vdouble(  0.  ,  10.  )
 )
 
