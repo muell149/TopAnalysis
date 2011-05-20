174f8a6ce19b7fa6aaeda9f7a643c55cdd2b34c7
mgoerner <>
2011-05-20 09:39:51 +0000

    introduce function drawRatio that draws data/MC ratio into active canvas, in function axesStyle: use axislabel and titlesize for x and y axis!

--- Configuration/analysis/semiLeptonic/diffXSection/basicFunctions.h
+++ Configuration/analysis/semiLeptonic/diffXSection/basicFunctions.h
@@ -178,7 +178,7 @@ void canvasStyle(TCanvas& canv)
   canv.SetTopMargin   ( 0.05 );
 }
 
-void axesStyle(TH1& hist, const char* titleX, const char* titleY, float yMin=-123, float yMax=-123, float yTitleSize=0.05, float yTitleOffset=1.2, float xLabelSize = 0.06)
+void axesStyle(TH1& hist, const char* titleX, const char* titleY, float yMin=-123, float yMax=-123, float titleSize=0.05, float yTitleOffset=1.2, float labelSize = 0.05)
 {
   // this function configures the axis style of a TH1 histogram "hist"
   // modified quantities: hist
@@ -195,20 +195,20 @@ void axesStyle(TH1& hist, const char* titleX, const char* titleY, float yMin=-12
   hist.SetTitle("");
   hist.GetXaxis()->SetTitle(titleX);
   //hist.GetXaxis()->CenterTitle();
-  hist.GetXaxis()->SetTitleSize  ( 0.05);
-  hist.GetXaxis()->SetTitleColor (    1);
-  hist.GetXaxis()->SetTitleOffset(  1.1);
-  hist.GetXaxis()->SetTitleFont  (   62);
-  hist.GetXaxis()->SetLabelSize  ( xLabelSize);
-  hist.GetXaxis()->SetLabelFont  (   62);
-  hist.GetXaxis()->SetNdivisions (  505);
+  hist.GetXaxis()->SetTitleSize  (titleSize);
+  hist.GetXaxis()->SetTitleColor (1);
+  hist.GetXaxis()->SetTitleOffset(1.1);
+  hist.GetXaxis()->SetTitleFont  (62);
+  hist.GetXaxis()->SetLabelSize  (labelSize);
+  hist.GetXaxis()->SetLabelFont  (62);
+  hist.GetXaxis()->SetNdivisions (505);
   hist.GetYaxis()->SetTitle(titleY);
-  hist.GetYaxis()->SetTitleSize  ( yTitleSize );
-  hist.GetYaxis()->SetTitleColor (    1);
+  hist.GetYaxis()->SetTitleSize  (titleSize);
+  hist.GetYaxis()->SetTitleColor (1);
   hist.GetYaxis()->SetTitleOffset(yTitleOffset);
-  hist.GetYaxis()->SetTitleFont  (   62);
-  hist.GetYaxis()->SetLabelSize  ( 0.05);
-  hist.GetYaxis()->SetLabelFont  (   62);
+  hist.GetYaxis()->SetTitleFont  (62);
+  hist.GetYaxis()->SetLabelSize  (labelSize);
+  hist.GetYaxis()->SetLabelFont  (62);
   //hist.GetYaxis()->CenterTitle   ( true);
   if(yMin!=-123) hist.SetMinimum(yMin);
   if(yMax!=-123) hist.SetMaximum(yMax);
@@ -338,7 +338,7 @@ TH1F* divideByBinwidth(TH1F* histo, bool calculateError=true)
   return outputHisto;
 }
 
-void DrawLabel(TString text, const double x1, const double y1, const double x2, const double y2, double textSize)
+void DrawLabel(TString text, const double x1, const double y1, const double x2, const double y2, double textSize=0)
 {
   // function to directly draw a label into the active canvas
   // the coordinates of the window are: "x1", "y1", "x2", "y2"
@@ -350,7 +350,7 @@ void DrawLabel(TString text, const double x1, const double y1, const double x2, 
   TPaveLabel *label = new TPaveLabel(x1, y1, x2, y2, text, "br NDC");
   label->SetFillStyle(0);
   label->SetBorderSize(0);
-  label->SetTextSize(textSize);
+  if(textSize!=0) label->SetTextSize(textSize);
   label->SetTextAlign(12);
   label->Draw("same");
 }
@@ -1231,5 +1231,132 @@ TString sysLabel(unsigned int sys)
   }
   return systematicVariationlabel;
 }
+
+void drawRatio(const TH1* histNumerator, TH1* histDenominator, const Double_t& ratioMin, const Double_t& ratioMax, unsigned int verbose=0, const std::vector<double> err_=std::vector<double>(0))
+{
+  // this function draws a pad with the ratio of 'histNumerator' and 'histDenominator'
+  // the range of the ratio is 'ratioMin' to 'ratioMax'
+  // to the systematic variation "sys" of the enumerator "systematicVariation"
+  // per default only the gaussian error of the 'histNumerator' is considered:
+  // (error(bin i) = sqrt(histNumerator->GetBinContent(i))/histDenominator->GetBinContent(i))
+  // if 'err_' is present and its size equals the number of bins in the histos,
+  // its values are considered as error for the ratio
+  // NOTE: x Axis is transferred from histDenominator to the bottom of the canvas
+  // modified quantities: none
+  // used functions: none
+  // used enumerators: none 
+
+  // check that histos have the same binning
+  if(histNumerator->GetNbinsX()!=histDenominator->GetNbinsX()){
+    std::cout << "error when calling drawRatio - histos have different number of bins" << std::endl;
+    return;
+  }
+  if(verbose>1){
+    std::cout << "building ratio plot of " << histNumerator->GetName();
+    std::cout << " and " << histDenominator->GetName() << std::endl;
+  }
+  // create ratio 
+  TH1F* ratio = (TH1F*)histNumerator->Clone();
+  ratio->Divide(histDenominator);
+  // calculate error for ratio
+  // a) from err_
+  if(err_.size()==(unsigned int)histNumerator->GetNbinsX()){
+    if(verbose>0) std::cout << "ratio error from vector" << std::endl;
+    for(int bin=1; bin<=histNumerator->GetNbinsX(); bin++){
+      ratio->SetBinError(bin, err_[bin-1]);
+    }
+  }
+  else{
+    // b) default: only gaussian error of histNumerator
+    if(verbose>0) std::cout << "ratio error from statistical error of " << histNumerator->GetName() << " only" << std::endl;
+    for(int bin=1; bin<=histNumerator->GetNbinsX(); bin++){
+      ratio->SetBinError(bin, sqrt(histNumerator->GetBinContent(bin))/histDenominator->GetBinContent(bin));
+    }
+  }
+  // get some values from old pad
+  Int_t     logx = gPad->GetLogx();
+  Double_t  left = gPad->GetLeftMargin();
+  Double_t right = gPad->GetRightMargin();
+  // y:x size ratio for canvas
+  double canvAsym = 4./3.;
+  // ratio size of pat with plot and pat with ratio
+  double ratioSize = 0.36;
+  // change old pad
+  gPad->SetBottomMargin(ratioSize);
+  gPad->SetRightMargin(right);
+  gPad->SetLeftMargin(left);
+  gPad->SetBorderMode(0);
+  gPad->SetBorderSize(0);
+  gPad->SetFillColor(10);
+  // create new pad for ratio plot
+  TPad *rPad;
+  rPad = new TPad("rPad","",0,0,1,ratioSize+0.001);
+  rPad->SetFillStyle(0);
+  rPad->SetFillColor(0);
+  rPad->SetBorderSize(0);
+  rPad->SetBorderMode(0);
+  rPad->Draw();
+  rPad->cd();
+  rPad->SetLogy(0);
+  rPad->SetLogx(logx);
+  rPad->SetTicky(1);
+  // configure ratio plot
+  double scaleFactor = 1./(canvAsym*ratioSize);
+  ratio->SetStats(kFALSE);
+  ratio->SetMarkerSize(0.1);
+  ratio->SetTitle("");
+  ratio->SetMaximum(ratioMax);
+  ratio->SetMinimum(ratioMin);
+  ratio->SetLineWidth(1);
+  // configure axis of ratio plot
+  ratio->GetXaxis()->SetTitleSize(histDenominator->GetXaxis()->GetTitleSize()*scaleFactor*1.3);
+  ratio->GetXaxis()->SetTitleOffset(histDenominator->GetXaxis()->GetTitleOffset()*0.9);
+  ratio->GetXaxis()->SetLabelSize(histDenominator->GetXaxis()->GetLabelSize()*scaleFactor*1.4);
+  ratio->GetXaxis()->SetTitle(histDenominator->GetXaxis()->GetTitle());
+  ratio->GetXaxis()->SetNdivisions(histDenominator->GetNdivisions());
+  ratio->GetYaxis()->CenterTitle();
+  ratio->GetYaxis()->SetTitle("#frac{N_{data}}{N_{MC}}");
+  ratio->GetYaxis()->SetTitleSize(histDenominator->GetYaxis()->GetTitleSize()*scaleFactor);
+  ratio->GetYaxis()->SetTitleOffset(histDenominator->GetYaxis()->GetTitleOffset()/scaleFactor);
+  ratio->GetYaxis()->SetLabelSize(histDenominator->GetYaxis()->GetLabelSize()*scaleFactor);
+  ratio->GetYaxis()->SetLabelOffset(histDenominator->GetYaxis()->GetLabelOffset()*3.3);
+  ratio->GetYaxis()->SetTickLength(0.03);
+  ratio->GetYaxis()->SetNdivisions(505);
+  ratio->GetXaxis()->SetRange(histDenominator->GetXaxis()->GetFirst(), histDenominator->GetXaxis()->GetLast());
+  // delete axis of initial plot
+  histDenominator->GetXaxis()->SetLabelSize(0);
+  histDenominator->GetXaxis()->SetTitleSize(0);
+  // draw ratio plot
+  ratio->DrawClone("pe1 X0");
+  ratio->SetMarkerSize(1.2);
+  ratio->DrawClone("pe1 X0 same");
+  rPad->SetTopMargin(0.0);
+  rPad->SetBottomMargin(0.15*scaleFactor);
+  rPad->SetRightMargin(right);
+  gPad->SetLeftMargin(left);
+  gPad->RedrawAxis();
+  // draw grid
+  rPad->SetGrid(1,1);
+
+  // draw a horizontal lines on a given histogram
+  // a) at 1
+  Double_t xmin = ratio->GetXaxis()->GetXmin();
+  Double_t xmax = ratio->GetXaxis()->GetXmax();
+  TString height = ""; height += 1;
+  TF1 *f = new TF1("f", height, xmin, xmax);
+  f->SetLineStyle(1);
+  f->SetLineWidth(1);
+  //  f->SetLineColor(lcolor);
+  f->Draw("L same");
+  // b) at upper end of ratio pad
+  TString height2 = ""; height2 += ratioMax;
+  TF1 *f2 = new TF1("f2", height2, xmin, xmax);
+  f2->SetLineStyle(1);
+  f2->SetLineWidth(1);
+  f2->Draw("L same");
+}
+
+
+
 
 #endif
