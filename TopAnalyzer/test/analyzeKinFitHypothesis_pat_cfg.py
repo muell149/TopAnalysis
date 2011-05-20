9a477bca3e7752697fa1e7c37cfd2dc20518c786
mgoerner <>
2011-05-20 13:40:51 +0000

    SSV btagging algo, PF objects, same kinFit configuration as in TopAnalysis/Configuration/analysis/semiLeptonic/diffXSection/analyzeTopDiffXSec_cfg.py

--- TopAnalyzer/test/analyzeKinFitHypothesis_pat_cfg.py
+++ TopAnalyzer/test/analyzeKinFitHypothesis_pat_cfg.py
@@ -9,7 +9,7 @@ process = cms.Process("TopQuark")
 ## configure message logger
 process.load("FWCore.MessageLogger.MessageLogger_cfi")
 process.MessageLogger.cerr.threshold = 'INFO'
-process.MessageLogger.cerr.FwkReport.reportEvery = 50
+process.MessageLogger.cerr.FwkReport.reportEvery = 1
 ## configure process options
 process.options = cms.untracked.PSet(
     wantSummary = cms.untracked.bool(True)
@@ -24,17 +24,18 @@ process.source = cms.Source("PoolSource",
     fileNames = cms.untracked.vstring(    
     ## add your favourite file here
     #'/store/user/jlange/Mu/PAT_Nov4RerecoL1IncludedUHH/e37a6f43ad6b01bd8486b714dc367330/DataNov4RerecoL1included_9_1_0dl.root'
-    '/store/user/henderle/TTJets_TuneD6T_7TeV-madgraph-tauola/PAT_FALL10HH/6c1c00d4602477b58cef63f182ce0614/fall10MC_9_1_WVh.root'
+    #'/store/user/henderle/TTJets_TuneD6T_7TeV-madgraph-tauola/PAT_FALL10HH/6c1c00d4602477b58cef63f182ce0614/fall10MC_9_1_WVh.root'
+    '/store/user/henderle/TTJets_TuneD6T_7TeV-madgraph-tauola/PAT_FALL10HH/6c1c00d4602477b58cef63f182ce0614/fall10MC_5_1_wzx.root'
     #'/store/user/mgoerner/WJetsToLNu_TuneD6T_7TeV-madgraph-tauola/PAT_FALL10HH/148435cd71339b79cc0025730c13472a/fall10MC_9_1_c8G.root'
     )
 )
 
 ## define maximal number of events to loop over
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(-1)
+    input = cms.untracked.int32(1000)
 )
 ## skip events
-process.source.skipEvents = cms.untracked.uint32(10000)
+process.source.skipEvents = cms.untracked.uint32(0)
 
 #-------------------------------------------------
 # top analysis
@@ -53,12 +54,12 @@ process.load("TopAnalysis.TopFilter.sequences.semiLeptonicSelection_cff")
 ## muon selection
 process.load("TopAnalysis.TopFilter.sequences.muonSelection_cff")
 process.muonSelection = process.muonSelection.clone       (src = 'tightMuons'      , minNumber = 1, maxNumber = 99999999)
-process.jetSelection  = process.leadingJetSelection.clone (src = 'tightLeadingJets', minNumber = 4)
+process.jetSelection  = process.leadingJetSelection.clone (src = 'tightLeadingPFJets', minNumber = 4)
 ## jet selection
 process.load("TopAnalysis.TopFilter.sequences.jetSelection_cff")
 ## redefine veto jets to be sure it is also replaced when running on PF
 from TopAnalysis.TopFilter.sequences.jetSelection_cff import goodJets
-process.vetoJets.src="goodJets"
+process.vetoJets.src="goodJetsPF30"
 process.vetoJets.cut=''
 
 ## ---
@@ -69,13 +70,13 @@ process.load('TopQuarkAnalysis.TopEventProducers.sequences.ttSemiLepEvtBuilder_c
 from TopQuarkAnalysis.TopEventProducers.sequences.ttSemiLepEvtBuilder_cff import *
 addTtSemiLepHypotheses(process,['kKinFit'])
 ## process.ttSemiLepJetPartonMatch.verbosity = 1
-process.kinFitTtSemiLepEventHypothesis.leps = 'selectedPatMuons' #'tightMuons'
+process.kinFitTtSemiLepEventHypothesis.leps = 'tightMuons' #'tightMuons'
 process.kinFitTtSemiLepEventHypothesis.jets = 'tightLeadingPFJets'#'selectedPatJetsAK5PF'
 process.kinFitTtSemiLepEventHypothesis.mets = 'patMETsPF'
 
 # maximum number of jets to be considered in the jet combinatorics
 # (has to be >= 4, can be set to -1 if you want to take all)
-process.kinFitTtSemiLepEventHypothesis.maxNJets = 4
+process.kinFitTtSemiLepEventHypothesis.maxNJets = 5
 
 # maximum number of jet combinations finally written into the event, starting from the "best"
 # (has to be >= 1, can be set to -1 if you want to take all)
@@ -85,18 +86,25 @@ process.kinFitTtSemiLepEventHypothesis.maxNComb = 3
 process.kinFitTtSemiLepEventHypothesis.constraints = [1, 2, 6]
 
 # consider b-tagging in event reconstruction
-process.kinFitTtSemiLepEventHypothesis.bTagAlgo = "trackCountingHighEffBJetTags"
+#process.kinFitTtSemiLepEventHypothesis.bTagAlgo = "trackCountingHighEffBJetTags"
+process.kinFitTtSemiLepEventHypothesis.bTagAlgo = "simpleSecondaryVertexHighEffBJetTags"
 
 # TCHE discr.values 7TeV: 1.7, 3.3, 10.2
-process.kinFitTtSemiLepEventHypothesis.minBDiscBJets     = 1.7
-process.kinFitTtSemiLepEventHypothesis.maxBDiscLightJets = 10.2
+process.kinFitTtSemiLepEventHypothesis.minBDiscBJets     = 1.74
+process.kinFitTtSemiLepEventHypothesis.maxBDiscLightJets = 1.74
 process.kinFitTtSemiLepEventHypothesis.useBTagging       = True
 
 # keep only events with unambigues parton matches
 # (no other partons exist in dR=0.3 cone) 
 # attention: improves purity but reduces efficiency
 process.ttSemiLepJetPartonMatch.algorithm = "unambiguousOnly"
-
+#process.ttSemiLepJetPartonMatch.algorithm = "totalMinDist"
+process.ttSemiLepJetPartonMatch.useMaxDist = True
+## set number of jets considered in jet-parton matching
+process.ttSemiLepJetPartonMatch.maxNJets=-1
+## choose jet collection considered in jet-parton matching
+process.ttSemiLepJetPartonMatch.jets='tightLeadingPFJets'
+    
 ## configure module you would like to test
 process.load("TopAnalysis.TopAnalyzer.HypothesisKinFit_cfi"    )
 hypoKinFit = cms.PSet(hypoKey = cms.string("kKinFit"),
@@ -113,19 +121,14 @@ process.load("TopAnalysis.TopAnalyzer.TopKinematics_cfi")
 
 ## define PSets for top reconstruction analyzer
 ## 1) event hypothesis built of objects from genmatch to partons (ttSemiLepJetPartonMatch)
-recoGenMatch      = cms.PSet(hypoKey=cms.string('kGenMatch'), useTree=cms.bool(True), matchForStabilityAndPurity=cms.bool(False), ttbarInsteadOfLepHadTop = cms.bool(True) )
+recoGenMatch      = cms.PSet(hypoKey=cms.string('kGenMatch'), useTree=cms.bool(True), matchForStabilityAndPurity=cms.bool(False), ttbarInsteadOfLepHadTop = cms.bool(False), maxNJets = process.kinFitTtSemiLepEventHypothesis.maxNJets)
 ## 2) event hypothesis built of objects as defined above
-recoKinFit        = cms.PSet(hypoKey=cms.string('kKinFit'  ), useTree=cms.bool(True), matchForStabilityAndPurity=cms.bool(False), ttbarInsteadOfLepHadTop = cms.bool(True) )
-## 3) event hypothesis built of objects as defined above including for every 1D histogram
-## only events where the reconstructed and matched object are within the same bin
-recoKinFitMatched = cms.PSet(hypoKey=cms.string('kKinFit'  ), useTree=cms.bool(True), matchForStabilityAndPurity=cms.bool(True ), ttbarInsteadOfLepHadTop = cms.bool(True) )
+recoKinFit        = cms.PSet(hypoKey=cms.string('kKinFit'  ), useTree=cms.bool(True), matchForStabilityAndPurity=cms.bool(False), ttbarInsteadOfLepHadTop = cms.bool(False), maxNJets = process.kinFitTtSemiLepEventHypothesis.maxNJets)
 ## 4) event hypothesis built with generator truth informations
-genTtbarSemiMu    = cms.PSet(hypoKey=cms.string("None"     ), useTree=cms.bool(True), matchForStabilityAndPurity=cms.bool(False), ttbarInsteadOfLepHadTop = cms.bool(True) )
+genTtbarSemiMu    = cms.PSet(hypoKey=cms.string("None"     ), useTree=cms.bool(True), matchForStabilityAndPurity=cms.bool(False), ttbarInsteadOfLepHadTop = cms.bool(False), maxNJets = process.kinFitTtSemiLepEventHypothesis.maxNJets)
 
-# a1) as reconstructed from kinFit after reco selection
+# a) as reconstructed from kinFit after reco selection
 process.analyzeTopRecoKinematicsKinFit        = process.analyzeTopRecKinematics.clone(analyze=recoKinFit  )
-# a2) as reconstructed from kinFit after reco selection including match to gen objects
-process.analyzeTopRecoKinematicsKinFitMatched = process.analyzeTopRecKinematics.clone(analyze=recoKinFitMatched )
 # b) as reconstructed from genmatched objects after reco selection
 process.analyzeTopRecoKinematicsGenMatch      = process.analyzeTopRecKinematics.clone(analyze=recoGenMatch)
 # c) as reconstructed from generator objects after gen selection
@@ -149,9 +152,13 @@ process.p1 = cms.Path(## ttbar semilept. mu only
                       process.analyzeHypothesisKinFitModule         *
                       process.analyzeTopRecoKinematicsKinFit        *
                       process.analyzeTopRecoKinematicsGenMatch      *
-                      process.analyzeTopGenLevelKinematics          *
-                      process.analyzeTopRecoKinematicsKinFitMatched                       
+                      process.analyzeTopGenLevelKinematics          
 #                      process.analyzeHypothesisKinFitMET *
 #                      process.analyzeHypothesisKinFitJets *
 #                      process.analyzeHypothesisKinFitMuon
                       )
+## switch to SSV btagging
+from PhysicsTools.PatAlgos.tools.helpers import massSearchReplaceAnyInputTag
+massSearchReplaceAnyInputTag(process.p1, 'tightBottomPFJets', 'simpleSecondaryVertexHighEffBJets')
+process.simpleSecondaryVertexHighEffBJets.src="goodJetsPF30"
+process.p1.replace(process.semiLeptonicSelection, process.semiLeptonicSelection*process.simpleSecondaryVertexHighEffBJets)
