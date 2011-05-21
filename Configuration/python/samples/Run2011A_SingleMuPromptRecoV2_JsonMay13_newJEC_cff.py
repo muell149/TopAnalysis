10c7c362d3f5b9fa3ebf2c3c265debdf6a1fa852
jlange <>
2011-05-21 13:01:33 +0000

    removed some wrong MuHad files from list of SingleMu files

--- Configuration/python/samples/Run2011A_SingleMuPromptRecoV2_JsonMay13_newJEC_cff.py
+++ Configuration/python/samples/Run2011A_SingleMuPromptRecoV2_JsonMay13_newJEC_cff.py
@@ -159,26 +159,7 @@ readFiles.extend( [
         '/store/user/jlange/SingleMu/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_156_4_112.root',
         '/store/user/jlange/SingleMu/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_155_1_No5.root',
         '/store/user/jlange/SingleMu/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_154_1_xE4.root',
-        '/store/user/jlange/SingleMu/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_153_1_JeP.root',  '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_9_1_lEI.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_8_1_f8d.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_7_1_Xut.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_6_1_aud.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_5_1_OIK.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_4_1_wsi.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_3_1_lUQ.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_2_1_zRT.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_20_1_1zy.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_1_1_6Se.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_19_1_f6z.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_18_1_p3e.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_17_1_M3O.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_16_1_BQz.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_15_1_q0l.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_14_1_zWu.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_13_1_LEo.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_12_1_xxl.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_11_1_10q.root',
-        '/store/user/jlange/MuHad/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_10_1_4FE.root'
+        '/store/user/jlange/SingleMu/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_153_1_JeP.root',
         '/store/user/jlange/SingleMu/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_152_1_krE.root',
         '/store/user/jlange/SingleMu/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_151_1_mSl.root',
         '/store/user/jlange/SingleMu/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_150_1_E10.root',
@@ -237,6 +218,7 @@ readFiles.extend( [
         '/store/user/jlange/SingleMu/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_102_1_3QA.root',
         '/store/user/jlange/SingleMu/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_101_1_TND.root',
         '/store/user/jlange/SingleMu/PAT_Data2011_PromptReco_UHH/3db61b3449ae3442edf2fbc560a5ed3b/Data2011_PromptReco_100_1_X2F.root'
+
 
 
       ] );
