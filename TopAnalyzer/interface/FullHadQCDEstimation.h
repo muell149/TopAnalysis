199b338d616d64c5b6a7cfeb63044081e418c69e
eschliec <>
2011-06-07 10:29:51 +0000

    switched FullHadQCDEstimation from SingleObjectAnalyzer to normal EDAnalyzer

--- TopAnalyzer/interface/FullHadQCDEstimation.h
+++ TopAnalyzer/interface/FullHadQCDEstimation.h
@@ -1,9 +1,18 @@
 #ifndef FullHadQCDEstimation_h
 #define FullHadQCDEstimation_h
 
-#include "DataFormats/PatCandidates/interface/Jet.h"
-#include "TopAnalysis/TopAnalyzer/interface/SingleObject.h"
-#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TTree.h"
+
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+//#include "DataFormats/PatCandidates/interface/Jet.h"
+//#include "TopAnalysis/TopAnalyzer/interface/SingleObject.h"
+//#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
 #include "TopQuarkAnalysis/TopKinFitter/interface/TtFullHadKinFitter.h"
 
 /**
@@ -17,30 +26,50 @@
    fwlite. 
 */
 
-class FullHadQCDEstimation : public SingleObject<const edm::View<pat::Jet> > {
+class FullHadQCDEstimation : public edm::EDAnalyzer {
+  //class FullHadQCDEstimation : public SingleObject<const edm::View<pat::Jet> > {
 
  public:
   /// default constructor for fw lite
-  explicit FullHadQCDEstimation();
+  //explicit FullHadQCDEstimation();
   /// default constructor for full fw
   explicit FullHadQCDEstimation(const edm::ParameterSet& configFile);
   /// default destructor
   ~FullHadQCDEstimation(){};
 
-  /**
-     The following functions have to be implemented for any class
-     derived from SingleObject<Collection>
-  **/
-  /// histogramm booking for fwlite 
-  void book();
-  /// histogramm booking for fwfull
-  void book(edm::Service<TFileService>& fileService);
-  /// histogram filling for fwlite and for fwfull from reco objects
-  void fill(const edm::View<pat::Jet>& jets, const double& weight=1.);
-  /// everything which needs to be done after the event loop
-  void process() {};
+  ///**
+  //   The following functions have to be implemented for any class
+  //   derived from SingleObject<Collection>
+  //**/
+  ///// histogramm booking for fwlite 
+  //void book();
+  ///// histogramm booking for fwfull
+  //void book(edm::Service<TFileService>& fileService);
+  ///// histogram filling for fwlite and for fwfull from reco objects
+  //void fill(const edm::View<pat::Jet>& jets, const double& weight=1.);
+  ///// everything which needs to be done after the event loop
+  //void process() {};
 
  private:
+  /// initiate histograms
+  virtual void beginJob();
+  /// produce n-tuple
+  virtual void analyze(const edm::Event& event, const edm::EventSetup& iSetup);
+  /// empty
+  virtual void endJob();
+
+  /// book histograms or tree variables
+  void bookVariable(edm::Service<TFileService>& fs, const char * variable,
+		    unsigned int binsX, float lowX, float upX, unsigned int binsY, float lowY, float upY,
+		    bool useTree);
+  void bookVariable(edm::Service<TFileService>& fs, const char * variable, unsigned int binsX, float lowX, float upX, bool useTree);
+  void bookVariable(edm::Service<TFileService>& fs, const char * variable);
+  /// fill values into map for histograms or tree
+  void fillValue(std::string variable, double value1, const double& weight);
+
+  /// src's for the different infos
+  edm::InputTag JetSrc_, FitSrc_, MultiJetMVADiscSrc_;
+
   /// produce a TTree as output instead of histograms
   bool useTree_;
   /// b-tagging algo and working point to be emulated
@@ -51,6 +80,56 @@ class FullHadQCDEstimation : public SingleObject<const edm::View<pat::Jet> > {
 
   /// kinematic fit interface
   TtFullHadKinFitter::KinFit* kinFitter;
+
+  /// histogram container
+  std::map<std::string, TH1*> hists_;
+  std::map<std::string, TH2*> hists2D_;
+  std::map<std::string, double> treeVars_;
+  TTree * tree;
 };
+
+/// book histograms or tree variables
+void FullHadQCDEstimation::bookVariable(edm::Service<TFileService>& fs, const char * variable,
+					unsigned int binsX, float lowX, float upX, unsigned int binsY, float lowY, float upY,
+					bool useTree=false)
+{
+  if(useTree && !binsY){
+    //std::cout << "Adding *" << variable << "* to TTree" << std::endl;
+    if(!tree){
+      tree = fs->make<TTree>("tree","tree",0);
+      treeVars_["weight"];
+      tree->Branch("weight", &treeVars_["weight"], (std::string("weight") + "/D").c_str());
+    }
+    treeVars_[variable];
+    tree->Branch(variable, &treeVars_[variable], (std::string(variable) + "/D").c_str());
+  }
+  else{
+    //std::cout << "Adding *" << variable << "* to Histograms" << std::endl;
+    if     (!binsY &&  !lowY && !upY )  hists_  [variable] = fs->make<TH1F>( variable, variable, binsX, lowX, upX );
+    else if( binsY && ( lowY ||  upY )) hists2D_[variable] = fs->make<TH2F>( variable, variable, binsX, lowX, upX, binsY, lowY, upY );
+  }
+}
+
+void FullHadQCDEstimation::bookVariable(edm::Service<TFileService>& fs, const char * variable,
+					unsigned int binsX, float lowX, float upX, bool useTree=false)
+{
+  bookVariable( fs, variable, binsX, lowX, upX, 0, 0, 0, useTree);
+}
+
+void FullHadQCDEstimation::bookVariable(edm::Service<TFileService>& fs, const char * variable)
+{
+  bookVariable( fs, variable, 0, 0, 0, 0, 0, 0, true);
+}
+
+void FullHadQCDEstimation::fillValue(std::string variable, double value, const double& weight=1.)
+{
+  if(treeVars_.find(variable) != treeVars_.end()){
+    treeVars_.find(variable)->second = value;
+    treeVars_.find("weight")->second = weight;
+  }
+  if(hists_.find(variable) != hists_.end()){
+    hists_.find(variable)->second->Fill(value, weight);
+  }
+}
 
 #endif
--- TopAnalyzer/python/FullHadQCDEstimation_cfi.py
+++ TopAnalyzer/python/FullHadQCDEstimation_cfi.py
@@ -2,26 +2,30 @@ import FWCore.ParameterSet.Config as cms
 
 from TopQuarkAnalysis.TopObjectResolutions.stringResolutions_etEtaPhi_cff import *
 
-analyzeFullHadQCDEstimation = cms.EDAnalyzer("FullHadQCDEstimationAnalyzer",
+analyzeFullHadQCDEstimation = cms.EDAnalyzer("FullHadQCDEstimation",
     ## input collection                             
-    src = cms.InputTag("selectedPatJets"),
+        
+    ## src of jets
+    JetSrc             = cms.InputTag("selectedPatJets"),
+    ## src of multijet mva discriminator
+    MultiJetMVADiscSrc = cms.InputTag("mvaDisc","DiscSel"),
+
     ## special parameters for 
     ## QCD background estimation for
     ## fully hadronic ttbar events
-    analyze   = cms.PSet(
-      ## choose TTree for output instead of histograms, if applicable
-      useTree    = cms.bool(False),
-      ## choose b-tagging algorithm and working point to be used
-      ## available are the following so far:
-      ## 1. TCHEM (TCHE algo @ medium WP)
-      ## 2. TCHPT (TCHP algo @ tight WP)
-      bTagAlgoWP = cms.string("TCHEM"),
-      # ------------------------------------------------
-      # resolutions used for the kinematic fit
-      # ------------------------------------------------
-      udscResolutions = udscResolution.functions,
-      bResolutions    = bjetResolution.functions
-    )
+
+    ## choose TTree for output instead of histograms, if applicable
+    useTree    = cms.bool(False),
+    ## choose b-tagging algorithm and working point to be used
+    ## available are the following so far:
+    ## 1. TCHEM (TCHE algo @ medium WP)
+    ## 2. TCHPT (TCHP algo @ tight WP)
+    bTagAlgoWP = cms.string("TCHEM"),
+    # ------------------------------------------------
+    # resolutions used for the kinematic fit
+    # ------------------------------------------------
+    udscResolutions = udscResolution.functions,
+    bResolutions    = bjetResolution.functions
 )
 
 
