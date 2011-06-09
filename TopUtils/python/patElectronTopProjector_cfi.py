ce28a10104164a4877a8519e67a807af44d3a3b4
eschliec <>
2011-06-09 16:37:00 +0000

    made the electronTopProjector work with electrons and not with muons

--- TopUtils/python/patElectronTopProjector_cfi.py
+++ TopUtils/python/patElectronTopProjector_cfi.py
@@ -1,10 +1,10 @@
 import FWCore.ParameterSet.Config as cms
 
 pfNoPatElectrons = cms.EDProducer(
-    "TPPatMuonsOnPFCandidates",
+    "TPPatElectronsOnPFCandidates",
     enable =  cms.bool( True ),
     verbose = cms.untracked.bool( False ),
-    name = cms.untracked.string("noTrackMuon"),
+    name = cms.untracked.string("noTrackElectron"),
     topCollection = cms.InputTag("selectedPatElectrons"),
     bottomCollection = cms.InputTag("pfNoPileUp"),
 )
