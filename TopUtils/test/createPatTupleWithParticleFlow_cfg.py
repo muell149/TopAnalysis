d37f337bccf8f6e61a4cd96c903967fa4f5471b8
eschliec <>
2011-06-14 15:47:45 +0000

    moved muon production to the analysis level, made runOnMC, isTTBarMC and globalTag configurable options from outside via commandline or crab

--- TopUtils/test/createPatTupleWithParticleFlow_cfg.py
+++ TopUtils/test/createPatTupleWithParticleFlow_cfg.py
@@ -1,13 +1,38 @@
+import FWCore.ParameterSet.VarParsing as VarParsing
+import sys
+
+# setup 'standard' options
+options = VarParsing.VarParsing ('standard')
+## decide whether to run on MC or data
+options.register('runOnMC', True, VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.bool, "decide to run on MC or data")
+## is the MC ttbar (has no effect once runOnMC==False)
+options.register('isTTBarMC', False, VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.bool, "decide to produce ttGenEvent")
+## global Tag to be used (if left empty pat default tag is used)
+options.register('globalTag', '', VarParsing.VarParsing.multiplicity.singleton, VarParsing.VarParsing.varType.string, "which globalTag should be used")
+
+# get and parse the command line arguments
+if( hasattr(sys, "argv") ):
+    for args in sys.argv :
+        arg = args.split(',')
+        for val in arg:
+            val = val.split('=')
+            if(len(val)==2):
+                setattr(options,val[0], val[1])
+
 ## import skeleton process
 from PhysicsTools.PatAlgos.patTemplate_cfg import *
 
+if not options.globalTag == '':
+    process.GlobalTag.globaltag = options.globalTag + '::All'
+
+### test configuration
+#process.source.fileNames = cms.untracked.vstring()
+#process.source.fileNames += 'file:/scratch/hh/current/cms/user/eschliec/TTBar_pythia6_Summer11/7ABCBB94-E67D-E011-9B16-00151796D80C.root',
+#process.source.fileNames += 'file:/scratch/hh/current/cms/user/eschliec/TTBar_pythia6_Summer11/4A5D814A-E67D-E011-9052-0015178C0190.root',
+#process.maxEvents.input = cms.untracked.int32(-1)
+
 ## run the particle flow classification as far as appropriate
 process.load("TopAnalysis.TopUtils.particleFlowSetup_cff")
-
-## adapt PAT to it
-from PhysicsTools.PatAlgos.tools.pfTools import adaptPFMuons
-adaptPFMuons(process, process.patMuons, '')
-process.patMuons.pfMuonSource = "pfSelectedMuons"
 
 ## create a good vertex collection
 pvSelection = cms.PSet(
@@ -30,85 +55,100 @@ process.goodOfflinePrimaryVerticesWithBS = cms.EDFilter(
     , src          = cms.InputTag('offlinePrimaryVerticesWithBS')
     )
 
-## use only good vertices for pfNoPileUp and muons
+## use only good vertices for pfNoPileUp
 process.pfPileUp.Vertices          = cms.InputTag('goodOfflinePrimaryVertices')
-process.pfMuonsFromVertex.vertices = cms.InputTag('goodOfflinePrimaryVertices')
 
-## adapt isolation cone for muons and electrons
-process.isoValMuonWithNeutral.deposits[0].deltaR = cms.double(0.3)
-process.isoValMuonWithCharged.deposits[0].deltaR = cms.double(0.3)
-process.isoValMuonWithPhotons.deposits[0].deltaR = cms.double(0.3)
+if options.runOnMC:
 
-## use beamspot instead of primary vertex for the muon creation
-process.patMuons.usePV = False
+    # reduce size
+    process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
+    process.genParticles = cms.EDProducer(
+        #see https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideGenParticlePruner
+        "GenParticlePruner",
+        src = cms.InputTag("genParticles"),
+        select = cms.vstring(
+            "drop  *  ",
+            "++keep++ pdgId = {Z0}",
+            "++keep++ pdgId = {W+}",   "++keep++ pdgId = {W-}",
+            "++keep++ pdgId = {t}",    "++keep++ pdgId = {tbar}",
+            "++keep++ pdgId = {b}",    "++keep++ pdgId = {bbar}",
+            "++keep++ pdgId = {c}",    "++keep++ pdgId = {cbar}",
+            # "drop pdgId = {Z0} & status = 2"
+            "++keep++ pdgId = {e+}",   "++keep++ pdgId = {e-}",
+            "++keep++ pdgId = {mu+}",  "++keep++ pdgId = {mu-}",
+            "++keep++ pdgId = {tau+}", "++keep++ pdgId = {tau-}",
+            )
+        )
+    
+    ## needed for redoing the ak5GenJets
+    process.load("TopAnalysis.TopUtils.GenJetParticles_cff")
+    process.load("RecoJets.Configuration.RecoGenJets_cff")
 
-process.patSequence = cms.Sequence(process.muonMatch *
-                                   process.patMuons  *
-                                   process.selectedPatMuons
-                                   )
+    ## make sure the genJets are clustered from the full genParticle collection and not the pruned one
+    process.genParticlesForJets.src = cms.InputTag("genParticles","","HLT")
+    
+    if options.isTTBarMC:
+        ## std sequence to produce the ttGenEvt
+        process.load("TopQuarkAnalysis.TopEventProducers.sequences.ttGenEvent_cff")
 
-# reduce size
-process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
-process.genParticles = cms.EDProducer(
-    #see https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideGenParticlePruner
-    "GenParticlePruner",
-    src = cms.InputTag("genParticles"),
-    select = cms.vstring(
-        "drop  *  ",
-        "++keep++ pdgId = {Z0}",
-        "++keep++ pdgId = {W+}",   "++keep++ pdgId = {W-}",
-        "++keep++ pdgId = {t}",    "++keep++ pdgId = {tbar}",
-        "++keep++ pdgId = {b}",    "++keep++ pdgId = {bbar}",
-        "++keep++ pdgId = {c}",    "++keep++ pdgId = {cbar}",
-        # "drop pdgId = {Z0} & status = 2"
-        "++keep++ pdgId = {e+}",   "++keep++ pdgId = {e-}",
-        "++keep++ pdgId = {mu+}",  "++keep++ pdgId = {mu-}",
-        "++keep++ pdgId = {tau+}", "++keep++ pdgId = {tau-}",
-    )
-)
-
-## needed for redoing the ak5GenJets
-process.load("TopAnalysis.TopUtils.GenJetParticles_cff")
-process.load("RecoJets.Configuration.RecoGenJets_cff")
-
-## make sure the genJets are clustered from the full genParticle collection and not the pruned one
-process.genParticlesForJets.src = cms.InputTag("genParticles","","HLT")
-
-## std sequence to produce the ttGenEvt
-process.load("TopQuarkAnalysis.TopEventProducers.sequences.ttGenEvent_cff")
+        ## make sure the ttGenEvent is made from the full genParticle collection and not the pruned one
+        process.initSubset.src  = cms.InputTag("genParticles","","HLT")
+        process.decaySubset.src = cms.InputTag("genParticles","","HLT")
 
 ## let it run
 process.p = cms.Path(
-    process.makeGenEvt      *
     process.goodOfflinePrimaryVertices *
     process.goodOfflinePrimaryVerticesWithBS *
-    process.particleFlow    *
-    process.patSequence     *
-    process.genParticles    *
-    process.genJetParticles *
-    process.ak5GenJets
-)
+    process.pfPileUp *
+    process.pfNoPileUp
+    )
 
-process.out.outputCommands+= ['keep recoGenParticles_genParticles_*_PAT']
-#process.out.outputCommands+= ['keep *_genParticles_*_*']
+if options.runOnMC:
+    if options.isTTBarMC:
+        process.p += process.makeGenEvt
+
+    process.p += process.genParticles
+    process.p += process.genJetParticles
+    process.p += process.ak5GenJets
+
+
 process.out.outputCommands+= ['keep *DcsStatus*_*_*_*']
+## all tracks in the event
 process.out.outputCommands+= ['keep recoTracks_generalTracks_*_*']
-process.out.outputCommands+= ['keep *_*Gsf*_*_*']
-process.out.outputCommands+= ['keep *_*gsf*_*_*']
-process.out.outputCommands+= ['keep *SuperCluster*_*_*_*']
+## beamspot und primary vertices
 process.out.outputCommands+= ['keep *_offlineBeamSpot_*_*']
 process.out.outputCommands+= ['keep *_offlinePrimaryVertices*_*_*']
 process.out.outputCommands+= ['keep *_goodOfflinePrimaryVertices*_*_*']
+## collections for PF2PAT
 process.out.outputCommands+= ['keep *_pfPileUp_*_*']
 process.out.outputCommands+= ['keep *_pfNoPileUp_*_*']
-#process.out.outputCommands+= ['keep *_particleFlow__*']
-process.out.outputCommands+= ["keep *_genMetTrue_*_*"]
-process.out.outputCommands+= ['keep recoGenJets_ak5GenJets_*_PAT']
-process.out.outputCommands+= ['keep *_genEvt_*_*']
-process.out.outputCommands+= ['keep *_initSubset_*_*']
-process.out.outputCommands+= ['keep *_decaySubset_*_*']
-process.out.outputCommands+= ['keep *_addPileupInfo_*_*']
+## trigger information
 process.out.outputCommands+= ['keep edmTriggerResults_*_*_*']
 process.out.outputCommands+= ['keep *_hltTriggerSummaryAOD_*_*']
-process.out.outputCommands+= ['keep GenEventInfoProduct_*_*_*']
-process.out.outputCommands+= ['keep *_selectedPatMuons_*_*']
+## data needed for muons
+process.out.outputCommands+= ['keep *_muons__*']
+process.out.outputCommands+= ['keep *_*_muCorrData_*']
+process.out.outputCommands+= ['keep *_tevMuons_*_*']
+process.out.outputCommands+= ['keep *_globalMuons__*']
+process.out.outputCommands+= ['keep *_standAloneMuons_*_*']
+## data needed for electrons
+process.out.outputCommands+= ['keep *_*Gsf*_*_*']
+process.out.outputCommands+= ['keep *_*gsf*_*_*']
+process.out.outputCommands+= ['keep *SuperCluster*_*_*_*']
+## generator information
+if options.runOnMC:
+    ## skimmed genParticles
+    process.out.outputCommands+= ['keep recoGenParticles_genParticles_*_PAT']
+    ## ak5GenJets
+    process.out.outputCommands+= ['keep recoGenJets_ak5GenJets_*_PAT']
+    ## genMET
+    process.out.outputCommands+= ["keep *_genMetTrue_*_*"]
+    ## pile-up information
+    process.out.outputCommands+= ['keep *_addPileupInfo_*_*']
+    ## further info on generated event (like pt-hat)
+    process.out.outputCommands+= ['keep GenEventInfoProduct_*_*_*']
+    ## ttGenEvent
+    if options.isTTBarMC:
+        process.out.outputCommands+= ['keep *_genEvt_*_*']
+        process.out.outputCommands+= ['keep *_initSubset_*_*']
+        process.out.outputCommands+= ['keep *_decaySubset_*_*']
