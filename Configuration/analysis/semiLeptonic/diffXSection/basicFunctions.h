2d0adf9b459b8b4b22083df48d041642966cd88d
mgoerner <>
2011-07-04 16:11:12 +0000

    first attemp to switch to tdr style: plug content as function in here, when dealing with binwidth: set binwidth to 1 when it is 0 e.g. to pick up also events in underflow/overflow bins when counting

--- Configuration/analysis/semiLeptonic/diffXSection/basicFunctions.h
+++ Configuration/analysis/semiLeptonic/diffXSection/basicFunctions.h
@@ -48,6 +48,165 @@ enum systematicVariation {/* 0:*/sysNo          , /* 1:*/sysLumiUp       , /* 2:
 bool newSpring11MC=true;
 bool newSummer11MC=false;
 
+
+#include "TStyle.h"
+
+// tdrGrid: Turns the grid lines on (true) or off (false)
+
+// void tdrGrid(bool gridOn) {
+//   tdrStyle->SetPadGridX(gridOn);
+//   tdrStyle->SetPadGridY(gridOn);
+// }
+
+// fixOverlay: Redraws the axis
+
+void fixOverlay() {
+  gPad->RedrawAxis();
+}
+
+void setTDRStyle() {
+  TStyle *tdrStyle = new TStyle("tdrStyle","Style for P-TDR");
+
+// For the canvas:
+  tdrStyle->SetCanvasBorderMode(0);
+  tdrStyle->SetCanvasColor(kWhite);
+  tdrStyle->SetCanvasDefH(600); //Height of canvas
+  tdrStyle->SetCanvasDefW(600); //Width of canvas
+  tdrStyle->SetCanvasDefX(0);   //POsition on screen
+  tdrStyle->SetCanvasDefY(0);
+
+// For the Pad:
+  tdrStyle->SetPadBorderMode(0);
+  // tdrStyle->SetPadBorderSize(Width_t size = 1);
+  tdrStyle->SetPadColor(kWhite);
+  tdrStyle->SetPadGridX(false);
+  tdrStyle->SetPadGridY(false);
+  tdrStyle->SetGridColor(0);
+  tdrStyle->SetGridStyle(3);
+  tdrStyle->SetGridWidth(1);
+
+// For the frame:
+  tdrStyle->SetFrameBorderMode(0);
+  tdrStyle->SetFrameBorderSize(1);
+  tdrStyle->SetFrameFillColor(0);
+  tdrStyle->SetFrameFillStyle(0);
+  tdrStyle->SetFrameLineColor(1);
+  tdrStyle->SetFrameLineStyle(1);
+  tdrStyle->SetFrameLineWidth(1);
+
+// For the histo:
+  // tdrStyle->SetHistFillColor(1);
+  // tdrStyle->SetHistFillStyle(0);
+  tdrStyle->SetHistLineColor(1);
+  tdrStyle->SetHistLineStyle(0);
+  tdrStyle->SetHistLineWidth(1);
+  // tdrStyle->SetLegoInnerR(Float_t rad = 0.5);
+  // tdrStyle->SetNumberContours(Int_t number = 20);
+
+  tdrStyle->SetEndErrorSize(2);
+  // tdrStyle->SetErrorMarker(20);
+  tdrStyle->SetErrorX(0.);
+  
+  tdrStyle->SetMarkerStyle(20);
+
+//For the fit/function:
+  tdrStyle->SetOptFit(1);
+  tdrStyle->SetFitFormat("5.4g");
+  tdrStyle->SetFuncColor(2);
+  tdrStyle->SetFuncStyle(1);
+  tdrStyle->SetFuncWidth(1);
+
+//For the date:
+  tdrStyle->SetOptDate(0);
+  // tdrStyle->SetDateX(Float_t x = 0.01);
+  // tdrStyle->SetDateY(Float_t y = 0.01);
+
+// For the statistics box:
+  tdrStyle->SetOptFile(0);
+  tdrStyle->SetOptStat(0); // To display the mean and RMS:   SetOptStat("mr");
+  tdrStyle->SetStatColor(kWhite);
+  tdrStyle->SetStatFont(42);
+  tdrStyle->SetStatFontSize(0.025);
+  tdrStyle->SetStatTextColor(1);
+  tdrStyle->SetStatFormat("6.4g");
+  tdrStyle->SetStatBorderSize(1);
+  tdrStyle->SetStatH(0.1);
+  tdrStyle->SetStatW(0.15);
+  // tdrStyle->SetStatStyle(Style_t style = 1001);
+  // tdrStyle->SetStatX(Float_t x = 0);
+  // tdrStyle->SetStatY(Float_t y = 0);
+
+// Margins:
+  tdrStyle->SetPadTopMargin(0.05);
+  tdrStyle->SetPadBottomMargin(0.13);
+  tdrStyle->SetPadLeftMargin(0.16);
+  tdrStyle->SetPadRightMargin(0.02);
+
+// For the Global title:
+
+  tdrStyle->SetOptTitle(0);
+  tdrStyle->SetTitleFont(42);
+  tdrStyle->SetTitleColor(1);
+  tdrStyle->SetTitleTextColor(1);
+  tdrStyle->SetTitleFillColor(10);
+  tdrStyle->SetTitleFontSize(0.05);
+  // tdrStyle->SetTitleH(0); // Set the height of the title box
+  // tdrStyle->SetTitleW(0); // Set the width of the title box
+  // tdrStyle->SetTitleX(0); // Set the position of the title box
+  // tdrStyle->SetTitleY(0.985); // Set the position of the title box
+  // tdrStyle->SetTitleStyle(Style_t style = 1001);
+  // tdrStyle->SetTitleBorderSize(2);
+
+// For the axis titles:
+
+  tdrStyle->SetTitleColor(1, "XYZ");
+  tdrStyle->SetTitleFont(42, "XYZ");
+  tdrStyle->SetTitleSize(0.06, "XYZ");
+  // tdrStyle->SetTitleXSize(Float_t size = 0.02); // Another way to set the size?
+  // tdrStyle->SetTitleYSize(Float_t size = 0.02);
+  tdrStyle->SetTitleXOffset(0.9);
+  tdrStyle->SetTitleYOffset(1.25);
+  // tdrStyle->SetTitleOffset(1.1, "Y"); // Another way to set the Offset
+
+// For the axis labels:
+
+  tdrStyle->SetLabelColor(1, "XYZ");
+  tdrStyle->SetLabelFont(42, "XYZ");
+  tdrStyle->SetLabelOffset(0.007, "XYZ");
+  tdrStyle->SetLabelSize(0.05, "XYZ");
+
+// For the axis:
+
+  tdrStyle->SetAxisColor(1, "XYZ");
+  tdrStyle->SetStripDecimals(kTRUE);
+  tdrStyle->SetTickLength(0.03, "XYZ");
+  tdrStyle->SetNdivisions(510, "XYZ");
+  tdrStyle->SetPadTickX(1);  // To get tick marks on the opposite side of the frame
+  tdrStyle->SetPadTickY(1);
+
+// Change for log plots:
+  tdrStyle->SetOptLogx(0);
+  tdrStyle->SetOptLogy(0);
+  tdrStyle->SetOptLogz(0);
+
+// Postscript options:
+  tdrStyle->SetPaperSize(20.,20.);
+  // tdrStyle->SetLineScalePS(Float_t scale = 3);
+  // tdrStyle->SetLineStyleString(Int_t i, const char* text);
+  // tdrStyle->SetHeaderPS(const char* header);
+  // tdrStyle->SetTitlePS(const char* pstitle);
+
+  // tdrStyle->SetBarOffset(Float_t baroff = 0.5);
+  // tdrStyle->SetBarWidth(Float_t barwidth = 0.5);
+  // tdrStyle->SetPaintTextFormat(const char* format = "g");
+  // tdrStyle->SetPalette(Int_t ncolors = 0, Int_t* colors = 0);
+  // tdrStyle->SetTimeOffset(Double_t toffset);
+  // tdrStyle->SetHistMinimumZero(kTRUE);
+
+  tdrStyle->cd();
+
+}
+
 TString sysLabel(unsigned int sys)
 {
   // this function returns a TString that corresponds 
@@ -168,6 +327,7 @@ void histogramStyle(TH1& hist, int sampleTyp, bool filled=true, double markersiz
     hist.SetLineColor(color);
     hist.SetFillColor(color);
   }
+  setTDRStyle();
 
 }
 
@@ -242,6 +402,7 @@ void canvasStyle(TCanvas& canv)
   canv.SetRightMargin ( 0.05 );
   canv.SetBottomMargin( 0.15 );
   canv.SetTopMargin   ( 0.05 );
+  setTDRStyle();
 }
 
 void axesStyle(TH1& hist, const char* titleX, const char* titleY, float yMin=-123, float yMax=-123, float titleSize=0.05, float yTitleOffset=1.2, float labelSize = 0.05)
@@ -278,6 +439,7 @@ void axesStyle(TH1& hist, const char* titleX, const char* titleY, float yMin=-12
   //hist.GetYaxis()->CenterTitle   ( true);
   if(yMin!=-123) hist.SetMinimum(yMin);
   if(yMax!=-123) hist.SetMaximum(yMax);
+  setTDRStyle();
 }
 
 template <class T>
@@ -407,13 +569,12 @@ TH1F* divideByBinwidth(TH1F* histo, bool calculateError=true)
     // take care of bins width width 0
     // (these could not be filled and 
     // exist because of technical reasons only)
-    if(binwidth!=0){
-      // recalculate error
-      double error = (double)(histo->GetBinError(i)) / binwidth;
-      // recalculate bin content
-      outputHisto->SetBinContent(i, ( (double)(histo->GetBinContent(i)) / binwidth) );
-      if(calculateError) outputHisto->SetBinError(i, error);
-    }
+    if(binwidth==0)binwidth=1;
+    // recalculate error
+    double error = (double)(histo->GetBinError(i)) / binwidth;
+    // recalculate bin content
+    outputHisto->SetBinContent(i, ( (double)(histo->GetBinContent(i)) / binwidth) );
+    if(calculateError) outputHisto->SetBinError(i, error);
   }
   // return result
   return outputHisto;
@@ -1265,6 +1426,7 @@ void DivideYieldByEfficiencyAndLumi(TH1F* yield, TH1F* efficiency, double lumino
     double content  = yield     ->GetBinContent(bin);
     double eff      = efficiency->GetBinContent(bin);
     double binwidth = yield     ->GetBinWidth  (bin);
+    if(binwidth==0) binwidth=1;
     double events   = binwidth*content;
     // value (events/binwidth/eff/lumi)
     double xSec = content/(eff*luminosity);
@@ -1496,7 +1658,9 @@ double getInclusiveXSec(TH1* hist, int verbose=0)
   double value=0;
   if(verbose>0) std::cout << "histo: " << hist->GetTitle() << std::endl;
   for(int bin=0; bin<=hist->GetNbinsX()+1; ++bin){
-    double thisBin=hist->GetBinWidth(bin)*hist->GetBinContent(bin);
+    double binWidth=hist->GetBinWidth(bin);
+    if(binWidth==0.) binWidth=1.;
+    double thisBin=binWidth*hist->GetBinContent(bin);
     value+=thisBin;
     if(verbose>1) std::cout << "xSec bin" << bin << ": " << thisBin << std::endl;
   }
