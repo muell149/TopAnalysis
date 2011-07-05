553aa51bc27d88a6ff448f229d4a2c5520adb1e1
eschliec <>
2011-07-05 17:34:09 +0000

    renamed default files to available ones

--- TopUtils/python/EventWeightPU_cfi.py
+++ TopUtils/python/EventWeightPU_cfi.py
@@ -3,9 +3,9 @@ import FWCore.ParameterSet.Config as cms
 eventWeightPU = cms.EDProducer("EventWeightPU",
                                
     PUSource          = cms.InputTag("addPileupInfo"),
-    MCSampleFile      = cms.FileInPath("TopAnalysis/TopUtils/data/MC_PUDist.root"),
+    MCSampleFile      = cms.FileInPath("TopAnalysis/TopUtils/data/MC_PUDist_TTbar.root"),
     MCSampleHistoName = cms.string("pileup"),
-    DataFile          = cms.FileInPath("TopAnalysis/TopUtils/data/Data_PUDist_110527.root"),
+    DataFile          = cms.FileInPath("TopAnalysis/TopUtils/data/Data_PUDist_160404-166861_7TeV_PromptReco_Collisions11.root"),
     DataHistoName     = cms.string("pileup")          
 )
 
