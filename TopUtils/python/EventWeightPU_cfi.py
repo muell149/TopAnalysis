18902394b33e1e67c41aa4df06574c05ecaca530
hermanns <>
2011-07-12 18:06:16 +0000

    Adapted tro new structure of output of MCPileUp analyzer

--- TopUtils/python/EventWeightPU_cfi.py
+++ TopUtils/python/EventWeightPU_cfi.py
@@ -3,9 +3,9 @@ import FWCore.ParameterSet.Config as cms
 eventWeightPU = cms.EDProducer("EventWeightPU",
                                
     PUSource          = cms.InputTag("addPileupInfo"),
-    MCSampleFile      = cms.FileInPath("TopAnalysis/TopUtils/data/MC_PUDist_TTbar.root"),
-    MCSampleHistoName = cms.string("pileup"),
-    DataFile          = cms.FileInPath("TopAnalysis/TopUtils/data/Data_PUDist_160404-166861_7TeV_PromptReco_Collisions11.root"),
+    MCSampleFile      = cms.FileInPath("TopAnalysis/TopUtils/data/MC_PUDist_Summer11_TTJets_TuneZ2_7TeV_madgraph_tauola.root"),
+    MCSampleHistoName = cms.string("MCPUDistribution/pileup"),
+    DataFile          = cms.FileInPath("TopAnalysis/TopUtils/data/Data_PUDist_160404-163869_7TeV_May10ReReco_Collisions11_v2_and_165088-167913_7TeV_PromptReco_Collisions11.root"),
     DataHistoName     = cms.string("pileup"),          
     PUSysShiftUp      = cms.double(0.6),
     PUSysShiftDown    = cms.double(-0.6)                           
