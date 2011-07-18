2f7c2d689c4110db93943bec76de5191860c16d6
wbehrenh <>
2011-07-18 13:36:10 +0000

    dont apply PU on realdata; raise exception if no PU info in MC

--- TopAnalyzer/src/PUEventWeight.cc
+++ TopAnalyzer/src/PUEventWeight.cc
@@ -1,8 +1,12 @@
 #include "TopAnalysis/TopAnalyzer/interface/PUEventWeight.h"
 
+/// return the PU event weight for the MC events;
+/// return 1 for data, raise exception if weight does not exist in MC
 double getPUEventWeight(const edm::Event& evt, const edm::InputTag& tag) {
+    if (evt.isRealData()) return 1;
     edm::Handle<double> weightHandle;
     if (tag.label().empty()) return 1; //no weight
     evt.getByLabel(tag, weightHandle);
+    // if (weightHandle.failedToGet()) return 1; //no weight
     return *weightHandle;
 }
