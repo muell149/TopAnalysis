aa9c169148455ba5c24e5a3cf94dabeb2d36ded7
wbehrenh <>
2011-11-30 09:52:46 +0000

    committed one file from the ttjets sample that can usuallz be read fast

---
+++ Configuration/python/ttjets_1file.py
@@ -0,0 +1,16 @@
+import FWCore.ParameterSet.Config as cms
+
+#####################################################################################
+# Dataset: /TTJets_TuneZ2_7TeV-madgraph-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM #
+# Events : 3701947                                                                  # 
+# eff    : 1.0                                                                      # 
+#####################################################################################
+
+
+maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+readFiles = cms.untracked.vstring()
+secFiles = cms.untracked.vstring() 
+source = cms.Source ("PoolSource", fileNames = cms.untracked.vstring(   
+'file:/scratch/hh/current/cms/user/wbehrenh/18202867-1D98-E011-A384-0018F3D09600.root'
+    ) 
+)
