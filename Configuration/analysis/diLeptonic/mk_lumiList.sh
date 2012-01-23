e396235aae13dcdd8c0e04d0ce1ed898d58e689a
wbehrenh <>
2012-01-23 09:05:10 +0000

    event count after totalKinematicsFilter

--- Configuration/analysis/diLeptonic/mk_lumiList.sh
+++ Configuration/analysis/diLeptonic/mk_lumiList.sh
@@ -1,17 +1,27 @@
 #!/bin/zsh
 
 for i in mumu emu ee
-
 do 
 
-mkdir -p OriginalRoot/$i
+    rm -f eventCount_$i.txt 2>/dev/null
+    mkdir -p OriginalRoot/$i
 
-cp naf*$i*/$i_*.root OriginalRoot/$i
-cp naf*$i*/$i\_*.txt OriginalRoot/$i
+    cp naf*$i*/$i_*.root OriginalRoot/$i
+    cp naf*$i*/$i\_*.txt OriginalRoot/$i
 
-ls OriginalRoot/$i/$i*.root >list_$i.txt
-cat list_$i.txt | awk -F $i\_ '{print $NF}' | awk '{sub(/.root/,"");print}'>samples_$i.txt
-cat list_$i.txt | awk '{sub(/.root/,".txt");print}' | xargs head -n2 | perl -nle '/Events total =\s*(\d+)/ && print $1' > eventCount_$i.txt
-paste list_$i.txt samples_$i.txt eventCount_$i.txt > lumi_$i.txt
+    ls OriginalRoot/$i/$i*.root >list_$i.txt
+    cat list_$i.txt | awk -F $i\_ '{print $NF}' | awk '{sub(/.root/,"");print}'>samples_$i.txt
+
+
+    for f in $(cat list_$i.txt | awk '{sub(/.root/,".txt");print}'); do 
+        kf=`grep 'totalKinematicsFilter' "$f" 2> /dev/null | head -n1`
+        if [ ! -z "$kf" ] ; then
+            echo "$kf" | awk '{print $5}' >> eventCount_$i.txt
+        else
+            perl -nle '/Events total =\s*(\d+)/ && print $1' < $f >> eventCount_$i.txt
+        fi
+    done
+
+    paste list_$i.txt samples_$i.txt eventCount_$i.txt > lumi_$i.txt
 
 done
