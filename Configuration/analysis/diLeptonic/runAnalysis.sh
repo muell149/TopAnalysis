f61d62ce41c9f54f196f981531cf5399e97fee1e
tdorland <>
2012-01-24 21:02:30 +0000

    little change

--- Configuration/analysis/diLeptonic/runAnalysis.sh
+++ Configuration/analysis/diLeptonic/runAnalysis.sh
@@ -11,7 +11,7 @@ foreach syst (Nominal JESUP JESDOWN RESUP RESDOWN PU_UP PU_DOWN)
       ls mergedRoot/$syst/$channel/*.root > selectionList.txt
       cp selectionList.txt FileLists/selectionList_$syst\_$channel.txt
 
-      root -l -b -q load_long.C
+      root -l -b -q load_Analysis.C
 
    end
    
