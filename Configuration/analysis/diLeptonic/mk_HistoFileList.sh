32d5fb4f6b2bb5b80c84973f12f947e4ba89a559
tdorland <>
2012-02-03 17:01:43 +0000

    change mc sample order for plots

--- Configuration/analysis/diLeptonic/mk_HistoFileList.sh
+++ Configuration/analysis/diLeptonic/mk_HistoFileList.sh
@@ -1,12 +1,13 @@
 #!/bin/zsh
 
 rm FileLists/Histo*
-#note that this order will be the order of the data samples in the stack
-foreach sample (run ttbarsignal ttbarbg single ww wz zz dytautau dyee dymumu wtolnu qcd)
+
+#foreach sample (run ttbarsignal ttbarbg single ww wz zz dytautau dyee dymumu wtolnu qcd)
+foreach sample (run qcd wtolnu dytautau dyee dymumu ww wz zz single ttbarbg ttbarsignal)
 
    foreach channel (ee emu mumu)
       
-      foreach Syst  (Nominal JESUP JESDOWN RESUP RESDOWN PU_UP PU_DOWN)
+      foreach Syst  (Nominal JESUP JESDOWN RESUP RESDOWN)
 
       ls -1 selectionRoot/$Syst/$channel/$sample*.root >> FileLists/HistoFileList_$Syst\_$channel.txt
       ls -1 selectionRoot/$Syst/$channel/$sample*.root >> FileLists/HistoFileList_$Syst\_combined.txt
