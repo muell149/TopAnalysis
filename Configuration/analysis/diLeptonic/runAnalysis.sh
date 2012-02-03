5ed92671ba224a1dc8d5d4ffd46411a13a755c89
tdorland <>
2012-02-03 04:32:35 +0000

    add Pileup systematics to selection

--- Configuration/analysis/diLeptonic/runAnalysis.sh
+++ Configuration/analysis/diLeptonic/runAnalysis.sh
@@ -2,7 +2,8 @@
 
 mkdir FileLists
 
-foreach syst (Nominal JESUP JESDOWN RESUP RESDOWN PU_UP PU_DOWN)
+foreach syst (Nominal JESUP JESDOWN RESUP RESDOWN)
+#foreach syst (Nominal)
 
    echo $syst > syst.txt
 
@@ -11,7 +12,24 @@ foreach syst (Nominal JESUP JESDOWN RESUP RESDOWN PU_UP PU_DOWN)
       ls mergedRoot/$syst/$channel/*.root > selectionList.txt
       cp selectionList.txt FileLists/selectionList_$syst\_$channel.txt
 
-      root -l -b -q load_Analysis.C
+      root -l -b -q load_long.C
+
+   end
+   
+   rm syst.txt
+
+end
+
+foreach syst (PU_UP PU_DOWN)
+
+   echo $syst > syst.txt
+
+   foreach channel (ee emu mumu)
+
+      ls mergedRoot/Nominal/$channel/*.root > selectionList.txt
+      cp selectionList.txt FileLists/selectionList_$syst\_$channel.txt
+
+      root -l -b -q load_long.C
 
    end
    
