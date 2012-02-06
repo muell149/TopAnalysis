f3f5338c7c7ddadf64c5fd3f207dca456a875fc4
tdorland <>
2012-02-06 22:07:03 +0000

    add signal systematics

--- Configuration/analysis/diLeptonic/mk_HistoFileList.sh
+++ Configuration/analysis/diLeptonic/mk_HistoFileList.sh
@@ -7,7 +7,7 @@ foreach sample (run qcd wtolnu dytautau dyee dymumu ww wz zz single ttbarbg ttba
 
    foreach channel (ee emu mumu)
       
-      foreach Syst  (Nominal JESUP JESDOWN RESUP RESDOWN)
+      foreach Syst  (Nominal JESUP JESDOWN RESUP RESDOWN PU_UP PU_DOWN SCALEUP SCALEDOWN MATCHUP MATCHDOWN MASSUP MASSDOWN)
 
       ls -1 selectionRoot/$Syst/$channel/$sample*.root >> FileLists/HistoFileList_$Syst\_$channel.txt
       ls -1 selectionRoot/$Syst/$channel/$sample*.root >> FileLists/HistoFileList_$Syst\_combined.txt
