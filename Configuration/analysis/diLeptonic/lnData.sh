c27d0459681f1c9d93af569506bfbbdd32fce511
dfischer <>
2012-02-13 14:13:22 +0000

    Bugfix

--- Configuration/analysis/diLeptonic/lnData.sh
+++ Configuration/analysis/diLeptonic/lnData.sh
@@ -2,10 +2,28 @@
 
 foreach channel (ee emu mumu)
      
-   foreach syst (JESDOWN  MASSDOWN  MATCHDOWN PU_DOWN PU_UP RESUP SCALEUP JESUP MASSUP MATCHUP PUDOWN PUUP RESDOWN SCALEDOWN)
+   foreach syst (JESDOWN  MASSDOWN  MATCHDOWN PU_DOWN PU_UP RESUP SCALEUP JESUP MASSUP MATCHUP RESDOWN SCALEDOWN)
 
-   ln -s ~/Analysis/selectionRoot/Nominal/$channel/* selectionRoot/$syst/$channel
-#   rm selectionRoot/$syst/$channel/run*
+   rm selectionRoot/$syst/$channel/run*
+   cd selectionRoot/$syst/$channel/
+   ln -s ../../Nominal/$channel/* .
+   cd ../../..
    end
 
 end
+
+rm selectionRoot/MATCHUP/*/ttbarbg.root
+rm selectionRoot/MATCHDOWN/*/ttbarbg.root
+rm selectionRoot/MASSUP/*/ttbarbg.root
+rm selectionRoot/MASSDOWN/*/ttbarbg.root
+rm selectionRoot/SCALEUP/*/ttbarbg.root
+rm selectionRoot/SCALEDOWN/*/ttbarbg.root
+
+rm selectionRoot/MATCHUP/*/ttbarsignalplustau.root
+rm selectionRoot/MATCHDOWN/*/ttbarsignalplustau.root
+rm selectionRoot/MASSUP/*/ttbarsignalplustau.root
+rm selectionRoot/MASSDOWN/*/ttbarsignalplustau.root
+rm selectionRoot/SCALEUP/*/ttbarsignalplustau.root
+rm selectionRoot/SCALEDOWN/*/ttbarsignalplustau.root
+
+
