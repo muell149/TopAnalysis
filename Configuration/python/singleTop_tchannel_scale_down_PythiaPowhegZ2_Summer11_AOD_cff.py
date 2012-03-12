ef723a5bbe93d7eb2965509fae499cdd05c1f286
hermanns <>
2012-03-12 15:12:14 +0000

    cff files for single top scale variations

--- Configuration/python/singleTop_tchannel_scale_down_PythiaPowhegZ2_Summer11_AOD_cff.py
+++ Configuration/python/singleTop_tchannel_scale_down_PythiaPowhegZ2_Summer11_AOD_cff.py
@@ -1,20 +1,78 @@
 import FWCore.ParameterSet.Config as cms
 
-################################################################################################
-# Dataset:
-# Events :
-# eff    : 1.0                                                                                 #
-################################################################################################
+#########################################################################################
+# Dataset: T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1  #
+# Events : 1041924                                                                      #
+# eff    : 1.0                                                                          #
+#########################################################################################
+import FWCore.ParameterSet.Config as cms
 
 maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring() 
 source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 readFiles.extend( [
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/00AFE1EA-6E5F-E111-83A2-001A64789470.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/0EB4D309-4B5F-E111-8DA1-003048D45F76.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/14CAB079-645F-E111-B3A5-0025902009CC.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/1601C77C-715F-E111-83A0-002590200988.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/16DD8701-645F-E111-88B3-001A6478935C.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/1A4D7A28-685F-E111-8C4B-003048D45FA8.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/1CBAB19D-6A5F-E111-A9FC-00E0817918D3.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/2A6F1454-485F-E111-8278-002590200A40.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/320E1229-655F-E111-911D-002590200814.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/3228045B-6D5F-E111-B15C-00304866C47A.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/3A206B3D-605F-E111-BFB7-00E08178C107.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/429137D0-465F-E111-B723-001A64789E6C.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/44C54868-765F-E111-83C2-003048D45FEA.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/464B381A-755F-E111-A652-002481E75CDE.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/5455EA7E-645F-E111-8BE3-0025902008C4.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/5857B32A-585F-E111-8491-0025902008F4.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/5C89C94A-4B5F-E111-B275-003048D46010.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/6092D7F8-635F-E111-92DC-001A64789E24.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/620A6B87-595F-E111-9A3A-001A64789D14.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/62226AB1-775F-E111-904A-003048D45F5C.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/660CF587-735F-E111-93FF-003048D47742.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/681C9DA3-4D5F-E111-A53B-003048D476AA.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/6A5C63AC-5A5F-E111-8C5C-0025B3E05E1C.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/6E961F35-505F-E111-B40B-002590200A28.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/6EE0BA44-655F-E111-A302-00E08179178D.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/76881927-655F-E111-8589-002590200930.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/7CF8F42D-625F-E111-AB2C-003048D47A42.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/800EEDCE-5E5F-E111-9C64-00E08178C175.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/844398EF-635F-E111-826B-002590200B6C.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/84973D44-525F-E111-B7BA-001A64789E40.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/8AC25AEA-495F-E111-82CE-001A6478A7B0.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/9C020C06-495F-E111-B40A-0025B3E05DCA.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/A612CCAF-565F-E111-B86A-003048D4608C.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/A6B140AC-5A5F-E111-BB77-00304867400E.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/AA806A5F-5A5F-E111-8550-00E08179188F.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/AAC489CA-595F-E111-8E20-001A64789D14.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/AC506D7A-745F-E111-ABB8-002590200978.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/AC8C7BEF-645F-E111-BDC9-0025902009E8.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/AEE115F5-635F-E111-8466-0025B3E063FA.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/B04848EB-635F-E111-962E-0025B3E05DB6.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/B207A844-535F-E111-AC77-0025B3E06468.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/B2D2C6EE-645F-E111-8401-00E08178C12F.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/B861CC50-6D5F-E111-B4F1-001A64789D20.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/BAEC72E0-635F-E111-8E6A-002590200938.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/BCC311D1-645F-E111-8078-002481E14F38.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/C4AF0FA8-645F-E111-8428-0025902008D0.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/C4B3755D-6D5F-E111-B65E-002590200B08.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/C6DC7A1B-485F-E111-A161-003048D479FC.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/C86A506E-495F-E111-8760-003048D4771E.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/C8F5183D-4F5F-E111-B2CC-00E08178C06F.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/D01BB352-675F-E111-85EE-003048D476F0.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/D6A58E9C-705F-E111-A9DF-002590200ADC.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/D85F2609-515F-E111-977B-003048D476BA.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/DA6CF68F-C25F-E111-95CF-00E08178C06B.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/DCBFD179-6C5F-E111-9E7F-001A64789CE8.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/E2AF7549-4E5F-E111-8FB1-002481E15522.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/F0B49E79-645F-E111-A699-002481E1501E.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/F8F387AF-535F-E111-952A-00E0817918A7.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/FC596342-675F-E111-AD5C-001A64789E20.root',
+       '/store/mc/Summer11/T_TuneZ2_scaledown_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/FEC67B27-695F-E111-A39C-002590200B34.root' ] );
 
-
-     ] );
 
 secFiles.extend( [
                ] )
-
--- Configuration/python/singleTop_tchannel_scale_up_PythiaPowhegZ2_Summer11_AOD_cff.py
+++ Configuration/python/singleTop_tchannel_scale_up_PythiaPowhegZ2_Summer11_AOD_cff.py
@@ -1,20 +1,83 @@
 import FWCore.ParameterSet.Config as cms
 
 ################################################################################################
-# Dataset:
-# Events :
+# Dataset: T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/Summer11-PU_S4_START42_V11-v1/AODSIM  #
+# Events : 1032197                                                                             #
 # eff    : 1.0                                                                                 #
 ################################################################################################
+
+import FWCore.ParameterSet.Config as cms
 
 maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
 readFiles = cms.untracked.vstring()
 secFiles = cms.untracked.vstring() 
 source = cms.Source ("PoolSource",fileNames = readFiles, secondaryFileNames = secFiles)
 readFiles.extend( [
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/04D3F051-675F-E111-9741-00259021A43E.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/068E213C-675F-E111-B149-0025B3E066A4.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/0809FC7E-6D5F-E111-83EC-003048D45FB0.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/0ABAFC35-625F-E111-A19F-003048D45F32.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/0EE76527-595F-E111-BE96-003048673E9A.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/1200D49B-545F-E111-8B6C-0025902009D8.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/127B6D3F-0961-E111-9F97-003048D47A56.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/18B2AEB5-4D5F-E111-BC81-0025B3E05BF4.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/1AE6BC35-575F-E111-BC7C-0025B3E05E1C.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/20AB2504-735F-E111-8682-003048D45FEE.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/26762049-625F-E111-AC18-003048D479DE.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/2A26AE60-725F-E111-895D-001A64789464.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/2AA50215-0261-E111-8F6C-00E08179173F.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/3832D1A3-0461-E111-8D5A-003048D47A14.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/3AEEFD8D-0761-E111-AAAA-0025B3E06378.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/4091F271-505F-E111-B916-003048D45F9C.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/48C3D489-495F-E111-A3E8-003048D45FDA.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/527FC6AE-0661-E111-B620-0025B3E064F8.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/5C751434-585F-E111-861C-00E08178C159.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/643EFFC8-525F-E111-AFD3-003048670BCC.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/7264968B-FF60-E111-A27D-003048D479F0.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/7425C1BB-515F-E111-9D80-002481E15522.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/8AA19837-6C5F-E111-BBE7-00E08178C0D5.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/8AB92849-4E5F-E111-B9B1-0025902008FC.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/8C9CA13F-4F5F-E111-B1BF-003048D4770E.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/8E550415-615F-E111-8854-0025B3E06612.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/8EB606BD-0D61-E111-8014-003048D46028.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/960E1855-8260-E111-9AE4-00E08178C189.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/9AFEA94A-765F-E111-ABB0-00E08178C0C7.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/A4F5001C-665F-E111-91D3-003048D46250.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/A8B92598-0561-E111-AEBE-00E08179181F.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/AAE8F3A6-625F-E111-A442-002590200934.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/AC4B3E19-465F-E111-9946-003048D46094.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/B0A4FE3D-ED61-E111-8AC2-003048635D6A.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/B6F76F10-495F-E111-A868-003048673F8A.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/BA0E1E3A-4C5F-E111-90AC-0025B3E06496.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/C20D063C-565F-E111-8093-00E08178C065.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/C88F2018-695F-E111-AEB0-002590200A98.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/CAAAD161-6F5F-E111-890D-001A6478A7A4.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/CC18E895-735F-E111-9281-0025B3E05BBE.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/CC854058-5A5F-E111-BBA9-003048D47730.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/D441B40C-4B5F-E111-9287-00304866C51E.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/D494D918-715F-E111-879C-002590200B10.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/D85607D9-625F-E111-A0D7-002590200B34.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/DA894F2E-685F-E111-952A-00E081791767.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/DC337BD2-465F-E111-8B66-003048D479FA.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/DC447A29-5D5F-E111-BAA6-001A64789E20.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/DE06EEC2-DE61-E111-8588-00E08178C18D.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/DED3920A-555F-E111-84A4-00E0817918B9.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/DED8EA53-4A5F-E111-A9D1-003048D45F92.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/E295E20C-835F-E111-9DB1-00E0817917E7.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/E49FF7DA-755F-E111-9ABC-001A647894E8.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/E4B9282C-1461-E111-8D00-0030486361BC.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/E87C20E9-5D5F-E111-AD95-0025B3E06378.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/EE9B0ACC-435F-E111-B532-001A64789D60.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/F20197CF-6C5F-E111-A694-003048D476B8.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/F2828387-8D5F-E111-96B2-0025B3E05DBE.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/F40E7F5A-775F-E111-BC62-001A64789DC4.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/F4218D9A-5C5F-E111-882D-001A64787064.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/F82FE9FA-795F-E111-B2D7-002481E1501E.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/FA0B9219-755F-E111-BC98-00E08178C123.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/FC8BEFDF-6A5F-E111-BF2C-00E08178C121.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/FE59AB7C-6F5F-E111-9CEB-F04DA23BBCCA.root',
+       '/store/mc/Summer11/T_TuneZ2_scaleup_t-channel_7TeV-powheg-tauola/AODSIM/PU_S4_START42_V11-v1/0000/FED4259D-4A5F-E111-BA96-002590200A40.root' ] );
 
-
-     ] );
 
 secFiles.extend( [
                ] )
-
