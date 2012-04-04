0289bc96c3924b57d1d783df9e629718e3c937ef
tdorland <>
2012-04-04 18:35:23 +0000

    *** empty log message ***

--- Configuration/analysis/diLeptonic/mk_HistoFileList.sh
+++ Configuration/analysis/diLeptonic/mk_HistoFileList.sh
@@ -3,11 +3,12 @@
 rm FileLists/Histo*
 
 #foreach sample (run ttbarsignal ttbarbg single ww wz zz dytautau dyee dymumu wtolnu qcd)
-foreach sample (run qcd wtolnu dytautau dyee dymumu ww wz zz single ttbarbg ttbarsignal)
+foreach sample (run dyee dymumu dytautau ww wz zz wtolnu single ttbarbg ttbarsignal)
 
    foreach channel (ee emu mumu)
       
-      foreach Syst  (Nominal JESUP JESDOWN RESUP RESDOWN PU_UP PU_DOWN SCALEUP SCALEDOWN MATCHUP MATCHDOWN MASSUP MASSDOWN)
+#      foreach Syst  (Nominal JESUP JESDOWN RESUP RESDOWN PU_UP PU_DOWN SCALEUP SCALEDOWN MATCHUP MATCHDOWN MASSUP MASSDOWN)
+      foreach Syst  (Nominal)
 
       ls -1 selectionRoot/$Syst/$channel/$sample*.root >> FileLists/HistoFileList_$Syst\_$channel.txt
       ls -1 selectionRoot/$Syst/$channel/$sample*.root >> FileLists/HistoFileList_$Syst\_combined.txt
