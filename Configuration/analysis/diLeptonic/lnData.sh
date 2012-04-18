06221eb520bc996ef164d51fdc1982f5e0373c2e
tdorland <>
2012-04-18 07:56:24 +0000

    Modify for the running of systematics. No matchdown right now

--- Configuration/analysis/diLeptonic/lnData.sh
+++ Configuration/analysis/diLeptonic/lnData.sh
@@ -2,28 +2,13 @@
 
 foreach channel (ee emu mumu)
      
-   foreach syst (JESDOWN  MASSDOWN  MATCHDOWN PU_DOWN PU_UP RESUP SCALEUP JESUP MASSUP MATCHUP RESDOWN SCALEDOWN)
+#   foreach syst (JESDOWN JESUP RESDOWN RESUP MATCHUP MATCHDOWN MASSUP MASSDOWN PU_UP PU_DOWN SCALEUP SCALEDOWN POWHEG)
+   foreach syst (JESDOWN JESUP RESDOWN RESUP MATCHUP MASSUP MASSDOWN PU_UP PU_DOWN SCALEUP SCALEDOWN POWHEG)
 
    rm selectionRoot/$syst/$channel/run*
    cd selectionRoot/$syst/$channel/
-   ln -s ../../Nominal/$channel/* .
+   ln -s ../../Nominal/$channel/run* .
    cd ../../..
    end
 
 end
-
-rm selectionRoot/MATCHUP/*/ttbarbg.root
-rm selectionRoot/MATCHDOWN/*/ttbarbg.root
-rm selectionRoot/MASSUP/*/ttbarbg.root
-rm selectionRoot/MASSDOWN/*/ttbarbg.root
-rm selectionRoot/SCALEUP/*/ttbarbg.root
-rm selectionRoot/SCALEDOWN/*/ttbarbg.root
-
-rm selectionRoot/MATCHUP/*/ttbarsignalplustau.root
-rm selectionRoot/MATCHDOWN/*/ttbarsignalplustau.root
-rm selectionRoot/MASSUP/*/ttbarsignalplustau.root
-rm selectionRoot/MASSDOWN/*/ttbarsignalplustau.root
-rm selectionRoot/SCALEUP/*/ttbarsignalplustau.root
-rm selectionRoot/SCALEDOWN/*/ttbarsignalplustau.root
-
-
