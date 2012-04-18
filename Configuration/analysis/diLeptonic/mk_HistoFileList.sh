10153cb442734254e4854abd0cbbf0da7e209185
tdorland <>
2012-04-18 07:57:27 +0000

    Remove MATCHDOWN from the nominal running for now

--- Configuration/analysis/diLeptonic/mk_HistoFileList.sh
+++ Configuration/analysis/diLeptonic/mk_HistoFileList.sh
@@ -3,12 +3,13 @@
 rm FileLists/Histo*
 
 #foreach sample (run ttbarsignal ttbarbg single ww wz zz dytautau dyee dymumu wtolnu qcd)
-foreach sample (run dyee dymumu dytautau ww wz zz wtolnu single ttbarbg ttbarsignal)
+foreach sample (run qcd dyee dymumu dytautau ww wz zz wtolnu single ttbarbg ttbarsignal)
 
    foreach channel (ee emu mumu)
       
 #      foreach Syst  (Nominal JESUP JESDOWN RESUP RESDOWN PU_UP PU_DOWN SCALEUP SCALEDOWN MATCHUP MATCHDOWN MASSUP MASSDOWN)
-      foreach Syst  (Nominal)
+      foreach Syst  (Nominal JESUP JESDOWN RESUP RESDOWN PU_UP PU_DOWN SCALEUP SCALEDOWN MATCHUP MASSUP MASSDOWN)
+#      foreach Syst  (Nominal)
 
       ls -1 selectionRoot/$Syst/$channel/$sample*.root >> FileLists/HistoFileList_$Syst\_$channel.txt
       ls -1 selectionRoot/$Syst/$channel/$sample*.root >> FileLists/HistoFileList_$Syst\_combined.txt
