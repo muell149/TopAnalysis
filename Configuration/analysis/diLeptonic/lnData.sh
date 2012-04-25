e79a13b05ae0b353804cdf1e1a0e5d65a953e9c4
tdorland <>
2012-04-25 16:56:13 +0000

    rm duplicate links

--- Configuration/analysis/diLeptonic/lnData.sh
+++ Configuration/analysis/diLeptonic/lnData.sh
@@ -12,3 +12,17 @@ foreach channel (ee emu mumu)
    end
 
 end
+
+rm selectionRoot/MATCHUP/*/ttbarbg.root
+rm selectionRoot/MATCHDOWN/*/ttbarbg.root
+rm selectionRoot/MASSUP/*/ttbarbg.root
+rm selectionRoot/MASSDOWN/*/ttbarbg.root
+rm selectionRoot/SCALEUP/*/ttbarbg.root
+rm selectionRoot/SCALEDOWN/*/ttbarbg.root
+
+rm selectionRoot/MATCHUP/*/ttbarsignalplustau.root
+rm selectionRoot/MATCHDOWN/*/ttbarsignalplustau.root
+rm selectionRoot/MASSUP/*/ttbarsignalplustau.root
+rm selectionRoot/MASSDOWN/*/ttbarsignalplustau.root
+rm selectionRoot/SCALEUP/*/ttbarsignalplustau.root
+rm selectionRoot/SCALEDOWN/*/ttbarsignalplustau.root
