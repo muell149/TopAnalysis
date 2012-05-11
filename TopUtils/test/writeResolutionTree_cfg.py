122a5c1407caa6d2b6319e2172765b5db023301f
henderle <>
2012-05-11 15:54:37 +0000

    Works on AOD now, minimal version without proper object selection and pile-up reweighting

--- TopUtils/test/writeResolutionTree_cfg.py
+++ TopUtils/test/writeResolutionTree_cfg.py
@@ -6,10 +6,13 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.threshold = 'INFO'
 process.MessageLogger.cerr.FwkReport.reportEvery = 1000
 
+process.load("Configuration.StandardSequences.FrontierConditions_GlobalTag_cff")
+process.load("Configuration.StandardSequences.Geometry_cff")
+process.GlobalTag.globaltag = cms.string('START42_V17::All')
+
 process.source = cms.Source("PoolSource",
-    # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-    '/store/user/henderle/7TeV/TTbar/PATtuple_1.root'
+    '/store/mc/Fall11/TTJets_TuneZ2_7TeV-madgraph-tauola/AODSIM/PU_S6_START42_V14B-v2/0000/0040932D-A80F-E111-BBF7-00304867BFAA.root'
     )
 )
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
@@ -21,8 +24,10 @@ process.TFileService = cms.Service("TFileService",
 from TopAnalysis.TopUtils.ResolutionTreeWriter_cfi import writeResolutionTree
 process.resolutionTreeWriter = writeResolutionTree
 
-process.dump = cms.EDAnalyzer('EventContentAnalyzer'
-)
-
 process.p = cms.Path(process.resolutionTreeWriter)
-#process.p = cms.Path(process.dump)
+
+from TopAnalysis.TopUtils.usePatTupleWithParticleFlow_cff import prependPF2PATSequence
+PFoptions = {
+    'runOnAOD': True
+    }
+prependPF2PATSequence(process, 'p', PFoptions)
