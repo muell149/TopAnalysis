e68748ccc8ef1c34af544515ee612d844dd32569
cdiez <>
2012-05-21 14:23:37 +0000

    Replaced HLT for SIM in genJets

--- TopUtils/python/HadronLevelBJetProducer_cfi.py
+++ TopUtils/python/HadronLevelBJetProducer_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 produceHadronLevelBJets = cms.EDProducer("HadronLevelBJetProducer",
     ttGenEvent = cms.InputTag('genEvt'),
-    genJets = cms.InputTag('ak5GenJets','','HLT'),
+    genJets = cms.InputTag('ak5GenJets','','SIM'), 
     deltaR = cms.double(0.5),
 )
 
