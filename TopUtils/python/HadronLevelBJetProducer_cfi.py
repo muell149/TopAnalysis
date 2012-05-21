a9cd71bdacad4a7d72a769c3b073d0783fb313b8
cdiez <>
2012-05-21 14:36:20 +0000

    make 110% sure the right GenJet collection gets used

--- TopUtils/python/HadronLevelBJetProducer_cfi.py
+++ TopUtils/python/HadronLevelBJetProducer_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 produceHadronLevelBJets = cms.EDProducer("HadronLevelBJetProducer",
     ttGenEvent = cms.InputTag('genEvt'),
-    genJets = cms.InputTag('ak5GenJets','','SIM'), 
+    genJets = cms.InputTag('ak5GenJets','','HLT'), 
     deltaR = cms.double(0.5),
 )
 
