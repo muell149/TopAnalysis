2983060456762e40b89e1482c4324600c9f960be
tdorland <>
2012-07-13 11:57:39 +0000

    Add MCATNLO and POWHEG links to accomodate the new HAD uncertainty structure

--- Configuration/analysis/diLeptonic/lnData.sh
+++ Configuration/analysis/diLeptonic/lnData.sh
@@ -4,7 +4,7 @@ BASEDIR=`pwd`
 
 foreach channel (ee emu mumu)
    
-   foreach syst (JESDOWN JESUP RESDOWN RESUP MATCHUP MATCHDOWN MASSUP MASSDOWN PU_UP PU_DOWN SCALEUP SCALEDOWN)   
+   foreach syst (JESDOWN JESUP RESDOWN RESUP MATCHUP MATCHDOWN MASSUP MASSDOWN PU_UP PU_DOWN SCALEUP SCALEDOWN MCATNLO POWHEG)   
    #foreach syst (JESDOWN JESUP RESDOWN RESUP MATCHUP MATCHDOWN MASSUP MASSDOWN PU_UP PU_DOWN SCALEUP SCALEDOWN MCNLOUP MCNLODOWN POWHEGUP POWHEGDOWN)
    #foreach syst (MCNLOUP MCNLODOWN POWHEGUP POWHEGDOWN)
    
@@ -19,9 +19,43 @@ foreach channel (ee emu mumu)
 
 end
 
-foreach SignalSyst (MATCHUP MATCHDOWN MASSUP MASSDOWN SCALEUP SCALEDOWN)
+foreach SignalSyst (MATCHUP MATCHDOWN MASSUP MASSDOWN SCALEUP SCALEDOWN MCATNLO POWHEG)
 
    rm selectionRoot/$SignalSyst/*/ttbarbg.root
    rm selectionRoot/$SignalSyst/*/ttbarsignalplustau.root
 
 end
+
+mkdir selectionRoot/HADUP selectionRoot/HADDOWN
+
+foreach channel (ee emu mumu)
+
+   mkdir selectionRoot/HADUP/$channel
+   mkdir selectionRoot/HADDOWN/$channel
+   
+   cp selectionRoot/MCATNLO/$channel/* selectionRoot/HADUP/$channel
+   cp selectionRoot/POWHEG/$channel/* selectionRoot/HADDOWN/$channel
+
+   cd selectionRoot/HADUP/$channel/
+   echo
+   echo "Creating Links in ... " 
+   pwd
+   echo 
+   ln -s ../../Nominal/$channel/* .
+   cd $BASEDIR
+
+   cd selectionRoot/HADDOWN/$channel/
+   echo
+   echo "Creating Links in ... " 
+   pwd
+   echo 
+   ln -s ../../Nominal/$channel/* .
+   cd $BASEDIR
+
+   rm selectionRoot/HADUP/$channel/ttbarbg.root
+   rm selectionRoot/HADUP/$channel/ttbarsignalplustau.root
+   rm selectionRoot/HADDOWN/$channel/ttbarbg.root
+   rm selectionRoot/HADDOWN/$channel/ttbarsignalplustau.root
+
+end
+
