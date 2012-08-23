ea3d79cff42683f4d2daf8de1ca6338b8b0c3ec9
jlange <>
2012-08-23 14:31:37 +0000

    shell script that automates the calculation for all decay channels, phase spaces and levels by running topDiffXSecAnalysis2011.sh multiple times

---
+++ Configuration/analysis/semiLeptonic/diffXSection/topDiffXSecRunAll.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+## this shell script runs the whole analysis (topDiffXSecAnalysis2011.sh) for all channels, phase spaces and levels
+
+## Hadron level visible phase space
+./topDiffXSecAnalysis2011.sh muon visible hadron
+./topDiffXSecAnalysis2011.sh electron visible hadron
+./topDiffXSecAnalysis2011.sh combined visible hadron
+
+## Parton level visible phase space
+./topDiffXSecAnalysis2011.sh muon visible parton
+./topDiffXSecAnalysis2011.sh electron visible parton
+./topDiffXSecAnalysis2011.sh combined visible parton
+
+## Parton level full phase space
+./topDiffXSecAnalysis2011.sh muon extrapolate parton
+./topDiffXSecAnalysis2011.sh electron extrapolate parton
+./topDiffXSecAnalysis2011.sh combined extrapolate parton
