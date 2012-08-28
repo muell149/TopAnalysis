77908c489f1728a2c113ac6b0fc049368e017fe1
mgoerner <>
2012-08-28 17:03:35 +0000

    prefer indices for b-jets that are within PS

--- TopUtils/plugins/GenJetSelector.cc
+++ TopUtils/plugins/GenJetSelector.cc
@@ -28,56 +28,84 @@ GenJetSelector::produce(edm::Event& evt, const edm::EventSetup& setup)
   edm::Handle<std::vector<int> > BHadJetIndex, AntiBHadJetIndex;
   evt.getByLabel(bHadJetIdx_    , BHadJetIndex    );
   evt.getByLabel(antibHadJetIdx_, AntiBHadJetIndex);
-  // save them in vector
+  // collect them in vector
   //std::cout << "BHadJetIndices: "  << std::endl;
   for (int i=0; i<(int) BHadJetIndex->size()    ; ++i){
-    VBHadJetIdx    .push_back(    BHadJetIndex->at(i));
-    //std::cout << BHadJetIndex->at(i)  << std::endl;
+    // avoid double listing of indices
+    if(i==0||(BHadJetIndex->at(i-1)!=BHadJetIndex->at(i))){
+      VBHadJetIdx    .push_back(    BHadJetIndex->at(i));
+      //std::cout << BHadJetIndex->at(i)  << std::endl;
+    }
   };
   //std::cout << "AntiBHadJetIndices: "  << std::endl;
   for (int i=0; i<(int) AntiBHadJetIndex->size(); ++i){
-    VAntiBHadJetIdx.push_back(AntiBHadJetIndex->at(i));
-    //std::cout << AntiBHadJetIndex->at(i) << std::endl;
-};
-  // use leading vector entries finally 
-  if(VBHadJetIdx    .size()>0) bIX   =VBHadJetIdx    [0];
-  if(VAntiBHadJetIdx.size()>0) bbarIX=VAntiBHadJetIdx[0];
-  // deal with more than one bjet index
-  // avoid using the same jets
-  if(VBHadJetIdx.size()>1&&VAntiBHadJetIdx.size()==1){
-    bIX=-1;
+    // avoid double listing of indices within each list
+    if(i==0||(AntiBHadJetIndex->at(i-1)!=AntiBHadJetIndex->at(i))){
+      VAntiBHadJetIdx.push_back(AntiBHadJetIndex->at(i));
+      //std::cout << AntiBHadJetIndex->at(i) << std::endl;
+    }
+  };
+//   // deal with more than one bjet index:
+//   // avoid using the same jets
+//   if(VBHadJetIdx.size()>1&&VAntiBHadJetIdx.size()==1){
+//     // scan B-jet for overlapping entries
+//     for(int i=0; i<(int) VBHadJetIdx.size(); ++i){
+//       if(VBHadJetIdx[i]==VAntiBHadJetIdx[0]){
+// 	// rm overlapping entry in collection with more than one entry
+// 	VBHadJetIdx.erase(VBHadJetIdx.begin()+i);
+//       }
+//     }
+//   }
+//   if(VAntiBHadJetIdx.size()>1&&VBHadJetIdx.size()==1){
+//     // scan ANTI-B-jet for overlapping entries
+//     for(int i=0; i<(int) VAntiBHadJetIdx.size(); ++i){
+//       if(VAntiBHadJetIdx[i]==VBHadJetIdx[0]){
+// 	// rm overlapping entry in collection with more than one entry
+// 	VAntiBHadJetIdx.erase(VAntiBHadJetIdx.begin()+i);
+//       }
+//     }
+//   }
+  // deal with bjet indices appearing in both index lists
+  if(VAntiBHadJetIdx.size()>1&&VBHadJetIdx.size()>1){
     for(int i=0; i<(int) VBHadJetIdx.size(); ++i){
-      if(VBHadJetIdx[i]!=bbarIX){
-	bIX=VBHadJetIdx[i];
-	break;
+      for(int j=0; j<(int) VAntiBHadJetIdx.size(); ++j){
+	if(VAntiBHadJetIdx[j]==VBHadJetIdx[i]){
+	  // rm overlapping entry in collection with more than one entry
+	  if(VBHadJetIdx.size()==1&&VAntiBHadJetIdx.size()>1) VAntiBHadJetIdx.erase(VAntiBHadJetIdx.begin()+j); 
+	  else if(VBHadJetIdx.size()>1&&VAntiBHadJetIdx.size()==1) VBHadJetIdx.erase(VBHadJetIdx.begin()+i);
+	  else{
+	    VAntiBHadJetIdx.erase(VAntiBHadJetIdx.begin()+j);
+	  }
+	}
       }
     }
   }
-  else if(VAntiBHadJetIdx.size()>1&&VBHadJetIdx.size()==1){
-    bbarIX=-1;
+  // choose B-jet index from list
+  if(VBHadJetIdx.size()>0){
+    for(int i=0; i<(int) VBHadJetIdx.size(); ++i){
+      // use indices of jets satisfying the PS cuts if possible
+      for(edm::View<reco::GenJet>::const_iterator p=genJet->begin(); p!=genJet->end(); ++p){
+	int currentIndex=p-genJet->begin();
+	// scan for jet index and check kinematic range
+	if(currentIndex==VBHadJetIdx[i]&&std::abs(p->eta())<2.1&&p->pt()>30) bIX=VBHadJetIdx[i];    
+      }
+    }
+    // if not: take leading index
+    if(bIX==-1)  bIX   =VBHadJetIdx    [0];
+  }
+  // choose Anti-B-jet index from list
+  if(VAntiBHadJetIdx.size()>0){
     for(int i=0; i<(int) VAntiBHadJetIdx.size(); ++i){
-      if(VAntiBHadJetIdx[i]!=bIX){
-	bbarIX=VAntiBHadJetIdx[i];
-	break;
+      // use indices of jets satisfying the PS cuts if possible
+      for(edm::View<reco::GenJet>::const_iterator p=genJet->begin(); p!=genJet->end(); ++p){
+	int currentIndex=p-genJet->begin();
+	// scan for jet index and check kinematic range
+	if(currentIndex==VAntiBHadJetIdx[i]&&std::abs(p->eta())<2.1&&p->pt()>30) bbarIX=VAntiBHadJetIdx[i]; 
       }
     }
+    // if not: take leading index
+    if(bbarIX==-1)  bbarIX   =VAntiBHadJetIdx    [0];
   }
-  else if(VAntiBHadJetIdx.size()>1&&VBHadJetIdx.size()>1){
-    // if conflict use leading jet as b-jet and 2nd leading as bbar-jet
-    bIX   =VBHadJetIdx[0];
-    bbarIX=-1;
-    for(int i=0; i<(int) VAntiBHadJetIdx.size(); ++i){
-      if(VAntiBHadJetIdx[i]!=bIX){
-	bbarIX=VAntiBHadJetIdx[i];
-	break;
-      }
-    }
-  }
-
-
-  //allIndices.push_back(bIX   );
-  //allIndices.push_back(bbarIX);
-  //std::cout << std::endl << "(bIX,bbarIX) = (" << bIX << "," << bbarIX << ")"<< std::endl;
 
   // loop input collection
   for(edm::View<reco::GenJet>::const_iterator p=genJet->begin(); p!=genJet->end(); ++p){
@@ -95,6 +123,22 @@ GenJetSelector::produce(edm::Event& evt, const edm::EventSetup& setup)
   // push out vector into the event
   evt.put(out);
 }
+
+bool checkKinematicRange(int index, edm::Handle<edm::View<reco::GenJet> > genJet){
+  // loop input collection
+  for(edm::View<reco::GenJet>::const_iterator p=genJet->begin(); p!=genJet->end(); ++p){
+    int currentIndex=p-genJet->begin();
+    // scan for jet index
+    if(currentIndex==index){
+      // check keinematic range
+      if(std::abs(p->eta())<2.1&&p->pt()>30) return true;
+      else return false;
+    }
+  }
+  return false;
+}
+
+
 
 #include "FWCore/Framework/interface/MakerMacros.h"
 DEFINE_FWK_MODULE(GenJetSelector);
