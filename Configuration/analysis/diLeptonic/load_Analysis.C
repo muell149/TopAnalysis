1e2a471220a957436e4f4160e67329732270624e
dafische <>
2012-09-03 09:20:23 +0000

    Added file counter and output

--- Configuration/analysis/diLeptonic/load_Analysis.C
+++ Configuration/analysis/diLeptonic/load_Analysis.C
@@ -5,15 +5,23 @@
 #include <TTree.h>
 
 void load_Analysis(){
+   
+  int filecounter = 0;
 
   ifstream infile ("selectionList.txt");
   TString filename;
 
   while(!infile.eof()){
     infile>>filename;
+    filecounter++;    
+
     TString file="";
     //channel selection for later BTag eff
     if(filename!=""){
+        std::cout << std::endl;
+        std::cout << "PROCESSING File " << filecounter << " ("<< filename << ") from selectionList.txt" << std::endl;
+        std::cout << std::endl;
+  
         if(filename.Contains("/mumu/"))    {file = TString("BTagEff/mumu/ttbarsignalplustau.root");}
         else if(filename.Contains("/emu/")){file = TString("BTagEff/emu/ttbarsignalplustau.root");}
         else if(filename.Contains("/ee/")) {file = TString("BTagEff/ee/ttbarsignalplustau.root");}
