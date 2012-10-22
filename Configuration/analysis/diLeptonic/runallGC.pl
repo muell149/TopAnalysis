5d3eac4f0977a0da715adeae249c0133fde31920
jkiesele <>
2012-10-22 08:41:23 +0000

    some bugfixes, fixed paths, added checkall and  script

--- Configuration/analysis/diLeptonic/runallGC.pl
+++ Configuration/analysis/diLeptonic/runallGC.pl
@@ -8,7 +8,7 @@ use File::Path;
 use POSIX;
 
 my %arg;
-getopts('d:c:r:sj:m:f:R:gh:', \%arg);
+getopts('d:c:r:sj:m:f:R:gh', \%arg);
 
 unless ( $arg{d} && $arg{c} && -f $arg{c}) {
     print <<'USAGE';
@@ -18,6 +18,8 @@ Syntax:
 
 Run runallGC.pl to run over all data samples given in files.txt using the
 configuration file configFile.py - and use GridControl to submit jobs
+
+the grid-control directory must be placed in the folder this script is run from.
 
 -g      run on the grid
 
@@ -30,6 +32,8 @@ If a regexp is given only jobs NOT matching the regexp will be submitted (case s
 -s                    really submit jobs using GridControl
 -j NJobs              submit NJobs for each dataset. 
 -m maxEventsPerJob    Overwrites default value from files.txt
+
+-o output            output directory when output is downloaded from dcache
 
 -f files.txt          (default value: files.txt)
 files.txt must contain lines of
@@ -45,6 +49,14 @@ my $source = $arg{f} ? $arg{f} : 'files.txt';
 open my $IN, '<', $source or die "Cannot open input file: $!\n";
 my @createDirs;
 my @runGCs;
+my @checkGCs;
+my @getGCs;
+my @killGCs;
+my @forHadd;
+
+my $hypernewsName = $arg{h} || $ENV{HN_USER};
+die "Who are you?\n" unless $hypernewsName;
+
 
 while(my $line = <$IN>) {
     chomp $line;
@@ -64,8 +76,7 @@ while(my $line = <$IN>) {
     my $jobdirWithSomeTimestamp = "runallGC-$outputFile-" . strftime("%FT%H-%M-%S",localtime);
     
     my $t = getGCtemplate();
-    my $hypernewsName = $arg{h} || $ENV{USER};
-    die "Who are you?\n" unless $hypernewsName;
+    
     for ($t) {
         s/##LOCAL_OR_GRID##/$arg{g} ? "grid" : "local"/eg;
         s/##WORKDIR##/$path/g;
@@ -88,10 +99,15 @@ while(my $line = <$IN>) {
     
     if ($arg{g}) {
         push @createDirs, "srmmkdir srm://dcache-se-cms.desy.de:8443/pnfs/desy.de/cms/tier2/store/user/$hypernewsName/$jobdirWithSomeTimestamp\n";
+	push @getGCs, "./grid-control/scripts/downloadFromSE.py -m -o /scratch/hh/dust/naf/cms/user/$ENV{USER}/$jobdirWithSomeTimestamp $gcConfig\n";
     } else {
-        push @createDirs, "mkdir -p /scratch/hh/dust/cms/user/$hypernewsName/$jobdirWithSomeTimestamp\n";
+        push @createDirs, "mkdir -p /scratch/hh/dust/naf/cms/user/$ENV{USER}/$jobdirWithSomeTimestamp\n";
     }
-    push @runGCs, "./grid-control/go.py -i $gcConfig\n";
+    push @runGCs, "starting $gcConfig\n./grid-control/go.py -i $gcConfig\n";
+    push @checkGCs, "echo checking $gcConfig\n./grid-control/go.py $gcConfig\n";
+    push @killGCs, "echo killing $gcConfig\n./grid-control/go.py -d ALL $gcConfig\n";
+    push @forHadd, "hadd /scratch/hh/dust/naf/cms/user/$ENV{USER}/${outputFile}.root /scratch/hh/dust/naf/cms/user/$ENV{USER}/$jobdirWithSomeTimestamp/*.root\n";
+
 }
 
 open my $OUT, '>', "$arg{d}/createDirsAndRun.sh" or die $!;
@@ -100,7 +116,35 @@ print $OUT "\n" x 3;
 print $OUT $_ for @runGCs;
 close $OUT;
 chmod 0755, "$arg{d}/createDirsAndRun.sh";
-print "run $arg{d}/createDirsAndRun.sh to create dirs and submit jobs\n";
+print "run ./$arg{d}/createDirsAndRun.sh to create dirs and submit jobs\n";
+
+open my $OUTC, '>', "$arg{d}/checkAllJobs.sh" or die $!;
+print $OUTC $_ for @checkGCs;
+close $OUTC;
+chmod 0755, "$arg{d}/checkAllJobs.sh";
+print "run ./$arg{d}/checkAllJobs.sh to check dirs and resubmit jobs\n";
+
+if ($arg{g}) {
+    open my $OUTG, '>', "$arg{d}/downloadAllFromSE.sh" or die $!;
+    print $OUTG $_ for @getGCs;
+    close $OUTG;
+    chmod 0755, "$arg{d}/downloadAllFromSE.sh";
+    print "run ./$arg{d}/downloadAllFromSE.sh to download from dcache to your sonar user space\n";
+}
+
+open my $OUTD, '>', "$arg{d}/killAllJobs.sh" or die $!;
+print $OUTD $_ for @killGCs;
+close $OUTD;
+chmod 0755, "$arg{d}/killAllJobs.sh";
+print "run ./$arg{d}/killAllJobs.sh to kill all jobs\n";
+
+open my $OUTR, '>', "$arg{d}/haddAllRoot.sh" or die $!;
+print $OUTR $_ for @forHadd;
+close $OUTR;
+chmod 0755, "$arg{d}/haddAllRoot.sh";
+print "run ./$arg{d}/haddAllRoot.sh to hadd all output\n";
+
+
 
 sub getGCtemplate
 {
@@ -118,7 +162,7 @@ site = hh
 
 [jobs]
 ##JOBS##
-wall time    = 11:59:00 ;queue dingens // thats the 12 hour cue!!!
+wall time    = 40:59:00 ;queue dingens // thats the 12 hour cue!!!
 ; in queue     = $maxjobsinqueue ; 1000 -1
 memory       = 3700
 monitor jobs = dashboard
@@ -130,7 +174,7 @@ project area       = ##CMSSW_BASE##
 se runtime         = True
 config file        = ##CMSSWConfigFile## ; 
 prepare config     = True
-arguments          = ##OPTIONS##  ; 
+arguments          = ##OPTIONS## outputFile=##OUTPUTFILE##  ; 
 
 dataset splitter   = EventBoundarySplitter
 events per job     = ##EVENTS_PER_JOB##   ; I think better than to restrict total number of jobs
@@ -140,9 +184,9 @@ dataset            = ##DATASET## ; /MuEG/Run2012A-recover-06Aug2012-v1/AOD
 
 
 [storage]
-se output files    = ##OUTPUTFILE##.root ;
-##SE_DCACHE## se path            = srm://dcache-se-cms.desy.de:8443/pnfs/desy.de/cms/tier2/store/user/##HN_USER##/##jobdirWithSomeTimestamp##
-##SE_SCRATCH## se path           = dir:///scratch/hh/dust/cms/user/##HN_USER##/##jobdirWithSomeTimestamp##
+se output files    = *.root ;
+##SE_DCACHE##se path            = srm://dcache-se-cms.desy.de:8443/pnfs/desy.de/cms/tier2/store/user/##HN_USER##/##jobdirWithSomeTimestamp##
+##SE_SCRATCH##se path           = dir:///scratch/hh/dust/naf/cms/user/##HN_USER##/##jobdirWithSomeTimestamp##
 
 
 
@@ -157,3 +201,4 @@ se output files    = ##OUTPUTFILE##.root ;
 ENDE_TEMPLATE
 
 }
+
