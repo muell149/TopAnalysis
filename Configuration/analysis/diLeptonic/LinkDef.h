ab12fb08be277e74d72666db63f894f71316cf04
wbehrenh <>
2012-10-22 13:27:54 +0000

    Dictionary to read vectors of LorentzVectors of ROOT::Math::PtEtaPhiM4D of double

---
+++ Configuration/analysis/diLeptonic/LinkDef.h
@@ -0,0 +1,14 @@
+#ifndef ROOT_TREE_VECTOR_LINKDEF_H 
+#define ROOT_TREE_VECTOR_LINKDEF_H 1
+
+#ifdef __CINT__
+
+#pragma link off all classes;
+
+#pragma link C++ class ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<double> >;
+#pragma link C++ class std::vector<ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<double> > >;
+
+#endif
+
+#endif // ROOT_TREE_VECTOR_LINKDEF_H
+
