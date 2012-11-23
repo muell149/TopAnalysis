c72cd2b5b411719d4a0915f62957e25202bcf9d2
mgoerner <>
2012-11-23 16:30:41 +0000

    adapt to 2012 script

--- Configuration/analysis/semiLeptonic/diffXSection/topDiffXSecRunAll.sh
+++ Configuration/analysis/semiLeptonic/diffXSection/topDiffXSecRunAll.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-## this shell script runs the whole analysis (topDiffXSecAnalysis2011.sh) for all channels, phase spaces and levels
+## this shell script runs the whole analysis (topDiffXSecAnalysis2012.sh) for all channels, phase spaces and levels
 
 ## Hadron level visible phase space
-./topDiffXSecAnalysis2011.sh muon visible hadron
-./topDiffXSecAnalysis2011.sh electron visible hadron
-./topDiffXSecAnalysis2011.sh combined visible hadron
+./topDiffXSecAnalysis2012.sh muon visible hadron
+./topDiffXSecAnalysis2012.sh electron visible hadron
+./topDiffXSecAnalysis2012.sh combined visible hadron
 
 ## Parton level visible phase space
-./topDiffXSecAnalysis2011.sh muon visible parton
-./topDiffXSecAnalysis2011.sh electron visible parton
-./topDiffXSecAnalysis2011.sh combined visible parton
+./topDiffXSecAnalysis2012.sh muon visible parton
+./topDiffXSecAnalysis2012.sh electron visible parton
+./topDiffXSecAnalysis2012.sh combined visible parton
 
 ## Parton level full phase space
-./topDiffXSecAnalysis2011.sh muon extrapolate parton
-./topDiffXSecAnalysis2011.sh electron extrapolate parton
-./topDiffXSecAnalysis2011.sh combined extrapolate parton
+./topDiffXSecAnalysis2012.sh muon extrapolate parton
+./topDiffXSecAnalysis2012.sh electron extrapolate parton
+./topDiffXSecAnalysis2012.sh combined extrapolate parton
 
 # combined event yield analysis
-#./topDiffXSecAnalysis2011.sh combined2 visible hadron
-#./topDiffXSecAnalysis2011.sh combined2 visible parton
-#./topDiffXSecAnalysis2011.sh combined2 extrapolate parton
+#./topDiffXSecAnalysis2012.sh combined2 visible hadron
+#./topDiffXSecAnalysis2012.sh combined2 visible parton
+#./topDiffXSecAnalysis2012.sh combined2 extrapolate parton
