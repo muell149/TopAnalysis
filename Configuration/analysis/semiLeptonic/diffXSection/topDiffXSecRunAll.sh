f9536ff5609d649c8e24787b4fbde86df082e241
mgoerner <>
2013-01-15 18:52:35 +0000

    re-arrange order in which channels/PS definitions are executed, prioritizing the the ones relevant for publication, use event yield level combination only and per default

--- Configuration/analysis/semiLeptonic/diffXSection/topDiffXSecRunAll.sh
+++ Configuration/analysis/semiLeptonic/diffXSection/topDiffXSecRunAll.sh
@@ -2,22 +2,27 @@
 
 ## this shell script runs the whole analysis (topDiffXSecAnalysis2012.sh) for all channels, phase spaces and levels
 
-## Hadron level visible phase space
-./topDiffXSecAnalysis2012.sh muon visible hadron
-./topDiffXSecAnalysis2012.sh electron visible hadron
-./topDiffXSecAnalysis2012.sh combined visible hadron
+# combined channel analysis on event yield level
+./topDiffXSecAnalysis2012.sh combined2 extrapolate parton
+./topDiffXSecAnalysis2012.sh combined2 visible hadron
+./topDiffXSecAnalysis2012.sh combined2 visible parton
 
-## Parton level visible phase space
-./topDiffXSecAnalysis2012.sh muon visible parton
-./topDiffXSecAnalysis2012.sh electron visible parton
-./topDiffXSecAnalysis2012.sh combined visible parton
+## single channels
 
 ## Parton level full phase space
 ./topDiffXSecAnalysis2012.sh muon extrapolate parton
 ./topDiffXSecAnalysis2012.sh electron extrapolate parton
-./topDiffXSecAnalysis2012.sh combined extrapolate parton
+#./topDiffXSecAnalysis2012.sh combined extrapolate parton
 
-# combined event yield analysis
-#./topDiffXSecAnalysis2012.sh combined2 visible hadron
-#./topDiffXSecAnalysis2012.sh combined2 visible parton
-#./topDiffXSecAnalysis2012.sh combined2 extrapolate parton
+## Hadron level visible phase space
+./topDiffXSecAnalysis2012.sh muon visible hadron
+./topDiffXSecAnalysis2012.sh electron visible hadron
+#./topDiffXSecAnalysis2012.sh combined visible hadron
+
+## Parton level visible phase space
+./topDiffXSecAnalysis2012.sh muon visible parton
+./topDiffXSecAnalysis2012.sh electron visible parton
+#./topDiffXSecAnalysis2012.sh combined visible parton
+
+
+
