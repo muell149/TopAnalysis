c697643ef960962c7c7da92cb08069ba22b45f56
hauk <>
2013-01-17 14:43:43 +0000

    EDProducer to select and store only GenParticles relevant for analysis of Higgs decay chain

---
+++ HiggsUtils/plugins/HiggsDecaySubset.cc
@@ -0,0 +1,432 @@
+// -*- C++ -*-
+//
+// Package:    HiggsDecaySubset
+// Class:      HiggsDecaySubset
+// 
+/**\class HiggsDecaySubset HiggsDecaySubset.cc TopAnalysis/HiggsUtils/plugins/HiggsDecaySubset.cc
+
+ Description: [one line class summary]
+
+ Implementation:
+     [Notes on implementation]
+*/
+//
+// Original Author:  Johannes Hauk,,,DESY
+//         Created:  Tue Jan 15 14:35:52 CET 2013
+// $Id$
+//
+//
+
+
+// system include files
+#include <memory>
+
+// user include files
+#include "FWCore/Framework/interface/Frameworkfwd.h"
+#include "FWCore/Framework/interface/EDProducer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/MessageLogger/interface/MessageLogger.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+#include "FWCore/Utilities/interface/EDMException.h"
+#include "FWCore/Utilities/interface/InputTag.h"
+#include "DataFormats/HepMCCandidate/interface/GenParticle.h"
+
+#include "TopQuarkAnalysis/TopEventProducers/interface/TopDecaySubset.h"
+
+//
+// class declaration
+//
+
+class HiggsDecaySubset : public edm::EDProducer {
+   public:
+      explicit HiggsDecaySubset(const edm::ParameterSet&);
+      ~HiggsDecaySubset();
+
+      static void fillDescriptions(edm::ConfigurationDescriptions& descriptions);
+
+   private:
+      virtual void beginJob() ;
+      virtual void produce(edm::Event&, const edm::EventSetup&);
+      virtual void endJob() ;
+      
+      virtual void beginRun(edm::Run&, edm::EventSetup const&);
+      virtual void endRun(edm::Run&, edm::EventSetup const&);
+      virtual void beginLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&);
+      virtual void endLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&);
+      
+      /// find Higgs in list of input particles
+      std::vector<const reco::GenParticle*> findHiggs(const reco::GenParticleCollection&);
+      
+      /// check the decay chain for the exploited shower model
+      TopDecaySubset::ShowerModel checkShowerModel(const std::vector<const reco::GenParticle*>&)const;
+      
+      /// clear references
+      void clearReferences();
+      
+      /// fill output vector for full decay chain 
+      void fillListing(const std::vector<const reco::GenParticle*>&, reco::GenParticleCollection&);
+      
+      /// calculate lorentz vector from input
+      reco::Particle::LorentzVector p4(const reco::GenParticle::const_iterator, int);
+      
+      /// fill vector including all radiations from quarks
+      void addRadiation(int&, const reco::GenParticle::const_iterator, reco::GenParticleCollection&);
+      
+      /// fill references for output vector
+      void fillReferences(const reco::GenParticleRefProd&, reco::GenParticleCollection&);
+      
+      // ----------member data ---------------------------
+      
+      /// input tag for the genParticle source
+      edm::InputTag genParticleSource_;
+      
+      /// add radiation or not?
+      bool addRadiation_;
+      
+      /// parton shower mode (filled in checkShowerModel)
+      TopDecaySubset::ShowerModel showerModel_;
+      
+      /// management of daughter indices for fillRefs
+      std::map<int,std::vector<int> > refs_;
+      /// index in new evt listing of parts with daughters; 
+      /// has to be set to -1 in produce to deliver consistent 
+      /// results!
+      int motherPartIdx_;                    
+      
+      /// mode of decaySubset creation 
+      TopDecaySubset::FillMode fillMode_;
+      
+};
+
+//
+// constants, enums and typedefs
+//
+
+
+//
+// static data member definitions
+//
+
+//
+// constructors and destructor
+//
+HiggsDecaySubset::HiggsDecaySubset(const edm::ParameterSet& iConfig): 
+genParticleSource_(iConfig.getParameter<edm::InputTag>("src")),
+addRadiation_(iConfig.getParameter<bool>("addRadiation")),
+showerModel_(TopDecaySubset::kStart)
+{
+  // mapping of the corresponding fillMode; see FillMode 
+  // enumerator of TopDecaySubset for available modes
+  std::string mode = iConfig.getParameter<std::string>( "fillMode" );
+  if(mode=="kME")
+    fillMode_ = TopDecaySubset::kME;
+  else if(mode=="kStable")
+    fillMode_ = TopDecaySubset::kStable;
+  else
+    throw cms::Exception("Configuration") << mode << " is not a supported FillMode!\n";
+  
+  // produces a set of GenParticles following
+  // the decay branch of Higgs
+  produces<reco::GenParticleCollection>(); 
+}
+
+
+HiggsDecaySubset::~HiggsDecaySubset()
+{
+}
+
+
+//
+// member functions
+//
+
+// ------------ method called to produce the data  ------------
+void
+HiggsDecaySubset::produce(edm::Event& iEvent, const edm::EventSetup& iSetup)
+{
+  // create target vector
+  std::auto_ptr<reco::GenParticleCollection> target( new reco::GenParticleCollection );
+
+  // get source collection
+  edm::Handle<reco::GenParticleCollection> src;
+  iEvent.getByLabel(genParticleSource_, src);
+  
+  // find Higgs in list of input particles
+  std::vector<const reco::GenParticle*> v_higgs = this->findHiggs(*src);
+  
+  // determine shower model (only in first event)
+  if(showerModel_==TopDecaySubset::kStart)showerModel_ = this->checkShowerModel(v_higgs);
+  
+  // find decay chain
+  if(showerModel_!=TopDecaySubset::kNone){
+    // get ref product from the event
+    const reco::GenParticleRefProd ref = iEvent.getRefBeforePut<reco::GenParticleCollection>();
+    // clear existing refs
+    this->clearReferences();
+    // fill output
+    this->fillListing(v_higgs, *target);
+    // fill references
+    this->fillReferences(ref, *target);
+  }
+  
+  // write vectors to the event
+  iEvent.put(target);
+}
+
+
+
+// find Higgs in list of input particles
+std::vector<const reco::GenParticle*>
+HiggsDecaySubset::findHiggs(const reco::GenParticleCollection& v_particle)
+{
+  std::vector<const reco::GenParticle*> v_higgs;
+  for(reco::GenParticleCollection::const_iterator i_particle=v_particle.begin(); i_particle!=v_particle.end(); ++i_particle){
+    if(i_particle->pdgId()==25 && i_particle->status()==3){
+      //std::cout<<"\t"<<i_particle->pdgId()<<" "<<i_particle->status()<<" "<<i_particle->numberOfMothers()<<" "<<i_particle->numberOfDaughters()<<std::endl;
+      v_higgs.push_back( &(*i_particle) );
+    }
+  }
+  if(v_higgs.size()!=1)edm::LogWarning("decayChain")<<"Not exactly 1 Higgs found in event, but: "<<v_higgs.size();
+  return v_higgs;
+}
+
+
+
+// check the decay chain for the exploited shower model
+TopDecaySubset::ShowerModel
+HiggsDecaySubset::checkShowerModel(const std::vector<const reco::GenParticle*>& v_higgs)const
+{
+  for(std::vector<const reco::GenParticle*>::const_iterator i_higgs=v_higgs.begin(); i_higgs!=v_higgs.end(); ++i_higgs){
+    const reco::GenParticle* higgs = *i_higgs;
+    // check for kHerwig type showers: here the status 3 Higgs will 
+    // have a single status 2 Higgs as daughter, which has again 3 
+    // or more status 2 daughters
+    // WARNING: I did not explicitely check for Higgs in HERWIG, only in PYTHIA, so HERWIG criterion can be wrong
+    if( higgs->numberOfDaughters()==1){
+      if( higgs->begin()->pdgId()==higgs->pdgId() && higgs->begin()->status()==2 && higgs->begin()->numberOfDaughters()>1)
+	return TopDecaySubset::kHerwig;
+    }
+    // check for kPythia type showers: here the status 3 Higgs will 
+    // have all decay products and a status 2 Higgs as daughters
+    // the status 2 Higgs will be w/o further daughters
+    if( higgs->numberOfDaughters()>1 ){
+      bool daughterHiggsHasNoDaughters(false);
+      for(reco::GenParticle::const_iterator higgsDaughter=higgs->begin(); higgsDaughter!=higgs->end(); ++higgsDaughter){
+	//std::cout<<"\t"<<higgsDaughter->pdgId()<<" "<<higgsDaughter->status()<<" "<<higgsDaughter->numberOfDaughters()<<std::endl;
+	if( higgsDaughter->pdgId() == 25 && higgsDaughter->status()==2 && higgsDaughter->numberOfDaughters()==0)daughterHiggsHasNoDaughters = true;
+      }
+      if(daughterHiggsHasNoDaughters)
+	return TopDecaySubset::kPythia;
+    }
+  }
+  // if neither Herwig nor Pythia like
+  if(v_higgs.size()==0)
+    edm::LogWarning("decayChain")
+      << " Failed to find Higgs in decay chain. Will assume that this a \n"
+      << " non-Higgs sample and produce an empty decaySubset.\n";
+  else
+    throw edm::Exception(edm::errors::LogicError,
+			 " Can not find back any of the supported hadronization models. Models \n"
+			 " which are supported are:                                            \n"
+			 " Pythia  LO(+PS): Higgs(status 3) --> particle(status 3) antiparticle(status 3)\n"
+			 " Herwig NLO(+PS): Higgs(status 2) --> Higgs(status 3) --> Higgs(status 2)  \n");
+  return TopDecaySubset::kNone;
+}
+
+
+
+// clear references
+void
+HiggsDecaySubset::clearReferences()
+{
+  // clear vector of references 
+  refs_.clear();  
+  // set idx for mother particles to a start value
+  // of -1 (the first entry will raise it to 0)
+  motherPartIdx_=-1;
+}
+
+
+
+void 
+HiggsDecaySubset::fillListing(const std::vector<const reco::GenParticle*>& tops, reco::GenParticleCollection& target)
+{
+  // determine status flag of the new 
+  // particle depending on the FillMode
+  unsigned int statusFlag;
+  fillMode_ == TopDecaySubset::kME ? statusFlag=3 : statusFlag=2;
+  
+  for(std::vector<const reco::GenParticle*>::const_iterator it=tops.begin(); it!=tops.end(); ++it){
+    const reco::GenParticle* t = *it;
+    
+    // Do we need a case distiction for additional radiation off the Higgs? I do not think so, so it is not implemented
+    //std::auto_ptr<reco::GenParticle> topPtr( new reco::GenParticle( t->threeCharge(), this->p4(it, statusFlag), t->vertex(), t->pdgId(), statusFlag, false ) );
+    std::auto_ptr<reco::GenParticle> topPtr( new reco::GenParticle( t->threeCharge(), (*it)->p4(), t->vertex(), t->pdgId(), statusFlag, false ) );
+    target.push_back( *topPtr );
+    ++motherPartIdx_;
+    
+    // keep the top index for the map to manage the daughter refs
+    int iTop=motherPartIdx_; 
+    std::vector<int> topDaughters;
+    
+    // sanity check
+    if(showerModel_!=TopDecaySubset::kPythia && t->begin()==t->end())
+      throw edm::Exception(edm::errors::LogicError,
+			   "showerModel_!=kPythia && t->begin()==t->end()\n");
+    
+    //iterate over top daughters
+    for(reco::GenParticle::const_iterator td=((showerModel_==TopDecaySubset::kPythia)?t->begin():t->begin()->begin()); td!=((showerModel_==TopDecaySubset::kPythia)?t->end():t->begin()->end()); ++td){
+      //std::cout<<"\t"<<td->pdgId()<<" "<<td->status()<<" "<<td->numberOfDaughters()<<std::endl;
+      
+      // if particle is beauty or other quark
+      if( td->status()==3 && std::abs( td->pdgId() )<=5 ){ 
+	std::auto_ptr<reco::GenParticle> bPtr( new reco::GenParticle( td->threeCharge(), this->p4( td, statusFlag ), td->vertex(), td->pdgId(), statusFlag, false ) );
+	target.push_back( *bPtr );	  
+	// increment & push index of the top daughter
+	topDaughters.push_back( ++motherPartIdx_ ); 
+	if(addRadiation_){
+	  addRadiation(motherPartIdx_,td,target); 
+	}
+      }
+      // here would be the space to implement other Higgs decays and their decay chains (e.g. ttbar, WW, tautau)
+    }
+    // can there be sisters of the Higgs? if yes, should be implemented here
+    
+    // fill the map for the administration of daughter indices
+    refs_[ iTop ] = topDaughters;
+  }
+}
+
+
+
+// calculate lorentz vector from input
+reco::Particle::LorentzVector 
+HiggsDecaySubset::p4(const reco::GenParticle::const_iterator part, int statusFlag)
+{
+  // calculate the four vector for all top daughters from 
+  // their daughters including additional radiation 
+  if(statusFlag==3){
+    // return 4 momentum as it is
+    return part->p4();
+  }
+  reco::Particle::LorentzVector vec;
+  for(reco::GenParticle::const_iterator p=part->begin(); p!=part->end(); ++p){
+    if( p->status()<=2 && std::abs(p->pdgId ())==24){
+      vec=p->p4();
+    }
+    else{
+      if(p->status()<=2){
+	// sum up the p4 of all stable particles 
+	// (of status 1 or 2)
+	vec+=p->p4();
+      }
+      else{
+	if( p->status()==3){
+	  // if the particle is unfragmented (i.e.
+	  // status 3) descend by one level
+	  vec+=p4(p, statusFlag);   
+	}
+      }
+    }
+  }
+  return vec;
+}
+
+
+
+// fill vector including all radiations from quarks
+void 
+HiggsDecaySubset::addRadiation(int& idx, const reco::GenParticle::const_iterator part, reco::GenParticleCollection& target)
+{
+  std::vector<int> daughters;
+  int idxBuffer = idx;
+  for(reco::GenParticle::const_iterator daughter=part->begin(); daughter!=part->end(); ++daughter){
+    if(daughter->status()<=2 && daughter->pdgId()!=part->pdgId()){
+      // skip comment lines and make sure that
+      // the particle is not double counted as 
+      // dasughter of itself
+      std::auto_ptr<reco::GenParticle> ptr(  new reco::GenParticle( daughter->threeCharge(), daughter->p4(), daughter->vertex(), daughter->pdgId(), daughter->status(), false) );
+      target.push_back( *ptr );
+      daughters.push_back( ++idx ); //push index of daughter
+    }
+  }  
+  if(daughters.size()) {
+    refs_[ idxBuffer ] = daughters;
+  }
+}
+
+
+
+// fill references for output vector
+void 
+HiggsDecaySubset::fillReferences(const reco::GenParticleRefProd& ref, reco::GenParticleCollection& sel)
+{ 
+  int idx=0;
+  for(reco::GenParticleCollection::iterator p=sel.begin(); p!=sel.end(); ++p, ++idx){
+    //find daughter reference vectors in refs_ and add daughters
+    std::map<int, std::vector<int> >::const_iterator daughters=refs_.find( idx );
+    if( daughters!=refs_.end() ){
+      for(std::vector<int>::const_iterator daughter = daughters->second.begin(); 
+	  daughter!=daughters->second.end(); ++daughter){
+	reco::GenParticle* part = dynamic_cast<reco::GenParticle* > (&(*p));
+	if(part == 0){
+	 throw edm::Exception( edm::errors::InvalidReference, "Not a GenParticle" );
+	}
+	part->addDaughter( reco::GenParticleRef(ref, *daughter) );
+	sel[*daughter].addMother( reco::GenParticleRef(ref, idx) );
+      }
+    }
+  }
+}
+
+
+
+// ------------ method called once each job just before starting event loop  ------------
+void 
+HiggsDecaySubset::beginJob()
+{
+}
+
+// ------------ method called once each job just after ending the event loop  ------------
+void 
+HiggsDecaySubset::endJob() {
+}
+
+// ------------ method called when starting to processes a run  ------------
+void 
+HiggsDecaySubset::beginRun(edm::Run&, edm::EventSetup const&)
+{
+}
+
+// ------------ method called when ending the processing of a run  ------------
+void 
+HiggsDecaySubset::endRun(edm::Run&, edm::EventSetup const&)
+{
+}
+
+// ------------ method called when starting to processes a luminosity block  ------------
+void 
+HiggsDecaySubset::beginLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&)
+{
+}
+
+// ------------ method called when ending the processing of a luminosity block  ------------
+void 
+HiggsDecaySubset::endLuminosityBlock(edm::LuminosityBlock&, edm::EventSetup const&)
+{
+}
+
+// ------------ method fills 'descriptions' with the allowed parameters for the module  ------------
+void
+HiggsDecaySubset::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
+  //The following says we do not know what parameters are allowed so do no validation
+  // Please change this to state exactly what you do use, even if it is no parameters
+  edm::ParameterSetDescription desc;
+  desc.setUnknown();
+  descriptions.addDefault(desc);
+}
+
+//define this as a plug-in
+DEFINE_FWK_MODULE(HiggsDecaySubset);
---
+++ HiggsUtils/test/higgsDecaySubset_cfg.py
@@ -0,0 +1,40 @@
+import FWCore.ParameterSet.Config as cms
+
+
+process = cms.Process("HiggsDecayTest")
+
+
+process.load("FWCore.MessageService.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 10
+
+
+process.options = cms.untracked.PSet(
+    wantSummary = cms.untracked.bool(True)
+)
+
+
+process.load("TopAnalysis.Configuration.Summer12.TTH_HToBB_M125_8TeV_pythia6_Summer12_DR53X_PU_S10_START53_V7A_v1_cff")
+process.maxEvents.input = 101
+
+
+process.load("TopAnalysis.HiggsUtils.producers.HiggsDecaySubset_cfi")
+process.decaySubsetHiggs.fillMode = 'kME'
+process.decaySubsetHiggs.addRadiation = False
+
+
+process.p = cms.Path(process.decaySubsetHiggs)
+
+
+
+
+
+process.out = cms.OutputModule("PoolOutputModule",
+    fileName = cms.untracked.string('test_higgsDecaySubset.root'),
+    outputCommands = cms.untracked.vstring(
+        'drop *',
+	'keep recoGenParticles_*_*_SIM',
+	'keep recoGenParticles_*_*_HiggsDecayTest',
+    ),
+)
+
+process.e = cms.EndPath(process.out)
