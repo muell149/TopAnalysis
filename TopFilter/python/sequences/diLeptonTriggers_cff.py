4759de6d390c9b8078437537a3de0de24f12d326
wbehrenh <>
2013-01-23 12:51:34 +0000

    only use the triggers from the reference selection page

--- TopFilter/python/sequences/diLeptonTriggers_cff.py
+++ TopFilter/python/sequences/diLeptonTriggers_cff.py
@@ -1,31 +1,47 @@
 import FWCore.ParameterSet.Config as cms
 
-mumuTriggers = cms.vstring( 'HLT_DoubleMu6_v*',
-                            'HLT_DoubleMu7_v*',
-                            'HLT_Mu13_Mu8_v*',
+mumuTriggers = cms.vstring(
                             'HLT_Mu17_Mu8_v*',
-                            'HLT_DoubleMu45_v*'
-			    'HLT_Mu17_TkMu8_v*',	# data
-                            'HLT_Mu17_Mu8_v16',		# only for MC
-			    'HLT_Mu17_TkMu8_v9',	# only for MC
+                            'HLT_Mu17_TkMu8_v*',
                             )
-emuTriggers  = cms.vstring( 'HLT_Mu10_Ele10_CaloIdL_*',
-                            'HLT_Mu8_Ele17_CaloIdL_*',
-                            'HLT_Mu17_Ele8_CaloIdL_*',
-                            'HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_v*',
-                            'HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_v*',
-                            'HLT_Mu10_Ele10_CaloIdL_v*',
-                            'HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*',	# data
-			    'HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*',	# data
-			    'HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v6',	# MC with GT=START52_V9::All  #To be updated for CMSSW53X 
-			    'HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v6',	# MC with GT=START52_V9::All  #To be updated for CMSSW53X 
+
+emuTriggers  = cms.vstring( 
+                            'HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*',
+                            'HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*',
                             )
-eeTriggers   = cms.vstring( 'HLT_Ele17_CaloIdL_CaloIsoVL_Ele8_CaloIdL_CaloIsoVL_v*',
-                            'HLT_Ele17_CaloIdT_TrkIdVL_CaloIsoVL_TrkIsoVL_Ele8_CaloIdT_TrkIdVL_CaloIsoVL_TrkIsoVL_v*',
-                            'HLT_DoubleEle45_CaloIdL_v*',
-			    'HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*',	# data
-			    'HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v17',	# MC with GT=START52_V9::All  #To be updated for CMSSW53X 
+
+eeTriggers   = cms.vstring( 
+                            'HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*',
                             )
+
+
+
+#mumuTriggers = cms.vstring( 'HLT_DoubleMu6_v*',
+                            #'HLT_DoubleMu7_v*',
+                            #'HLT_Mu13_Mu8_v*',
+                            #'HLT_Mu17_Mu8_v*',
+                            #'HLT_DoubleMu45_v*'
+			    #'HLT_Mu17_TkMu8_v*',	# data
+                            #'HLT_Mu17_Mu8_v16',		# only for MC
+			    #'HLT_Mu17_TkMu8_v9',	# only for MC
+                            #)
+#emuTriggers  = cms.vstring( 'HLT_Mu10_Ele10_CaloIdL_*',
+                            #'HLT_Mu8_Ele17_CaloIdL_*',
+                            #'HLT_Mu17_Ele8_CaloIdL_*',
+                            #'HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_v*',
+                            #'HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_v*',
+                            #'HLT_Mu10_Ele10_CaloIdL_v*',
+                            #'HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*',	# data
+			    #'HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*',	# data
+			    #'HLT_Mu17_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v6',	# MC with GT=START52_V9::All  #To be updated for CMSSW53X 
+			    #'HLT_Mu8_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v6',	# MC with GT=START52_V9::All  #To be updated for CMSSW53X 
+                            #)
+#eeTriggers   = cms.vstring( 'HLT_Ele17_CaloIdL_CaloIsoVL_Ele8_CaloIdL_CaloIsoVL_v*',
+                            #'HLT_Ele17_CaloIdT_TrkIdVL_CaloIsoVL_TrkIsoVL_Ele8_CaloIdT_TrkIdVL_CaloIsoVL_TrkIsoVL_v*',
+                            #'HLT_DoubleEle45_CaloIdL_v*',
+			    #'HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v*',	# data
+			    #'HLT_Ele17_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_Ele8_CaloIdT_CaloIsoVL_TrkIdVL_TrkIsoVL_v17',	# MC with GT=START52_V9::All  #To be updated for CMSSW53X 
+                            #)
 
 #METTriggers  = cms.vstring( 'HLT_CentralJet80_MET100_v*',
 #                            'HLT_CentralJet80_MET110_v*',
