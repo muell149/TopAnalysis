e3093f7daf0d762142eecf0d73af42aaace5fbf1
mgoerner <>
2013-02-12 17:10:13 +0000

    small macro to derive the bin center corrections once and save the results in a .root file as TVectorD

---
+++ Configuration/analysis/semiLeptonic/diffXSection/applyBCCs.C
@@ -0,0 +1,107 @@
+#include "basicFunctions.h"
+#include "BCC.C"
+
+void applyBCCs(TString inputFolderName="RecentAnalysisRun8TeV", bool save=true, TString decayChannel="combined", bool extrapolate=true, bool hadron=false, bool mergeLepHad=true, bool addCrossCheckVariables=false)
+{
+    // INFO: this is the macro part to process the variables needed for differenetial cross sections
+
+    // name of file to calculate BCC from
+    TString inputFile=TopFilename(kSig, 0, "muon");
+    if(decayChannel=="electron") inputFile.ReplaceAll("muon", "elec");
+    else if (decayChannel=="combined"){
+      inputFile.ReplaceAll("muon", "combined");
+      inputFile.ReplaceAll(".root", "Large.root");
+    }
+    inputFile="/afs/naf.desy.de/group/cms/scratch/tophh/"+inputFolderName+"/"+inputFile;
+    std::cout << std::endl << "input file: " << inputFile << std::endl;
+  
+    // name of file to add calculted BCC x values to
+    TString outputFile="diffXSecBCC.root";
+    std::cout << "output file: " << outputFile << std::endl;
+    TString LV="Parton";
+    if(!extrapolate&&hadron) LV="Hadron";
+    TString PS="";
+    if(!extrapolate) PS="PhaseSpace";
+      
+    // list of variables of interest
+    std::vector<TString> xSecVariables_;
+    xSecVariables_.insert( xSecVariables_.begin(), xSecVariables,     xSecVariables     + sizeof(xSecVariables)/sizeof(TString)    );
+    xSecVariables_.insert( xSecVariables_.end(),   xSecVariablesNorm, xSecVariablesNorm + sizeof(xSecVariablesNorm)/sizeof(TString));
+    if (addCrossCheckVariables && !hadron){
+      // cross check variables presently only available for parton level cross-sections
+      xSecVariables_.insert( xSecVariables_.end(),   xSecVariablesCCVar,     xSecVariablesCCVar     + sizeof(xSecVariablesCCVar)/sizeof(TString)    );
+      xSecVariables_.insert( xSecVariables_.end(),   xSecVariablesCCVarNorm, xSecVariablesCCVarNorm + sizeof(xSecVariablesCCVarNorm)/sizeof(TString));
+    }
+    xSecVariables_.insert( xSecVariables_.end(),   xSecVariablesIncl,      xSecVariablesIncl      + sizeof(xSecVariablesIncl)/sizeof(TString)     );
+    // collect tree folder names and branchnames of variables for BCC
+    std::vector<TString> xSecVariableBranchNames_, folderNamesBCC_;
+    for(unsigned int i=0; i<xSecVariables_.size(); ++i){
+      if(!xSecVariables_[i].Contains("Norm")&&xSecVariables_[i]!="inclusive"){
+	// collect names of branch entries and names of cross section folder(s)
+	if(xSecVariables_[i]=="topPt" || xSecVariables_[i]=="topY"){ 
+	  xSecVariableBranchNames_.push_back(xSecVariables_[i]+"Had");
+	  xSecVariableBranchNames_.push_back(xSecVariables_[i]+"Lep");
+	  folderNamesBCC_.push_back("analyzeTop"+LV+"LevelKinematics"+PS);
+	  folderNamesBCC_.push_back("analyzeTop"+LV+"LevelKinematics"+PS);
+	}
+	else if(xSecVariables_[i].Contains("bq")){
+	  if(!extrapolate&&hadron){
+	    xSecVariableBranchNames_.push_back(xSecVariables_[i]+"GenLep");
+	    TString antib=xSecVariables_[i]+"GenHad";
+	    antib.ReplaceAll("bq", "bbarq");
+	    xSecVariableBranchNames_.push_back(antib);
+	    folderNamesBCC_.push_back("analyzeTop"+LV+"LevelKinematics"+"Bjets"+PS);
+	    folderNamesBCC_.push_back("analyzeTop"+LV+"LevelKinematics"+"Bjets"+PS);
+	  }
+	  else{
+	    xSecVariableBranchNames_.push_back(xSecVariables_[i]+"Had");
+	    xSecVariableBranchNames_.push_back(xSecVariables_[i]+"Lep");
+	    folderNamesBCC_.push_back("analyzeTop"+LV+"LevelKinematics"+PS);
+	    folderNamesBCC_.push_back("analyzeTop"+LV+"LevelKinematics"+PS);
+	  }
+	}
+	else if(xSecVariables_[i].Contains("lep")&&!extrapolate&&hadron&&!xSecVariables_[i].Contains("Plus")&&!xSecVariables_[i].Contains("Minus")){
+	  xSecVariableBranchNames_.push_back(xSecVariables_[i]+"Gen");
+	  folderNamesBCC_.push_back("analyzeTop"+LV+"LevelKinematics"+"Lepton"+PS);
+	}
+	else{
+	  xSecVariableBranchNames_.push_back(xSecVariables_[i]);
+  	  folderNamesBCC_.push_back("analyzeTop"+LV+"LevelKinematics"+PS); 
+	}
+      }
+    }
+    std::cout << "variables of interest: " << std::endl;
+    for(unsigned int i=0; i<folderNamesBCC_.size(); ++i){
+      std::cout << folderNamesBCC_[i] << "/tree/" << xSecVariableBranchNames_[i] << std::endl;
+    }
+
+    // run calculation
+    BCC b(inputFile, folderNamesBCC_, xSecVariableBranchNames_, mergeLepHad, addCrossCheckVariables);
+    b.runBCCCalculation();
+    // container for results of BCC
+    std::map<TString, std::vector<double> > correctedCenters_= b.getMapWithCorrectedCentersInX();
+    std::map<TString, std::vector<double> > corrCenterErrors_= b.getMapWithCenterErrorsInX();
+    // do saving
+    if(save){
+      std::cout << "saving:" << std::endl;
+      // loop variables
+      for(unsigned int i=0; i<xSecVariables_.size(); ++i){
+	TString candidate=xSecVariables_[i].ReplaceAll("Norm","");
+	std::cout << "candidate " << candidate;
+	if(correctedCenters_.count(candidate)!=0){
+	  std::cout << std::endl << "-> ";
+	  // convert map to TArrayD
+	  TVectorD* temp=new TVectorD(correctedCenters_[candidate].size(), correctedCenters_[candidate].data());
+	  // cross check
+// 	  for(int vec=0; vec<temp->GetNrows();++vec){	    
+// 	    double wert=temp->operator[](vec);
+// 	    if(vec==0) std::cout << "size: "<< correctedCenters_[candidate].size() << std::endl;
+// 	    std::cout << correctedCenters_[candidate][vec] << "->" << wert << std::endl;
+// 	  }
+          // save to file
+	  saveToRootFile(outputFile, temp, true, 1, "BCC/"+LV+PS+"/"+candidate);
+	}
+	else std::cout << "-> not found" << std::endl;
+      }
+    }
+}
