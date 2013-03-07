e0774656e52e70c0848faff4a7c1ac1fc26dff22
hauk <>
2013-03-07 00:16:20 +0000

    Polishing

--- Configuration/analysis/ttH/eventYields.C
+++ Configuration/analysis/ttH/eventYields.C
@@ -20,11 +20,11 @@ void EventYields::produceYields(){
     std::vector<TString> v_eventHistoName;
     v_eventHistoName = fileReader_->findHistos(samples_.getSamples(Sample::emu, Sample::nominal)[0].inputFile(), "step");
     
-    // Loop over systematics (exclude all but Nominal) and channels
-    for(auto systematicChannelSample : samples_.getSystematicChannelSamples()){
-        const Sample::Systematic& systematic(systematicChannelSample.first);
-        if(systematic!=Sample::Systematic::nominal)continue;
-        for(auto channelSample : systematicChannelSample.second){
+    // Loop over systematics (exclude all but Nominal - so outer loop could be removed) and channels
+    for(auto systematicChannelSamples : samples_.getSystematicChannelSamples()){
+        const Sample::Systematic& systematic(systematicChannelSamples.first);
+        if(systematic!=Sample::nominal)continue;
+        for(auto channelSample : systematicChannelSamples.second){
             const Sample::Channel& channel(channelSample.first);
             this->writeYields(channel, channelSample.second, v_eventHistoName);
         }
@@ -37,6 +37,7 @@ void EventYields::writeYields(const Sample::Channel& channel, const std::vector<
     // Loop over all selection steps writing out event yields
     for(std::vector<TString>::const_iterator i_eventHistoName = v_eventHistoName.begin(); i_eventHistoName != v_eventHistoName.end(); ++i_eventHistoName){
         
+        // Assign histogram to sample
         std::vector<SampleHistPair> v_numhist;
         for(auto sample : v_sample){
             TH1D *temp_hist = fileReader_->GetClone<TH1D>(sample.inputFile(), *i_eventHistoName);
@@ -45,9 +46,10 @@ void EventYields::writeYields(const Sample::Channel& channel, const std::vector<
             v_numhist.push_back(SampleHistPair(sample, temp_hist));
         }
         
+        // Apply Drell-Yan scaling
         for(auto sampleHistPair : v_numhist){
             const bool isDyll(sampleHistPair.first.sampleType() == Sample::SampleType::dyll);
-            if(isDyll && channel!=Sample::Channel::emu){
+            if(isDyll && channel!=Sample::emu){
                 // FIXME: which DY scale factor is here applied, isn't it always the same instead of the step dependent one ?
                 sampleHistPair.second->Scale(1.);
                 //numhist.first->Scale(dyScaleFactors_(channel));
