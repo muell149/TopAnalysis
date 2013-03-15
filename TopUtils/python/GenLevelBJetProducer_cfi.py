431f205d9926184be6d175f9100c19a3408e29b9
nbartosi <>
2013-03-15 15:37:48 +0000

    Added options for improved jet-hadron-quark matching. Default values don't change functionality.

--- TopUtils/python/GenLevelBJetProducer_cfi.py
+++ TopUtils/python/GenLevelBJetProducer_cfi.py
@@ -2,11 +2,14 @@ import FWCore.ParameterSet.Config as cms
 
 produceGenLevelBJets = cms.EDProducer("GenLevelBJetProducer",
     ttGenEvent = cms.InputTag('genEvt'),
-    genJets = cms.InputTag('ak5GenJets','','HLT'),
+    genJets = cms.InputTag('ak5GenJets','','SIM'),   
     deltaR = cms.double(0.5),
+    flavour = cms.int32(5),
     resolveParticleName = cms.bool(False),
     requireTopBquark = cms.bool(False),
-    noBBbarResonances = cms.bool(True)
+    noBBbarResonances = cms.bool(True),
+    doImprovedHadronMatching = cms.bool(False),
+    doValidationPlotsForImprovedHadronMatching = cms.bool(False)
 )
 
 
