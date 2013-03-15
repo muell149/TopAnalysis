12711383c3ff462b215af279c2b11e7a108f7211
nbartosi <>
2013-03-15 13:18:36 +0000

    Renamed to improvedJetHadronQuarkMatching_cff.py

--- TopUtils/python/sequences/improvedBHadronMatching_cff.py
+++
@@ -1,9 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-from RecoJets.Configuration.RecoGenJets_cff import ak5GenJets
-from TopAnalysis.TopUtils.GenJetParticles_cff import genParticlesForJetsPlusBHadronNoNu
-
-ak5GenJetsPlusHadron = ak5GenJets.clone()
-ak5GenJetsPlusHadron.src = "genParticlesForJetsPlusBHadronNoNu"
-
-improvedHadronMatchingSequence = cms.Sequence(genParticlesForJetsPlusBHadronNoNu * ak5GenJetsPlusHadron)
-
