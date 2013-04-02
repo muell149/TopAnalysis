fdb2c104e08b01e089683248867ee138fcf93761
hauk <>
2013-04-02 18:51:45 +0000

    Allow option -m for this script, using this option in load_HiggsAnalysis

--- Configuration/analysis/ttH/scripts/runNominalParallel.sh
+++ Configuration/analysis/ttH/scripts/runNominalParallel.sh
@@ -1,28 +1,37 @@
 #!/bin/sh
 
 
+
+if [ $# -ge 1 ] && [[ ! "$1" == "-m" ]] ; then
+    echo "Option is not allowed here: $1"
+    echo "Only '-m OPTION' can be used here with this script"
+    exit 1
+fi
+
+
+
 source $(dirname `readlink -f $0`)/parallelTools.sh
 
 
 for c in ee emu mumu; do
-    $LA -f dy -d 11 -c $c &
-    $LA -f dy -d 13 -c $c &
-    $LA -f dy -d 15 -c $c &
-    $LA -f ttbarsignalplustau.root -c $c &
-    $LA -f ttbarH125 -c $c &
+    $LA -f dy -d 11 -c $c $@ &
+    $LA -f dy -d 13 -c $c $@ &
+    $LA -f dy -d 15 -c $c $@ &
+    $LA -f ttbarsignalplustau.root -c $c $@ &
+    $LA -f ttbarH125 -c $c $@ &
 done
 
 for c in ee emu mumu; do
-    $LA -f ${c}_run2012A -c $c &
-    $LA -f ${c}_run2012B -c $c &
-    $LA -f ${c}_run2012C -c $c &
+    $LA -f ${c}_run2012A -c $c $@ &
+    $LA -f ${c}_run2012B -c $c $@ &
+    $LA -f ${c}_run2012C -c $c $@ &
 done 
 
 for i in qcd single ttbarbg.root wtol ww wz zz ttbarW ttbarZ; do
     w
-    $LA -f $i -c ee&
-    $LA -f $i -c emu&
-    $LA -f $i -c mumu&
+    $LA -f $i -c ee $@ &
+    $LA -f $i -c emu $@ &
+    $LA -f $i -c mumu $@ &
 done
 
 wait
