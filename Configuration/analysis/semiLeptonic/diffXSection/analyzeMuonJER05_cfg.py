a24f1f37e6de5b22df3d4b7b1f0a977609f071f0
henderle <>
2013-04-05 15:12:50 +0000

    remove JER variation in KinFit to avoid underestimation of the JER uncertainty

--- Configuration/analysis/semiLeptonic/diffXSection/analyzeMuonJER05_cfg.py
+++ Configuration/analysis/semiLeptonic/diffXSection/analyzeMuonJER05_cfg.py
@@ -11,10 +11,6 @@ execfile("analyzeMuonDiffXSecCorrected_cfg.py")
 process.scaledJetEnergy.resolutionFactors   = cms.vdouble( 0.990 , 1.001 , 1.032 , 1.042 , 1.089 )
 #process.scaledJetEnergy.resolutionEtaRanges = cms.vdouble(0.0,0.5,0.5,1.1,1.1,1.7,1.7,2.3,2.3,-1.)
 
-if(applyKinFit==True):
-# eta-dependent scaling of the JER (input to kinFit) according to the smearing
-    process.kinFitTtSemiLepEventHypothesis.jetEnergyResolutionScaleFactors = cms.vdouble( 0.990 , 1.001 , 1.032 , 1.042 , 1.089   )
-
 ## change output name 
 #process.TFileService.fileName = 'analyzeDiffXSecJERdown_testSig.root'
 process.TFileService.fileName = outputFileNamePart+"JERDown"+options.mctag+"PF.root"
