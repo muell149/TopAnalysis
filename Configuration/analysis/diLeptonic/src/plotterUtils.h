968738aac71abe62db4b57b14c653b1b58c96420
hauk <>
2013-04-27 17:13:53 +0000

    Utilities which are exclusively for the plotting step

---
+++ Configuration/analysis/diLeptonic/src/plotterUtils.h
@@ -0,0 +1,46 @@
+#ifndef ttbar_histoUtils_h
+#define ttbar_histoUtils_h
+
+#include <vector>
+
+#include <Rtypes.h>
+
+class TH1;
+class THStack;
+class TStyle;
+
+
+
+
+namespace ttbar{
+    
+    /// Draw ratio of two histograms
+    void drawRatio(const TH1* histNumerator, 
+                   const TH1* histDenominator,
+                   const Double_t& ratioMin, 
+                   const Double_t& ratioMax, 
+                   const TStyle& myStyle = *gStyle, 
+                   const int verbose=0, 
+                   const std::vector<double>& err=std::vector<double>(0));
+
+
+
+    /// Set histogram style to HH definition
+    void setHHStyle(TStyle& HHStyle);
+
+
+
+    /** Sum the histograms in a stack and
+     * return the sum in a new TH1
+     */
+    TH1* summedStackHisto(const THStack* stack);
+}
+
+
+
+
+
+#endif
+
+
+
