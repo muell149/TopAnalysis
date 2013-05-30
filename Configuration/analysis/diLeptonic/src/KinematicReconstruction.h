f1240f396414582ea545624a0c235e535598bcff
dolinska <>
2013-05-30 13:06:34 +0000

    New classes for improved Kinematic Reconstruction

---
+++ Configuration/analysis/diLeptonic/src/KinematicReconstruction.h
@@ -0,0 +1,50 @@
+#ifndef KinematicReconstruction_h
+#define KinematicReconstruction_h
+
+#include <TLorentzVector.h>
+#include <Math/LorentzVector.h>
+#include <Math/PtEtaPhiM4D.h>
+#include <vector>
+#include <TMath.h>
+#include <TF2.h>
+#include <Rtypes.h>
+#include <iostream>
+#include <stdio.h>
+
+#include "classesFwd.h"
+
+
+//typedef ROOT::Math::LorentzVector<ROOT::Math::PtEtaPhiM4D<double> > LV;
+
+struct struct_KinematicReconstruction {
+    TLorentzVector lp, lm;
+    TLorentzVector jetB, jetBbar;
+    size_t jetB_index, jetBbar_index;
+    TLorentzVector met;
+    TLorentzVector Wplus, Wminus;
+    TLorentzVector top, topBar;
+    TLorentzVector neutrino, neutrinoBar;
+    TLorentzVector ttbar;
+    double recMtop;
+    double weight;
+    int ntags;
+};
+
+class KinematicReconstruction {
+
+public:
+  KinematicReconstruction(const LV& leptonMinus, const LV& leptonPlus, const VLV *jets, const std::vector<double> *btags, const LV* met);
+
+int GetNSol();
+struct_KinematicReconstruction GetSol();
+
+private:
+
+//private:
+    int nSol;
+    struct_KinematicReconstruction sol;
+
+  };
+
+
+#endif
