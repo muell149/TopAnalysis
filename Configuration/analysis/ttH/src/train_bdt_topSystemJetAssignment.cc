ee7af84d9bac7d430a614b18309629c7a8002aff
hauk <>
2013-06-12 14:29:58 +0000

    Make it more module-based and add swapped combinations

--- Configuration/analysis/ttH/src/train_bdt_topSystemJetAssignment.cc
+++ Configuration/analysis/ttH/src/train_bdt_topSystemJetAssignment.cc
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <cstdlib>
 #include <vector>
+#include <string>
+#include <algorithm>
 
 #include "TCut.h"
 #include "TFile.h"
@@ -10,16 +12,10 @@
 #include "TSystem.h"
 #include "TList.h"
 
-#include "TMVA/Tools.h"
-#include "TMVA/Factory.h"
-#include "TMVA/Types.h"
-#include "TMVA/Config.h"
-
 #include "MvaInputVariables.h"
 #include "../../diLeptonic/src/sampleHelpers.h"
-
-//#include "TMVAGui.C"
-
+#include "../../diLeptonic/src/CommandLineParameters.h"
+#include "../../diLeptonic/src/utils.h"
 
 
 
@@ -30,14 +26,24 @@ constexpr const char* MvaInputDIR = "mvaInput";
 /// The MVA output base folder
 constexpr const char* MvaOutputDIR = "mvaOutput";
 
+/// The MVA weights output folder
+constexpr const char* MvaWeightFileDIR = "/weights";
+
+
+
 /// Input base for the file lists containing the samples to be processed
 constexpr const char* FileListBASE = "FileLists_mva/HistoFileList_";
 
 
+/// The output file name for the control and separation power plots
+constexpr const char* PlotOutputFILE = "plots.root";
 
 
 
-void trainBdtTopSystemJetAssignment()
+
+
+
+void trainBdtTopSystemJetAssignment(const std::vector<std::string>& modes)
 {
     std::cout<<"\n--- Beginning MVA training\n";
     
@@ -105,119 +111,67 @@ void trainBdtTopSystemJetAssignment()
     gSystem->MakeDirectory(mergedTreesName);
     mergedTreesName.Append("mergedTrees.root");
     TFile* mergedTrees = new TFile(mergedTreesName, "RECREATE");
-    TTree* treeTraining1 = TTree::MergeTrees(listTraining);
-    treeTraining1->SetName("mvaInputTopJets_training");
-    TTree* treeTesting1 = TTree::MergeTrees(listTesting);
-    treeTesting1->SetName("mvaInputTopJets_testing");
-    treeTraining1->Write();
-    treeTesting1->Write();
+    TTree* treeTraining = TTree::MergeTrees(listTraining);
+    treeTraining->SetName("mvaInputTopJets_training");
+    TTree* treeTesting = TTree::MergeTrees(listTesting);
+    treeTesting->SetName("mvaInputTopJets_testing");
+    treeTraining->Write();
+    treeTesting->Write();
     mergedTrees->Close();
     
+    // Print all separation power plots
     TString outputPlots(MvaOutputDIR);
-    outputPlots.Append("/plots.root");
+    outputPlots.Append("/").Append(PlotOutputFILE);
     MvaInputTopJetsVariables mvaInputTopJetsVariables;
     mvaInputTopJetsVariables.importTree(mergedTreesName.Data(), "mvaInputTopJets_training");
-    mvaInputTopJetsVariables.mvaInputVariablesControlPlots(outputPlots.Data());
+    if(std::find(modes.begin(), modes.end(), "cp") != modes.end()) mvaInputTopJetsVariables.mvaInputVariablesControlPlots(outputPlots.Data());
     
     mergedTrees = TFile::Open(mergedTreesName);
-    treeTraining1 = (TTree*)mergedTrees->Get("mvaInputTopJets_training");
-    treeTesting1 = (TTree*)mergedTrees->Get("mvaInputTopJets_testing");
+    treeTraining = (TTree*)mergedTrees->Get("mvaInputTopJets_training");
+    treeTesting = (TTree*)mergedTrees->Get("mvaInputTopJets_testing");
     
     
     
+    constexpr const char* mvaOutputFilenameCorrect = "correct.root";
+    constexpr const char* methodNameCorrect = "correct";
+    
+    const TCut cutSignalCorrect = "correctCombination != 0";
+    const TCut cutBackgroundCorrect = "correctCombination == 0 && swappedCombination == 0";
+    
+    if(std::find(modes.begin(), modes.end(), "mva") != modes.end())
+        mvaInputTopJetsVariables.runMva(MvaOutputDIR, MvaWeightFileDIR, mvaOutputFilenameCorrect,
+                                        methodNameCorrect, cutSignalCorrect, cutBackgroundCorrect,
+                                        treeTraining, treeTesting);
     
     
-    // Get a TMVA instance
-    TMVA::Tools::Instance();
     
-    // Create a ROOT output file for TMVA
-    TString mvaOutputFilename(MvaOutputDIR);
-    mvaOutputFilename.Append("/");
-    gSystem->MakeDirectory(mvaOutputFilename);
-    mvaOutputFilename.Append("test1.root");
-    TFile* outputFile = TFile::Open(mvaOutputFilename, "RECREATE");
+    constexpr const char* mvaOutputFilenameSwapped = "swapped.root";
+    constexpr const char* methodNameSwapped = "swapped";
     
-    // Set the output directory for the weights (if not specified, default is "weights")
-    TString mvaOutputWeightsFilename(MvaOutputDIR);
-    mvaOutputWeightsFilename.Append("/weights");
-    (TMVA::gConfig().GetIONames()).fWeightFileDir = mvaOutputWeightsFilename;
+    const TCut cutSignalSwapped = "swappedCombination != 0";
+    const TCut cutBackgroundSwapped = "correctCombination == 0 && swappedCombination == 0";
     
-    // Create the factory
-    TMVA::Factory* factory(0);
-    factory = new TMVA::Factory( "MVA", outputFile, "!V:!Silent");
-    
-    // Set all branches of MVA input which should be used for training
-    factory->AddVariable("meanDeltaPhi_b_met", 'F');
-    factory->AddVariable("massDiff_recoil_bbbar", 'F');
-    factory->AddVariable("pt_b_antiLepton", 'F');
-    factory->AddVariable("pt_antiB_lepton", 'F');
-    factory->AddVariable("deltaR_b_antiLepton", 'F');
-    factory->AddVariable("deltaR_antiB_lepton", 'F');
-    factory->AddVariable("btagDiscriminatorSum", 'F');
-    factory->AddVariable("deltaPhi_antiBLepton_bAntiLepton", 'F');
-    factory->AddVariable("massDiff_fullBLepton_bbbar", 'F');
-    factory->AddVariable("meanMt_b_met", 'F');
-    factory->AddVariable("massSum_antiBLepton_bAntiLepton", 'F');
-    factory->AddVariable("massDiff_antiBLepton_bAntiLepton", 'F');
-    
-    // Set all branches of MVA input which should NOT be used for training,
-    // but are needed otherwise (e.g. for defining separation cuts)
-    factory->AddSpectator("bQuarkRecoJetMatched", 'I');
-    factory->AddSpectator("correctCombination", 'I');
-    factory->AddSpectator("swappedCombination", 'I');
-    
-    // Set global weights for individual input
-    Double_t signalWeight = 1.;
-    Double_t backgroundWeight = 1.;
-    
-    // Register the training trees
-    factory->AddSignalTree(treeTraining1, signalWeight, TMVA::Types::kTraining);
-    factory->AddBackgroundTree(treeTraining1, backgroundWeight, TMVA::Types::kTraining);
-//     for(const auto& treeTraining : v_treeTraining){
-//         factory->AddSignalTree(treeTraining, signalWeight, TMVA::Types::kTraining);
-//         factory->AddBackgroundTree(treeTraining, backgroundWeight, TMVA::Types::kTraining);
-//     }
-    
-    // Register the testing trees
-    factory->AddSignalTree(treeTesting1, signalWeight, TMVA::Types::kTesting);
-    factory->AddBackgroundTree(treeTesting1, backgroundWeight, TMVA::Types::kTesting);
-//     for(const auto& treeTesting : v_treeTesting){
-//         factory->AddSignalTree(treeTesting, signalWeight, TMVA::Types::kTesting);
-//         factory->AddBackgroundTree(treeTesting, backgroundWeight, TMVA::Types::kTesting);
-//     }
-    
-    // Set the branch from which the event weight is taken
-    factory->SetSignalWeightExpression("eventWeight");
-    factory->SetBackgroundWeightExpression("eventWeight");
-    
-    // Apply additional cuts on the signal and background samples
-    // If cut is empty (""), no selection is applied
-    TCut cutSignal = "correctCombination != 0";
-    TCut cutBackground = "correctCombination == 0";
-    
-    // Prepare the training and test trees
-    factory->PrepareTrainingAndTestTree(cutSignal, cutBackground,
-                                        "SplitMode=Block:SplitSeed=0:NormMode=NumEvents:!V" );
-    
-    // Book the MVA method (e.g. boosted decision tree with specific setup)
-    factory->BookMethod(TMVA::Types::kBDT,
-                        "test2",
-                        "!H:!V:NTrees=600:nEventsMin=400:MaxDepth=3:BoostType=AdaBoost:AdaBoostBeta=0.45:UseRandomisedTrees=False:UseNVars=12:nCuts=1000:PruneMethod=CostComplexity:PruneStrength=-1");
-    factory->TrainAllMethods();
-    factory->TestAllMethods();
-    factory->EvaluateAllMethods();
-    
-    // Cleanup
-    outputFile->Close();
-    delete factory;
+    if(std::find(modes.begin(), modes.end(), "mva") != modes.end())
+        mvaInputTopJetsVariables.runMva(MvaOutputDIR, MvaWeightFileDIR, mvaOutputFilenameSwapped,
+                                        methodNameSwapped, cutSignalSwapped, cutBackgroundSwapped,
+                                        treeTraining, treeTesting);
     
     std::cout<<"=== Finishing MVA training\n\n";
 }
 
 
 
-int main(int argc, char** argv) {
-    trainBdtTopSystemJetAssignment();
+int main(int argc, char** argv)
+{
+    CLParameter<std::string> opt_mode("m", "Mode: separation plots (cp), run MVA (mva). Default is cp", false, 1, 2,
+            //[](const std::string& m){return m=="" || m=="cp" || m=="mva";});
+            ttbar::makeStringCheck({"", "cp", "mva"}));
+    CLAnalyser::interpretGlobal(argc, argv);
+    
+    
+    const std::vector<std::string> modes = opt_mode.isSet() ? opt_mode.getArguments() : std::vector<std::string>{"cp"};
+    
+    trainBdtTopSystemJetAssignment(modes);
 }
 
 
