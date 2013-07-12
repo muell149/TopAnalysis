24e1e2984f5aaa27eb1d44e12cf56e973b9d75d0
Eike Schlieckau <eschliec@uh2ulnmpc40.desy.de>
2013-07-12 16:22:47 +0200

    - added CSVT - added possibility to not create a histogram of the weights

--- TopUtils/python/BTagSFEventWeight_cfi.py
+++ TopUtils/python/BTagSFEventWeight_cfi.py
@@ -3,7 +3,7 @@ import FWCore.ParameterSet.Config as cms
 bTagSFEventWeight = cms.EDProducer("BTagSFEventWeight",
   jets  = cms.InputTag("tightLeadingPFJets"), ## jet collection (after jet selection, before b-tagging)
   bTagAlgo = cms.string("CSVM"),              ## name of b tag algorithm (short, i.e. "CSVM")
-  version  = cms.string("2012"),            ## "12-470" for value from AN for moriond 2013,
+  version  = cms.string("2012"),              ## "12-470" for value from AN for moriond 2013,
   sysVar   = cms.string(""),                  ## bTagSFUp, bTagSFDown, misTagSFUp, misTagSFDown possible;
                                               ## bTagSFShapeUpPt, bTagSFShapeDownPt, bTagSFShapeUpEta, bTagSFShapeDownEta,
                                               ## everything else: no systematic variation is made
@@ -12,6 +12,7 @@ bTagSFEventWeight = cms.EDProducer("BTagSFEventWeight",
   uncertaintySFb = cms.double(-1),            ## uncertainty of SFb (0.05 means 5%); if set to <0, the values from the b-tag DB are taken
   shapeDistortionFactor = cms.double(0.5),    ## for shape uncertainty calculation (fraction of normalisation uncertainty)
   verbose  = cms.int32(  0),                  ## set to 1 if terminal text output is desired
-  filename = cms.FileInPath("")               ## if filename != "", the efficiencies are read from histos
+  filename = cms.FileInPath(""),              ## if filename != "", the efficiencies are read from histos
                                               ## provided in that file
+  noHistograms = cms.bool(False)              ## deactivate the histogram creation
 )
