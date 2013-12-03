44433951e4941b213332db9c8d5ee2d97949100e
Jasone Garay <jasone.garay.garcia@cern.ch>
2013-12-03 12:35:14 +0100

    cleaning and indentation correction

--- HiggsUtils/interface/JetProperties.h
+++ HiggsUtils/interface/JetProperties.h
@@ -11,39 +11,34 @@
 
 
 class JetProperties{
-
+	
 public:
-
-    JetProperties();
-    
-    JetProperties(const double& jetChargeGlobalPtWeighted, const double& jetChargeRelativePtWeighted,
-                  const int& jetAssociatedPartonPdgId, const math::PtEtaPhiMLorentzVectorD& jetAssociatedParton,
-		 const std::vector<math::PtEtaPhiMLorentzVectorD>& jetTrack, const std::vector<int>& jetTrackCharge/*,
-		 const std::vector<int>& jetTrackIndex*/
- 		);
-    
-//     JetProperties(const double& jetChargeGlobalPtWeighted, const double& jetChargeRelativePtWeighted,
-// 		  const int& jetAssociatedPartonPdgId, const math::PtEtaPhiMLorentzVectorD& jetAssociatedParton);
-    
-    JetProperties(const JetProperties& jetProperties);
-    
+	
+	JetProperties();
+	
+	JetProperties(const double& jetChargeGlobalPtWeighted, const double& jetChargeRelativePtWeighted,
+		      const int& jetAssociatedPartonPdgId, const math::PtEtaPhiMLorentzVectorD& jetAssociatedParton,
+	       const std::vector<math::PtEtaPhiMLorentzVectorD>& jetTrack, const std::vector<int>& jetTrackCharge
+	);
+	
+	
+	JetProperties(const JetProperties& jetProperties);
+	
     double jetChargeGlobalPtWeighted()const;
-    double jetChargeRelativePtWeighted()const;
-    int jetAssociatedPartonPdgId()const;
-    math::PtEtaPhiMLorentzVectorD jetAssociatedParton()const;
-    std::vector<math::PtEtaPhiMLorentzVectorD> jetTrack()const;
-    std::vector<int> jetTrackCharge()const;
-//     std::vector<int> jetTrackIndex()const;
-    
+	double jetChargeRelativePtWeighted()const;
+	int jetAssociatedPartonPdgId()const;
+	math::PtEtaPhiMLorentzVectorD jetAssociatedParton()const;
+	std::vector<math::PtEtaPhiMLorentzVectorD> jetTrack()const;
+	std::vector<int> jetTrackCharge()const;
+	
 private:
-    
-    double jetChargeGlobalPtWeighted_;
-    double jetChargeRelativePtWeighted_;
-    int jetAssociatedPartonPdgId_;
-    math::PtEtaPhiMLorentzVectorD jetAssociatedParton_;
-    std::vector<math::PtEtaPhiMLorentzVectorD> jetTrack_;
-    std::vector<int> jetTrackCharge_;
-//     std::vector<int> jetTrackIndex_;
+	
+	double jetChargeGlobalPtWeighted_;
+	double jetChargeRelativePtWeighted_;
+	int jetAssociatedPartonPdgId_;
+	math::PtEtaPhiMLorentzVectorD jetAssociatedParton_;
+	std::vector<math::PtEtaPhiMLorentzVectorD> jetTrack_;
+	std::vector<int> jetTrackCharge_;
 };
 
 
