071ef4b417814bb37d3f2eb7ed1ee1c5617f42a7
Nazar <nazar.bartosik@gmail.com>
2014-01-09 13:51:24 +0100

    Completed the CSV SF parameterisations (light jets)

--- ZTopUtils/src/bTagBase.cc
+++ ZTopUtils/src/bTagBase.cc
@@ -516,7 +516,7 @@ float bTagBase::BJetSF(const float & pt, const float& abs_eta,
 
             //put the parametrization of the scale factor here (copy/paste from BTagPOG payload)
             //do NOT use static (should not compile anyway)
-            float SF = (0.939158 + (0.000158694 * x))+( -2.53962e-07 * (x * x));
+            float SF = (0.9203 + (-3.32421e-05 * x)) + (-7.74664e-08 * (x * x));
 
             //don't change:
             //takes care of systematic variations (if any)
@@ -583,53 +583,96 @@ float bTagBase::LJetSF(const float& pt, const float& abs_eta,
 
             if (syst_ != lightdown && syst_ != lightup) { //don't change
                 if (abs_eta <= 0.5)
-                    return ((1.01177 + (0.0023066 * x))
-                            + (-4.56052e-06 * (x * x)))
-                            + (2.57917e-09 * (x * (x * x)));
+                    return ((1.01177 + (0.0023066 * x)) + (-4.56052e-06 * (x * x))) + (2.57917e-09 * (x * (x * x)));
                 else if (abs_eta <= 1.0)
-                    return ((0.975966 + (0.00196354 * x))
-                            + (-3.83768e-06 * (x * x)))
-                            + (2.17466e-09 * (x * (x * x)));
+                    return ((0.975966 + (0.00196354 * x)) + (-3.83768e-06 * (x * x))) + (2.17466e-09 * (x * (x * x)));
                 else if (abs_eta <= 1.5)
-                    return ((0.93821 + (0.00180935 * x))
-                            + (-3.86937e-06 * (x * x)))
-                            + (2.43222e-09 * (x * (x * x)));
+                    return ((0.93821 + (0.00180935 * x)) + (-3.86937e-06 * (x * x))) + (2.43222e-09 * (x * (x * x)));
                 else
-                    return ((1.00022 + (0.0010998 * x))
-                            + (-3.10672e-06 * (x * x)))
-                            + (2.35006e-09 * (x * (x * x)));
+                    return ((1.00022 + (0.0010998 * x)) + (-3.10672e-06 * (x * x))) + (2.35006e-09 * (x * (x * x)));
             } else if (syst_ == lightdown) { //don't change
                 if (abs_eta <= 0.5)
-                    return ((0.977761 + (0.00170704 * x))+ (-3.2197e-06 * (x * x)))+ (1.78139e-09 * (x * (x * x)));
+                    return ((0.977761 + (0.00170704 * x)) + (-3.2197e-06 * (x * x))) + (1.78139e-09 * (x * (x * x)));
                 else if (abs_eta <= 1.0)
-                    return ((0.945135 + (0.00146006 * x))
-                            + (-2.70048e-06 * (x * x)))
-                            + (1.4883e-09 * (x * (x * x)));
+                    return ((0.945135 + (0.00146006 * x)) + (-2.70048e-06 * (x * x))) + (1.4883e-09 * (x * (x * x)));
                 else if (abs_eta <= 1.5)
-                    return ((0.911657 + (0.00142008 * x))
-                            + (-2.87569e-06 * (x * x)))
-                            + (1.76619e-09 * (x * (x * x)));
+                    return ((0.911657 + (0.00142008 * x)) + (-2.87569e-06 * (x * x))) + (1.76619e-09 * (x * (x * x)));
                 else
-                    return ((1.03039 + (0.0013358 * x))
-                            + (-3.89284e-06 * (x * x)))
-                            + (3.01155e-09 * (x * (x * x)));
+                    return ((1.03039 + (0.0013358 * x)) + (-3.89284e-06 * (x * x))) + (3.01155e-09 * (x * (x * x)));
             } else if (syst_ == lightup) { //don't change
                 if (abs_eta <= 0.5)
-                    return ((1.04582 + (0.00290226 * x))
-                            + (-5.89124e-06 * (x * x)))
-                            + (3.37128e-09 * (x * (x * x)));
+                    return ((1.04582 + (0.00290226 * x)) + (-5.89124e-06 * (x * x))) + (3.37128e-09 * (x * (x * x)));
                 else if (abs_eta <= 1.0)
-                    return ((1.00683 + (0.00246404 * x))
-                            + (-4.96729e-06 * (x * x)))
-                            + (2.85697e-09 * (x * (x * x)));
+                    return ((1.00683 + (0.00246404 * x)) + (-4.96729e-06 * (x * x))) + (2.85697e-09 * (x * (x * x)));
                 else if (abs_eta <= 1.5)
-                    return ((0.964787 + (0.00219574 * x))
-                            + (-4.85552e-06 * (x * x)))
-                            + (3.09457e-09 * (x * (x * x)));
+                    return ((0.964787 + (0.00219574 * x)) + (-4.85552e-06 * (x * x))) + (3.09457e-09 * (x * (x * x)));
                 else
-                    return ((1.1388 + (0.000468418 * x))
-                            + (-1.36341e-06 * (x * x)))
-                            + (1.19256e-09 * (x * (x * x)));
+                    return ((1.1388 + (0.000468418 * x)) + (-1.36341e-06 * (x * x))) + (1.19256e-09 * (x * (x * x)));
+            }
+        } else if (wp_ == csvm_wp) {
+            // Please give some information about the SF here. E.g
+            // -------
+            // SF from:
+            // https://twiki.cern.ch/twiki/pub/CMS/BtagPOG (EPS 2013 prescription)
+            // From https://twiki.cern.ch/twiki/pub/CMS/BtagPOG/SFlightFuncs_EPS2013.C  (EPS 2013 prescription)
+            // -------
+            //
+            // make sure, the range is appropriate (check the prescription)
+
+            if (x > 800 && abs_eta <= 1.5)
+                x = 800;
+            else if (x > 700 && abs_eta > 1.5)
+                x = 700;
+
+            // Please copy-paste the formulas here. Check the correct eta range
+            // systematics are handled directly here. use "else if" where applicable
+
+            if (syst_ != lightdown && syst_ != lightup) { //don't change
+                if (abs_eta <= 0.8)
+                    return ((1.07541+(0.00231827*x))+(-4.74249e-06*(x*x)))+(2.70862e-09*(x*(x*x)));
+                else if (abs_eta <= 1.6)
+                    return ((1.05613+(0.00114031*x))+(-2.56066e-06*(x*x)))+(1.67792e-09*(x*(x*x)));
+                else 
+                    return ((1.05625+(0.000487231*x))+(-2.22792e-06*(x*x)))+(1.70262e-09*(x*(x*x)));
+            } else if (syst_ == lightdown) { //don't change
+                if (abs_eta <= 0.8)
+                    return ((0.964527+(0.00149055*x))+(-2.78338e-06*(x*x)))+(1.51771e-09*(x*(x*x)));
+                else if (abs_eta <= 1.6)
+                    return ((0.946051+(0.000759584*x))+(-1.52491e-06*(x*x)))+(9.65822e-10*(x*(x*x)));
+                else
+                    return ((0.956736+(0.000280197*x))+(-1.42739e-06*(x*x)))+(1.0085e-09*(x*(x*x)));
+            } else if (syst_ == lightup) { //don't change
+                if (abs_eta <= 0.8)
+                    return ((1.18638+(0.00314148*x))+(-6.68993e-06*(x*x)))+(3.89288e-09*(x*(x*x)));
+                else if (abs_eta <= 1.6)
+                    return ((1.16624+(0.00151884*x))+(-3.59041e-06*(x*x)))+(2.38681e-09*(x*(x*x)));
+                else
+                    return ((1.15575+(0.000693344*x))+(-3.02661e-06*(x*x)))+(2.39752e-09*(x*(x*x)));
+            }
+        } else if (wp_ == csvt_wp) {
+            // Please give some information about the SF here. E.g
+            // -------
+            // SF from:
+            // https://twiki.cern.ch/twiki/pub/CMS/BtagPOG (EPS 2013 prescription)
+            // From https://twiki.cern.ch/twiki/pub/CMS/BtagPOG/SFlightFuncs_EPS2013.C  (EPS 2013 prescription)
+            // -------
+            //
+            // make sure, the range is appropriate (check the prescription)
+
+            if (x > 800 && abs_eta <= 1.5)
+                x = 800;
+            else if (x > 700 && abs_eta > 1.5)
+                x = 700;
+
+            // Please copy-paste the formulas here. Check the correct eta range
+            // systematics are handled directly here. use "else if" where applicable
+
+            if (syst_ != lightdown && syst_ != lightup) { //don't change
+                return ((1.00462+(0.00325971*x))+(-7.79184e-06*(x*x)))+(5.22506e-09*(x*(x*x)));
+            } else if (syst_ == lightdown) { //don't change
+                return ((0.845757+(0.00186422*x))+(-4.6133e-06*(x*x)))+(3.21723e-09*(x*(x*x)));
+            } else if (syst_ == lightup) { //don't change
+                return ((1.16361+(0.00464695*x))+(-1.09467e-05*(x*x)))+(7.21896e-09*(x*(x*x)));
             }
         }
 
